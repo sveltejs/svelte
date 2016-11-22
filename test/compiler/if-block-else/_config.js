77b600d726449cd6e6fdf788c36b2aadee96498d
Rich-Harris <richard.a.harris@gmail.com>
2016-11-22 11:56:09 -0500

    add {{else}} tests

---
+++ test/compiler/if-block-else/_config.js
@@ -0,0 +1,17 @@
+import * as assert from 'assert';
+
+export default {
+	data: {
+		foo: true,
+		bar: false
+	},
+	html: '<p>foo</p><!--#if foo--><p>not bar</p><!--#if bar-->',
+	test ( component, target ) {
+		component.set({ foo: false });
+		assert.equal( target.innerHTML, '<p>not foo</p><!--#if foo--><p>not bar</p><!--#if bar-->' );
+		component.set({ bar: true });
+		assert.equal( target.innerHTML, '<p>not foo</p><!--#if foo--><p>bar</p><!--#if bar-->' );
+		component.set({ foo: true });
+		assert.equal( target.innerHTML, '<p>foo</p><!--#if foo--><p>bar</p><!--#if bar-->' );
+	}
+};
