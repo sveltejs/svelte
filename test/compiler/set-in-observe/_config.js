978a2bcae75947a873d1f82d394c640b023d2a4d
Rich-Harris <richard.a.harris@gmail.com>
2016-11-23 10:47:49 -0500

    some more tests

---
+++ test/compiler/set-in-observe/_config.js
@@ -0,0 +1,14 @@
+export default {
+	html: `
+		<p>1</p>
+		<p>2</p>
+	`,
+
+	test ( assert, component, target ) {
+		component.set({ foo: 2 });
+		assert.htmlEqual( target.innerHTML, `
+			<p>2</p>
+			<p>4</p>
+		` );
+	}
+};
