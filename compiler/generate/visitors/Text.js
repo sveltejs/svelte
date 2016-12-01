8f2dad716052d15a9bbbf7bf35370cdf29714290
Arpad Borsos <arpad.borsos@googlemail.com>
2016-12-01 11:35:51 +0100

    various optimizations
    * avoid teardown for nested textnodes
    * avoid detaching children of a nested each

--- compiler/generate/visitors/Text.js
+++ compiler/generate/visitors/Text.js
@@ -2,21 +2,7 @@ import deindent from '../utils/deindent.js';
 
 export default {
 	enter ( generator, node ) {
-		if ( generator.elementDepth > 1 ) {
-			generator.current.initStatements.push( deindent`
-				${generator.current.target}.appendChild( document.createTextNode( ${JSON.stringify( node.data )} ) );
-			` );
-		} else {
-			const name = generator.current.counter( `text` );
-
-			generator.current.initStatements.push( deindent`
-				var ${name} = document.createTextNode( ${JSON.stringify( node.data )} );
-				${generator.appendToTarget( name )};
-			` );
-
-			generator.current.teardownStatements.push( deindent`
-				if ( detach ) ${name}.parentNode.removeChild( ${name} );
-			` );
-		}
+		const name = generator.current.counter( `text` );
+		generator.addElement( name, `document.createTextNode( ${JSON.stringify( node.data )} )` );
 	}
 };
