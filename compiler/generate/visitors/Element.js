435abfae7351518cd5805500e9bbee154b9613e2
Rich-Harris <richard.a.harris@gmail.com>
2016-12-04 09:01:35 -0500

    rename parent to root

--- compiler/generate/visitors/Element.js
+++ compiler/generate/visitors/Element.js
@@ -58,7 +58,7 @@ export default {
 
 					var ${name} = new template.components.${node.name}({
 						target: ${generator.current.target},
-						parent: component._parent || component,
+						root: component.root || component,
 						data: ${name}_initialData
 					});
 				` );
@@ -66,7 +66,7 @@ export default {
 				local.init.unshift( deindent`
 					var ${name} = new template.components.${node.name}({
 						target: ${generator.current.target},
-						parent: component._parent || component
+						root: component.root || component
 					});
 				` );
 			}
