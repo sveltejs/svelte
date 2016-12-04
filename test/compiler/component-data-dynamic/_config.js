29e7dea375b5f2edefbae483cef3248b805c4752
mrkishi <mauriciokishi@gmail.com>
2016-12-03 22:09:38 -0200

    Fix typo in generator's contextualise; solves #46

--- test/compiler/component-data-dynamic/_config.js
+++ test/compiler/component-data-dynamic/_config.js
@@ -2,16 +2,18 @@ export default {
 	data: {
 		bar: 'lol',
 		x: 2,
-		compound: 'piece of'
+		compound: 'piece of',
+		go: { deeper: 'core' }
 	},
-	html: `<div><p>foo: lol</p>\n<p>baz: 42 (number)</p>\n<p>qux: this is a piece of string</p></div>`,
+	html: `<div><p>foo: lol</p>\n<p>baz: 42 (number)</p>\n<p>qux: this is a piece of string</p>\n<p>quux: core</p></div>`,
 	test ( assert, component, target ) {
 		component.set({
 			bar: 'wut',
 			x: 3,
-			compound: 'rather boring'
+			compound: 'rather boring',
+			go: { deeper: 'heart' }
 		});
 
-		assert.equal( target.innerHTML, `<div><p>foo: wut</p>\n<p>baz: 43 (number)</p>\n<p>qux: this is a rather boring string</p></div>` );
+		assert.equal( target.innerHTML, `<div><p>foo: wut</p>\n<p>baz: 43 (number)</p>\n<p>qux: this is a rather boring string</p>\n<p>quux: heart</p></div>` );
 	}
 };
