3586296a0040b042f601a03c09b7a76b041fcf42
Rich-Harris <richard.a.harris@gmail.com>
2016-12-04 21:09:06 -0500

    test update while yield fragment is torn down, and tweak code for consistency

--- test/compiler/component-yield-if/_config.js
+++ test/compiler/component-yield-if/_config.js
@@ -1,13 +1,24 @@
 export default {
 	html: '<div><p></p></div>',
+
 	test ( assert, component, target ) {
 		const widget = component.refs.widget;
+
 		assert.equal( widget.get( 'show' ), false );
+
 		widget.set({show: true});
-		assert.equal( target.innerHTML, '<div><p>Hello<!--yield--><!--#if show--></p></div>' );
+		assert.htmlEqual( target.innerHTML, '<div><p>Hello</p></div>' );
+
 		component.set({data: 'World'});
-		assert.equal( target.innerHTML, '<div><p>World<!--yield--><!--#if show--></p></div>' );
+		assert.htmlEqual( target.innerHTML, '<div><p>World</p></div>' );
+
 		widget.set({show: false});
-		assert.equal( target.innerHTML, '<div><p><!--#if show--></p></div>' );
+		assert.htmlEqual( target.innerHTML, '<div><p></p></div>' );
+
+		component.set({data: 'Goodbye'});
+		assert.htmlEqual( target.innerHTML, '<div><p></p></div>' );
+
+		widget.set({show: true});
+		assert.htmlEqual( target.innerHTML, '<div><p>Goodbye</p></div>' );
 	}
-}
+};
