4210c238d43629234708228d9f3c104777eaa5f9
Rich-Harris <richard.a.harris@gmail.com>
2016-12-04 21:07:45 -0500

    separate detach statements, per #110

--- compiler/generate/visitors/Element.js
+++ compiler/generate/visitors/Element.js
@@ -48,6 +48,7 @@ export default {
 					initStatements: [],
 					mountStatements: [],
 					updateStatements: [],
+					detachStatements: [],
 					teardownStatements: [],
 
 					counter: counter()
