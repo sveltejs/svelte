b1314f476bd9a71ecc2bcc5a49b4f3801a67f474
Rich Harris <richard.a.harris@gmail.com>
2016-12-07 17:08:48 -0500

    remove logging

--- src/server-side-rendering/register.js
+++ src/server-side-rendering/register.js
@@ -3,11 +3,5 @@ import compile from './compile.js';
 
 require.extensions[ '.html' ] = function ( module, filename ) {
 	const { code } = compile( fs.readFileSync( filename, 'utf-8' ), filename );
-
-	try {
-		return module._compile( code, filename );
-	} catch ( err ) {
-		console.log( code );
-		throw err;
-	}
+	return module._compile( code, filename );
 };
