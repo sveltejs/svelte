d37f6baaa947de6836a60facaebd1b79ee34f46b
Rich-Harris <richard.a.harris@gmail.com>
2016-12-06 22:01:39 -0500

    dont render text nodes inside SVG elements (#142)

--- compiler/generate/visitors/Text.js
+++ compiler/generate/visitors/Text.js
@@ -1,5 +1,9 @@
 export default {
 	enter ( generator, node ) {
+		if ( generator.current.namespace && !/\S/.test( node.data ) ) {
+			return;
+		}
+
 		const name = generator.current.getUniqueName( `text` );
 		generator.addElement( name, `document.createTextNode( ${JSON.stringify( node.data )} )` );
 	}
---
+++ test/compiler/svg-no-whitespace/main.html
@@ -0,0 +1,5 @@
+<svg>
+	<rect/>
+
+	<rect/>
+</svg>
