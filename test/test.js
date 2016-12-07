e5b9ca3638abcb2567f1747c7012acb945bac750
Rich Harris <richard.a.harris@gmail.com>
2016-12-07 11:46:08 -0500

    test with correct files

--- test/test.js
+++ test/test.js
@@ -17,8 +17,8 @@ sourceMapSupport.install();
 // for coverage purposes, we need to test source files,
 // but for sanity purposes, we need to test dist files
 const svelte = process.env.COVERAGE ?
-	require( '../compiler/index.js' ) :
-	require( '../dist/svelte.js' );
+	require( '../src/index.js' ) :
+	require( '../compiler/svelte.js' );
 
 const cache = {};
 
