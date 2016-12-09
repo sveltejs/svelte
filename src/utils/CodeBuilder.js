a1225d5adf7000e09a4f0cfe0b6ec379dade62a9
Rich-Harris <richard.a.harris@gmail.com>
2016-12-09 09:37:50 -0500

    add CodeBuilder utility for easier codegen

---
+++ src/utils/CodeBuilder.js
@@ -0,0 +1,29 @@
+const LINE = {};
+const BLOCK = {};
+
+export default class CodeBuilder {
+	constructor () {
+		this.result = '';
+		this.last = null;
+	}
+
+	addLine ( line ) {
+		if ( this.last === BLOCK ) {
+			this.result += `\n\n${line}`;
+		} else {
+			this.result += `\n${line}`;
+		}
+
+		this.last = LINE;
+	}
+
+	addBlock ( block ) {
+		this.result += `\n\n${block}`;
+
+		this.last = BLOCK;
+	}
+
+	toString () {
+		return this.result.trim();
+	}
+}
