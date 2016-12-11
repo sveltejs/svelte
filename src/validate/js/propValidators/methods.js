a6c648b086b056c0d4f1a65225d157af5757dee6
Rich-Harris <richard.a.harris@gmail.com>
2016-12-11 10:31:23 -0500

    error if method is an arrow function expression and uses `this` or `arguments` (#179)

--- src/validate/js/propValidators/methods.js
+++ src/validate/js/propValidators/methods.js
@@ -1,5 +1,6 @@
 import checkForDupes from '../utils/checkForDupes.js';
 import checkForComputedKeys from '../utils/checkForComputedKeys.js';
+import usesThisOrArguments from '../utils/usesThisOrArguments.js';
 
 const builtin = {
 	set: true,
@@ -22,6 +23,12 @@ export default function methods ( validator, prop ) {
 	prop.value.properties.forEach( prop => {
 		if ( builtin[ prop.key.name ] ) {
 			validator.error( `Cannot overwrite built-in method '${prop.key.name}'` );
+		}
+
+		if ( prop.value.type === 'ArrowFunctionExpression' ) {
+			if ( usesThisOrArguments( prop.value.body ) ) {
+				validator.error( `Method '${prop.key.name}' should be a function expression, not an arrow function expression`, prop.start );
+			}
 		}
 	});
 }
