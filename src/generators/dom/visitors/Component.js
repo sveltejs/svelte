ef933ae93b0453aa890433078fb2ef85773c90fc
Rich-Harris <richard.a.harris@gmail.com>
2016-12-18 12:29:46 -0500

    remove _mount indirection

--- src/generators/dom/visitors/Component.js
+++ src/generators/dom/visitors/Component.js
@@ -83,7 +83,7 @@ export default {
 		` );
 
 		if ( isToplevel ) {
-			generator.current.builders.mount.addLine( `${name}._mount( target, anchor );` );
+			generator.current.builders.mount.addLine( `${name}._fragment.mount( target, anchor );` );
 		}
 
 		if ( local.dynamicAttributes.length ) {
