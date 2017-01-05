20e1b05c45dc9fcddfe2e7c5c9fc3109f0d45fa9
Arpad Borsos <arpad.borsos@googlemail.com>
2017-01-05 20:14:51 +0100

    correctly update yield fragment inside each, #230

--- test/generator/component-yield-multiple-in-each/_config.js
+++ test/generator/component-yield-multiple-in-each/_config.js
@@ -1,3 +1,10 @@
 export default {
-	html: '<p>Hello Alice</p><p>Hello Bob</p><p>Hello Charles</p>'
+	html: '<p>Hello Alice</p><p>Hello Bob</p><p>Hello Charles</p>',
+
+	test ( assert, component, target ) {
+		component.set({
+			people: [ 'Alice', 'Charles', 'Bob' ]
+		});
+		assert.htmlEqual( target.innerHTML, `<p>Hello Alice</p><p>Hello Charles</p><p>Hello Bob</p>` );
+	}
 };
