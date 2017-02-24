a8abe61fb29b75eb81374ad6a047535fd80e65ce
Rich Harris <richard.a.harris@gmail.com>
2017-02-24 16:57:11 -0500

    failing tests for second bug in #290

---
+++ test/generator/component-binding-each/_config.js
@@ -0,0 +1,28 @@
+export default {
+	html: `
+		<input>
+		<input>
+		<input>
+		
+		<p>foo, bar, baz</p>
+	`,
+
+	test ( assert, component, target, window ) {
+		const change = new window.MouseEvent( 'change' );
+		const inputs = target.querySelectorAll( 'input' );
+
+		inputs[0].value = 'blah';
+		inputs[0].dispatchEvent( change );
+
+		assert.deepEqual( component.get( 'a' ), [ 'blah', 'bar', 'baz' ] );
+		assert.htmlEqual( target.innerHTML, `
+			<input>
+			<input>
+			<input>
+			
+			<p>blah, bar, baz</p>
+		` );
+		
+		component.teardown();
+	}
+};
