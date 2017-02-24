42497fd1aad2f198bb94ec062d215f652896b0a6
Rich Harris <richard.a.harris@gmail.com>
2017-02-24 15:37:53 -0500

    populate map.sources and map.sourcesContent even if none of the original code is used

---
+++ test/sourcemaps/static-no-script/test.js
@@ -0,0 +1,9 @@
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+export function test ({ assert, map }) {
+	assert.deepEqual( map.sources, [ 'input.html' ]);
+	assert.deepEqual( map.sourcesContent, [
+		fs.readFileSync( path.join( __dirname, 'input.html' ), 'utf-8' )
+	]);
+}
