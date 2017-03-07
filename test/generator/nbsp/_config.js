bb2cd0d33fc2cc04cd49cb88e943b3f1a342f0e6
Rich-Harris <richard.a.harris@gmail.com>
2017-03-06 22:34:45 -0500

    differentiate between nbsp and other whitespace (#348)

---
+++ test/generator/nbsp/_config.js
@@ -0,0 +1,8 @@
+export default {
+	html: `<span>&nbsp;</span>`,
+
+	test ( assert, component, target ) {
+		const text = target.querySelector( 'span' ).textContent;
+		assert.equal( text.charCodeAt( 0 ), 160 );
+	}
+};
