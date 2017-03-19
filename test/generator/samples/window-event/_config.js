f5c612520eee5fba977b68e61eb4fb5f4616f3ed
Rich-Harris <richard.a.harris@gmail.com>
2017-03-19 12:36:02 -0400

    ugh node 4

--- test/generator/samples/window-event/_config.js
+++ test/generator/samples/window-event/_config.js
@@ -1,6 +1,7 @@
 export default {
 	html: `<div>undefinedxundefined</div>`,
 
+	skip: /^v4/.test( process.version ), // node 4 apparently does some dumb stuff
 	'skip-ssr': true, // there's some kind of weird bug with this test... it compiles with the wrong require.extensions hook for some bizarre reason
 
 	test ( assert, component, target, window ) {
