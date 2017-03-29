3d1d7c43604c01fca816565237b60b5e2197fc76
Dexter <dexter@dexterlabs.nl>
2017-03-29 15:43:56 +0200

    Added test for binding bug

---
+++ test/generator/samples/component-binding-deep/_config.js
@@ -0,0 +1,22 @@
+export default {
+	html: `
+		<input>
+		<p>foo</p>
+	`,
+
+	test ( assert, component, target, window ) {
+		const event = new window.MouseEvent( 'input' );
+		const input = target.querySelector( 'input' );
+
+		input.value = 'blah';
+		input.dispatchEvent( event );
+
+		assert.deepEqual( component.get( 'deep' ), { name: 'blah' } );
+		assert.htmlEqual( target.innerHTML, `
+			<input>
+			<p>blah</p>
+		` );
+		
+		component.destroy();
+	}
+};
