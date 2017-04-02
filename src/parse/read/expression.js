e77b998adf3a9afe2bfd84d7f8ba3164cd2a1882
Rich-Harris <richard.a.harris@gmail.com>
2017-04-02 09:25:32 -0400

    special case literals

--- src/parse/read/expression.js
+++ src/parse/read/expression.js
@@ -1,7 +1,35 @@
 import { parseExpressionAt } from 'acorn';
 
+const literals = {
+	true: true,
+	false: false,
+	null: null
+};
+
 export default function readExpression ( parser ) {
 	const start = parser.index;
+
+	const name = parser.readUntil( /\s*}}/ );
+	if ( name && /^[a-z]+$/.test( name ) ) {
+		const end = start + name.length;
+
+		if ( name in literals ) {
+			return {
+				type: 'Literal',
+				start,
+				end,
+				value: literals[ name ],
+				raw: name
+			};
+		}
+
+		return {
+			type: 'Identifier',
+			start,
+			end: start + name.length,
+			name
+		};
+	}
 
 	parser.index = start;
 
@@ -11,15 +39,6 @@ export default function readExpression ( parser ) {
 
 		return node;
 	} catch ( err ) {
-		const name = parser.readUntil( /\s*}}/ );
-		if ( name && /^[a-z]+$/.test( name ) ) {
-			return {
-				type: 'Identifier',
-				start,
-				end: start + name.length,
-				name
-			};
-		}
 		parser.acornError( err );
 	}
 }
