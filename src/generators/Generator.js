f8e5353f831717917ac4f3077e86264f952ac31e
Conduitry <git@fur.army>
2017-04-04 17:13:12 -0400

    recognize component name as a global identifier to be avoided (#451)

--- src/generators/Generator.js
+++ src/generators/Generator.js
@@ -39,7 +39,7 @@ export default class Generator {
 		// Svelte's builtin `import { get, ... } from 'svelte/shared.js'`;
 		this.importedNames = new Set();
 		this._aliases = new Map();
-		this._usedNames = new Set();
+		this._usedNames = new Set( [ name ] );
 	}
 
 	addSourcemapLocations ( node ) {
