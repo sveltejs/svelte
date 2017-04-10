182a04e8fc0a73fede64410613190a781b77ea07
Rich Harris <richard.a.harris@gmail.com>
2017-04-10 14:54:19 -0400

    dont create whitespace nodes inside elements like <select> (#189)

--- src/generators/dom/visitors/Text.js
+++ src/generators/dom/visitors/Text.js
@@ -1,6 +1,21 @@
+// Whitespace inside one of these elements will not result in
+// a whitespace node being created in any circumstances. (This
+// list is almost certainly very incomplete)
+const elementsWithoutText = new Set([
+	'audio',
+	'datalist',
+	'dl',
+	'ol',
+	'optgroup',
+	'select',
+	'ul',
+	'video'
+]);
+
 export default function visitText ( generator, block, state, node ) {
-	if ( state.namespace && !/\S/.test( node.data ) ) {
-		return;
+	if ( !/\S/.test( node.data ) ) {
+		if ( state.namespace ) return;
+		if ( elementsWithoutText.has( state.parentNodeName) ) return;
 	}
 
 	const name = block.getUniqueName( `text` );
