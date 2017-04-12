24c87821650b202882c06097ef8fc05d21bca598
Rich-Harris <richard.a.harris@gmail.com>
2017-04-12 08:27:52 -0400

    replace all invalid characters in attribute names when creating variables (fixes #470)

---
+++ test/runtime/samples/attribute-namespaced/_config.js
@@ -0,0 +1,14 @@
+export default {
+	data: { foo: 'bar' },
+
+	html: `
+		<svg>
+			<use xlink:href="#bar"/>
+		</svg>
+	`,
+
+	test ( assert, component, target ) {
+		const use = target.querySelector( 'use' );
+		assert.equal( use.getAttributeNS( 'http://www.w3.org/1999/xlink', 'href' ), '#bar' );
+	}
+};
