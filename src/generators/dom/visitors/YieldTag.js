e11c991023092dc0a486512a96687d7a633a7056
Rich-Harris <richard.a.harris@gmail.com>
2017-04-19 14:07:55 -0400

    yield blocks never need an anchor

--- src/generators/dom/visitors/YieldTag.js
+++ src/generators/dom/visitors/YieldTag.js
@@ -1,9 +1,6 @@
 export default function visitYieldTag ( generator, block, state ) {
-	const anchor = `yield_anchor`;
-	block.createAnchor( anchor, state.parentNode );
-
 	block.builders.mount.addLine(
-		`${block.component}._yield && ${block.component}._yield.mount( ${state.parentNode || block.target}, ${anchor} );`
+		`${block.component}._yield && ${block.component}._yield.mount( ${state.parentNode || block.target}, null );`
 	);
 
 	block.builders.destroy.addLine(
