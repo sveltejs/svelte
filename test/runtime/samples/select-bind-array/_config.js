634ea662d39829e4073760103ec3c14376428e88
Rich-Harris <richard.a.harris@gmail.com>
2017-04-19 09:06:57 -0400

    formatting

--- test/runtime/samples/select-bind-array/_config.js
+++ test/runtime/samples/select-bind-array/_config.js
@@ -1,22 +1,22 @@
 const items = [ { id: 'a' }, { id: 'b' } ];
 
 export default {
-    'skip-ssr': true,
+	'skip-ssr': true,
 
-    data: {
-        foo: 'b',
-        items
-    },
+	data: {
+		foo: 'b',
+		items
+	},
 
-    test ( assert, component, target ) {
-        const options = target.querySelectorAll( 'option' );
+	test ( assert, component, target ) {
+		const options = target.querySelectorAll( 'option' );
 
-        assert.equal( options[0].selected, false );
-        assert.equal( options[1].selected, true );
+		assert.equal( options[0].selected, false );
+		assert.equal( options[1].selected, true );
 
-        component.set( { foo: items[0].id } );
+		component.set( { foo: items[0].id } );
 
-        assert.equal( options[0].selected, true );
-        assert.equal( options[1].selected, false );
-    }
+		assert.equal( options[0].selected, true );
+		assert.equal( options[1].selected, false );
+	}
 };
