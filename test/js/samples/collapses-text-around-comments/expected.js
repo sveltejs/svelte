e6d2571e34d7dd8b91ad9315fdd52d99cdb58b64
Conduitry <git@fur.army>
2017-05-03 14:31:14 -0400

    label <style> element with id instead of with attribute, for performance

--- test/js/samples/collapses-text-around-comments/expected.js
+++ test/js/samples/collapses-text-around-comments/expected.js
@@ -10,7 +10,7 @@ var template = (function () {
 
 function add_css () {
 	var style = createElement( 'style' );
-	setAttribute( style, "svelte-3842350206", '' );
+	style.id = "svelte-3842350206-style";
 	style.textContent = "\n\tp[svelte-3842350206], [svelte-3842350206] p {\n\t\tcolor: red;\n\t}\n";
 	appendNode( style, document.head );
 }
@@ -57,7 +57,7 @@ function SvelteComponent ( options ) {
 	this._yield = options._yield;
 
 	this._torndown = false;
-	if ( !document.querySelector( 'style[svelte-3842350206]' ) ) add_css();
+	if ( !document.getElementById( "svelte-3842350206-style" ) ) add_css();
 
 	this._fragment = create_main_fragment( this._state, this );
 	if ( options.target ) this._fragment.mount( options.target, null );
