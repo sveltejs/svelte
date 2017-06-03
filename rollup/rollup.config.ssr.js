66e4c7307a5793a557e8f550de337351ee4ce5b1
Rich Harris <richard.a.harris@gmail.com>
2017-06-02 21:59:43 -0400

    prettierfy rollup configs

--- rollup/rollup.config.ssr.js
+++ rollup/rollup.config.ssr.js
@@ -6,9 +6,7 @@ import buble from 'rollup-plugin-buble';
 export default {
 	entry: 'src/server-side-rendering/register.js',
 	moduleName: 'svelte',
-	targets: [
-		{ dest: 'ssr/register.js', format: 'cjs' }
-	],
+	targets: [{ dest: 'ssr/register.js', format: 'cjs' }],
 	plugins: [
 		nodeResolve({ jsnext: true, module: true }),
 		commonjs(),
@@ -20,9 +18,9 @@ export default {
 			}
 		})
 	],
-	external: [ path.resolve( 'src/index.ts' ), 'fs', 'path' ],
+	external: [path.resolve('src/index.ts'), 'fs', 'path'],
 	paths: {
-		[ path.resolve( 'src/index.ts' ) ]: '../compiler/svelte.js'
+		[path.resolve('src/index.ts')]: '../compiler/svelte.js'
 	},
 	sourceMap: true
 };
