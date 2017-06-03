05a7a262c3e6245cededb12b854b8f587607948b
Conduitry <git@fur.army>
2017-06-02 22:46:28 -0400

    also escape back-ticks and backslashes in text nodes in SSR code (#616)

--- src/generators/server-side-rendering/visitors/Text.ts
+++ src/generators/server-side-rendering/visitors/Text.ts
@@ -3,5 +3,5 @@ import Block from '../Block';
 import { Node } from '../../../interfaces';
 
 export default function visitText ( generator: SsrGenerator, block: Block, node: Node ) {
-	generator.append( node.data.replace( /\${/g, '\\${' ) );
+	generator.append( node.data.replace( /(\${|`|\\)/g, '\\$1' ) );
 }
