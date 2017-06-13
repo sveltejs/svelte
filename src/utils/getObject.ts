1191dfc95524a8b136c481f2ec1986b49f76b31d
Rich Harris <richard.a.harris@gmail.com>
2017-06-12 20:39:42 -0400

    remove TODO, is now taken care of

--- src/utils/getObject.ts
+++ src/utils/getObject.ts
@@ -1,7 +1,6 @@
 import { Node } from '../interfaces';
 
 export default function getObject(node: Node) {
-	// TODO validation should ensure this is an Identifier or a MemberExpression
 	while (node.type === 'MemberExpression') node = node.object;
 	return node;
 }
