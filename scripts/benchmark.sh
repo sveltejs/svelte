efcc70a26f6a4c0bd2006044e63b21f692bee5d1
Paul Sauve <paul@burngames.net>
2017-06-15 22:17:27 -0500

    Forgot what this is

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
-git clone https://github.com/PaulBGD/svelte-bench.git ./tmp/svelte-bench
-cd ./tmp/svelte-bench
+DIR=./tmp/svelte-bench
+REPO=https://github.com/PaulBGD/svelte-bench.git
+
+if [ ! -d $DIR ]; then
+    git clone $REPO $DIR
+fi
+
+cd $DIR
+git pull origin selenium
 git checkout selenium
+
 yarn
 yarn run build:benchmarks
 node ./selenium/run.js "$@"
