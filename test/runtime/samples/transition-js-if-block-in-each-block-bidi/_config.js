729f909b1b22eca83e6eab9203237f4f18039be7
Rich Harris <richard.a.harris@gmail.com>
2017-06-23 16:18:01 -0400

    only run create() for new if blocks - fixes #665

---
+++ test/runtime/samples/transition-js-if-block-in-each-block-bidi/_config.js
@@ -0,0 +1,45 @@
+export default {
+	data: {
+		threshold: 5
+	},
+
+	html: `
+		<div>1</div>
+		<div>2</div>
+		<div>3</div>
+		<div>4</div>
+		<div>5</div>
+	`,
+
+	test ( assert, component, target, window, raf ) {
+		const divs = target.querySelectorAll('div');
+
+		assert.equal(divs[0].foo, 0);
+
+		raf.tick(100);
+		assert.equal(divs[0].foo, 1);
+
+		component.set({ threshold: 4 });
+		assert.equal( divs[4].foo, 1 );
+
+		raf.tick( 200 );
+		assert.htmlEqual(target.innerHTML, `
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+			<div>4</div>
+		`);
+
+		component.set({ threshold: 3 });
+		assert.equal( divs[3].foo, 1 );
+
+		raf.tick( 300 );
+		assert.htmlEqual(target.innerHTML, `
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+		`);
+
+		component.destroy();
+	}
+};
