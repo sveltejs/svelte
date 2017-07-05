01ec3000f3653cbb227038f170cbab1bd905a935
Rich Harris <richard.a.harris@gmail.com>
2017-07-05 12:33:47 -0400

    fix context for bindings (#685)

---
+++ test/runtime/samples/binding-select-in-each-block/_config.js
@@ -0,0 +1,32 @@
+export default {
+	skip: true, // JSDOM...
+
+	html: `
+		<select>
+			<option value='hullo'>Hullo</option>
+			<option value='world'>World</option>
+		</select>
+
+		<select>
+			<option value='hullo'>Hullo</option>
+			<option value='world'>World</option>
+		</select>
+	`,
+
+	data: {
+		items: [{ value: 'hullo' }, { value: 'world' }]
+	},
+
+	test(assert, component, target, window) {
+		const selects = [...target.querySelectorAll('select')];
+
+		const change = new window.Event('change');
+
+		selects[1].options[0].selected = true;
+		selects[1].dispatchEvent(change);
+
+		assert.deepEqual(component.get('items'), [
+			{ value: 'hullo' }, { value: 'hullo' }
+		]);
+	}
+};
