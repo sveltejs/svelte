72802b78c8c839708a507c1094b1f5d6f74171d2
Rich Harris <richard.a.harris@gmail.com>
2017-07-30 01:04:23 -0400

    get things working in node 4 again

--- test/setup.js
+++ test/setup.js
@@ -2,14 +2,33 @@ const nodeVersionMatch = /^v(\d)/.exec(process.version);
 const legacy = +nodeVersionMatch[1] < 6;
 const babelrc = require("../package.json").babel;
 const fs = require('fs');
+const path = require('path');
+
+require('console-group').install();
+require('source-map-support').install();
 
 if (legacy) {
-	require("babel-register")(babelrc);
+	require("babel-polyfill");
+	require("babel-register")(Object.assign(babelrc, {
+		ignore: function(filename) {
+			const relative = path.relative(process.cwd(), filename);
+
+			if (relative.startsWith('node_modules')) {
+				if (relative.startsWith('node_modules/jsdom')) return false;
+				if (relative.startsWith('node_modules/whatwg-url')) return false;
+				return true;
+			}
+
+			if (relative === 'compiler/svelte.js') return true;
+
+			return false;
+		}
+	}));
 } else {
 	require.extensions['.js'] = function(module, filename) {
 		const exports = [];
 
-		let code = fs.readFileSync(filename, 'utf-8')
+		var code = fs.readFileSync(filename, 'utf-8')
 			.replace(/^import (?:\* as )?(\w+) from ['"]([^'"]+)['"];?/gm, 'var $1 = require("$2");')
 			.replace(/^import {([^}]+)} from ['"](.+)['"];?/gm, 'var {$1} = require("$2");')
 			.replace(/^export default /gm, 'module.exports = ')
@@ -38,3 +57,4 @@ if (legacy) {
 		}
 	};
 }
+
