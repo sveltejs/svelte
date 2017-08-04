e2a803cd0b2865e863cb9cb9ca0f37eaf8dd498d
Rich Harris <richard.a.harris@gmail.com>
2017-08-04 13:59:33 -0400

    reuse helper

--- src/utils/stringify.ts
+++ src/utils/stringify.ts
@@ -1,5 +1,5 @@
 export function stringify(data: string) {
-	return JSON.stringify(data.replace(/([^\\@#])?([@#])/g, '$1\\$2'));
+	return JSON.stringify(escape(data));
 }
 
 export function escape(data: string) {
