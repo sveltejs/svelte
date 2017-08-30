a197c185236aed678ac02c125359fa172dcbe1db
Rich Harris <richard.a.harris@gmail.com>
2017-08-29 21:35:02 -0400

    failing tests for #637

---
+++ test/runtime/samples/raw-anchor-first-last-child/_config.js
@@ -0,0 +1,11 @@
+export default {
+	data: {
+		raw: `<span>foo</span>`
+	},
+
+	test ( assert, component, target ) {
+		const span = target.querySelector('span');
+		assert.equal(!span.previousSibling);
+		assert.equal(!span.nextSibling);
+	}
+};
---
+++ test/runtime/samples/raw-anchor-previous-sibling/_config.js
@@ -0,0 +1,10 @@
+export default {
+	data: {
+		raw: `<span>foo</span>`
+	},
+
+	test ( assert, component, target ) {
+		const span = target.querySelector('span');
+		assert.equal(span.previousSibling.nodeName, 'BR');
+	}
+};
