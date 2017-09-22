9c7c5b9636b60841c433cb862b60da7a4cc4f55b
Conduitry <git@fur.army>
2017-09-22 11:24:05 -0400

    more testing

--- test/runtime/samples/escaped-text/_config.js
+++ test/runtime/samples/escaped-text/_config.js
@@ -1,17 +1,26 @@
 export default {
 	html: `
+		@x
 		@@x
+		#foo
+		##foo
 		%1
 		%%2
 
 		<div>
+			@x
 			@@x
+			#foo
+			##foo
 			%1
 			%%2
 		</div>
 
 		<div>
+			@x
 			@@x
+			#foo
+			##foo
 			%1
 			%%2
 			<span>inner</span>
