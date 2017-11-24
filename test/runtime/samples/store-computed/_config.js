5ec57f8b9ad336fe1b3b8c2b63482939c5b32f1c
Rich Harris <richard.a.harris@gmail.com>
2017-11-24 16:49:37 -0500

    allow computed properties to depend on store props

---
+++ test/runtime/samples/store-computed/_config.js
@@ -0,0 +1,65 @@
+import Store from '../../../../store.js';
+
+class MyStore extends Store {
+	setFilter(filter) {
+		this.set({ filter });
+	}
+
+	toggleTodo(todo) {
+		todo.done = !todo.done;
+		this.set({ todos: this.get('todos') });
+	}
+}
+
+const todos = [
+	{
+		description: 'Buy some milk',
+		done: true,
+	},
+	{
+		description: 'Do the laundry',
+		done: true,
+	},
+	{
+		description: "Find life's true purpose",
+		done: false,
+	}
+];
+
+const store = new MyStore({
+	filter: 'all',
+	todos
+});
+
+export default {
+	solo: true,
+
+	store,
+
+	html: `
+		<div class='done'>Buy some milk</div>
+		<div class='done'>Do the laundry</div>
+		<div class='pending'>Find life's true purpose</div>
+	`,
+
+	test(assert, component, target) {
+		store.setFilter('pending');
+
+		assert.htmlEqual(target.innerHTML, `
+			<div class='pending'>Find life's true purpose</div>
+		`);
+
+		store.toggleTodo(todos[1]);
+
+		assert.htmlEqual(target.innerHTML, `
+			<div class='pending'>Do the laundry</div>
+			<div class='pending'>Find life's true purpose</div>
+		`);
+
+		store.setFilter('done');
+
+		assert.htmlEqual(target.innerHTML, `
+			<div class='done'>Buy some milk</div>
+		`);
+	}
+};
