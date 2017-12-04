d744b038e459899857e0742153b011561b56f347
Tim Hall <tim.hall.engr@gmail.com>
2017-12-04 16:10:29 -0500

    Failing test for #975

---
+++ test/runtime/samples/await-then-catch-if/_config.js
@@ -0,0 +1,43 @@
+let fulfil;
+
+let thePromise = new Promise(f => {
+	fulfil = f;
+});
+
+export default {
+	data: {
+		show: true,
+		thePromise
+	},
+
+	html: `
+		<p>loading...</p>
+	`,
+
+	test(assert, component, target) {
+		fulfil(42);
+
+		return thePromise
+			.then(() => {
+				assert.htmlEqual(target.innerHTML, `
+					<p>the value is 42</p>
+				`);
+
+				component.set({
+					show: false
+				});
+
+				assert.htmlEqual(target.innerHTML, `
+					<p>Else</p>
+				`);
+
+				component.set({
+					show: true
+				});
+
+				assert.htmlEqual(target.innerHTML, `
+					<p>the value is 42</p>
+				`);
+			});
+	}
+};
