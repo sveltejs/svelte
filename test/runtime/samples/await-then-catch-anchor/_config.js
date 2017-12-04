aeabf1cca51432c637eeba1115fbfb6376d378df
Tim Hall <tim.hall.engr@gmail.com>
2017-12-04 16:00:55 -0500

    Failing test for #974

---
+++ test/runtime/samples/await-then-catch-anchor/_config.js
@@ -0,0 +1,49 @@
+let fulfil;
+
+let thePromise = new Promise(f => {
+	fulfil = f;
+});
+
+export default {
+	data: {
+		thePromise
+	},
+
+	html: `
+		<div><p>loading...</p></div>
+	`,
+
+	test(assert, component, target) {
+		fulfil(42);
+
+		return thePromise
+			.then(() => {
+				assert.htmlEqual(target.innerHTML, `
+					<div><p>the value is 42</p></div>
+				`);
+
+				let reject;
+
+				thePromise = new Promise((f, r) => {
+					reject = r;
+				});
+
+				component.set({
+					thePromise
+				});
+
+				assert.htmlEqual(target.innerHTML, `
+					<div><p>loading...</p></div>
+				`);
+
+				reject(new Error('something broke'));
+
+				return thePromise.catch(() => {});
+			})
+			.then(() => {
+				assert.htmlEqual(target.innerHTML, `
+					<div><p>oh no! something broke</p></div>
+				`);
+			});
+	}
+};
