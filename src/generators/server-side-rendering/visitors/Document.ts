545ed7f0ed0dc2eccfa5743f605c3c0d54f7fe3a
Rich Harris <richard.a.harris@gmail.com>
2017-12-13 09:52:04 -0500

    generate title in SSR compiler

--- src/generators/server-side-rendering/visitors/Document.ts
+++ src/generators/server-side-rendering/visitors/Document.ts
@@ -1,3 +1,16 @@
-export default function visitDocument() {
-	// noop
+import { SsrGenerator } from '../index';
+import Block from '../Block';
+import { Node } from '../../../interfaces';
+import stringifyAttributeValue from './shared/stringifyAttributeValue';
+
+export default function visitDocument(
+	generator: SsrGenerator,
+	block: Block,
+	node: Node
+) {
+	const title = node.attributes.find(attribute => attribute.type === 'Attribute' && attribute.name === 'title');
+
+	if (title) {
+		generator.append('${(__result.title = `' + stringifyAttributeValue(block, title.value) + '`, "")}');
+	}
 }
