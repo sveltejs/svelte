e847fd71ca50bdce7e540d7546ab376c2e8383d9
Rich Harris <richard.a.harris@gmail.com>
2017-12-12 22:13:33 -0500

    remove unused code

--- src/validate/js/propValidators/preload.ts
+++ src/validate/js/propValidators/preload.ts
@@ -1,13 +1,6 @@
 import { Validator } from '../../';
 import { Node } from '../../../interfaces';
 
-const validTypes = new Set([
-	'FunctionExpression',
-	'ArrowFunctionExpression',
-	'Identifier',
-	'MemberExpression'
-]);
-
 export default function preload(validator: Validator, prop: Node) {
 	// not sure there's anything we need to check here...
 }
