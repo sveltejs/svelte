8609956e22ae5080a28b6026242c476f31f4ba3d
Emil Ajdyna <emil@ajdyna.com>
2017-12-15 01:47:42 +0100

    Add state() method handling for components

---
+++ src/validate/js/propValidators/store.ts
@@ -0,0 +1,6 @@
+import { Validator } from '../../';
+import { Node } from '../../../interfaces';
+
+export default function store(validator: Validator, prop: Node) {
+	// not sure there's anything we need to check here...
+}
