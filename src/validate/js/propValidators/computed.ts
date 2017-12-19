d561c93841f51c89a0228804f91ae9ea8f536d82
Emil Ajdyna <emil@ajdyna.com>
2017-12-19 02:53:21 +0100

    Check against this usage in computed functions

--- src/validate/js/propValidators/computed.ts
+++ src/validate/js/propValidators/computed.ts
@@ -2,6 +2,8 @@ import checkForDupes from '../utils/checkForDupes';
 import checkForComputedKeys from '../utils/checkForComputedKeys';
 import { Validator } from '../../';
 import { Node } from '../../../interfaces';
+import walkThroughTopFunctionScope from '../../../utils/walkThroughTopFunctionScope';
+import isThisGetCallExpression from '../../../utils/isThisGetCallExpression';
 
 const isFunctionExpression = new Set([
 	'FunctionExpression',
@@ -27,7 +29,23 @@ export default function computed(validator: Validator, prop: Node) {
 			);
 		}
 
-		const params = computation.value.params;
+		const { body, params } = computation.value;
+
+		walkThroughTopFunctionScope(body, (node: Node) => {
+			if (isThisGetCallExpression(node) && !node.callee.property.computed) {
+				validator.error(
+					`Cannot use this.get(...) — it must be passed into the computed function as an argument`,
+					node.start
+				);
+			}
+
+			if (node.type === 'ThisExpression') {
+				validator.error(
+					`Computed should be pure functions — they do not have access to the component instance and cannot use 'this'. Did you mean to put this in 'methods'?`,
+					node.start
+				);
+			}
+		});
 
 		if (params.length === 0) {
 			validator.error(
