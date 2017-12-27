c9ad4a3a2822f20e7d0b8df3dbf4b9d592284e78
Emil Ajdyna <emil@ajdyna.com>
2017-12-27 22:38:04 +0100

    Check if data is a function

--- src/validate/js/propValidators/data.ts
+++ src/validate/js/propValidators/data.ts
@@ -1,14 +1,12 @@
 import { Validator } from '../../';
 import { Node } from '../../../interfaces';
 
-const disallowed = new Set(['Literal', 'ObjectExpression', 'ArrayExpression']);
+const allowed = new Set(['FunctionExpression', 'ArrowFunctionExpression'])
 
 export default function data(validator: Validator, prop: Node) {
 	while (prop.type === 'ParenthesizedExpression') prop = prop.expression;
 
-	// TODO should we disallow references and expressions as well?
-
-	if (disallowed.has(prop.value.type)) {
+	if (!allowed.has(prop.value.type)) {
 		validator.error(`'data' must be a function`, prop.value.start);
 	}
 }
