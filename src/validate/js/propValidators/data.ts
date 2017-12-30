8b5dba9f4a9dce79ece0b4cb4f9eabb54d540634
Rich Harris <richard.a.harris@gmail.com>
2017-12-30 10:25:06 -0500

    remove TODO

--- src/validate/js/propValidators/data.ts
+++ src/validate/js/propValidators/data.ts
@@ -6,8 +6,6 @@ const disallowed = new Set(['Literal', 'ObjectExpression', 'ArrayExpression']);
 export default function data(validator: Validator, prop: Node) {
 	while (prop.type === 'ParenthesizedExpression') prop = prop.expression;
 
-	// TODO should we disallow references and expressions as well?
-
 	if (disallowed.has(prop.value.type)) {
 		validator.error(`'data' must be a function`, prop.value.start);
 	}
