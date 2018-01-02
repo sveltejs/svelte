976c278d379f46363ed168082f5f6c379288bbe4
Rich Harris <richard.a.harris@gmail.com>
2018-01-01 23:02:39 -0500

    prevent main fragment being created twice - fixes #1063

---
+++ test/custom-elements/samples/nested/test.js
@@ -0,0 +1,17 @@
+import * as assert from 'assert';
+import './main.html';
+
+export default function (target) {
+	target.innerHTML = '<my-app/>';
+	const el = target.querySelector('my-app');
+	const counter = el.shadowRoot.querySelector('my-counter');
+	const button = counter.shadowRoot.querySelector('button');
+
+	assert.equal(counter.count, 0);
+	assert.equal(counter.shadowRoot.innerHTML, `<button>count: 0</button>`);
+
+	button.dispatchEvent(new MouseEvent('click'));
+
+	assert.equal(counter.count, 1);
+	assert.equal(counter.shadowRoot.innerHTML, `<button>count: 1</button>`);
+}
