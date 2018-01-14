610b5a8225b35980cae04f4258e4e9abb179bba2
Rich Harris <richard.a.harris@gmail.com>
2018-01-13 22:45:15 -0500

    use acorn.isIdentifierStart and isIdentifierChar to determine validity

---
+++ src/utils/isValidIdentifier.ts
@@ -0,0 +1,11 @@
+import { isIdentifierStart, isIdentifierChar } from 'acorn';
+
+export default function isValidIdentifier(str) {
+	if (!isIdentifierStart(str.charCodeAt(0), true)) return false;
+
+	for (let i = 0; i < str.length; i += 1) {
+		if (!isIdentifierChar(str.charCodeAt(i), true)) return false;
+	}
+
+	return true;
+}
