e6f92bb0e4b10af663ba3ab84489199960a39aac
Rich Harris <richard.a.harris@gmail.com>
2018-02-07 08:06:29 -0500

    failing test for #648

---
+++ test/runtime/samples/transition-css-deferred-removal/_config.js
@@ -0,0 +1,26 @@
+export default {
+	// solo: true,
+
+	data: {
+		visible: true
+	},
+
+	test(assert, component, target, window, raf) {
+		component.set({ visible: false });
+
+		const outer = target.querySelector('.outer');
+		const inner = target.querySelector('.inner');
+
+		const animations = [
+			outer.style.animation,
+			inner.style.animation
+		];
+
+		raf.tick(150);
+
+		assert.deepEqual([
+			outer.style.animation,
+			inner.style.animation
+		], animations);
+	},
+};
