34f18989d8a9bdf5af410cbbd2cf1a497e50f8a2
Rich Harris <richard.a.harris@gmail.com>
2018-02-25 08:45:59 -0500

    only use page[XY]Offset

--- test/runtime/samples/window-bind-scroll-update/_config.js
+++ test/runtime/samples/window-bind-scroll-update/_config.js
@@ -2,10 +2,10 @@ export default {
 	skip: true, // JSDOM
 
 	test ( assert, component, target, window ) {
-		assert.equal( window.pageYOffset || window.scrollY, 0 );
+		assert.equal( window.pageYOffset, 0 );
 
 		component.set({ scrollY: 100 });
-		assert.equal( window.pageYOffset || window.scrollY, 100 );
+		assert.equal( window.pageYOffset, 100 );
 
 		component.destroy();
 	}
