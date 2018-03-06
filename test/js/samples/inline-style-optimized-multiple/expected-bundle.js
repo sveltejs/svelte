7f4cc3c2af2dff6ddba185acd8392932145437b8
ekhaled <me.khaled@gmail.com>
2018-03-06 18:49:49 +0000

    fix snapshots for the actual mounting code

--- test/js/samples/inline-style-optimized-multiple/expected-bundle.js
+++ test/js/samples/inline-style-optimized-multiple/expected-bundle.js
@@ -231,7 +231,7 @@ function SvelteComponent(options) {
 
 	if (options.target) {
 		this._fragment.c();
-		this._mount(options.target, options.anchor || null);
+		this._mount(options.target, options.anchor);
 	}
 }
 
--- test/js/samples/legacy-default/expected.js
+++ test/js/samples/legacy-default/expected.js
@@ -71,7 +71,7 @@ function SvelteComponent(options) {
 
 	if (options.target) {
 		this._fragment.c();
-		this._mount(options.target, options.anchor || null);
+		this._mount(options.target, options.anchor);
 
 		this._lock = true;
 		callAll(this._beforecreate);
