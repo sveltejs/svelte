b7637142225fd7613089f0efd4d71a01ee104ef2
Rich Harris <richard.a.harris@gmail.com>
2018-03-12 08:14:27 -0400

    allow SVG elements to have scoped CSS - fixes #1224

---
+++ test/runtime/samples/svg-with-style/_config.js
@@ -0,0 +1,17 @@
+export default {
+	compileOptions: {
+		cascade: false
+	},
+
+	data: {
+		x: 'bar'
+	},
+
+	html: `
+		<svg>
+			<circle class="svelte-i03x00" cx=50 cy=50 r=50 />
+			<circle class="foo svelte-i03x00" cx=150 cy=50 r=50 />
+			<circle class="bar svelte-i03x00" cx=250 cy=50 r=50 />
+		</svg>
+	`
+};
