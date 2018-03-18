4cb8effccdf8493cd93b61bb7ad590b90f06f6d7
Rich Harris <richard.a.harris@gmail.com>
2018-03-18 19:33:09 -0400

    failing test for #1254

---
+++ test/runtime/samples/deconflict-elements-indexes/_config.js
@@ -0,0 +1,25 @@
+export default {
+	solo: true,
+	show: true,
+
+	html: `
+		<div>
+			<i>one</i>
+		</div>
+	`,
+
+	preserveIdentifiers: true,
+
+	test(assert, component, target) {
+		const { tagList } = component.get();
+		tagList.push('two');
+		component.set({ tagList });
+
+		assert.htmlEqual(target.innerHTML, `
+			<div>
+				<i>one</i>
+				<i>two</i>
+			</div>
+		`);
+	}
+};
