33afb7e49a1b194dca7848416c856d486eaeca61
Rich Harris <richard.a.harris@gmail.com>
2018-04-15 11:30:36 -0400

    add some onstate/onupdate tests

---
+++ test/runtime/samples/onupdate/_config.js
@@ -0,0 +1,21 @@
+export default {
+	solo: true,
+	'skip-ssr': true,
+
+	data: {
+		value: 'hello!'
+	},
+
+	html: `
+		<p>hello!</p>
+		<p>hello!</p>
+	`,
+
+	test(assert, component, target) {
+		component.set({ value: 'goodbye!' });
+		assert.htmlEqual(target.innerHTML, `
+			<p>goodbye!</p>
+			<p>goodbye!</p>
+		`);
+	}
+};
