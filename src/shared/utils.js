e0dc854a5fc04abaa5785037ad78eda48e44a34d
Rich Harris <richard.a.harris@gmail.com>
2018-04-22 10:36:30 -0400

    correctly report changed properties in initial state/update events - fixes #1356

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -4,3 +4,11 @@ export function assign(tar, src) {
 	for (var k in src) tar[k] = src[k];
 	return tar;
 }
+
+export function makePropertyMap(obj) {
+	const map = {};
+	for (const key in obj) {
+		map[key] = 1;
+	}
+	return map;
+}
