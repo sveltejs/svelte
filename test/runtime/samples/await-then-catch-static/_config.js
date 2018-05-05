6437d7bfa779499a1d545afe958be8e616f6e97d
Rich Harris <richard.a.harris@gmail.com>
2018-05-05 13:38:54 -0400

    failing test for #1417

---
+++ test/runtime/samples/await-then-catch-static/_config.js
@@ -0,0 +1,48 @@
+let fulfil;
+
+let promise = new Promise(f => {
+	fulfil = f;
+});
+
+export default {
+	solo: 1,
+	data: {
+		promise
+	},
+
+	html: `
+		<p>loading...</p>
+	`,
+
+	test(assert, component, target) {
+		fulfil(42);
+
+		return promise
+			.then(() => {
+				assert.htmlEqual(target.innerHTML, `
+					<p>loaded</p>
+				`);
+
+				promise = new Promise((f, r) => {
+					fulfil = f;
+				});
+
+				component.set({
+					promise
+				});
+
+				assert.htmlEqual(target.innerHTML, `
+					<p>loading...</p>
+				`);
+
+				fulfil(43);
+
+				return promise.then(() => {});
+			})
+			.then(() => {
+				assert.htmlEqual(target.innerHTML, `
+					<p>loaded</p>
+				`);
+			});
+	}
+};
