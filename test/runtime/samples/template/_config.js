5ff7cb51dcb676aa76cfe1cda36371672f280c69
Rich Harris <richard.a.harris@gmail.com>
2018-07-01 14:04:04 -0400

    use template.content in place of template where appropriate - fixes #1571

---
+++ test/runtime/samples/template/_config.js
@@ -0,0 +1,23 @@
+export default {
+	// solo: 1,
+
+	html: `
+		<template>
+			<div>foo</div>
+		</template>
+	`,
+
+	test(assert, component, target) {
+		const template = target.querySelector('template');
+
+		assert.htmlEqual(template.innerHTML, `
+			<div>foo</div>
+		`);
+
+		const content = template.content.cloneNode(true);
+		const div = content.children[0];
+		assert.htmlEqual(div.outerHTML, `
+			<div>foo</div>
+		`);
+	}
+};
