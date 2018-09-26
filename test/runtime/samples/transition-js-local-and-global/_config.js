1c081cb62331d0749eb346b9ed43c50919b15e29
Rich Harris <richard.a.harris@gmail.com>
2018-09-25 21:54:51 -0400

    more tests

---
+++ test/runtime/samples/transition-js-local-and-global/_config.js
@@ -0,0 +1,68 @@
+export default {
+	nestedTransitions: true,
+
+	data: {
+		x: false,
+		y: true
+	},
+
+	test(assert, component, target, window, raf) {
+		// first, toggle x — first element should snap in
+		// and out while second one transitions
+		component.set({ x: true });
+
+		let divs = target.querySelectorAll('div');
+		assert.equal(divs[0].foo, undefined);
+		assert.equal(divs[1].foo, 0);
+
+		raf.tick(50);
+		assert.equal(divs[0].foo, undefined);
+		assert.equal(divs[1].foo, 0.5);
+
+		raf.tick(100);
+
+		component.set({ x: false });
+		assert.htmlEqual(target.innerHTML, `
+			<div>snaps if x changes</div>
+			<div>transitions if x changes</div>
+		`);
+
+		raf.tick(150);
+		assert.equal(divs[0].foo, undefined);
+		assert.equal(divs[1].foo, 0.5);
+
+		raf.tick(200);
+		assert.htmlEqual(target.innerHTML, '');
+
+		// then toggle y
+		component.set({ x: true, y: false });
+		component.set({ y: true });
+
+		assert.htmlEqual(target.innerHTML, `
+			<div>snaps if x changes</div>
+			<div>transitions if x changes</div>
+		`);
+		divs = target.querySelectorAll('div');
+
+		raf.tick(250);
+		assert.equal(divs[0].foo, 0.5);
+		assert.equal(divs[1].foo, 0.5);
+
+		raf.tick(300);
+		assert.equal(divs[0].foo, 1);
+		assert.equal(divs[1].foo, 1);
+
+		component.set({ y: false });
+		assert.htmlEqual(target.innerHTML, `
+			<div>snaps if x changes</div>
+			<div>transitions if x changes</div>
+		`);
+
+		raf.tick(320);
+		assert.equal(divs[0].foo, 0.8);
+		assert.equal(divs[1].foo, 0.8);
+
+		raf.tick(400);
+		assert.htmlEqual(target.innerHTML, '');
+	},
+};
