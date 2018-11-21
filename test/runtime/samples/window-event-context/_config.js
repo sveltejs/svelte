2fc75429e81bd67af1302bb52a82b20f9024b04d
Rich Harris <richard.a.harris@gmail.com>
2018-11-21 08:58:35 -0500

    fix test

--- test/runtime/samples/window-event-context/_config.js
+++ test/runtime/samples/window-event-context/_config.js
@@ -8,14 +8,14 @@ export default {
 	skip: /^v4/.test(process.version), // node 4 apparently does some dumb stuff
 	'skip-ssr': true, // there's some kind of weird bug with this test... it compiles with the wrong require.extensions hook for some bizarre reason
 
-	test(assert, component, target, window) {
+	async test(assert, component, target, window) {
 		const event = new window.Event('click');
 
-		window.dispatchEvent(event);
+		await window.dispatchEvent(event);
 		assert.equal(component.foo, false);
 		assert.htmlEqual(target.innerHTML, `false`);
 
-		window.dispatchEvent(event);
+		await window.dispatchEvent(event);
 		assert.equal(component.foo, true);
 		assert.htmlEqual(target.innerHTML, `true`);
 	}
