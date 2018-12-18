e57637b3ca3a3416369fa5376a4aecb03e24ee05
Rich Harris <richard.a.harris@gmail.com>
2018-12-18 08:51:54 -0500

    fix gitignore

---
+++ site/src/routes/repl/_components/_codemirror.js
@@ -0,0 +1,10 @@
+const CodeMirror = require('codemirror');
+require('./codemirror.css');
+require('codemirror/mode/javascript/javascript.js');
+require('codemirror/mode/shell/shell.js');
+require('codemirror/mode/handlebars/handlebars.js');
+require('codemirror/mode/htmlmixed/htmlmixed.js');
+require('codemirror/mode/xml/xml.js');
+require('codemirror/mode/css/css.js');
+
+module.exports = CodeMirror;
