4f5f187cbe3f9b115cf24a4b144d771d446d50dc
Richard Harris <richard.a.harris@gmail.com>
2018-12-31 20:17:18 -0500

    fix easing.mjs

--- easing.mjs
+++ easing.mjs
@@ -3,7 +3,7 @@ Adapted from https://github.com/mattdesl
 Distributed under MIT License https://github.com/mattdesl/eases/blob/master/LICENSE.md
 */
 
-import { identity as linear } from 'svelte/internal';
+export { identity as linear } from './internal';
 
 export function backInOut(t) {
 	var s = 1.70158 * 1.525;
