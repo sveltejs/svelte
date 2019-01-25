33131d0b704679213bfa4a47dbd521444d5f79d4
Chris Reeves <evschris@gmail.com>
2019-01-25 15:18:48 -0500

    make sure a hoistable name is not also a shadowed name rendering an expression - fixes #2002

--- src/compile/nodes/shared/Expression.ts
+++ src/compile/nodes/shared/Expression.ts
@@ -240,7 +240,7 @@ export default class Expression {
 							dependencies.add(name);
 							component.template_references.add(name);
 						}
-					} else if (!is_synthetic && !component.hoistable_names.has(name) && !component.imported_declarations.has(name)) {
+					} else if (!is_synthetic && !(component.hoistable_names.has(name) && template_scope.isTopLevel(name)) && !component.imported_declarations.has(name)) {
 						code.prependRight(node.start, key === 'key' && parent.shorthand
 							? `${name}: ctx.`
 							: 'ctx.');
