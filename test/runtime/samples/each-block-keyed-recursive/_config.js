45a017f41252975eceeec35ceff5960d76131da1
pk <aphitiel@users.noreply.github.com>
2019-01-30 18:27:11 +0100

    Failing test for #1999

---
+++ test/runtime/samples/each-block-keyed-recursive/_config.js
@@ -0,0 +1,26 @@
+export default {
+	props: {
+		titles: [{ name: 'b' }, { name: 'c' }],
+		tree: [
+			{id: 1, sub: null},
+			{id: 2, sub: [{id: 11}]}
+		]
+	},
+
+	html: `
+		<div>1</div>
+		<div>2\n<div>11</div></div>
+	`,
+
+	test({ assert, component, target }) {
+		component.tree = [
+			{id: 1, sub: null},
+			{id: 2, sub: null}
+		];
+
+		assert.htmlEqual(target.innerHTML, `
+			<div>1</div>
+			<div>2</div>
+		`);
+	}
+};
