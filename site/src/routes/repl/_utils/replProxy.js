969c884e31502615de0d1a88d11a62960d73f6ae
halfnelson <dpershouse@gmail.com>
2019-02-03 10:51:19 +1000

    repl improvements - Dont bind props until after component is created - Remove debug logs - destroy repl proxy event listener on compont destory - all repl-proxy commands now return a value

--- site/src/routes/repl/_utils/replProxy.js
+++ site/src/routes/repl/_utils/replProxy.js
@@ -5,20 +5,23 @@ export default class ReplProxy {
         this.pendingCmds = new Map();
         this.onPropUpdate = null;
         this.onFetchProgress = null;
-
-        window.addEventListener("message", ev => this.handleReplMessage(ev), false);
+        this.handle_event = (ev) => this.handleReplMessage(ev);
+        window.addEventListener("message", this.handle_event, false);
     }
 
+    destroy() {
+        window.removeEventListener("message", this.handle_event);
+    }
   
     iframeCommand(command, args) {
         return new Promise( (resolve, reject) => {
             this.cmdId = this.cmdId + 1; 
+            this.pendingCmds.set(this.cmdId, { resolve: resolve, reject: reject });
             this.iframe.contentWindow.postMessage({
                 action: command,
                 cmdId: this.cmdId,
                 args: args
             }, '*')
-            this.pendingCmds.set(this.cmdId, { resolve: resolve, reject: reject });
         });
     }
 
@@ -27,21 +30,21 @@ export default class ReplProxy {
         let id = cmdData.cmdId;
         let handler = this.pendingCmds.get(id);
         if (handler) {
+           
             this.pendingCmds.delete(id);
             if (action == "cmdError") {
                 let { message, stack } = cmdData;
                 let e = new Error(message);
                 e.stack = stack;
-                console.log("cmd fail");
+                console.log("repl cmd fail");
                 handler.reject(e)
             }
 
             if (action == "cmdOk") {
-                console.log("cmd okay");
                 handler.resolve(cmdData.args)
             }
         } else {
-            console.error("command not found", id);
+            console.error("command not found", id, cmdData, [...this.pendingCmds.keys()]);
         } 
     }
 
