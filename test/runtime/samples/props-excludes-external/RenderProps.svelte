a50c84ea36ee217114304336d011793da70ff531
Rich Harris <richard.a.harris@gmail.com>
2019-02-14 18:48:37 -0400

    remove internal gubbins when using bind:props - fixes #2038

---
+++ test/runtime/samples/props-excludes-external/RenderProps.svelte
@@ -0,0 +1,7 @@
+<svelte:options bind:props/>
+
+<script>
+	let props;
+</script>
+
+<p>{JSON.stringify(props)}</p>
---
+++ test/runtime/samples/props-excludes-external/main.svelte
@@ -0,0 +1,9 @@
+<script>
+	import RenderProps from './RenderProps.svelte';
+
+	export let x;
+</script>
+
+<RenderProps {x}>
+	<p>some (unused) slotted content, to create an internal prop</p>
+</RenderProps>
