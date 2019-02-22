e9537d40a7f3faea83bdcb4ec09a09960b9181c3
Iván Sánchez Ortega <ivan@sanchezortega.es>
2019-02-22 17:46:25 +0100

    Check if slotted component has an update function during runtime

--- src/compile/render-dom/wrappers/Slot.ts
+++ src/compile/render-dom/wrappers/Slot.ts
@@ -141,7 +141,7 @@ export default class SlotWrapper extends Wrapper {
 		if (this.dependencies.size > 1) update_conditions = `(${update_conditions})`;
 
 		block.builders.update.addBlock(deindent`
-			if (${slot} && ${update_conditions}) {
+			if (${slot} && ${slot}.p && ${update_conditions}) {
 				${slot}.p(@assign(@assign({}, ${get_slot_changes}(changed)), ctx.$$scope.changed), @get_slot_context(${slot_definition}, ctx, ${get_slot_context}));
 			}
 		`);
