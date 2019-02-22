5d0184c6285b7719e6211c506c4537939b138123
Conduitry <git@chor.date>
2019-02-22 07:40:44 -0500

    expose implicit vars in script-less components

--- test/vars/samples/implicit/_config.js
+++ test/vars/samples/implicit/_config.js
@@ -1,5 +1,16 @@
 export default {
 	test(assert, vars) {
-		assert.deepEqual(vars, []);
+		assert.deepEqual(vars, [
+			{
+				export_name: 'foo',
+				injected: false,
+				module: false,
+				mutated: false,
+				name: 'foo',
+				reassigned: false,
+				referenced: true,
+				writable: true,
+			},
+		]);
 	},
 };
