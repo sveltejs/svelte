614c613a961ea18fc4e4ff0982dbef4b0e964960
Conduitry <git@chor.date>
2019-03-05 16:11:02 -0500

    another test

---
+++ test/runtime/samples/reactive-values-subscript-assignment/_config.js
@@ -0,0 +1,11 @@
+export default {
+	test({ assert, component }) {
+		assert.deepEqual(component.foo, {});
+		component.bar = 'hello';
+		assert.deepEqual(component.foo, { hello: true });
+		component.bar = 'world';
+		assert.deepEqual(component.foo, { hello: true, world: true });
+		component.bar = false;
+		assert.deepEqual(component.foo, { hello: true, world: true });
+	}
+};
