63f1d0988ffc540a8961865ca3c0ca8016ae5922
Richard Harris <richard.a.harris@gmail.com>
2019-03-09 21:35:00 -0500

    tweak semantics of reactive statements - fixes #2178

---
+++ test/runtime/samples/reactive-values-self-dependency-b/_config.js
@@ -0,0 +1,19 @@
+export default {
+	html: `
+		<p>count: 0</p>
+	`,
+
+	test({ assert, component, target }) {
+		component.count = 5;
+
+		assert.htmlEqual(target.innerHTML, `
+			<p>count: 5</p>
+		`);
+
+		component.count = 50;
+
+		assert.htmlEqual(target.innerHTML, `
+			<p>count: 9</p>
+		`);
+	}
+};
