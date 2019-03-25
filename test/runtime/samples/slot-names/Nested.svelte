ca0e8a34e93b06dbe1b24a8e0565461f41c7ca43
Rich Harris <richard.a.harris@gmail.com>
2019-03-25 17:29:48 -0400

    test for conflicting slot names

--- test/runtime/samples/slot-names/Nested.svelte
+++ test/runtime/samples/slot-names/Nested.svelte
@@ -3,4 +3,6 @@
 	<slot name="-header2_" />
 	<slot name="3header" />
 	<slot name="_header4" />
+	<slot name="header-5" />
+	<slot name="header&5" />
 </div>
--- test/runtime/samples/slot-names/_config.js
+++ test/runtime/samples/slot-names/_config.js
@@ -5,6 +5,8 @@ export default {
 			<h2 slot="-header2_">Header 2</h2>
 			<h3 slot="3header">Header 3</h3>
 			<h4 slot="_header4">Header 4</h4>
+			<h5 slot="header-5">Header 5</h5>
+			<h5 slot="header&5">Header 5b</h5>
 		</div>
 	`
 };
