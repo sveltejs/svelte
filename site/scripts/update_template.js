ad81484d1e82757faebad5013dde6821b3276f82
Richard Harris <richard.a.harris@gmail.com>
2019-04-07 12:18:31 -0400

    update @sveltejs/svelte-repl version

--- site/scripts/update_template.js
+++ site/scripts/update_template.js
@@ -7,9 +7,6 @@ sh.cd(__dirname+'/../')
 sh.rm('-rf','scripts/svelte-app')
 sh.exec('npx degit sveltejs/template#v3 scripts/svelte-app')
 
-// update repl-viewer.css based on template
-sh.cp('scripts/svelte-app/public/global.css', 'static/repl-viewer.css')
-
 // remove src (will be recreated client-side) and node_modules
 sh.rm('-rf', 'scripts/svelte-app/src')
 sh.rm('-rf', 'scripts/svelte-app/node_modules')
@@ -19,8 +16,7 @@ const appPath = 'scripts/svelte-app'
 let files = []
 
 for (const path of sh.find(appPath).filter(p => fs.lstatSync(p).isFile()) ) {
-    files.push({ path: path.slice(appPath.length + 1), data: fs.readFileSync(path).toString() });
+	files.push({ path: path.slice(appPath.length + 1), data: fs.readFileSync(path).toString() });
 }
 
 fs.writeFileSync('static/svelte-app.json', JSON.stringify(files));
-
