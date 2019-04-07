026266e35fa9c6e626f3f748d1a915be58d6b0fa
Richard Harris <richard.a.harris@gmail.com>
2019-04-07 08:03:26 -0400

    add a few organisations using Svelte

---
+++ site/src/components/WhosUsingSvelte/index.svelte
@@ -0,0 +1,35 @@
+<style>
+	.logos {
+		margin: 1em 0 0 0;
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	a {
+		height: 60px;
+		margin: 0 2em 2em 0;
+		text-decoration: none;
+	}
+
+	img {
+		height: 100%;
+	}
+</style>
+
+<div class="logos">
+	<a href="https://beyonk.com"><img src="organisations/beyonk.svg" alt="Beyonk logo"></a>
+	<a href="https://chess.com" style="background-color: rgb(49,46,43); padding: 5px;"><img src="organisations/chess.svg" alt="Chess.com logo"></a>
+	<a href="https://datawrapper.de"><img src="organisations/datawrapper.svg" alt="Datawrapper logo"></a>
+	<a href="https://db.nomics.world"><img src="organisations/dbnomics.png" alt="DBNomics logo"></a>
+	<a href="https://from-now-on.com"><img src="organisations/from-now-on.png" alt="From-Now-On logo"></a>
+	<a href="https://itslearning.com"><img src="organisations/itslearning.svg" alt="itslearning logo"></a>
+	<a href="http://mustlab.ru"><img src="organisations/mustlab.png" alt="Mustlab logo"></a>
+	<a href="https://www.nesta.org.uk"><img src="organisations/nesta.svg" alt="Nesta logo"></a>
+	<a href="https://nytimes.com"><img src="organisations/nyt.svg" alt="The New York Times logo"></a>
+	<a href="https://www.stone.co"><img src="organisations/stone.svg" alt="Stone Payments logo"></a>
+	<a href="TK" style="    display: flex;
+    align-items: center;
+    border: 1px solid #eee;
+    border-radius: 30px;
+    padding: 0 1em;"><span>+ your company?</span></a>
+</div>
