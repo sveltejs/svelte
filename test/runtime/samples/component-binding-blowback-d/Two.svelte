ce704677078c48b41d9e03efc47e6696d1215685
Richard Harris <richard.a.harris@gmail.com>
2019-04-14 12:29:30 -0400

    failing tests for #2356

---
+++ test/runtime/samples/component-binding-blowback-d/Two.svelte
@@ -0,0 +1,3 @@
+<script>
+	export let value = 'x';
+</script>
---
+++ test/runtime/samples/component-binding-blowback-e/_config.js
@@ -0,0 +1,23 @@
+export default {
+	html: `
+		<button>click me</button>
+		<button>click me</button>
+
+		<p>{"value":{"x":true}}</p>
+		<p></p>
+	`,
+
+	async test({ assert, target, window }) {
+		const button = target.querySelectorAll('button')[1];
+
+		await button.dispatchEvent(new window.Event('click'));
+
+		assert.htmlEqual(target.innerHTML, `
+			<button>click me</button>
+			<button>click me</button>
+
+			<p>{"value":{"x":true}}</p>
+			<p>{"value":{"x":true}}</p>
+		`);
+	}
+};
