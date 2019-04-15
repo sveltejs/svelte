96713f29e0f27e24fe0ec38a50e08042d07386f4
Rich Harris <richard.a.harris@gmail.com>
2019-04-15 11:23:29 -0400

    add tutorial chapter for mutations - fixes #2243

---
+++ site/content/tutorial/02-reactivity/04-updating-arrays-and-objects/app-a/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+		numbers.push(numbers.length + 1);
+	}
+
+	$: sum = numbers.reduce((t, n) => t + n, 0);
+</script>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+	Add a number
+</button>
