850dc2fbd3e6188138fc6f9d26188417c07fd3e7
Richard Harris <richard.a.harris@gmail.com>
2019-04-19 23:01:40 -0400

    fix wacky margins on mobile

--- site/src/routes/_components/Blurb.svelte
+++ site/src/routes/_components/Blurb.svelte
@@ -144,8 +144,8 @@
 		<p><a href="TODO-svelte-3-blog-post">Read the introductory blog post</a> to learn more.</p>
 	</div>
 
-	<div style="grid-area: start; display: flex; flex-direction: column">
-		<pre class="language-bash" style="margin: 0 0 1em 0">
+	<div style="grid-area: start; display: flex; flex-direction: column; min-width: 0">
+		<pre class="language-bash" style="margin: 0 0 1em 0; min-width: 0; min-height: 0">
 npx degit sveltejs/template my-svelte-project
 cd my-svelte-project
 
