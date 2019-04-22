04a6b9ad8bfd7b83a207fc0f6517d1170517e22b
Richard Harris <richard.a.harris@gmail.com>
2019-04-21 22:39:22 -0400

    add a preloading indicator - fixes #2449

--- site/src/routes/_layout.svelte
+++ site/src/routes/_layout.svelte
@@ -1,10 +1,15 @@
 <script>
-	import { page } from '@sapper/app';
+	import { page, preloading } from '@sapper/app';
 	import InlineSvg from '../components/InlineSvg.svelte';
+	import PreloadingIndicator from '../components/PreloadingIndicator.svelte';
 	import Nav from '../components/TopNav.svelte';
 
 	export let segment;
 </script>
+
+{#if $preloading}
+	<PreloadingIndicator/>
+{/if}
 
 <InlineSvg />
 {#if $page.path !== '/repl/embed'}
