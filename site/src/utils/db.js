d6f0b69b72242bfd98c1e0891d4ccc7b0a97228c
Luke Edwards <luke.edwards05@gmail.com>
2019-04-25 18:08:37 -0700

    feat: add `db` util w/ pool

---
+++ site/src/utils/db.js
@@ -0,0 +1,15 @@
+import { Pool } from 'pg';
+
+const { DATABASE_URL } = process.env;
+
+export const DB = new Pool({
+	connectionString: process.env.DATABASE_URL
+});
+
+export function query(text, values=[]) {
+	return DB.query(text, values).then(r => r.rows);
+}
+
+export function find(text, values=[]) {
+	return query(text, values).then(arr => arr[0]);
+}
