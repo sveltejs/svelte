fc5727c187bfa86b67cd43effcb76d52eb465799
Conduitry <git@chor.date>
2019-05-04 22:16:53 -0400

    fix instrumentation of auto-subscription self-assignments (#2681)

---
+++ test/runtime/samples/instrumentation-auto-subscription-self-assignment/main.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { writable } from 'svelte/store';
+	const foo = writable([]);
+	$: bar = $foo;
+	export function go() {
+		$foo.push(42);
+		$foo = $foo;
+	}
+</script>
+
+{JSON.stringify(bar)}
