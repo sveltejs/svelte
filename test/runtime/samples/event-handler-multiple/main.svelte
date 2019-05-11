65654b33e6bf743275c83eef84cce9231363a267
Benjamin Milde <benni@kobrakai.de>
2019-05-11 17:42:59 +0200

    Switch test to runtime test

---
+++ test/runtime/samples/event-handler-multiple/main.svelte
@@ -0,0 +1,6 @@
+<script>
+	export let clickHandlerOne = 0;
+	export let clickHandlerTwo = 0;
+</script>
+
+<button on:click='{() => clickHandlerOne++}' on:click='{() => clickHandlerTwo++}'>click me</button>
