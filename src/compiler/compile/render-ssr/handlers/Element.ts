f5dde02b9912287cdd4f53fd5af0935af80b57a0
Vlad Glushchuk <vgluschuk@gmail.com>
2019-06-06 10:57:41 +0200

    Rebase and fix linter warning

--- src/compiler/compile/render-ssr/handlers/Element.ts
+++ src/compiler/compile/render-ssr/handlers/Element.ts
@@ -58,7 +58,7 @@ export default function(node: Element, renderer: Renderer, options: RenderOption
 	const contenteditable = (
 		node.name !== 'textarea' &&
 		node.name !== 'input' &&
-		node.attributes.some((attribute: Node) => attribute.name === 'contenteditable')
+		node.attributes.some((attribute) => attribute.name === 'contenteditable')
 	);
 
 	const slot = node.get_static_attribute_value('slot');
