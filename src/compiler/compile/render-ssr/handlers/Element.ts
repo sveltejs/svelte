3dca08b647964f338e28b01d56c1f9c4d6127a59
Rich Harris <richard.a.harris@gmail.com>
2019-06-09 10:58:13 -0400

    avoid creating extra function expression

--- src/compiler/compile/render-ssr/handlers/Element.ts
+++ src/compiler/compile/render-ssr/handlers/Element.ts
@@ -148,7 +148,7 @@ export default function(node: Element, renderer: Renderer, options: RenderOption
 			// TODO server-render group bindings
 		} else if (binding.name === 'value' && node.name === 'textarea') {
 			const snippet = snip(expression);
-			textarea_contents='${(v => v ? v : "")(' + snippet + ')}';
+			textarea_contents='${(' + snippet + ') || ""}';
 		} else {
 			const snippet = snip(expression);
 			opening_tag += ' ${(v => v ? ("' + name + '" + (v === true ? "" : "=" + JSON.stringify(v))) : "")(' + snippet + ')}';
