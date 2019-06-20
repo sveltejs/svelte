e7e9aff7576651c6c02f17a31007e568a03b636a
Bryan Terce <bryan.terce@gmail.com>
2019-06-20 02:17:27 -0700

    Add trailing whitespace js tests

---
+++ test/js/samples/fragment-trailing-whitespace/input.svelte
@@ -0,0 +1,15 @@
+<script>
+    let message = "the quick brown fox jumps over the lazy dog"
+</script>
+
+<div>
+    {#each message as char}
+        <span>{char} </span>
+    {/each}
+</div>
+
+<div>
+    {#each message as char}
+        <span>{char} </span>
+    {/each}
+</div>
