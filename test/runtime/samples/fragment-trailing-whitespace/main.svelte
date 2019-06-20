26e53cc5498802d307e65e3fee7b1edd97cc2416
Bryan Terce <bryan.terce@gmail.com>
2019-06-20 10:22:29 -0700

    Rewrite js tests as runtime tests

--- test/runtime/samples/fragment-trailing-whitespace/main.svelte
+++ test/runtime/samples/fragment-trailing-whitespace/main.svelte
@@ -2,14 +2,10 @@
     let message = "the quick brown fox jumps over the lazy dog"
 </script>
 
-<div>
+<div id="first">
     {#each message as char}
         <span>{char} </span>
     {/each}
 </div>
 
-<div>
-    {#each message as char}
-        <span>{char} </span>
-    {/each}
-</div>
+<div id="second">{#each message as char}<span>{char} </span>{/each}</div>
