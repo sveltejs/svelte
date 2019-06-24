57c6b2e94a4ee18fcabe542e81e49d5f83b8d3c0
Rich Harris <richard.a.harris@gmail.com>
2019-06-24 17:30:14 -0400

    always run onDestroy functions - fixes #3058

---
+++ test/runtime/samples/ondestroy-deep/A.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { onDestroy } from 'svelte';
+	import { destroyed } from './destroyed.js';
+	import B from './B.svelte';
+
+	let yes = 1;
+
+	onDestroy(() => destroyed.push('A'));
+</script>
+
+<div>
+	{#if yes}
+		<B/>
+	{/if}
+</div>
