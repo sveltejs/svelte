bdd4bb1f950fb53f9db235d022d7f2a3a993ec38
Richard Harris <richard.a.harris@gmail.com>
2019-06-24 22:32:16 -0400

    remove unused code

--- src/compiler/compile/nodes/Text.ts
+++ src/compiler/compile/nodes/Text.ts
@@ -10,15 +10,5 @@ export default class Text extends Node {
 	constructor(component: Component, parent: INode, scope: TemplateScope, info: any) {
 		super(component, parent, scope, info);
 		this.data = info.data;
-
-		if (!component.component_options.preserveWhitespace && !/[\S\u00A0]/.test(info.data)) {
-			let node = parent;
-			while (node) {
-				if (node.type === 'Element' && node.name === 'pre') {
-					return;
-				}
-				node = node.parent;
-			}
-		}
 	}
 }
