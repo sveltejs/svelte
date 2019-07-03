e85f8afa1c8bc0ce28655a47d95b9f5395c1f07c
Richard Harris <richard.a.harris@gmail.com>
2019-07-03 19:34:24 -0400

    huh apparently this works

--- src/compiler/compile/nodes/shared/Expression.ts
+++ src/compiler/compile/nodes/shared/Expression.ts
@@ -140,6 +140,7 @@ export default class Expression {
 					if (template_scope.is_let(name)) {
 						if (!function_expression) { // TODO should this be `!lazy` ?
 							contextual_dependencies.add(name);
+							dependencies.add(name);
 						}
 					} else if (template_scope.names.has(name)) {
 						expression.uses_context = true;
