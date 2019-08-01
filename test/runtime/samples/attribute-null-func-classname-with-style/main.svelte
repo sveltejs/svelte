ccf2ad819a8cfd0b86d02e4991695b5369ef28e6
bre30kra69cs <an.krasowsckij@gmail.com>
2019-08-01 15:46:53 +0300

    optimize class name runtime calc & add tests & ref

---
+++ test/runtime/samples/attribute-null-func-classname-with-style/main.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let testName;
+
+	function myHelper(testName) {
+		return testName;
+	}
+</script>
+
+<style>
+  div {
+    color: purple;
+  }
+</style>
+
+<div class={myHelper(testName)}></div>
