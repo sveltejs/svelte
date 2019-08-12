8669c76921fa3b8e0bb83df2c869fe86995426bb
Richard Harris <richard.a.harris@gmail.com>
2019-08-12 07:16:22 -0400

    -> v3.8.0

--- test/runtime/samples/reactive-value-coerce/_config.js
+++ test/runtime/samples/reactive-value-coerce/_config.js
@@ -3,7 +3,7 @@ export default {
 
 	test: ({ assert, component, target }) => {
 		component.a.b[0] = 2;
-		component.a = component.a;
+		component.a = component.a; // eslint-disable-line no-self-assign
 
 		assert.htmlEqual(target.innerHTML, `2-2`);
 	}
