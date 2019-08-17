511632f93eb39ea1cecd89cc37cb2ba068169b5d
Richard Harris <richard.a.harris@gmail.com>
2019-08-16 23:30:47 -0400

    start building UI for saved REPLs

---
+++ site/src/routes/repls/index.svelte
@@ -0,0 +1,15 @@
+<!-- TODO we need to be able to fetch the data in preload -->
+
+<script context="module">
+	export async function preload() {
+		const r = await this.fetch('repls.json');
+
+		return {
+			list: await r.json()
+		};
+	}
+</script>
+
+<script>
+	export let list;
+</script>
