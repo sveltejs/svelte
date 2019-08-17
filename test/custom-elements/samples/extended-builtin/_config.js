7624dbea703877b735693079a43fc8fea401b769
Richard Harris <richard.a.harris@gmail.com>
2019-08-17 00:24:12 -0400

    support is attribute, with a warning - fixes #3182

---
+++ test/custom-elements/samples/extended-builtin/_config.js
@@ -0,0 +1,17 @@
+export default {
+	warnings: [{
+		code: "avoid-is",
+		message: "The 'is' attribute is not supported cross-browser and should be avoided",
+		pos: 97,
+		start: {
+			character: 97,
+			column: 8,
+			line: 7
+		},
+		end: {
+			character: 114,
+			column: 25,
+			line: 7
+		}
+	}]
+};
