2e23c7a684d10dc953ea15d1f274fddb87696fc5
Sander Hahn <sanderhahn@gmail.com>
2019-08-17 02:20:31 +0200

    make offset query

--- site/src/routes/repl/archive.json.js
+++ site/src/routes/repl/archive.json.js
@@ -10,13 +10,13 @@ export async function get(req, res) {
 	const user = await isUser(req, res);
 	if (!user) return; // response already sent
 
-	const offset = req.params.offset || 0;
+	const offset = req.query.offset || 0;
 	const rows = await query(`
 		select g.uid, g.name, coalesce(g.updated_at, g.created_at) as updated_at
 		from gists g
 		where g.user_id = $1
 		order by g.updated_at desc, g.created_at desc
-		limit 100
+		limit 20
 		offset $2
 	`, [user.id, offset]);
 
