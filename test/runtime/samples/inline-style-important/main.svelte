97f3d56e0c134d2da76b93d4749a1053ee329c27
Richard Harris <richard.a.harris@gmail.com>
2019-08-20 08:24:42 -0400

    handle important declarations in inline styles - fixes #1834

---
+++ test/runtime/samples/inline-style-important/main.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let color = `red`;
+</script>
+
+<p style="color: {color} !important; font-size: 20px !important; opacity: 1;">{color}</p>
+
+<style>
+	p {
+		color: blue !important;
+		font-size: 10px !important;
+	}
+</style>
