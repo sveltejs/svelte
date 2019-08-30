11866de7cf955178a936182908f542fb26e6453b
devbox <cbolgiano625@gmail.com>
2019-08-30 17:55:49 -0400

    Adding buy.* logo.

--- site/src/routes/_components/WhosUsingSvelte.svelte
+++ site/src/routes/_components/WhosUsingSvelte.svelte
@@ -46,6 +46,7 @@
 
 <div class="logos">
 	<a target="_blank" rel="noopener" href="https://beyonk.com"><img src="organisations/beyonk.svg" alt="Beyonk logo" loading="lazy"></a>
+        <a target="_blank" rel="noopener" href="https://buydotstar.com"><img src="organisations/buydotstar.svg" alt="buy.* logo" loading="lazy"></a>
 	<a target="_blank" rel="noopener" href="https://chess.com" style="background-color: rgb(49,46,43);"><img src="organisations/chess.svg" alt="Chess.com logo" loading="lazy"></a>
 	<a target="_blank" rel="noopener" href="https://comigosaude.com.br"><img src="organisations/comigo.svg" alt="Comigo logo" loading="lazy"></a>
 	<a target="_blank" rel="noopener" href="https://datawrapper.de"><img src="organisations/datawrapper.svg" alt="Datawrapper logo" loading="lazy"></a>
