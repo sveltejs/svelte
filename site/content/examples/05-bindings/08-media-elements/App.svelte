1ab3dcb3427d5f3ab16c7c3c8ac68cb047c5ac39
Conduitry <git@chor.date>
2019-09-04 08:06:55 -0400

    use assets served from github pages

--- site/content/examples/05-bindings/08-media-elements/App.svelte
+++ site/content/examples/05-bindings/08-media-elements/App.svelte
@@ -109,8 +109,8 @@
 
 <div>
 	<video
-		poster="http://svelte-assets.surge.sh/caminandes-llamigos.jpg"
-		src="http://svelte-assets.surge.sh/caminandes-llamigos.mp4"
+		poster="http://sveltejs.github.io/assets/caminandes-llamigos.jpg"
+		src="http://sveltejs.github.io/assets/caminandes-llamigos.mp4"
 		on:mousemove={handleMousemove}
 		on:mousedown={handleMousedown}
 		bind:currentTime={time}
