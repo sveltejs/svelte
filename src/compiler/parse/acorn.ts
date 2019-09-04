71be8a6683d6c76c26230ac9d7e3ff27a1f2094a
Conduitry <git@chor.date>
2019-09-03 21:10:59 -0400

    update dependencies

--- src/compiler/parse/acorn.ts
+++ src/compiler/parse/acorn.ts
@@ -1,15 +1,16 @@
 import * as acorn from 'acorn';
-import dynamicImport from 'acorn-dynamic-import';
 
-const Parser = acorn.Parser.extend(dynamicImport);
+const Parser = acorn.Parser;
 
 export const parse = (source: string) => Parser.parse(source, {
 	sourceType: 'module',
-	ecmaVersion: 9,
+	// @ts-ignore TODO pending release of fixed types
+	ecmaVersion: 11,
 	preserveParens: true
 });
 
 export const parse_expression_at = (source: string, index: number) => Parser.parseExpressionAt(source, index, {
-	ecmaVersion: 9,
+	// @ts-ignore TODO pending release of fixed types
+	ecmaVersion: 11,
 	preserveParens: true
 });
