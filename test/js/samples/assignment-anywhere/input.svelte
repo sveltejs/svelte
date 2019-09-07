94b583afff3cf4a698e98cef66f47239268d359e
Bendegúz Hajnal <hajnalbendeguz@gmail.com>
2019-09-08 01:42:49 +0200

    Added test

---
+++ test/js/samples/assignment-anywhere/input.svelte
@@ -0,0 +1,25 @@
+<script>
+		let toggle = false;
+		let num = 1
+
+    function _toggleMe() {
+
+        if (toggle = !toggle) {
+            console.log(toggle);
+        } else {
+            console.log(toggle);
+        }
+		}
+
+		function _increaseMe(){
+			if(num += 3){
+				console.log('num has been increased')
+			}
+		}
+
+</script>
+
+<button on:click={_toggleMe}>Click</button>
+<span class="text {toggle ? "red" : "blue"}">
+    text + {num}
+</span>
