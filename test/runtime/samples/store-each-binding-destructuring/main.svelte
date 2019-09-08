249b2a114c0b15e16bf487bef4872a8e98d107c5
Bryan Terce <bryan.terce@gmail.com>
2019-09-08 00:48:47 -0700

    Add destructuring tests

---
+++ test/runtime/samples/store-each-binding-destructuring/main.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { writable } from 'svelte/store';
+
+	const items = writable([
+		 { id: 0, text: 'inital' }
+	]);
+</script>
+
+{#each $items as { text }, index}
+	<input bind:value={text}>
+{/each}
+
+<p>{$items[0].text}</p>
