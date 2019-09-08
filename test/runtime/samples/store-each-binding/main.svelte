ad827a6db449c4651d8820f4f429f5c85b998fef
Bryan Terce <bryan.terce@gmail.com>
2019-09-08 00:46:20 -0700

    Add tests

---
+++ test/runtime/samples/store-each-binding/main.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { writable } from 'svelte/store';
+
+	const items = writable([
+		 { id: 0, text: 'inital' }
+	]);
+</script>
+
+{#each $items as item, index}
+	<input bind:value={item.text}>
+{/each}
+
+<p>{$items[0].text}</p>
