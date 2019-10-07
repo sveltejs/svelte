b1a8297ac1fb67f90b339c63737da01ce0d8830e
Jesse Skinner <jesse@thefutureoftheweb.com>
2019-10-07 14:58:19 -0400

    Rename, move and update test to be consistent with similar tests, re: issue #3544

---
+++ test/css/samples/omit-scoping-attribute-multiple-descendants/input.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let unknown = 'whatever';
+</script>
+
+<div class='root'>
+	<section class='{unknown}'>
+		<p>hello</p>
+	</section>
+</div>
+
+<style>
+	.root p {
+		color: red;
+	}
+</style>
