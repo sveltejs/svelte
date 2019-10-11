3fcce3773270d73059a761a94a24aaa1d138dff6
pngwn <hello@pngwn.io>
2019-10-11 14:20:54 +0100

    Prevent element property set from throwing errors for readonly properties. Fixes #3681.

---
+++ test/runtime/samples/spread-element-readonly/_config.js
@@ -0,0 +1,9 @@
+export default {
+	skip_if_ssr: true, // DOM and SSR output is different, a separate SSR test exists
+	html: `<input form="qux" list="quu" />`,
+
+	test({ assert, target }) {
+		const div = target.querySelector('input');
+		assert.equal(div.value, 'bar');
+	}
+};
