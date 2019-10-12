9d82aaafc00ec8ef719a1a286ea990edf13ca29d
Tan Li Hau <lhtan93@gmail.com>
2019-10-13 00:13:51 +0800

    fix test

--- test/runtime/samples/each-block-scope-shadow-self/_config.js
+++ test/runtime/samples/each-block-scope-shadow-self/_config.js
@@ -1,16 +1,12 @@
 export default {
-	html: '<input type="text"><input type="text"><input type="text">',
 	async test({ assert, component, target }) {
+		assert.equal(target.querySelectorAll('input').length, 3);
+
 		const input = target.querySelector('input');
 		input.value = 'svelte';
 		await input.dispatchEvent(new window.Event('input'));
 
-		assert.htmlEqual(
-			target.innerHTML,
-			`
-		<input type="text"><input type="text"><input type="text">
-		`
-		);
+		assert.equal(target.querySelectorAll('input').length, 3);
 		assert.deepEqual(component.data, { a: 'svelte', b: 'B', c: 'C' });
 		assert.deepEqual(component.x, ['a', 'b', 'c']);
 	},
