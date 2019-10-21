a0960d0ee47b8551748080c8faf93e24e5ca2456
Conduitry <git@chor.date>
2019-10-20 22:42:59 -0400

    update code-red etc (#3752)

--- src/compiler/parse/acorn.ts
+++ src/compiler/parse/acorn.ts
@@ -4,13 +4,11 @@ const Parser = acorn.Parser;
 
 export const parse = (source: string) => Parser.parse(source, {
 	sourceType: 'module',
-	// @ts-ignore TODO pending release of fixed types
 	ecmaVersion: 11,
 	locations: true
 });
 
 export const parse_expression_at = (source: string, index: number) => Parser.parseExpressionAt(source, index, {
-	// @ts-ignore TODO pending release of fixed types
 	ecmaVersion: 11,
 	locations: true
 });
