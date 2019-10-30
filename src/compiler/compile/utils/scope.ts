74f8ea3b3c0d22716ab793f078ec21ae1b42c8d0
Tan Li Hau <lhtan93@gmail.com>
2019-10-30 22:31:46 +0800

    fix lint

--- src/compiler/compile/utils/scope.ts
+++ src/compiler/compile/utils/scope.ts
@@ -1,9 +1,8 @@
 import { Node } from 'estree';
 import { analyze, Scope, extract_names, extract_identifiers } from 'periscopic';
 
-// TODO replace this with periscopic?
 export function create_scopes(expression: Node) {
 	return analyze(expression);
 }
 
-export { Scope, extract_names, extract_identifiers }
+export { Scope, extract_names, extract_identifiers };
