798a47b1da69b00980239dee071385ac18f957b8
Tan Li Hau <lhtan93@gmail.com>
2019-10-31 01:02:03 +0800

    feat unused css selector understands string concat

---
+++ test/css/samples/unused-selector-ternary-bailed/input.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let active;
+  export let hover;
+</script>
+
+<div class="thing {active ? 'active' : hover}">
+  some stuff
+</div>
+
+<style>
+  .thing {color: blue;}
+  .active {color: blue;}
+  .thing.active {color: blue;}
+  .hover { color: blue; }
+  .hover.unused { color: blue; }
+
+  .unused {color: blue;}
+</style>
