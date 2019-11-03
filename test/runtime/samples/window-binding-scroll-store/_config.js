821021e9e7b475ee85443e7d7566ba99b4a3e67b
Tan Li Hau <lhtan93@gmail.com>
2019-11-03 15:37:01 +0800

    reenable skipped test due to jsdom

--- test/runtime/samples/window-binding-scroll-store/_config.js
+++ test/runtime/samples/window-binding-scroll-store/_config.js
@@ -1,6 +1,13 @@
 export default {
 	skip_if_ssr: true,
-
+	before_test() {
+		Object.defineProperties(window, {
+			pageYOffset: {
+				value: 0,
+				configurable: true,
+			},
+		});
+	},
 	async test({ assert, component, target, window }) {
 		assert.equal(window.pageYOffset, 0);
 
@@ -18,14 +25,5 @@ export default {
 			target.innerHTML,
 			`<p style="position: fixed; top: 1em; left: 1em;">scroll\ny\nis\n234.\n234\n*\n234\n=\n54756</p><div style="height: 9999px;"></div>`
 		);
-	},
-
-	after_test() {
-		Object.defineProperties(window, {
-			pageYOffset: {
-				value: 0,
-				configurable: true,
-			},
-		});
 	},
 };
