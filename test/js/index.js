d8b25afacd5857610068f90a625f04270fa5e161
Richard Harris <richard.a.harris@gmail.com>
2019-11-09 12:59:56 -0500

    update tests

--- test/js/index.js
+++ test/js/index.js
@@ -3,7 +3,7 @@ import * as fs from "fs";
 import * as path from "path";
 import { loadConfig, svelte } from "../helpers.js";
 
-describe("js", () => {
+describe.only("js", () => {
 	fs.readdirSync(`${__dirname}/samples`).forEach(dir => {
 		if (dir[0] === ".") return;
 
