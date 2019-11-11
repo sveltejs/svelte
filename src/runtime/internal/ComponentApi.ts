77fd87296e94e3f64bc8f4e7893595e52fc138a6
Nitay Rabinovich <nitayr@wix.com>
2019-11-11 11:47:46 +0200

    Fix linting and exporting

--- src/runtime/internal/ComponentApi.ts
+++ src/runtime/internal/ComponentApi.ts
@@ -1,16 +1,15 @@
 type Props = Record<string, any>;
 export declare class SvelteComponentApi {
-    constructor(options: {
+    constructor (options: {
         target: Element;
         anchor?: Element;
         props?: Props;
         hydrate?: boolean;
         intro?: boolean;
     });
-
     $set(props: Props): void;
     $on<T = any>(event: string, callback: (event: CustomEvent<T>) => void): () => void;
     $destroy(): void;
-
     [accessor: string]: any;
 }
+export default SvelteComponentApi;
