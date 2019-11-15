bc63d007a3e482d54671479263debbbe2c92dcec
Rich Harris <richard.a.harris@gmail.com>
2019-11-15 11:52:53 -0500

    better tutorial for parameterised actions

--- site/content/tutorial/12-actions/02-adding-parameters-to-actions/app-a/App.svelte
+++ site/content/tutorial/12-actions/02-adding-parameters-to-actions/app-a/App.svelte
@@ -1,70 +1,20 @@
 <script>
-	let language = "english";
+	import { longpress } from './longpress.js';
 
-	const translations = {
-		english: {
-			tooltip: "Switch Languages",
-		},
-		latin: {
-			tooltip: "Itchsway Anguageslay",
-		}
-	};
-
-	function tooltip(node, text) {
-		const tooltip = document.createElement('div');
-		tooltip.textContent = text;
-
-		Object.assign(tooltip.style, {
-			position: 'absolute',
-			background: 'black',
-			color: 'white',
-			padding: '0.5em 1em',
-			fontSize: '12px',
-			pointerEvents: 'none',
-			transform: 'translate(5px, -50%)',
-			borderRadius: '2px',
-			transition: 'opacity 0.4s'
-		});
-
-		function position() {
-			const { top, right, bottom } = node.getBoundingClientRect();
-			tooltip.style.top = `${(top + bottom) / 2}px`;
-			tooltip.style.left = `${right}px`;
-		}
-
-		function append() {
-			document.body.appendChild(tooltip);
-			tooltip.style.opacity = 0;
-			setTimeout(() => tooltip.style.opacity = 1);
-			position();
-		}
-
-		function remove() {
-			tooltip.remove();
-		}
-
-		node.addEventListener('mouseenter', append);
-		node.addEventListener('mouseleave', remove);
-
-		return {
-			update(text) {
-				tooltip.textContent = text;
-				position();
-			},
-
-			destroy() {
-				tooltip.remove();
-				node.removeEventListener('mouseenter', append);
-				node.removeEventListener('mouseleave', remove);
-			}
-		};
-	}
-
-	function toggleLanguage() {
-		language = language === 'english' ? 'latin' : 'english'
-	}
+	let pressed = false;
+	let duration = 2000;
 </script>
 
-<button on:click={toggleLanguage} use:tooltip={translations[language].tooltip}>
-	{language}
-</button>
+<label>
+	<input type=range bind:value={duration} max={2000} step={100}>
+	{duration}ms
+</label>
+
+<button use:longpress
+	on:longpress="{() => pressed = true}"
+	on:mouseout="{() => pressed = false}"
+>press and hold</button>
+
+{#if pressed}
+	<p>congratulations, you pressed and held for {duration}ms</p>
+{/if}
