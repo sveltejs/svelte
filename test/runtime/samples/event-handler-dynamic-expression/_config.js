58b7b5b325a8668b3ab80733767ceb78ef75d0d8
Tan Li Hau <lhtan93@gmail.com>
2019-11-16 10:35:37 +0800

    fix dynamic event handler expression

---
+++ test/runtime/samples/event-handler-dynamic-expression/_config.js
@@ -0,0 +1,20 @@
+export default {
+	html: `<button>bar</button>`,
+
+	async test({ assert, component, target, window }) {
+		const [button] = target.querySelectorAll(
+			'button'
+		);
+
+		const event = new window.MouseEvent('click');
+
+		await button.dispatchEvent(event);
+		assert.htmlEqual(target.innerHTML, `<button>foo</button>`);
+		
+		await button.dispatchEvent(event);
+		assert.htmlEqual(target.innerHTML, `<button>bar</button>`);
+		
+		await button.dispatchEvent(event);
+		assert.htmlEqual(target.innerHTML, `<button>foo</button>`);
+	},
+};
