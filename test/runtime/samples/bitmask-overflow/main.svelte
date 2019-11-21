128b066f2a39d680bc5bd96e3ad6c841247575b8
Rich Harris <richard.a.harris@gmail.com>
2019-11-21 14:25:09 -0500

    add test for component with more than 31 dynamic values

---
+++ test/runtime/samples/bitmask-overflow/main.svelte
@@ -0,0 +1,85 @@
+<script>
+	export let _0 = '0';
+	export let _1 = '1';
+	export let _2 = '2';
+	export let _3 = '3';
+	export let _4 = '4';
+	export let _5 = '5';
+	export let _6 = '6';
+	export let _7 = '7';
+	export let _8 = '8';
+	export let _9 = '9';
+	export let _10 = '10';
+	export let _11 = '11';
+	export let _12 = '12';
+	export let _13 = '13';
+	export let _14 = '14';
+	export let _15 = '15';
+	export let _16 = '16';
+	export let _17 = '17';
+	export let _18 = '18';
+	export let _19 = '19';
+	export let _20 = '20';
+	export let _21 = '21';
+	export let _22 = '22';
+	export let _23 = '23';
+	export let _24 = '24';
+	export let _25 = '25';
+	export let _26 = '26';
+	export let _27 = '27';
+	export let _28 = '28';
+	export let _29 = '29';
+	export let _30 = '30';
+	export let _31 = '31';
+	export let _32 = '32';
+	export let _33 = '33';
+	export let _34 = '34';
+	export let _35 = '35';
+	export let _36 = '36';
+	export let _37 = '37';
+	export let _38 = '38';
+	export let _39 = '39';
+	export let _40 = '40';
+</script>
+
+<p>{_0}</p>
+<p>{_1}</p>
+<p>{_2}</p>
+<p>{_3}</p>
+<p>{_4}</p>
+<p>{_5}</p>
+<p>{_6}</p>
+<p>{_7}</p>
+<p>{_8}</p>
+<p>{_9}</p>
+<p>{_10}</p>
+<p>{_11}</p>
+<p>{_12}</p>
+<p>{_13}</p>
+<p>{_14}</p>
+<p>{_15}</p>
+<p>{_16}</p>
+<p>{_17}</p>
+<p>{_18}</p>
+<p>{_19}</p>
+<p>{_20}</p>
+<p>{_21}</p>
+<p>{_22}</p>
+<p>{_23}</p>
+<p>{_24}</p>
+<p>{_25}</p>
+<p>{_26}</p>
+<p>{_27}</p>
+<p>{_28}</p>
+<p>{_29}</p>
+<p>{_30}</p>
+<p>{_31}</p>
+<p>{_32}</p>
+<p>{_33}</p>
+<p>{_34}</p>
+<p>{_35}</p>
+<p>{_36}</p>
+<p>{_37}</p>
+<p>{_38}</p>
+<p>{_39}</p>
+<p>{_40}</p>
