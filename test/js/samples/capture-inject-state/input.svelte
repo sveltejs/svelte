8cdcdc0163225e63a9dcc91d72234c236717046f
rixo <rixo@rixo.fr>
2019-12-04 01:19:59 +0100

    add test of $capture_state & $inject_state behaviour

---
+++ test/js/samples/capture-inject-state/input.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let prop;
+
+  let realName;
+  export { realName as alias };
+
+  let local;
+
+  const priv = 'priv';
+</script>
+<!-- NOTE $prop ensures store subscriptions are not part of captured state -->
+<p>{$prop} {realName} {local} {priv}</p>
