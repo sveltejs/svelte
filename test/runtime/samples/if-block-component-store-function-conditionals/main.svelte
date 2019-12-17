e0288d53026f8eb5c6901fd27a0322af0636f991
David A Kondrad <david.a.kondrad@jci.com>
2019-12-17 12:50:41 -0500

    tests: runtime: add another case where if/else fails with compound statements

---
+++ test/runtime/samples/if-block-component-store-function-conditionals/main.svelte
@@ -0,0 +1,11 @@
+<script>
+	import {writable} from 'svelte/store';
+	import Widget from './Widget.svelte';
+	let a = (writable({}));
+	let b = () => true;
+</script>
+{#if $a || b() }
+		<Widget></Widget>
+{:else}
+	<pre>fail</pre>
+{/if}
