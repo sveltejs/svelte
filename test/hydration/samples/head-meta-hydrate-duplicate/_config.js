a80259a6fc3e165ee3848db42793ed097b1421b8
Conduitry <git@chor.date>
2020-01-13 11:48:48 -0500

    tidy test

--- test/hydration/samples/head-meta-hydrate-duplicate/_config.js
+++ test/hydration/samples/head-meta-hydrate-duplicate/_config.js
@@ -1,6 +1,5 @@
 export default {
 	test(assert, target, snapshot, component, window) {
-		// console.log(window.document.querySelectorAll('meta'));
-		// assert.equal(window.document.querySelectorAll('meta').length, 2);
+		assert.equal(window.document.querySelectorAll('meta').length, 2);
 	}
 };
