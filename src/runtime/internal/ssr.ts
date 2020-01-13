e3d66869df5b75b81da6183c5d68cde53b2439aa
Jesse Skinner <jesse@thefutureoftheweb.com>
2020-01-13 11:24:34 -0500

    fix stringifying of attributes in presence of spread in SSR (#4247)

--- src/runtime/internal/ssr.ts
+++ src/runtime/internal/ssr.ts
@@ -25,9 +25,7 @@ export function spread(args, classes_to_add) {
 		else if (boolean_attributes.has(name.toLowerCase())) {
 			if (value) str += " " + name;
 		} else if (value != null) {
-			str += " " + name + "=" + JSON.stringify(String(value)
-				.replace(/"/g, '&#34;')
-				.replace(/'/g, '&#39;'));
+			str += ` ${name}="${String(value).replace(/"/g, '&#34;').replace(/'/g, '&#39;')}"`;
 		}
 	});
 
