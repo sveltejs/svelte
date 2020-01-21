011b8bcf504533f10128e07870fc070094e76f89
Conduitry <git@chor.date>
2020-01-20 21:17:24 -0500

    preserve js comments where possible (#4293)

--- src/compiler/parse/acorn.ts
+++ src/compiler/parse/acorn.ts
@@ -1,14 +1,13 @@
-import * as acorn from 'acorn';
+import { Node } from 'acorn';
+import * as code_red from 'code-red';
 
-const Parser = acorn.Parser;
-
-export const parse = (source: string) => Parser.parse(source, {
+export const parse = (source: string): Node => code_red.parse(source, {
 	sourceType: 'module',
 	ecmaVersion: 11,
 	locations: true
 });
 
-export const parse_expression_at = (source: string, index: number) => Parser.parseExpressionAt(source, index, {
+export const parse_expression_at = (source: string, index: number): Node => code_red.parseExpressionAt(source, index, {
 	ecmaVersion: 11,
 	locations: true
 });
