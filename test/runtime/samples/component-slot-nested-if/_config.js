95cc9ba69d26d158a8fccc1e430787668ff8216f
Tan Li Hau <lhtan93@gmail.com>
2020-01-21 10:50:50 +0800

    fix nested block not reactive

---
+++ test/runtime/samples/component-slot-nested-if/_config.js
@@ -0,0 +1,30 @@
+export default {
+	html: `
+    <input>
+	`,
+	async test({ assert, target, snapshot, component, window }) {
+		const input = target.querySelector('input');
+
+		input.value = 'a';
+		await input.dispatchEvent(new window.Event('input'));
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+			<input>
+			Display: a
+		`
+		);
+
+		input.value = 'abc';
+		await input.dispatchEvent(new window.Event('input'));
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+			<input>
+			Display: abc
+		`
+		);
+	},
+};
