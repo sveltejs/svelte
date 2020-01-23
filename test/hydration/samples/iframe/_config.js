9dadb5008b76998c8a69734773150759fc8ba7f5
Avi Marcus <avim@wix.com>
2020-01-23 12:45:54 +0200

    added test that fails on master

---
+++ test/hydration/samples/iframe/_config.js
@@ -0,0 +1,65 @@
+export default {
+	props: {
+		done: false,
+	},
+
+	snapshot(target) {
+		let domMutated;
+		const mutationRecords = [];
+		const mutationObserver = new global.window.MutationObserver(records => {
+			Array.from(
+				records
+			).forEach(
+				({ type, childList, target, attributes, addedNodes, removedNodes }) => {
+					childList = Array.from(childList|| []).map(n => n.nodeName);
+					addedNodes = Array.from(addedNodes|| []).map(n => n.nodeName);
+					removedNodes = Array.from(removedNodes|| []).map(n => n.nodeName);
+					mutationRecords.push({
+						type,
+						childList,
+						target,
+						attributes,
+						addedNodes,
+						removedNodes,
+					});
+				});
+			domMutated && domMutated(mutationRecords);
+		});
+		mutationObserver.observe(target, { childList: true, subtree: true });
+		mutationRecords.length = 0;
+		const trigger = new Promise(resolve => (domMutated = resolve));
+
+		return {
+			mutationRecords,
+			mutationObserver,
+			trigger,
+		};
+	},
+
+	async test(assert, target, snapshot, component, window) {
+		component.$set({done:true});
+		
+		await snapshot.trigger;
+		snapshot.mutationObserver.disconnect();
+
+		console.log(snapshot.mutationRecords);
+
+		const iframeMutations = snapshot.mutationRecords.filter(({addedNodes, removedNodes}) => {
+			console.log({addedNodes, removedNodes});
+			return addedNodes.includes('IFRAME')|| removedNodes.includes('IFRAME');
+		});
+		console.log({iframeMutations});
+		assert(iframeMutations.length === 0, 'iframe added/removed');
+		// assert(false, 'test')
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+			<div>
+				<iframe title="test"></iframe>
+				<span>done</span>
+			</div>
+		`
+		);
+	},
+};
