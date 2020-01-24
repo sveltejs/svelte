164622ea64b619b0ad3a748e122dd87f87d417fd
David A Kondrad <david.a.kondrad@jci.com>
2020-01-24 17:19:42 -0500

    internal: scheduler: add failing test for infinite onmount loop
    This test shows how onMount is called repeatedly when creating a component dynamically.
    The loop counter is totally arbitrary and is there to prevent a hard-crash in the CI.

---
+++ test/runtime/samples/lifecycle-onmount-infinite-loop/main.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { onMount } from 'svelte';
+	import Child from './Child.svelte';
+
+	let root;
+	export let count = 0;
+
+	onMount(() => {
+		if (count < 5) {
+			count++;
+			let child = new Child({target:root});
+		}
+	});
+</script>
+
+<div bind:this={root}></div>
