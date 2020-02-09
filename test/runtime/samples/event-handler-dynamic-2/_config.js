c13321d4ddf2bfbe2571962e8ab2632e847011fc
Conduitry <git@chor.date>
2020-02-09 09:27:47 -0500

    tidy test

--- test/runtime/samples/event-handler-dynamic-2/_config.js
+++ test/runtime/samples/event-handler-dynamic-2/_config.js
@@ -7,48 +7,27 @@ export default {
 	`,
 
 	async test({ assert, target, window }) {
-		const [toggle, handler_a, handler_b] = target.querySelectorAll(
-			'button'
-		);
+		const [toggle, handler_a, handler_b] = target.querySelectorAll('button');
+		const p = target.querySelector('p');
 
 		const event = new window.MouseEvent('click');
 
 		await handler_a.dispatchEvent(event);
-		assert.htmlEqual(target.innerHTML, `
-			<button>toggle</button>
-			<p>1</p>
-			<button>handler_a</button>
-			<button>handler_b</button>
-		`);
+		assert.equal(p.innerHTML, '1');
 
 		await toggle.dispatchEvent(event);
 
 		await handler_a.dispatchEvent(event);
-		assert.htmlEqual(target.innerHTML, `
-			<button>toggle</button>
-			<p>2</p>
-			<button>handler_a</button>
-			<button>handler_b</button>
-		`);
+		assert.equal(p.innerHTML, '2');
 
 		await toggle.dispatchEvent(event);
 
 		await handler_b.dispatchEvent(event);
-		assert.htmlEqual(target.innerHTML, `
-			<button>toggle</button>
-			<p>1</p>
-			<button>handler_a</button>
-			<button>handler_b</button>
-		`);
+		assert.equal(p.innerHTML, '1');
 
 		await toggle.dispatchEvent(event);
 
 		await handler_b.dispatchEvent(event);
-		assert.htmlEqual(target.innerHTML, `
-			<button>toggle</button>
-			<p>2</p>
-			<button>handler_a</button>
-			<button>handler_b</button>
-		`);
+		assert.equal(p.innerHTML, '2');
 	},
 };
