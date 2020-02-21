c8232107a6d34ebfcbab723e60c57fda1f308475
Tan Li Hau <lhtan93@gmail.com>
2020-02-21 09:23:03 +0800

    fix some hydration bug, more to go

--- test/runtime/samples/ondestroy-deep/_config.js
+++ test/runtime/samples/ondestroy-deep/_config.js
@@ -2,6 +2,9 @@ import { destroyed, reset } from './destroyed.js';
 
 export default {
 	test({ assert, component }) {
+		// for hydration, ssr may have pushed to `destroyed`
+		reset();
+
 		component.visible = false;
 		assert.deepEqual(destroyed, ['A', 'B', 'C']);
 
