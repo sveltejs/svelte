d531657126987ef67be460ff357c2eeec3a93763
Conduitry <git@chor.date>
2020-02-23 16:35:25 -0500

    fix dev mode each block validation when using strings (#4451)

--- src/runtime/internal/dev.ts
+++ src/runtime/internal/dev.ts
@@ -80,7 +80,7 @@ export function set_data_dev(text, data) {
 }
 
 export function validate_each_argument(arg) {
-	if (!arg || !('length' in arg)) {
+	if (typeof arg !== 'string' && !(arg && typeof arg === 'object' && 'length' in arg)) {
 		let msg = '{#each} only iterates over array-like objects.';
 		if (typeof Symbol === 'function' && arg && Symbol.iterator in arg) {
 			msg += ' You can use a spread to convert this iterable into an array.';
