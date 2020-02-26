9777fcd2ca61c6eda2371eac204aaa658255c61d
Tan Li Hau <lhtan93@gmail.com>
2020-02-26 19:05:14 +0800

    mark module variable

---
+++ test/vars/samples/modules-vars/_config.js
@@ -0,0 +1,72 @@
+export default {
+	test(assert, vars) {
+		assert.deepEqual(vars, [
+			{
+				name: "a",
+				export_name: null,
+				injected: false,
+				module: true,
+				mutated: false,
+				reassigned: true,
+				referenced: false,
+				referenced_from_script: false,
+				writable: true
+			},
+			{
+				name: "b",
+				export_name: null,
+				injected: false,
+				module: true,
+				mutated: true,
+				reassigned: false,
+				referenced: false,
+				referenced_from_script: false,
+				writable: true
+			},
+			{
+				name: "c",
+				export_name: null,
+				injected: false,
+				module: true,
+				mutated: false,
+				reassigned: false,
+				referenced: false,
+				referenced_from_script: false,
+				writable: true
+			},
+			{
+				name: "d",
+				export_name: null,
+				injected: false,
+				module: true,
+				mutated: false,
+				reassigned: false,
+				referenced: false,
+				referenced_from_script: false,
+				writable: true
+			},
+			{
+				name: "c",
+				export_name: null,
+				injected: false,
+				module: false,
+				mutated: false,
+				reassigned: true,
+				referenced: false,
+				referenced_from_script: true,
+				writable: true
+			},
+			{
+				name: "foo",
+				export_name: null,
+				injected: false,
+				module: false,
+				mutated: false,
+				reassigned: false,
+				referenced: false,
+				referenced_from_script: false,
+				writable: false
+			}
+		]);
+	}
+};
