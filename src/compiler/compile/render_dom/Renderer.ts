41a23a1eb38dc07ae005224313eab205b37fd265
Tan Li Hau <lhtan93@gmail.com>
2020-03-05 09:06:50 +0800

    fix bitmask overflow if initial dirty value

--- src/compiler/compile/render_dom/Renderer.ts
+++ src/compiler/compile/render_dom/Renderer.ts
@@ -230,6 +230,7 @@ export default class Renderer {
 			return bitmask;
 		};
 
+		// TODO: context-overflow make it less gross
 		return {
 			// Using a ParenthesizedExpression allows us to create
 			// the expression lazily. TODO would be better if
