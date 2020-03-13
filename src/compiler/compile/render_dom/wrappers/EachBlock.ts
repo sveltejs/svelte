7f36492a4e3b2d15226d7279e949f0b116e6d9e0
r.proserpio <r.proserpio@reply.it>
2020-03-13 20:43:46 +0100

    fix failing test case for each-block-keyed-else (#4536 and #4549)

--- src/compiler/compile/render_dom/wrappers/EachBlock.ts
+++ src/compiler/compile/render_dom/wrappers/EachBlock.ts
@@ -423,7 +423,7 @@ export default class EachBlockWrapper extends Wrapper {
 		if (all_dependencies.size) {
 			block.chunks.update.push(b`
 				if (${block.renderer.dirty(Array.from(all_dependencies))}) {
-					const ${this.vars.each_block_value} = ${snippet};
+					${this.vars.each_block_value} = ${snippet};
 					${this.renderer.options.dev && b`@validate_each_argument(${this.vars.each_block_value});`}
 
 					${this.block.has_outros && b`@group_outros();`}
