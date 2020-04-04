fce1136b14244e0eb6593d4eafe254ff82d06133
Фролов Игорь <i.frolov@corp.mail.ru>
2020-04-04 12:55:49 +0300

    feat(internal): Support globalThis in modern environments
    fix #3561

--- src/runtime/internal/globals.ts
+++ src/runtime/internal/globals.ts
@@ -1,3 +1,7 @@
 declare const global: any;
 
-export const globals = (typeof window !== 'undefined' ? window : global) as unknown as typeof globalThis;
+export const globals = (typeof globalThis !== 'undefined'
+	? globalThis
+	: typeof window !== 'undefined'
+	? window
+	: global) as unknown as typeof globalThis;
