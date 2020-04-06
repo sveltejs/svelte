738e8b1261df7681432172db5d6a0f9ea3b0c08b
Robin Cussol <RobinCsl@users.noreply.github.com>
2020-04-06 16:06:17 +0200

    fix(examples): hide "undefined" domain in HN example
    This commit introduces a null-undefined check against the domain of the HN items in the Hacker News example.

--- site/content/examples/21-miscellaneous/01-hacker-news/Item.svelte
+++ site/content/examples/21-miscellaneous/01-hacker-news/Item.svelte
@@ -26,7 +26,9 @@
 <article>
 	<a href="{item.url}">
 		<h1>{item.title}</h1>
-		<small>{item.domain}</small>
+		{#if item.domain != null}
+			<small>{item.domain}</small>
+		{/if}
 	</a>
 
 	<p class="meta">submitted by {item.user} {item.time_ago}
