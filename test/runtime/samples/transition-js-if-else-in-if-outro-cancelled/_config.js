cffeb65a82c024cdaac8ca77f8aba0c16ca74c3c
pushkine <pushkine.github@gmail.com>
2020-04-20 23:05:41 +0200

    test

--- test/runtime/samples/transition-js-if-else-in-if-outro-cancelled/_config.js
+++ test/runtime/samples/transition-js-if-else-in-if-outro-cancelled/_config.js
@@ -2,9 +2,9 @@ export default {
 	async test({ assert, target, window, raf }) {
 		const button = target.querySelector("button");
 		const event = new window.MouseEvent("click");
-		assert.htmlEqual(target.innerHTML, "A");
+		assert.htmlEqual(target.innerHTML, "<button>TOGGLE</button><div>A</div>");
 		await button.dispatchEvent(event);
 		raf.tick(500);
-		assert.htmlEqual(target.innerHTML, "A");
+		assert.htmlEqual(target.innerHTML, "<button>TOGGLE</button><div>A</div>");
 	},
 };
