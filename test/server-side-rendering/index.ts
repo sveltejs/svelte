76ef93b4385eb36cfb5390ab5775acfa84f0600b
Luis Confraria <lconfraria@gmail.com>
2020-04-29 02:19:16 +0100

    add test to server-side-rendering/samples

--- test/server-side-rendering/index.ts
+++ test/server-side-rendering/index.ts
@@ -81,7 +81,11 @@ describe('ssr', () => {
 				if (css.code) fs.writeFileSync(`${dir}/_actual.css`, css.code);
 
 				try {
-					assert.htmlEqual(html, expectedHtml);
+					if (compileOptions.preserveComments) {
+						assert.equal(html.trim(), expectedHtml.trim());
+					} else {
+						assert.htmlEqual(html, expectedHtml);
+					}
 				} catch (error) {
 					if (shouldUpdateExpected()) {
 						fs.writeFileSync(`${dir}/_expected.html`, html);
