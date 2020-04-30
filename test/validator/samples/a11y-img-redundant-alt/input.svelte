f0959bafb82f0c82cec62d939a91be19c026abee
André Lins <andrelucas01@hotmail.com>
2020-04-29 23:44:12 -0300

    a11y-img-redudant-alt

---
+++ test/validator/samples/a11y-img-redundant-alt/input.svelte
@@ -0,0 +1,5 @@
+<img src="foo" alt="Foo eating a sandwich." />
+<img src="bar" aria-hidden alt="Picture of me taking a photo of an image" />
+<img src="foo" alt="Photo of foo being weird." />
+<img src="bar" alt="Image of me at a bar!" />
+<img src="baz" alt="Picture of baz fixing a bug." />
