b4a7c7eb155fe265429ef660c58f95ef19782ced
pushkine <pushkine.github@gmail.com>
2020-05-17 15:28:23 +0200

    remove expectations for raf.tick(0)

--- test/runtime/samples/animation-js-delay/_config.js
+++ test/runtime/samples/animation-js-delay/_config.js
@@ -5,8 +5,8 @@ export default {
 			{ id: 2, name: 'b' },
 			{ id: 3, name: 'c' },
 			{ id: 4, name: 'd' },
-			{ id: 5, name: 'e' }
-		]
+			{ id: 5, name: 'e' },
+		],
 	},
 
 	html: `
@@ -19,8 +19,8 @@ export default {
 
 	test({ assert, component, target, window, raf }) {
 		let divs = document.querySelectorAll('div');
-		divs.forEach(div => {
-			div.getBoundingClientRect = function() {
+		divs.forEach((div) => {
+			div.getBoundingClientRect = function () {
 				const index = [...this.parentNode.children].indexOf(this);
 				const top = index * 30;
 
@@ -28,33 +28,31 @@ export default {
 					left: 0,
 					right: 100,
 					top,
-					bottom: top + 20
+					bottom: top + 20,
 				};
 			};
 		});
 
 		component.things = [
-			{ id: 5, name: 'e' },
-			{ id: 2, name: 'b' },
-			{ id: 3, name: 'c' },
-			{ id: 4, name: 'd' },
-			{ id: 1, name: 'a' }
+			{ id: 5, name: 'e' }, // 0 delay
+			{ id: 2, name: 'b' }, // 10 delay
+			{ id: 3, name: 'c' }, // 20 delay
+			{ id: 4, name: 'd' }, // 30 delay
+			{ id: 1, name: 'a' }, // 40 delay
 		];
 
 		divs = document.querySelectorAll('div');
-		assert.equal(divs[0].dy, 120);
-		assert.equal(divs[4].dy, -120);
 
 		raf.tick(50);
-		assert.equal(divs[0].dy, 108);
-		assert.equal(divs[4].dy, -60);
+		assert.equal(divs[0].dy, 60);
+		assert.equal(divs[4].dy, -108);
 
 		raf.tick(100);
-		assert.equal(divs[0].dy, 48);
-		assert.equal(divs[4].dy, 0);
+		assert.equal(divs[0].dy, 0);
+		assert.equal(divs[4].dy, -48);
 
 		raf.tick(150);
 		assert.equal(divs[0].dy, 0);
 		assert.equal(divs[4].dy, 0);
-	}
+	},
 };
--- test/runtime/samples/animation-js/_config.js
+++ test/runtime/samples/animation-js/_config.js
@@ -5,8 +5,8 @@ export default {
 			{ id: 2, name: 'b' },
 			{ id: 3, name: 'c' },
 			{ id: 4, name: 'd' },
-			{ id: 5, name: 'e' }
-		]
+			{ id: 5, name: 'e' },
+		],
 	},
 
 	html: `
@@ -19,8 +19,8 @@ export default {
 
 	test({ assert, component, target, window, raf }) {
 		let divs = document.querySelectorAll('div');
-		divs.forEach(div => {
-			div.getBoundingClientRect = function() {
+		divs.forEach((div) => {
+			div.getBoundingClientRect = function () {
 				const index = [...this.parentNode.children].indexOf(this);
 				const top = index * 30;
 
@@ -28,7 +28,7 @@ export default {
 					left: 0,
 					right: 100,
 					top,
-					bottom: top + 20
+					bottom: top + 20,
 				};
 			};
 		});
@@ -38,12 +38,10 @@ export default {
 			{ id: 2, name: 'b' },
 			{ id: 3, name: 'c' },
 			{ id: 4, name: 'd' },
-			{ id: 1, name: 'a' }
+			{ id: 1, name: 'a' },
 		];
 
 		divs = document.querySelectorAll('div');
-		assert.equal(divs[0].dy, 120);
-		assert.equal(divs[4].dy, -120);
 
 		raf.tick(50);
 		assert.equal(divs[0].dy, 60);
@@ -58,13 +56,10 @@ export default {
 			{ id: 2, name: 'b' },
 			{ id: 3, name: 'c' },
 			{ id: 4, name: 'd' },
-			{ id: 5, name: 'e' }
+			{ id: 5, name: 'e' },
 		];
 
 		divs = document.querySelectorAll('div');
-
-		assert.equal(divs[0].dy, 120);
-		assert.equal(divs[4].dy, -120);
 
 		raf.tick(150);
 		assert.equal(divs[0].dy, 60);
@@ -73,5 +68,5 @@ export default {
 		raf.tick(200);
 		assert.equal(divs[0].dy, 0);
 		assert.equal(divs[4].dy, 0);
-	}
+	},
 };
