e79ee34c258d6550ad5600e11ef918294c8f41e6
Billy Levin <billy.levin97@gmail.com>
2020-05-17 20:40:54 +0100

    react to array mutation in each block with static content

---
+++ test/runtime/samples/each-block-static-update/main.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { onMount } from 'svelte';
+	let arr = [1];
+	
+	onMount(() => {
+		arr.push(arr.length + 1);
+		arr = arr;
+	});
+</script>
+
+{#each arr as num}
+	static
+{/each}
