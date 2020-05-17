c67d585b387b6105ee37b53ed30c72c3609b137c
pushkine <pushkine.github@gmail.com>
2020-05-17 15:39:23 +0200

    remove css hashes from assertions

--- test/runtime/samples/attribute-null-classname-with-style/_config.js
+++ test/runtime/samples/attribute-null-classname-with-style/_config.js
@@ -1,40 +1,39 @@
 export default {
-	html: `<div class=" svelte-x1o6ra"></div>`,
-
 	test({ assert, component, target }) {
 		const div = target.querySelector('div');
 
 		component.testName = null;
-		assert.equal(div.className, ' svelte-x1o6ra');
+		const startsWith = (str) => assert.ok(div.className.startsWith(str));
+		startsWith(' svelte-');
 
 		component.testName = undefined;
-		assert.equal(div.className, ' svelte-x1o6ra');
+		startsWith(' svelte-');
 
 		component.testName = undefined + '';
-		assert.equal(div.className, 'undefined svelte-x1o6ra');
+		startsWith('undefined svelte-');
 
 		component.testName = null + '';
-		assert.equal(div.className, 'null svelte-x1o6ra');
+		startsWith('null svelte-');
 
 		component.testName = 1;
-		assert.equal(div.className, '1 svelte-x1o6ra');
+		startsWith('1 svelte-');
 
 		component.testName = 0;
-		assert.equal(div.className, '0 svelte-x1o6ra');
+		startsWith('0 svelte-');
 
 		component.testName = false;
-		assert.equal(div.className, 'false svelte-x1o6ra');
+		startsWith('false svelte-');
 
 		component.testName = true;
-		assert.equal(div.className, 'true svelte-x1o6ra');
+		startsWith('true svelte-');
 
 		component.testName = {};
-		assert.equal(div.className, '[object Object] svelte-x1o6ra');
+		startsWith('[object Object] svelte-');
 
 		component.testName = '';
-		assert.equal(div.className, ' svelte-x1o6ra');
+		startsWith(' svelte-');
 
 		component.testName = 'testClassName';
-		assert.equal(div.className, 'testClassName svelte-x1o6ra');
-	}
+		startsWith('testClassName svelte-');
+	},
 };
