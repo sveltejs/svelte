11967804afbf748d92cfaa93f467ec83ae702ef1
Tan Li Hau <tanhauhau@users.noreply.github.com>
2020-05-27 07:10:47 +0800

    fix reactivity when binding to each context (#4878)

---
+++ test/runtime/samples/reactive-function-called-reassigned/_config.js
@@ -0,0 +1,27 @@
+let value;
+let called = 0;
+function callback(_value) {
+	called ++;
+	value = _value;
+}
+
+export default {
+	props: {
+		callback,
+	},
+	async test({ assert, component, target, window }) {
+		assert.equal(called, 1);
+
+		const input = target.querySelector('input');
+
+		const event = new window.Event('input');
+		input.value = 'h';
+		await input.dispatchEvent(event);
+
+		assert.equal(called, 2);
+		assert.equal(value.length, 3);
+		assert.equal(value[0], 'h');
+		assert.equal(value[1], '2');
+		assert.equal(value[2], '3');
+	}
+};
