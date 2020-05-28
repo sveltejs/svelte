9cbd760d86263b391d1d6718404cd59f22e6109d
pushkine <pushkine.github@gmail.com>
2020-05-28 02:09:46 +0200

    add error `end` check

--- test/parser/index.js
+++ test/parser/index.js
@@ -38,9 +38,9 @@ describe('parse', () => {
 			} catch (err) {
 				if (err.name !== 'ParseError') throw err;
 				if (!expectedError) throw err;
-				const { code, message, pos, start } = err
+				const { code, message, pos, start, end } = err;
 				try {
-					assert.deepEqual({ code, message, pos, start }, expectedError);
+					assert.deepEqual({ code, message, pos, start, end }, expectedError);
 				} catch (err2) {
 					const e = err2.code === 'MODULE_NOT_FOUND' ? err : err2;
 					throw e;
