a84b528fa96b8a4c35e33dd0b322f4ead85362e2
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-06-01 15:33:05 -0700

    Remove unused method parameter

--- src/compiler/compile/render_dom/wrappers/RawMustacheTag.ts
+++ src/compiler/compile/render_dom/wrappers/RawMustacheTag.ts
@@ -22,7 +22,7 @@ export default class RawMustacheTagWrapper extends Tag {
 		this.not_static_content();
 	}
 
-	render(block: Block, parent_node: Identifier, _parent_nodes: Identifier) {
+	render(block: Block, parent_node: Identifier) {
 		const in_head = is_head(parent_node);
 
 		const can_use_innerhtml = !in_head && parent_node && !this.prev && !this.next;
