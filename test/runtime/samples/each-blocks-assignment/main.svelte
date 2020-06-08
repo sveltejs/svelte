73b86b4e003801f0dc206908057f174c23dea1e6
Tan Li Hau <tanhauhau@users.noreply.github.com>
2020-06-08 22:43:59 +0800

    fix reactivity with assigning item in each block (#4945)

---
+++ test/runtime/samples/each-blocks-assignment/main.svelte
@@ -0,0 +1,13 @@
+<script>
+  let obj = {
+    prop: "foo"
+  };
+
+  let arr = [1, 2, 3]
+</script>
+
+<button on:click={() => arr = [...arr, arr.length + 1]}>Add</button>
+{#each arr as o}
+  <span class="content">{o}</span>
+  <button on:click={() => { o *= 2; }}>Test</button>
+{/each}
