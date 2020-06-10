8728a9df8c736ffae77951d10291e98c1f249759
Tan Li Hau <tanhauhau@users.noreply.github.com>
2020-06-11 01:09:19 +0800

    fix bind:group in each (#4868)

--- src/compiler/compile/utils/flatten_reference.ts
+++ src/compiler/compile/utils/flatten_reference.ts
@@ -3,14 +3,18 @@ import { Node, Identifier } from 'estree';
 export default function flatten_reference(node: Node) {
 	const nodes = [];
 	const parts = [];
-
+	
 	while (node.type === 'MemberExpression') {
 		nodes.unshift(node.property);
 
 		if (!node.computed) {
 			parts.unshift((node.property as Identifier).name);
+		} else {
+			const computed_property = to_string(node.property);
+			if (computed_property) {
+				parts.unshift(`[${computed_property}]`);
+			}
 		}
-
 		node = node.object;
 	}
 
@@ -20,9 +24,16 @@ export default function flatten_reference(node: Node) {
 
 	nodes.unshift(node);
 
-	if (!(node as any).computed) {
-		parts.unshift(name);
-	}
+	parts.unshift(name);
 
 	return { name, nodes, parts };
+}
+
+function to_string(node: Node) {
+	switch (node.type) {
+		case 'Literal':
+			return String(node.value);
+		case 'Identifier':
+			return node.name;
+	}
 }
