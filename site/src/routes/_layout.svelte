8bb0c80c2cc3d5aea6182ac5f50e917f321780b4
Rich Harris <richard.a.harris@gmail.com>
2020-06-12 06:04:40 -0400

    remove BLM banner (#5007)

--- site/src/routes/_layout.svelte
+++ site/src/routes/_layout.svelte
@@ -54,33 +54,6 @@
 			<Icon name="github"/>
 		</NavItem>
 	</Nav>
-	<div class="BLM">
-		<a
-			target="_blank"
-			rel="noopener noreferrer"
-			href="https://blacklivesmatter.com/"
-		>Black Lives Matter</a>.
-
-		Support
-
-		<a
-			target="_blank"
-			rel="noopener noreferrer"
-			href="https://www.joincampaignzero.org/"
-		>Campaign Zero</a>, the
-
-		<a
-			target="_blank"
-			rel="noopener noreferrer"
-			href="https://support.eji.org/give/153413/#!/donation/checkout"
-		>Equal&nbsp;Justice&nbsp;Initiative</a>, and
-
-		<a
-			target="_blank"
-			rel="noopener noreferrer"
-			href="https://bailfunds.github.io/"
-		>local bail funds</a>.
-	</div>
 {/if}
 
 <main>
@@ -88,40 +61,11 @@
 </main>
 
 <style>
-	.BLM {
-		background-color: black;
-		color: white;
-		position: fixed;
-		bottom: 0;
-		width: 100vw;
-		text-align: center;
-		padding: 1em;
-		z-index: 999;
-	}
-	.BLM a {
-		white-space: nowrap;
-	}
 	main {
 		position: relative;
 		margin: 0 auto;
 		/* padding: var(--nav-h) var(--side-nav) 0 var(--side-nav); */
 		padding: var(--nav-h) 0 0 0;
-		padding-bottom: 64px; /* BLM */
 		overflow-x: hidden;
-	}
-
-	@media screen and (max-width: 769px) {
-		main {
-			padding-bottom: 90px; /* BLM */
-		}
-	}
-
-	@media screen and (max-width: 377px) {
-		.BLM {
-			font-size: 85%;
-		}
-		main {
-			padding-bottom: 75px; /* BLM */
-		}
 	}
 </style>
