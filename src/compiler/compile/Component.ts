df790f6355a0eb1a30589aa2a73338383ef408b8
Bassam Ismail <skippednote@gmail.com>
2020-06-23 20:57:16 +0530

    fix(5019): make variable if hoisted if subscrible

--- src/compiler/compile/Component.ts
+++ src/compiler/compile/Component.ts
@@ -1152,7 +1152,7 @@ export default class Component {
 			for (const specifier of specifiers) {
 				const variable = var_lookup.get(specifier.local.name);
 
-				if (!variable.mutated || var_lookup.get(`$${specifier.local.name}`)) {
+				if (!variable.mutated || variable.subscribable) {
 					variable.hoistable = true;
 				}
 			}
