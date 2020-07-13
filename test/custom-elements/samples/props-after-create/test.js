8be9968e40b4123f003ade7be4ee82c8355aeee5
Christopher Mardell <christopher.mardell@lifelenz.com>
2020-07-13 21:10:24 +0930

    Fix props-after-create test

--- test/custom-elements/samples/props-after-create/test.js
+++ test/custom-elements/samples/props-after-create/test.js
@@ -2,17 +2,22 @@ import * as assert from 'assert';
 import CustomElement from './main.svelte';
 
 export default async function (target) {
+
+	// initialize without options to simulate instantiation within HTML
 	const el = new CustomElement();
 
 	assert.equal(el.outerHTML, '<custom-element></custom-element>');
 
-	// const el = target.querySelector('custom-element');
-
-	assert.equal(el.shadowRoot, undefined);
-
 	el.items = ['a', 'b', 'c'];
-	const [p1, p2] = el.shadowRoot.querySelectorAll('p');
+	const p0 = el.shadowRoot.querySelector('p');
 
+	// shouldn't be instantitated yet
+	assert.equal(p0, undefined);
+
+	// simulate adding to DOM to trigger setup
+	el.connectedCallback();
+
+	const [p1, p2] = el.shadowRoot.querySelectorAll('p');
 	assert.equal(p1.textContent, '3 items');
 	assert.equal(p2.textContent, 'a, b, c');
 
