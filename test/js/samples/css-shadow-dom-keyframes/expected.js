83a86d0e7273e77b0d79a755b9c6258c52f61b5c
Christopher Mardell <christopher.mardell@lifelenz.com>
2020-07-13 21:38:58 +0930

    Update expected output for css-shadow-dom-keyframes

--- test/js/samples/css-shadow-dom-keyframes/expected.js
+++ test/js/samples/css-shadow-dom-keyframes/expected.js
@@ -33,8 +33,26 @@ function create_fragment(ctx) {
 class Component extends SvelteElement {
 	constructor(options) {
 		super();
+
+		if (options) {
+			this.$$setup(options);
+		}
+	}
+
+	$$setup(options) {
 		this.shadowRoot.innerHTML = `<style>div{animation:foo 1s}@keyframes foo{0%{opacity:0}100%{opacity:1}}</style>`;
-		init(this, { target: this.shadowRoot }, null, create_fragment, safe_not_equal, {});
+
+		init(
+			this,
+			{
+				props: options ? options.props : null,
+				target: this.shadowRoot
+			},
+			null,
+			create_fragment,
+			safe_not_equal,
+			{}
+		);
 
 		if (options) {
 			if (options.target) {
