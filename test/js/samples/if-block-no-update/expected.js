cb40eb7cb6b3cc241b549d1a919c938b2d4e4a2e
Tan Li Hau <tanhauhau@users.noreply.github.com>
2020-07-18 04:12:26 +0800

    add updating guard to binding callback (#5126)

--- test/js/samples/if-block-no-update/expected.js
+++ test/js/samples/if-block-no-update/expected.js
@@ -88,7 +88,7 @@ function create_fragment(ctx) {
 function instance($$self, $$props, $$invalidate) {
 	let { foo } = $$props;
 
-	$$self.$set = $$props => {
+	$$self.$$set = $$props => {
 		if ("foo" in $$props) $$invalidate(0, foo = $$props.foo);
 	};
 
