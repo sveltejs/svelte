c07fc010e93e6b9da570acc8f007151f2e18c781
Tan Li Hau <lhtan93@gmail.com>
2020-08-12 22:28:13 +0800

    fix not passing child_ctx for catch block

---
+++ test/runtime/samples/await-with-update-2/_config.js
@@ -0,0 +1,64 @@
+export default {
+	props: {
+		thePromise: new Promise((_) => {}),
+		count: 0,
+	},
+
+	html: `
+		<div><p>loading...</p></div>
+	`,
+
+	async test({ assert, component, target }) {
+		await (component.thePromise = Promise.resolve({ value: "success", Component: component.Component }));
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+			<div>Resolved:
+				<div>count: 0</div>
+				<div>value: success</div>
+			</div>
+			`
+		);
+
+		component.count = 5;
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+			<div>Resolved:
+				<div>count: 5</div>
+				<div>value: success</div>
+			</div>
+			`
+		);
+
+		try {
+			await (component.thePromise = Promise.reject({ value: "failure", Component: component.Component }));
+		} catch (error) {
+			// ignore
+		}
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+			<div>Rejected:
+				<div>count: 5</div>
+				<div>value: failure</div>
+			</div>
+			`
+		);
+
+		component.count = 10;
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+			<div>Rejected:
+				<div>count: 10</div>
+				<div>value: failure</div>
+			</div>
+			`
+		);
+	},
+};
---
+++ test/runtime/samples/await-with-update-2/main.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Component from './Component.svelte';
+	export let thePromise;
+	export let count;
+	export { Component }
+</script>
+
+<div>
+	{#await thePromise}
+		<p>loading...</p>
+	{:then { value: theValue, Component }}
+		Resolved: <svelte:component this={Component} {count} value={theValue} />
+	{:catch { value: theError, Component } }
+		Rejected: <svelte:component this={Component} {count} value={theError} />
+	{/await}
+</div>
