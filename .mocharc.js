c01dafa5f6f086c386b051fb6216a10d95f6839c
Ben McCann <322311+benmccann@users.noreply.github.com>
2020-09-10 08:28:37 -0700

    Move .mocharc to normal location and upgrade mohca

--- .mocharc.js
+++ .mocharc.js
@@ -1,5 +1,3 @@
-// paths are relative to project root
-
 module.exports = {
   diff: true,
   extension: ['js'],
@@ -8,10 +6,7 @@ module.exports = {
   slow: 75,
   timeout: 2000,
   ui: 'bdd',
-  // files to import before tests
   file: [
     'test/test.js'
   ]
-  // not used. test files are called from test/test.js
-  //'watch-files': ['test/*/index.js'],
 };
