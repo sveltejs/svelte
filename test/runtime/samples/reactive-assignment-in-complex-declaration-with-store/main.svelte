9c13282f8f316bdf6fadad3064c017b01a88e4a0
Conduitry <git@chor.date>
2020-09-15 08:56:58 -0400

    formatting

--- test/runtime/samples/reactive-assignment-in-complex-declaration-with-store/main.svelte
+++ test/runtime/samples/reactive-assignment-in-complex-declaration-with-store/main.svelte
@@ -1,16 +1,16 @@
 <script>
-  import { writable } from 'svelte/store';
+	import { writable } from 'svelte/store';
 
-  let eid = writable(1);
-  let foo;
-  let u;
-  let v;
+	let eid = writable(1);
+	let foo;
+	let u;
+	let v;
 	let w;
-  [u, v, w] = [
-    {id: eid = writable(foo = 2), name: 'xxx'},
-    5,
+	[u, v, w] = [
+		{id: eid = writable(foo = 2), name: 'xxx'},
+		5,
 		writable(6)
-  ];
+	];
 </script>
 
 <h1>{foo} {$eid} {u.name} {v} {$w}</h1>
--- test/runtime/samples/store-resubscribe-c/main.svelte
+++ test/runtime/samples/store-resubscribe-c/main.svelte
@@ -2,13 +2,13 @@
 	import { writable } from 'svelte/store';
 	const context = {
 		store1: writable(31),
-		store2: writable(42),
-	}
+		store2: writable(42)
+	};
 	let store1;
 	let store2;
 	({
 		store1,
-		store2,
+		store2
 	} = context);
 </script>
 
