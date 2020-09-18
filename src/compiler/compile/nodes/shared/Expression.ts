a8e0a322873583a138a84b3dd24dd049f9d1b422
Tan Li Hau <lhtan93@gmail.com>
2020-09-18 23:11:47 +0800

    no missing-declaration warning for import.meta

--- src/compiler/compile/nodes/shared/Expression.ts
+++ src/compiler/compile/nodes/shared/Expression.ts
@@ -64,6 +64,8 @@ export default class Expression {
 			enter(node: any, parent: any, key: string) {
 				// don't manipulate shorthand props twice
 				if (key === 'value' && parent.shorthand) return;
+				// don't manipulate `import.meta`, `new.target`
+				if (node.type === 'MetaProperty') return this.skip();
 
 				if (map.has(node)) {
 					scope = map.get(node);
