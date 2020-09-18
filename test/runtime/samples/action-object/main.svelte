8c299f7f3c90c2fe02c3eed4b48e03966993699d
Tan Li Hau <tanhauhau@users.noreply.github.com>
2020-09-19 00:50:18 +0800

    support methods as actions (#5398)

---
+++ test/runtime/samples/action-object/main.svelte
@@ -0,0 +1,10 @@
+<script>
+	const obj = {
+		foo : "bar",
+		action(element, { leet }) {
+			element.foo = this.foo + leet;
+		},
+	}
+</script>
+
+<button use:obj.action={{ leet: 1337 }}>action</button>
