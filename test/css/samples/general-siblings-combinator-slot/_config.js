19af61a474040db65f4fdbbd28a0981de4c6f1e9
Conduitry <git@chor.date>
2020-09-24 16:00:55 -0400

    lint

--- test/css/samples/general-siblings-combinator-slot/_config.js
+++ test/css/samples/general-siblings-combinator-slot/_config.js
@@ -12,7 +12,7 @@ export default {
 			message: 'Unused CSS selector ".a ~ .b"',
 			pos: 111,
 			start: { character: 111, column: 1, line: 10 },
-			end: { character: 118, column: 8, line: 10 },
+			end: { character: 118, column: 8, line: 10 }
 		},
 		{
 			code: "css-unused-selector",
@@ -26,7 +26,7 @@ export default {
 			message: 'Unused CSS selector ".b ~ .c"',
 			pos: 138,
 			start: { character: 138, column: 1, line: 11 },
-			end: { character: 145, column: 8, line: 11 },
+			end: { character: 145, column: 8, line: 11 }
 		},
 		{
 			code: "css-unused-selector",
@@ -40,7 +40,7 @@ export default {
 			message: 'Unused CSS selector ".c ~ .f"',
 			pos: 165,
 			start: { character: 165, column: 1, line: 12 },
-			end: { character: 172, column: 8, line: 12 },
+			end: { character: 172, column: 8, line: 12 }
 		},
 		{
 			code: "css-unused-selector",
@@ -54,7 +54,7 @@ export default {
 			message: 'Unused CSS selector ".f ~ .g"',
 			pos: 192,
 			start: { character: 192, column: 1, line: 13 },
-			end: { character: 199, column: 8, line: 13 },
+			end: { character: 199, column: 8, line: 13 }
 		},
 		{
 			code: "css-unused-selector",
@@ -68,7 +68,7 @@ export default {
 			message: 'Unused CSS selector ".b ~ .f"',
 			pos: 219,
 			start: { character: 219, column: 1, line: 14 },
-			end: { character: 226, column: 8, line: 14 },
+			end: { character: 226, column: 8, line: 14 }
 		},
 		{
 			code: "css-unused-selector",
@@ -82,7 +82,7 @@ export default {
 			message: 'Unused CSS selector ".b ~ .g"',
 			pos: 246,
 			start: { character: 246, column: 1, line: 15 },
-			end: { character: 253, column: 8, line: 15 },
-		},
-	],
+			end: { character: 253, column: 8, line: 15 }
+		}
+	]
 };
--- test/css/samples/general-siblings-combinator/_config.js
+++ test/css/samples/general-siblings-combinator/_config.js
@@ -12,7 +12,7 @@ export default {
 			message: 'Unused CSS selector "article ~ div"',
 			pos: 275,
 			start: { character: 275, column: 1, line: 12 },
-			end: { character: 288, column: 14, line: 12 },
+			end: { character: 288, column: 14, line: 12 }
 		},
 		{
 			code: "css-unused-selector",
@@ -26,7 +26,7 @@ export default {
 			message: 'Unused CSS selector "span ~ article"',
 			pos: 308,
 			start: { character: 308, column: 1, line: 13 },
-			end: { character: 322, column: 15, line: 13 },
+			end: { character: 322, column: 15, line: 13 }
 		},
 		{
 			code: "css-unused-selector",
@@ -40,7 +40,7 @@ export default {
 			message: 'Unused CSS selector "b ~ article"',
 			pos: 342,
 			start: { character: 342, column: 1, line: 14 },
-			end: { character: 353, column: 12, line: 14 },
+			end: { character: 353, column: 12, line: 14 }
 		},
 		{
 			code: "css-unused-selector",
@@ -54,7 +54,7 @@ export default {
 			message: 'Unused CSS selector "span ~ div"',
 			pos: 373,
 			start: { character: 373, column: 1, line: 15 },
-			end: { character: 383, column: 11, line: 15 },
-		},
-	],
+			end: { character: 383, column: 11, line: 15 }
+		}
+	]
 };
