8a7d3cd259ee8cbef204a7fbfb17161b3bded111
pushkine <pushkine.github@gmail.com>
2020-10-31 17:18:03 +0100

    tests

---
+++ test/runtime/samples/action-update-this/main.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let on_destroy;
+	export let text;
+	function tooltip(node, text) {
+		node.textContent = text;
+		return {
+			node,
+			update(text) {
+				this.node.textContent = text;
+			},
+			destroy() {
+				on_destroy(this.node.textContent);
+			},
+		};
+	}
+</script>
+
+<button use:tooltip={text} />
