c7e7ea6ad15ecccbb44e3a3ad512cbde5e9163ae
pushkin <pushkine.github@gmail.com>
2020-11-03 07:11:01 +0100

    lint

--- test/runtime/samples/action-update-this/_config.js
+++ test/runtime/samples/action-update-this/_config.js
@@ -1,15 +1,15 @@
 export default {
-	props: { text: "first" },
+	props: { text: 'first' },
 	test({ assert, target, component }) {
-		assert.htmlEqual(target.innerHTML, "<button>first</button>");
-		component.text = "second";
-		assert.htmlEqual(target.innerHTML, "<button>second</button>");
+		assert.htmlEqual(target.innerHTML, '<button>first</button>');
+		component.text = 'second';
+		assert.htmlEqual(target.innerHTML, '<button>second</button>');
 		let last_text;
-		component.text = "third";
+		component.text = 'third';
 		component.on_destroy = (text) => {
 			last_text = text;
 		};
 		component.$destroy();
-		assert.equal(last_text, "third");
-	},
+		assert.equal(last_text, 'third');
+	}
 };
