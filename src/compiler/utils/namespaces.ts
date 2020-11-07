c9635a9afdd006b2f71a4d4535cd36010e25b8ce
halfnelson <dpershouse@gmail.com>
2020-11-07 15:31:28 +1000

    Introduce the foreign namespace and skip certain handling and warnings
    Allows the use of svelte for DOM implementations that aren't html5

--- src/compiler/utils/namespaces.ts
+++ src/compiler/utils/namespaces.ts
@@ -1,3 +1,4 @@
+export const foreign = 'https://svelte.dev/docs#svelte_options';
 export const html = 'http://www.w3.org/1999/xhtml';
 export const mathml = 'http://www.w3.org/1998/Math/MathML';
 export const svg = 'http://www.w3.org/2000/svg';
@@ -5,13 +6,16 @@ export const xlink = 'http://www.w3.org/1999/xlink';
 export const xml = 'http://www.w3.org/XML/1998/namespace';
 export const xmlns = 'http://www.w3.org/2000/xmlns';
 
+
 export const valid_namespaces = [
+	'foreign',
 	'html',
 	'mathml',
 	'svg',
 	'xlink',
 	'xml',
 	'xmlns',
+	foreign,
 	html,
 	mathml,
 	svg,
@@ -20,4 +24,4 @@ export const valid_namespaces = [
 	xmlns
 ];
 
-export const namespaces: Record<string, string> = { html, mathml, svg, xlink, xml, xmlns };
+export const namespaces: Record<string, string> = { foreign, html, mathml, svg, xlink, xml, xmlns };
