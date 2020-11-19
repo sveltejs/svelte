1509748b31221864e0911558756c1f633d828062
AngangGuo <AngangGuo@users.noreply.github.com>
2020-11-19 14:06:44 -0800

    using meaningfule variable name

--- site/content/tutorial/06-bindings/03-checkbox-inputs/app-a/App.svelte
+++ site/content/tutorial/06-bindings/03-checkbox-inputs/app-a/App.svelte
@@ -1,18 +1,18 @@
 <script>
-	let yes = false;
+	let subscribed = false;
 </script>
 
 <label>
-	<input type=checkbox checked={yes}>
+	<input type=checkbox checked={subscribed}>
 	Yes! Send me regular email spam
 </label>
 
-{#if yes}
+{#if subscribed}
 	<p>Thank you. We will bombard your inbox and sell your personal details.</p>
 {:else}
 	<p>You must opt in to continue. If you're not paying, you're the product.</p>
 {/if}
 
-<button disabled={!yes}>
+<button disabled={!subscribed}>
 	Subscribe
 </button>
