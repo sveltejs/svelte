dcfbd6951634408d97757f2f66cbfbe622d107c2
halfnelson <dpershouse@gmail.com>
2020-11-20 04:38:41 +1000

    Preprocessor sourcemap support (#5584)
    Co-authored-by: Milan Hauth <milahu@gmail.com>
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

---
+++ test/sourcemaps/samples/preprocessed-styles/test.js
@@ -0,0 +1,32 @@
+export function test({ assert, input, css }) {
+	const expectedBar = input.locate('--baritone');
+	const expectedBaz = input.locate('--baz');
+
+	let start = css.locate('--bar');
+
+	const actualbar = css.mapConsumer.originalPositionFor({
+		line: start.line + 1,
+		column: start.column
+	});
+
+	assert.deepEqual(actualbar, {
+		source: 'input.svelte',
+		name: null,
+		line: expectedBar.line + 1,
+		column: expectedBar.column
+	}, "couldn't find bar in source");
+
+	start = css.locate('--baz');
+
+	const actualbaz = css.mapConsumer.originalPositionFor({
+		line: start.line + 1,
+		column: start.column
+	});
+
+	assert.deepEqual(actualbaz, {
+		source: 'input.svelte',
+		name: null,
+		line: expectedBaz.line + 1,
+		column: expectedBaz.column
+	}, "couldn't find baz in source");
+}
