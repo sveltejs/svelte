ed25c0bf1f1eae64cb0dab3fc58e309bfd3992e7
Nick Vincent <the.nick.vincent@gmail.com>
2020-12-04 10:59:03 -0800

    Interpolation no longer needed in this string

--- src/runtime/internal/dom.ts
+++ src/runtime/internal/dom.ts
@@ -269,7 +269,7 @@ export function add_resize_listener(node: HTMLElement, fn: () => void) {
 	const iframe = element('iframe');
 	iframe.setAttribute('style',
 		'display: block; position: absolute; top: 0; left: 0; width: 100%; height: 100%; ' +
-		`overflow: hidden; border: 0; opacity: 0; pointer-events: none; z-index: -1;`
+		'overflow: hidden; border: 0; opacity: 0; pointer-events: none; z-index: -1;'
 	);
 	iframe.setAttribute('aria-hidden', 'true');
 	iframe.tabIndex = -1;
