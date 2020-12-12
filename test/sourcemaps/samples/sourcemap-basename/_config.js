20499ee77ef0224aef62cd4f0f3fb2a6bac683d7
dmitrage <dmitrage@gmail.com>
2020-12-12 19:19:53 +0300

    Add test for basename

---
+++ test/sourcemaps/samples/sourcemap-basename/_config.js
@@ -0,0 +1,33 @@
+import { magic_string_bundle } from '../../helpers';
+
+export const component_filepath = 'src/input.svelte';
+
+export const component_file_basename = 'input.svelte';
+
+// as output by external tool for src/external_code.css (relative to src/input.svelte)
+export const external_relative_filename = 'external_code.css';
+
+const external_code = `
+span {
+	--external_code-var: 1px;
+}
+`;
+
+export default {
+	css_map_sources: [external_relative_filename],
+	js_map_sources: [],
+	preprocess: [
+		{
+			style: ({ content, filename }) => {
+				const external =`/* Filename from preprocess: ${filename} */` + external_code;
+				return magic_string_bundle([
+					{ code: external, filename: external_relative_filename },
+					{ code: content, filename }
+				]);
+			}
+		}
+	],
+	options: {
+		filename: component_filepath
+	}
+};
