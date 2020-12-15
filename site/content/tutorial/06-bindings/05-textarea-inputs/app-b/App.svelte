342faf251a31a5988ad9ac54546a2e6039ec9169
Aswin <aswin@deducely.com>
2020-12-14 19:37:42 -0500

    changed message for better understanding

--- site/content/tutorial/06-bindings/05-textarea-inputs/app-b/App.svelte
+++ site/content/tutorial/06-bindings/05-textarea-inputs/app-b/App.svelte
@@ -1,6 +1,6 @@
 <script>
 	import marked from 'marked';
-	let value = `Some words are *italic*, some are **bold** (Scroll down to see markdown output).`;
+	let value = `Some words are *italic*, some are **bold** (You may have to scroll down to see the markdown output).`;
 </script>
 
 <style>
