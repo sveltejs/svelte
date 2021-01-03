a2be133b7805d88c4ab84b39ed1205b8386cb129
Melonai <einebeere@gmail.com>
2021-01-03 18:49:13 +0100

    Test required ARIA props check

---
+++ test/validator/samples/a11y-role-has-required-aria-props/input.svelte
@@ -0,0 +1,7 @@
+<div role="heading"></div>
+<span role="checkbox"></span>
+<div role="meter"></div>
+
+<div role="heading" aria-level="1"></div>
+<span role="checkbox" aria-checked="false"></span>
+<div role="meter" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
