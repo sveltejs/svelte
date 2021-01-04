82fcdfc2fa0321cd1014fe9e5c9d79b8454f2f15
Tan Li Hau <tanhauhau@users.noreply.github.com>
2021-01-05 03:41:11 +0800

    rework SSR store autosubscription (#5419)

--- test/runtime/samples/reactive-assignment-in-complex-declaration-with-store/_config.js
+++ test/runtime/samples/reactive-assignment-in-complex-declaration-with-store/_config.js
@@ -1,7 +1,6 @@
 // destructure to store
 export default {
 	html: '<h1>2 2 xxx 5 6 9 10 2</h1>',
-	skip_if_ssr: true,
 	async test({ assert, target, component }) {
 		await component.update();
 		assert.htmlEqual(target.innerHTML, '<h1>11 11 yyy 12 13 14 15 11</h1>');
