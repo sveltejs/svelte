fd20fd5c4590c3672fbdb5d11ceb8b0a28697720
J Delaney <j.delaney.sd@gmail.com>
2021-01-26 20:21:49 -0800

    Add test for store val and exported unreactive val

--- test/js/samples/reactive-class-optimized/input.svelte
+++ test/js/samples/reactive-class-optimized/input.svelte
@@ -4,8 +4,12 @@
 </script>
 
 <script>
+	import { reactiveStoreVal, unreactiveExport } from './store';
+
 	nonReactiveGlobal = Math.random();
 	const reactiveConst = {x: Math.random()};
+
+	$: reactiveDeclaration = reactiveModuleVar * 2;
 
 	reactiveModuleVar += 1;
 	if (Math.random()) {
@@ -17,8 +21,11 @@
 <div class:update1={reactiveModuleVar}></div>
 <div class:update2={reactiveConst.x}></div>
 <div class:update3={nonReactiveGlobal && reactiveConst.x}></div>
+<div class:update4={$reactiveStoreVal}></div>
+<div class:update5={$reactiveDeclaration}></div>
 
 <!--These shouldn't get updates because they're purely non-reactive-->
 <div class:static1={nonReactiveModuleVar}></div>
-<div class:static1={nonReactiveGlobal}></div>
-<div class:static1={nonReactiveModuleVar && nonReactiveGlobal}></div>
+<div class:static2={nonReactiveGlobal}></div>
+<div class:static3={nonReactiveModuleVar && nonReactiveGlobal}></div>
+<div class:static4={unreactiveExport}></div>
