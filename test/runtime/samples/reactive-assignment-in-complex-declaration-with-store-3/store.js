2f544cd9fb58f033adfdb30fd4f010310b9a41df
Conduitry <git@chor.date>
2021-01-29 11:02:57 -0500

    fix indentation

--- test/runtime/samples/reactive-assignment-in-complex-declaration-with-store-3/store.js
+++ test/runtime/samples/reactive-assignment-in-complex-declaration-with-store-3/store.js
@@ -3,16 +3,16 @@ const _store = writable(0);
 let count = 0;
 
 export const store = {
-  ..._store,
+	..._store,
 	subscribe(fn) {
-    count++;
+		count++;
 		return _store.subscribe(fn);
-  },
-  reset() {
-    count = 0;
-    _store.set(0);
-  },
-  numberOfTimesSubscribeCalled() {
-    return count;
-  }
+	},
+	reset() {
+		count = 0;
+		_store.set(0);
+	},
+	numberOfTimesSubscribeCalled() {
+		return count;
+	}
 };
