44f41c9edd61de5c27e093840b9d7c9b42ee48a6
Tan Li Hau <tanhauhau@users.noreply.github.com>
2021-02-09 00:26:46 +0800

    fix :host and :global css scoping (#5957)

---
+++ test/css/samples/host/input.svelte
@@ -0,0 +1,27 @@
+<style>
+  :host h1 {
+    color: red;
+  }
+
+  :host > h1 {
+    color: red;
+  }
+
+  :host > * {
+    color: red;
+	}
+
+	:host span {
+		color: red;
+	}
+		
+	:host > span {
+		color: red;
+	}
+</style>
+
+<h1>Hello!</h1>
+
+<div>
+  <span>World!</span>
+</div>
