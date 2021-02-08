149dfc3059b857936fdb783c4e5aa63a7905590b
Conduitry <git@chor.date>
2021-02-08 14:07:42 -0500

    lint

--- test/runtime/samples/component-binding-reactive-property-no-extra-call/main.svelte
+++ test/runtime/samples/component-binding-reactive-property-no-extra-call/main.svelte
@@ -1,11 +1,11 @@
 <script>
-	import Component from "./Component.svelte";
+	import Component from './Component.svelte';
 
 	export let primitive_updates = 0;
 	export let object_updates = 0;
 
-	let obj = { foo: "" };
-	let foo = "bar";
+	const obj = { foo: '' };
+	let foo = 'bar';
 	$: if (obj) object_updates++;
 	$: if (foo) primitive_updates++;
 </script>
