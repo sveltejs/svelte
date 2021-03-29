153a1163bf769503638c3e6e670094e03452f03a
Chris Chua <chris.sirhc@gmail.com>
2021-03-28 19:12:57 -0700

    test cleanup

--- test/runtime/samples/transition-css-in-out-in-with-static/_config.js
+++ test/runtime/samples/transition-css-in-out-in-with-static/_config.js
@@ -16,8 +16,15 @@ export default {
 		raf.tick(75);
 		component.visible = true;
 
-		// reset original styles
+		// reset to in styles (in transition in progress)
 		assert.equal(div.style.animation, '__svelte_3809512021_1 100ms linear 0ms 1 both');
 		assert.equal(div.className, '__svelte_2375698960');
+
+		// 100ms after in transition started at 75
+		raf.tick(175);
+
+		// reset original styles after in transition is complete
+		assert.equal(div.style.animation, '');
+		assert.equal(div.className, '');
 	}
 };
