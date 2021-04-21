dcb004658ca5ca7e6e808cf1555ff185f369d11d
Mauricio Paternina <49292621+spaceinvadev@users.noreply.github.com>
2021-04-21 11:46:00 -0500

    tutorial: fix custombutton colors for a11y contrast (#6212)

--- site/content/tutorial/05-events/06-dom-event-forwarding/app-b/CustomButton.svelte
+++ site/content/tutorial/05-events/06-dom-event-forwarding/app-b/CustomButton.svelte
@@ -2,18 +2,18 @@
 	button {
 		height: 4rem;
 		width: 8rem;
-		background-color: #aaa;
-		border-color: #f1c40f;
-		color: #f1c40f;
+		background-color: #ddd;
+		border-color: #ff3e00;
+		color: #ff3e00;
 		font-size: 1.25rem;
-		background-image: linear-gradient(45deg, #f1c40f 50%, transparent 50%);
+		background-image: linear-gradient(45deg, #ff3e00 50%, transparent 50%);
 		background-position: 100%;
 		background-size: 400%;
 		transition: background 300ms ease-in-out;
 	}
 	button:hover {
 		background-position: 0;
-		color: #aaa;
+		color: #ddd;
 	}
 </style>
 
