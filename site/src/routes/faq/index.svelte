a852fc10fb219d4140fa7efe408b5a9c52c7c5ee
Babak F <babak.bxf@gmail.com>
2021-04-22 08:09:27 -0700

    Take back 'horizontal align fix'.

--- site/src/routes/faq/index.svelte
+++ site/src/routes/faq/index.svelte
@@ -40,7 +40,7 @@
 		grid-gap: 1em;
 		min-height: calc(100vh - var(--nav-h));
 		padding: var(--top-offset) var(--side-nav) 6rem var(--side-nav);
-		max-width: 30em;
+		max-width: var(--main-width);
 		margin: 0 auto;
 	}
 
@@ -67,6 +67,7 @@
 
 	.faq p {
 		font-size: var(--h5);
+		max-width: 30em;
 		color: var(--second);
 	}
 
