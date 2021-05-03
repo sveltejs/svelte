3f97f70b2bf85ee91a73a00435e87c153b6a3feb
Joffrey Leveugle <joffrey.leveugle@decathlon.com>
2021-05-03 19:45:52 +0200

    add security warning for anchor element (rel attribute)

---
+++ test/validator/samples/security-anchor-rel-noopener/input.svelte
@@ -0,0 +1,34 @@
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="https://svelte.dev" target="_blank">svelte website (invalid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="https://svelte.dev" target="_blank" rel="">svelte website (invalid)</a>
+<a href="https://svelte.dev" target="_blank" rel="noreferrer">svelte website (invalid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href={'https://svelte.dev'} target="_blank">svelte website (invalid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href={'https://svelte.dev'} target="_blank" rel="">svelte website (invalid)</a>
+<a href={'https://svelte.dev'} target="_blank" rel="noreferrer">svelte website (invalid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="//svelte.dev" target="_blank">svelte website (invalid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="//svelte.dev" target="_blank" rel="">svelte website (invalid)</a>
+<a href="//svelte.dev" target="_blank" rel="noreferrer">svelte website (invalid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="http://svelte.dev" target="_blank">svelte website (invalid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="http://svelte.dev" target="_blank" rel="">svelte website (invalid)</a>
+<a href="http://svelte.dev" target="_blank" rel="noreferrer">svelte website (invalid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="same-host" target="_blank">Same host (valid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="same-host" target="_blank" rel="">Same host (valid)</a>
+<a href="same-host" target="_blank" rel="noreferrer">Same host (valid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="http://svelte.dev" target="_blank" rel="noopener">svelte website (valid)</a>
+<a href="http://svelte.dev" target="_blank" rel="noreferrer noopener">svelte website (valid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="https://svelte.dev" target="_blank" rel="noopener">svelte website (valid)</a>
+<a href="https://svelte.dev" target="_blank" rel="noreferrer noopener">svelte website (valid)</a>
+<!-- svelte-ignore security-anchor-rel-noreferrer -->
+<a href="//svelte.dev" target="_blank" rel="noopener">svelte website (valid)</a>
+<a href="//svelte.dev" target="_blank" rel="noreferrer noopener">svelte website (valid)</a>
