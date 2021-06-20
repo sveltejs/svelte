3890efe26793ee7130cf97e90bc0b7a04cd0eed1
tanhauhau <lhtan93@gmail.com>
2021-06-21 00:56:56 +0800

    bind this to the forwarded event

---
+++ test/runtime/samples/component-events-this/main.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Widget from './Widget.svelte';
+	import Inner from './Inner.svelte';
+
+	export let logs = [];
+	export { Inner };
+
+	function foo() {
+		logs.push(this);
+	}
+	function bar() {
+		logs.push(this);
+	}
+</script>
+
+<Widget on:click={foo} on:bar={bar} />
