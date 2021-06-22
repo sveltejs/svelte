c09bc954d37f1155d51c722b570f84bf625fbe81
tanhauhau <lhtan93@gmail.com>
2021-06-22 19:26:18 +0800

    test normalise with preserve comments

--- test/server-side-rendering/index.ts
+++ test/server-side-rendering/index.ts
@@ -81,11 +81,9 @@ describe('ssr', () => {
 				if (css.code) fs.writeFileSync(`${dir}/_actual.css`, css.code);
 
 				try {
-					if (compileOptions.preserveComments) {
-						assert.equal(html.trim(), expectedHtml.trim());
-					} else {
-						assert.htmlEqual(html, expectedHtml);
-					}
+					(compileOptions.preserveComments
+						? assert.htmlEqualWithComments
+						: assert.htmlEqual)(html, expectedHtml);
 				} catch (error) {
 					if (shouldUpdateExpected()) {
 						fs.writeFileSync(`${dir}/_expected.html`, html);
