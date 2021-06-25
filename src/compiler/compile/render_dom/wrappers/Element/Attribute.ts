be00e035ba540f00fd5ef2ce49e6a366c566cd78
Altan Birler <altan.birler@tum.de>
2021-06-25 18:29:55 +0200

    Normale src urls
    Normalize both sides of the equality to be sure

--- src/compiler/compile/render_dom/wrappers/Element/Attribute.ts
+++ src/compiler/compile/render_dom/wrappers/Element/Attribute.ts
@@ -132,7 +132,7 @@ export default class AttributeWrapper extends BaseAttributeWrapper {
 			`);
 		} else if (this.is_src) {
 			block.chunks.hydrate.push(
-				b`if (${element.var}.src !== (new URL(${init}, location)).href) ${method}(${element.var}, "${name}", ${this.last});`
+				b`if (new URL(${element.var}.src, location).href !== (new URL(${init}, location)).href) ${method}(${element.var}, "${name}", ${this.last});`
 			);
 			updater = b`${method}(${element.var}, "${name}", ${should_cache ? this.last : value});`;
 		} else if (property_name) {
@@ -193,7 +193,7 @@ export default class AttributeWrapper extends BaseAttributeWrapper {
 
 		if (should_cache) {
 			condition = this.is_src
-				? x`${condition} && (${element.var}.src !== (new URL((${last} = ${value}), location)).href)`
+				? x`${condition} && (new URL(${element.var}.src, location).href !== (new URL((${last} = ${value}), location)).href)`
 				: x`${condition} && (${last} !== (${last} = ${value}))`;
 		}
 
