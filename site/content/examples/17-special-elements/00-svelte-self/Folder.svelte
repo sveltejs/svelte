44395c602747824e9297942489fa838dd1183372
Jatin Hemnani <73883835+jatinhemnani01@users.noreply.github.com>
2021-06-26 21:19:53 +0530

    added transition on toggle folder
    without transition it was looking odd and now it's smooth

--- site/content/examples/17-special-elements/00-svelte-self/Folder.svelte
+++ site/content/examples/17-special-elements/00-svelte-self/Folder.svelte
@@ -1,6 +1,6 @@
 <script>
 	import File from './File.svelte';
-
+	import {slide} from 'svelte/transition'
 	export let expanded = false;
 	export let name;
 	export let files;
@@ -13,7 +13,7 @@
 <span class:expanded on:click={toggle}>{name}</span>
 
 {#if expanded}
-	<ul>
+	<ul transition:slide={{duration:300}}>
 		{#each files as file}
 			<li>
 				{#if file.type === 'folder'}
