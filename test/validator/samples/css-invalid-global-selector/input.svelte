286fb9873a86533750521e60be907dd76f1dabff
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-06-28 09:54:59 +0700

    separate test suite

--- test/validator/samples/css-invalid-global-selector/input.svelte
+++ test/validator/samples/css-invalid-global-selector/input.svelte
@@ -1,13 +1,4 @@
 <style>
-	div :global(:is(div, h1)) {
-		color: red;
-	}
-	div :global(:where(div, h1)) {
-		color: red;
-	}
-	div :global(div > :where(h1, h2)) {
-		color: red;
-	}
 	div :global(.h1\,h2\,h3) {
 		color: red;
 	}
