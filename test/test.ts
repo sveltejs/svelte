cf5401c32ba90631532754aafb4f2cdfb574f2f5
tanhauhau <lhtan93@gmail.com>
2021-08-01 10:24:34 +0800

    split out unit test and browser test

--- test/test.ts
+++ test/test.ts
@@ -8,7 +8,12 @@ require('../internal');
 
 console.clear();
 
-const test_folders = glob('*/index.ts', { cwd: 'test' });
+const browser_tests = ['custom-elements', 'runtime-puppeteer'];
+const to_run_browser_tests = get_to_run_browser_tests();
+
+const test_folders = glob('*/index.ts', { cwd: 'test' })
+	.filter(folder => to_run_browser_tests === browser_tests.some(test => folder.startsWith(test)));
+
 const solo_folders = test_folders.filter(folder => /\.solo/.test(folder));
 
 if (solo_folders.length) {
@@ -18,4 +23,10 @@ if (solo_folders.length) {
 	solo_folders.forEach(name => require('./' + name));
 } else {
 	test_folders.forEach(name => require('./' + name));
+}
+
+function get_to_run_browser_tests() {
+	const browser_flag = '--browser=';
+	const argv = process.argv.find(argv => argv.startsWith(browser_flag));
+	return !!argv && argv.slice(browser_flag.length) === 'true';
 }
