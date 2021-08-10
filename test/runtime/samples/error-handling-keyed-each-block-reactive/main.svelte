90c8f972492f3eea4aa826a929228e4105d30868
rster20002 <rster2002dev@gmail.com>
2021-08-10 15:25:22 +0200

    Add keyed each tests

---
+++ test/runtime/samples/error-handling-keyed-each-block-reactive/main.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onError } from "svelte";
+
+    var a = [{
+        a: {
+            b: 10
+        }
+    }];
+
+    export var error = false;
+    
+    onError(e => {
+        error = true;
+    });
+
+    function clickEvent() {
+        a = [{}];
+    }
+</script>
+
+<button on:click={clickEvent}></button>
+
+{#each a as item (item.a.b)}
+    {item}
+{/each}
---
+++ test/runtime/samples/error-handling-keyed-each-block/main.svelte
@@ -0,0 +1,14 @@
+<script>
+    import { onError } from "svelte";
+
+    var a = [{}]
+    export var error = false;
+    
+    onError(e => {
+        error = true;
+    });
+</script>
+
+{#each a as item (item.a.b)}
+    {item}
+{/each}
