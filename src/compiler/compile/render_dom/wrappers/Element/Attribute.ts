fc4797c6f84cf299117243e7193bf77d96914e85
Yuichiro Yamashita <xydybaseball@gmail.com>
2021-08-28 04:08:38 +0900

    [fix] only use optimized 'src' handling for 'html' namespace (#6580)

--- src/compiler/compile/render_dom/wrappers/Element/Attribute.ts
+++ src/compiler/compile/render_dom/wrappers/Element/Attribute.ts
@@ -86,8 +86,9 @@ export default class AttributeWrapper extends BaseAttributeWrapper {
 			this.is_select_value_attribute = this.name === 'value' && this.parent.node.name === 'select';
 			this.is_input_value = this.name === 'value' && this.parent.node.name === 'input';
 		}
-		
-		this.is_src = this.name === 'src'; // TODO retire this exception in favour of https://github.com/sveltejs/svelte/issues/3750
+
+		// TODO retire this exception in favour of https://github.com/sveltejs/svelte/issues/3750
+		this.is_src = this.name === 'src' && (!this.parent.node.namespace || this.parent.node.namespace === namespaces.html);
 		this.should_cache = should_cache(this);
 	}
 
