8998079d03061baa2329580d383bba5bb650889b
shinyaigeek <philispaxil@gmail.com>
2021-08-31 13:43:20 +0900

    [refactor] bundle up two filter into one

--- src/compiler/compile/utils/is_interactive_role.ts
+++ src/compiler/compile/utils/is_interactive_role.ts
@@ -1,11 +1,11 @@
 import { ARIARoleDefintionKey, roles as rolesMap } from 'aria-query';
 
 const roles = [...rolesMap.keys()];
-const _interactiveRoles = roles
-	.filter((name) => !rolesMap.get(name).abstract)
-	.filter((name) =>
+const _interactiveRoles = roles.filter(
+	(name) =>
+		!rolesMap.get(name).abstract &&
 		rolesMap.get(name).superClass.some((c) => c.includes('widget'))
-	);
+);
 
 // 'toolbar' does not descend from widget, but it does support
 // aria-activedescendant, thus in practice we treat it as a widget.
