e34c5712c8ac63248aa611f6100280fef28e0a41
ZhiyueYi <lane0223@hotmail.com>
2021-10-10 00:47:09 +0800

    fix: missing null value of append_styles in init function

--- src/compiler/compile/render_dom/index.ts
+++ src/compiler/compile/render_dom/index.ts
@@ -584,7 +584,7 @@ export default function dom(
 			class ${name} extends ${superclass} {
 				constructor(options) {
 					super(${options.dev && 'options'});
-					@init(this, options, ${definition}, ${has_create_fragment ? 'create_fragment' : 'null'}, ${not_equal}, ${prop_indexes}, ${optional_parameters});
+					@init(this, options, ${definition}, ${has_create_fragment ? 'create_fragment' : 'null'}, ${not_equal}, ${prop_indexes}, null, ${optional_parameters});
 					${options.dev && b`@dispatch_dev("SvelteRegisterComponent", { component: this, tagName: "${name.name}", options, id: create_fragment.name });`}
 
 					${dev_props_check}
