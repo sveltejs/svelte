5815b2b5584d35d5f85a8a2e4d3223840bcead76
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-06 11:47:28 +0300

    Refactor and Optimize trim utils

--- src/compiler/utils/patterns.ts
+++ src/compiler/utils/patterns.ts
@@ -1,3 +1,5 @@
 export const whitespace = /[ \t\r\n]/;
+export const startWhitespace = /^[ \t\r\n]*/;
+export const endWhitespace = /[ \t\r\n]*$/;
 
 export const dimensions = /^(?:offset|client)(?:Width|Height)$/;
