5a0185a5bbe113645f3d5af3549d37feb8d2fb97
Aykut Kardaş <aykutkrds@gmail.com>
2021-11-07 12:35:40 +0300

    Update trim util tests

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -4,13 +4,13 @@ import { trim_start, trim_end } from '../../src/compiler/utils/trim';
 describe('utils', () => {
 	describe('trim', () => {
 		it('trim_start', () => {
-			const value = trim_start('    content');
-			assert.equal(value, 'content');
+			const value = trim_start('	\r\n\t svelte content \r\n\t	');
+			assert.equal(value, 'svelte content \r\n\t	');
 		});
 
     it('trim_end', () => {
-			const value = trim_end('content    ');
-			assert.equal(value, 'content');
+			const value = trim_end('	\r\n\t svelte content \r\n\t	');
+			assert.equal(value, '	\r\n\t svelte content');
 		});
 	});
 });
