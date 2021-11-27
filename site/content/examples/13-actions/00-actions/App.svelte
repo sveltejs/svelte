45daefd9da449749516e8d3216f773c73dd01da7
Nayan Gautam <50981692+698969@users.noreply.github.com>
2021-11-27 15:42:20 +0545

    Add new tutorial to examples

---
+++ site/content/examples/13-actions/00-actions/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { clickOutside } from "./click_outside.js";
+
+	let showModal = true;
+
+	function handleOutclick(){
+		showModal = false;
+	}
+</script>
+
+<style>
+	.box {
+		--width: 100px;
+		--height: 100px;
+		position: absolute;
+		width: var(--width);
+		height: var(--height);
+		left: calc(50% - var(--width) / 2);
+		top: calc(50% - var(--height) / 2);
+		border-radius: 4px;
+		background-color: #ff3e00;
+		color: #fff;
+		text-align: center;
+		font-style: bold;
+	}
+</style>
+
+<button on:click={() => (showModal = true)}>Show Modal</button>
+{#if showModal}
+	<div class="box" use:clickOutside on:outclick={handleOutclick}>
+		Click outside me!
+	</div>
+{/if}
