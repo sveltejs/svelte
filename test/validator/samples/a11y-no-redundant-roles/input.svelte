7eeca8e364de0d6c9d45bd89d9ab7798bc7e6a5c
James Bradbury <me@jamesbradbury.net>
2021-12-27 23:09:54 +0000

    add a test case for redundant roles

---
+++ test/validator/samples/a11y-no-redundant-roles/input.svelte
@@ -0,0 +1,7 @@
+<button role="button" />
+<nav role="navigation" />
+<a href='/' role="link">a link</a>
+
+<header role='banner'></header>
+<footer role='contentinfo'></footer>
+<main role='main'></main>
