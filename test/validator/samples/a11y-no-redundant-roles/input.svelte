fdfbbed22f53461bda6c67d423c140d64fa930a5
James Bradbury <me@jamesbradbury.net>
2021-12-31 14:01:20 +0000

    update test to reflect more cases that can be checked

--- test/validator/samples/a11y-no-redundant-roles/input.svelte
+++ test/validator/samples/a11y-no-redundant-roles/input.svelte
@@ -1,7 +1,44 @@
+<a href="/" role="link">a link</a>
+<article role="article" />
+<aside role="complementary" />
+<body role="document" />
 <button role="button" />
+<datalist role="listbox" />
+<dd role="definition" />
+<dfn role="term" />
+<details role="group" />
+<dialog role="dialog" />
+<dt role="term" />
+<fieldset role="group" />
+<figure role="figure" />
+<form role="form">foo</form>
+<h1 role="heading">heading</h1>
+<h2 role="heading">heading</h2>
+<h3 role="heading">heading</h3>
+<h4 role="heading">heading</h4>
+<h5 role="heading">heading</h5>
+<h6 role="heading">heading</h6>
+<hr role="separator" />
+<li role="listitem" />
+<link role="link" />
+<main role="main"></main>
+<menu role="list" />
 <nav role="navigation" />
-<a href='/' role="link">a link</a>
+<ol role="list" />
+<optgroup role="group" />
+<option role="option" />
+<output role="status" />
+<progress role="progressbar" />
+<section role="region" />
+<summary role="button" />
+<table role="table" />
+<tbody role="rowgroup" />
+<textarea role="textbox" />
+<tfoot role="rowgroup" />
+<thead role="rowgroup" />
+<tr role="row" />
+<ul role="list" />
 
-<header role='banner'></header>
-<footer role='contentinfo'></footer>
-<main role='main'></main>
+<!-- Tested header/footer not nested in section/article -->
+<header role="banner"></header>
+<footer role="contentinfo"></footer>
