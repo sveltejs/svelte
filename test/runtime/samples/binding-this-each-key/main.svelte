9f2b5afdf777b2144d7e48feb3b264ed16b6d8de
Tan Li Hau <tanhauhau@users.noreply.github.com>
2022-01-07 04:28:53 +0800

    [fix] binding member expression should only invalidate the object, not the member property (#7008)

---
+++ test/runtime/samples/binding-this-each-key/main.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let data = [ { id: '1' }, { id: '2' }, { id: '3' } ];
+
+	export let refs = [];
+
+	// note that this is NOT data.slice().reverse()
+	// as that wouldn't have triggered an infinite loop
+	$: list = data.reverse();
+</script>
+
+{#each list as { id }, index (id)}
+	<div bind:this={refs[index]}>
+		content
+	</div>
+{/each}
