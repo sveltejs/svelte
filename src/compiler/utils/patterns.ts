45221bed0e475acf712f7888ce68675a577afbdf
bluwy <bjornlu.dev@gmail.com>
2022-01-16 23:48:08 +0800

    fix sizing regex

--- src/compiler/utils/patterns.ts
+++ src/compiler/utils/patterns.ts
@@ -4,4 +4,4 @@ export const end_whitespace = /[ \t\r\n]*$/;
 
 export const dimensions = /^(?:offset|client)(?:Width|Height)$/;
 
-export const sizing = /^contentRect|contentBoxSize|borderBoxSize|devicePixelContentBoxSize$/;
+export const sizing = /^(?:contentRect|contentBoxSize|borderBoxSize|devicePixelContentBoxSize)$/;
