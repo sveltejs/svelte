33fbe4b1a51c24b3368c092ef8d08951ace79927
lidlanca <8693091+lidlanca@users.noreply.github.com>
2022-03-12 18:18:33 -0500

    ensure innerHTML for tempalte is done on template element. depends on:#7297 fixes #7315 #7319

--- src/runtime/internal/dom.ts
+++ src/runtime/internal/dom.ts
@@ -650,7 +650,7 @@ export class HtmlTag {
 	// html tag nodes
 	n: ChildNode[];
 	// target
-	t: HTMLElement;
+	t: HTMLElement | DocumentFragment;
 	// anchor
 	a: HTMLElement;
 
@@ -665,7 +665,7 @@ export class HtmlTag {
 	m(html: string, target: HTMLElement, anchor: HTMLElement = null) {
 		if (!this.e) {
 			this.e = element(target.nodeName as keyof HTMLElementTagNameMap);
-			this.t = target;
+			this.t = target.tagName !== 'TEMPLATE' ? target : (target as HTMLTemplateElement).content;
 			this.c(html);
 		}
 
@@ -674,7 +674,7 @@ export class HtmlTag {
 
 	h(html: string) {
 		this.e.innerHTML = html;
-		this.n = Array.from(this.e.childNodes);
+		this.n = Array.from(this.e.nodeName == 'TEMPLATE' ? (this.e as HTMLTemplateElement).content.childNodes : this.e.childNodes);
 	}
 
 	i(anchor) {
--- test/runtime/samples/template/main.svelte
+++ test/runtime/samples/template/main.svelte
@@ -2,3 +2,4 @@
 	<div>foo</div>
 </template>
 <template id="t2">123</template>
+<template id="t3">1{@html '<b>B</b>'}1</template>
