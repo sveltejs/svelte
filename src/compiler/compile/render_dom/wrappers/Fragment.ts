03dff4652290d469f10e9499a41b6a964f853f9e
yosuke ota <otameshiyo23@gmail.com>
2022-04-11 10:19:59 +0900

    revert Fragment

--- src/compiler/compile/render_dom/wrappers/Fragment.ts
+++ src/compiler/compile/render_dom/wrappers/Fragment.ts
@@ -127,10 +127,9 @@ export default class FragmentWrapper {
 		if (strip_whitespace) {
 			const first = this.nodes[0] as Text;
 
-			if (first && first.node.type === 'Text') {
-				if (!first.node.keep_space()) {
-					first.data = trim_start(first.data);
-				}
+
+			if (first && first.node.type === 'Text' && !first.node.keep_space()) {
+				first.data = trim_start(first.data);
 				if (!first.data) {
 					first.var = null;
 					this.nodes.shift();
