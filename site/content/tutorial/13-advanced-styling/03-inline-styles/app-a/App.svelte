032805c2688718428178b620f43f5e2720f26cd0
gtmnayan <50981692+gtm-nayan@users.noreply.github.com>
2022-04-12 23:46:32 +0545

    [docs] style directive tutorial (#7161)

---
+++ site/content/tutorial/13-advanced-styling/03-inline-styles/app-a/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	let bgOpacity = 0.5;
+	$: color = bgOpacity < 0.6 ? '#000' : '#fff';
+</script>
+
+<input type="range" min="0" max="1" step="0.1" bind:value={bgOpacity} />
+
+<p>This is a paragraph.</p>
+
+<style>
+	p {
+		font-family: "Comic Sans MS", cursive;
+		background: rgba(255, 62, 0, var(--opacity));
+	}
+</style>
