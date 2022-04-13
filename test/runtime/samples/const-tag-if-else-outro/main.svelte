f45160c53d8c12ef93a5eceb64ddba2c2047c9fa
tanhauhau <lhtan93@gmail.com>
2022-04-13 23:54:20 +0800

    support const tag for if block

---
+++ test/runtime/samples/const-tag-if-else-outro/main.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let boxes;
+	function fade() {
+		return {
+			duration: 0,
+		}
+	}
+</script>
+
+{#if boxes.length > 1}
+	{@const box1 = boxes[0]}
+	{@const box2 = boxes[1]}
+	{@const { width, height } = box1}
+	<div>{width} x {height}</div>
+	<div>{box2.width} x {box2.height}</div>
+{:else if boxes.length > 0}
+	{@const box = boxes[0]}
+	{@const { width, height } = box}
+	<div out:fade>{width} x {height}</div>
+{/if}
+
+{#if boxes.length > 1}
+	<div>{boxes[0].width} x {boxes[0].height}</div>
+	<div>{boxes[1].width} x {boxes[1].height}</div>
+{:else if boxes.length > 0}
+	{@const box = boxes[0]}
+	{@const { width, height } = box}
+	<div out:fade>{width} x {height}</div>
+{/if}
