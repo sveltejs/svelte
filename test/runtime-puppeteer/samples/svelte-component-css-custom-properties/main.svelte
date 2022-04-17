5d13a8dab6176248020cadde98f110ed8ec6abd9
baseballyama <xydybaseball@gmail.com>
2022-04-17 14:46:29 +0900

    add test

---
+++ test/runtime-puppeteer/samples/svelte-component-css-custom-properties/main.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Slider from './Slider.svelte';
+</script>
+
+<svelte:component
+	this={Slider}
+	id="slider-1"
+	--rail-color="rgb(0, 0, 0)"
+	--track-color="rgb(255, 0, 0)"
+/>
+
+<svelte:component
+	this={Slider}
+	id="slider-2"
+	--rail-color="rgb(0, 255, 0)"
+	--track-color="rgb(0, 0, 255)"
+/>
