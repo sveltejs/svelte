3bec11a012433c69e9748230e0cee060ce6b733a
Nayan Gautam <gtmnayan@gmail.com>
2022-06-15 08:52:43 +0000

    fix test

--- test/runtime/samples/inline-style-directive-and-style-attr-merged/_config.js
+++ test/runtime/samples/inline-style-directive-and-style-attr-merged/_config.js
@@ -2,16 +2,19 @@ export default {
 	html: `
 		<p style="height: 40px; color: red;"></p>
 	`,
+	solo: true,
 
 	test({ assert, target, window, component }) {
 		const p = target.querySelector("p");
-
 		const styles = window.getComputedStyle(p);
 		assert.equal(styles.color, "red");
 		assert.equal(styles.height, "40px");
 
 		component.color2 = "yellow";
-		const updated_styles = window.getComputedStyle(p);
-		assert.equal(updated_styles.color, "red");
+		{
+			const p = target.querySelector("p");
+			const styles = window.getComputedStyle(p);
+			assert.equal(styles.color, "red");
+		}
 	},
 };
