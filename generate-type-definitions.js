2779f98d72af0a2b93fdef094b49ff7fefd5a22b
Simon H <5968653+dummdidumm@users.noreply.github.com>
2022-07-06 11:58:43 +0200

    [fix] export CompileOptions (#7658)
    It's used by SvelteKit and in order to properly use it under the new TS moduleResolution NodeNext it needs to be part of a file that is defined in the exports map

--- generate-type-definitions.js
+++ generate-type-definitions.js
@@ -5,9 +5,20 @@ const { readFileSync, writeFileSync } = require('fs');
 
 execSync('tsc -p src/compiler --emitDeclarationOnly && tsc -p src/runtime --emitDeclarationOnly');
 
-// We need to add these types to the index.d.ts here because if we add them before building, the build will fail,
+// We need to add these types to the .d.ts files here because if we add them before building, the build will fail,
 // because the TS->JS transformation doesn't know these exports are types and produces code that fails at runtime.
 // We can't use `export type` syntax either because the TS version we're on doesn't have this feature yet.
-const path = 'types/runtime/index.d.ts';
-const content = readFileSync(path, 'utf8');
-writeFileSync(path, content.replace('SvelteComponentTyped', 'SvelteComponentTyped, ComponentType, ComponentConstructorOptions, ComponentProps'));
+
+function modify(path, modifyFn) {
+    const content = readFileSync(path, 'utf8');
+    writeFileSync(path, modifyFn(content));
+}
+
+modify(
+    'types/runtime/index.d.ts',
+    content => content.replace('SvelteComponentTyped', 'SvelteComponentTyped, ComponentType, ComponentConstructorOptions, ComponentProps')
+);
+modify(
+    'types/compiler/index.d.ts',
+    content => content + '\nexport { CompileOptions, ModuleFormat, EnableSourcemap, CssHashGetter } from "./interfaces"'
+);
