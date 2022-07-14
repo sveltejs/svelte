3ddb4f6e2085b3b9950a8dc6cff3c0a20482690f
chientrm <chientrm@gmail.com>
2022-07-14 14:36:22 +0000

    fix linting

--- src/compiler/parse/state/fragment.ts
+++ src/compiler/parse/state/fragment.ts
@@ -1,10 +1,10 @@
-import { Parser } from "../index";
-import mustache from "./mustache";
-import tag from "./tag";
-import text from "./text";
+import { Parser } from '../index';
+import mustache from './mustache';
+import tag from './tag';
+import text from './text';
 
 export default (parser: Parser) =>
 	[
-		{ match: "<", fragment: tag },
-		{ match: "{", fragment: mustache },
+		{ match: '<', fragment: tag },
+		{ match: '{', fragment: mustache }
 	].find(({ match }) => parser.match(match))?.fragment ?? text;
