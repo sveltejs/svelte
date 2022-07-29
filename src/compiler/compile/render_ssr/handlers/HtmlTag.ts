bb02faec16e0b1b713bb81a2bf162404e251fd31
Maximiliano Ruani <maxi.ruani@hotmail.com>
2022-07-29 17:50:07 -0300

    Fix hydration duplicate head tag issue sveltejs#7444

--- src/compiler/compile/render_ssr/handlers/HtmlTag.ts
+++ src/compiler/compile/render_ssr/handlers/HtmlTag.ts
@@ -3,7 +3,13 @@ import RawMustacheTag from '../../nodes/RawMustacheTag';
 import { Expression } from 'estree';
 
 export default function(node: RawMustacheTag, renderer: Renderer, options: RenderOptions) {
-	if (options.hydratable) renderer.add_string('<!-- HTML_TAG_START -->');
+	if (options.hydratable) {
+		renderer.add_string(`<!-- HTML_TAG_START ${options.head_id ? `data-svelte="${options.head_id}" ` : ''}-->`);
+	}
+
 	renderer.add_expression(node.expression.node as Expression);
-	if (options.hydratable) renderer.add_string('<!-- HTML_TAG_END -->');
+
+	if (options.hydratable) {
+		renderer.add_string(`<!-- HTML_TAG_END ${options.head_id ? `data-svelte="${options.head_id}" ` : ''}-->`);
+	}
 }
