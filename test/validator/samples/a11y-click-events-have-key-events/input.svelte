4578d242f8b81c07c30e93b3c0626b651d5594d7
jeel2331 <jeelrajodiyajeel@gmail.com>
2022-08-27 22:45:43 +0530

    lick-events-have-key-events

---
+++ test/validator/samples/a11y-click-events-have-key-events/input.svelte
@@ -0,0 +1,16 @@
+<script>
+    const handleKeyDown = ()=>{}
+    const handleKeyUp = ()=>{}
+    const handleKeyPress = ()=>{}
+</script>
+
+<!-- succeed -->
+<div onClick={() => {}} onKeyDown={handleKeyDown} />
+    <div onClick={() => {}} onKeyUp={handleKeyUp} />
+    <div onClick={() => {}} onKeyPress={handleKeyPress} />
+    <button onClick={() => {}} />
+    <div onClick{() => {}} aria-hidden="true" />
+
+<!-- fail -->
+<div onClick={() => {}} />
+
