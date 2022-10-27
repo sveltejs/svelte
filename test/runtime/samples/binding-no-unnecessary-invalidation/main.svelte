80b09b51231d5b1601f6c691ee6507d4512b1277
Simon Holthausen <simon.holthausen@vercel.com>
2022-10-27 17:00:06 +0200

    (fix) don't run binding init unnecessarily
    Fixes part of #7032
    Fixes #6298

---
+++ test/runtime/samples/binding-no-unnecessary-invalidation/main.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { writable } from "svelte/store";
+	import Tab from "./Tab.svelte";
+
+	let i = 0;
+	const { set, subscribe } = writable({ id: 1, name: "tab1" });
+	const tab = {
+		set(value) {
+			i++;
+			set(value);
+		},
+		subscribe,
+	};
+</script>
+
+<Tab bind:tab={$tab} />
+<p>{i}</p>
