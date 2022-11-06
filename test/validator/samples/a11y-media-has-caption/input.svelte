cb42498c91f75b37a6bfa194d8ba31a9cc733a68
Carlos Ivanchuk <buhodev@proton.me>
2022-11-06 10:43:56 -0400

    fix: move new <video> sample tests to the bottom

--- test/validator/samples/a11y-media-has-caption/input.svelte
+++ test/validator/samples/a11y-media-has-caption/input.svelte
@@ -1,6 +1,6 @@
 <video><track kind="captions"/></video>
-<video aria-hidden></video>
-<video aria-hidden="false"></video>
 <video></video>
 <video><track /></video>
 <audio></audio>
+<video aria-hidden></video>
+<video aria-hidden="false"></video>
