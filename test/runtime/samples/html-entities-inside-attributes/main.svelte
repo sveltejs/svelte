f2b8240739d649cc5a15d508560c36ab41bf3d4f
xxkl1 <lmmonster@aliyun.com>
2022-12-09 23:54:36 +0800

    [fix] html entities decode error

---
+++ test/runtime/samples/html-entities-inside-attributes/main.svelte
@@ -0,0 +1,15 @@
+<span data-xxx="&copy=value" style="&copy=value"></span>
+
+<span data-xxx="&copy=value" style="&copy=value"></span>
+
+<span data-xxx="&copy" style="&copy"></span>
+
+<span data-xxx="&copy;=value" style="&copy;=value"></span>
+
+<span data-xxx="&copyotherstring=value" style="&copyotherstring=value"></span>
+
+<span data-xxx="&copy123=value" style="&copy123=value"></span>
+
+<span data-xxx="&rect=value" style="&rect=value"></span>
+
+<span data-xxx="&rect;=value" style="&rect;=value"></span>
