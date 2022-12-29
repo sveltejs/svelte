948435886228b74975255f7d328dc703a3a90ba3
adiguba <frederic.martini@gmail.com>
2022-12-29 18:24:27 +0100

    unit test

---
+++ test/runtime/samples/component-transition/main.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { slide } from 'svelte/transition';
+	let tag = 'div';
+	function toggle() {
+		tag = (tag) ? null : 'div';
+	}
+</script>
+
+<button id="button" on:click={toggle}>toggle</button> TAG={tag}
+
+<div id="container">
+	<svelte:element this={tag} transition:slide={{duration:500}}>CONTENT</svelte:element>	
+</div>
