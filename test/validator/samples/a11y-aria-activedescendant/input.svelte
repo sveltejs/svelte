41e19db486c9b1f3aafd437c804a7ffcf52d9dad
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-01-06 19:19:52 -0500

    Add tests for active descendant a11y rule

---
+++ test/validator/samples/a11y-aria-activedescendant/input.svelte
@@ -0,0 +1,17 @@
+<!-- VALID -->
+<input />
+<input tabindex="0" />
+<input aria-activedescendant="some-id" />
+<input aria-activedescendant="some-id" tabindex={0} />
+<input aria-activedescendant="some-id" tabindex={1} />
+<input aria-activedescendant="some-id" tabindex="0" />
+<input aria-activedescendant="some-id" tabindex={-1} />
+<input aria-activedescendant="some-id" tabindex="-1" />
+
+<div />
+<div aria-activedescendant="some-id" role="tablist" tabindex={-1} />
+<div aria-activedescendant="some-id" role="tablist" tabindex="-1" />
+
+<!-- INVALID -->
+<div aria-activedescendant="some-id" />
+
