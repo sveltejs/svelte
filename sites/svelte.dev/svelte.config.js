f73f07ac855c493af3a0a3c6c5ab43abd969e142
Puru Vijay <devpuruvj@gmail.com>
2023-02-05 17:47:37 +0530

    Ignore missing ID for now

--- sites/svelte.dev/svelte.config.js
+++ sites/svelte.dev/svelte.config.js
@@ -3,6 +3,10 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		prerender: {
+			// TODO: REMOVE
+			handleMissingId: 'ignore',
+		},
+	},
 };
