eb97b539c9720c34a82870f3a78a775822fdf5c9
Yuichiro Yamashita <xydybaseball@gmail.com>
2023-02-23 17:45:51 +0900

    enhance type

--- src/runtime/internal/lifecycle.ts
+++ src/runtime/internal/lifecycle.ts
@@ -31,7 +31,7 @@ export function beforeUpdate(fn: () => any) {
  * 
  * https://svelte.dev/docs#run-time-svelte-onmount
  */
-export function onMount(fn: (() => () => void) | (() => void | Promise<void>)) {
+export function onMount<T>(fn: (() =>  T extends Promise<void> ? T : T extends Promise<unknown> ? never : T)): void {
 	get_current_component().$$.on_mount.push(fn);
 }
 
