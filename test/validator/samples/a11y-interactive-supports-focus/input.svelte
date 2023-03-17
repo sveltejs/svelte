9ee2272436df1133ab4d2bec08da5a780abea990
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-03-17 13:39:55 -0400

    Add some elementary tests for the rule

---
+++ test/validator/samples/a11y-interactive-supports-focus/input.svelte
@@ -0,0 +1,11 @@
+<!-- VALID -->
+<div aria-hidden role="button" on:keypress={() => {}} />
+<div aria-disabled role="button" on:keypress={() => {}} />
+<div role="presentation" on:keypress={() => {}} />
+<button on:click={() => {}} />
+<div role="menuitem" tabindex="0" on:click={() => {}} on:keypress={() => {}} />
+<div role="button" tabindex="-1" on:click={() => {}} on:keypress={() => {}} />
+
+<!-- INVALID -->
+<div role="button" on:keypress={() => {}} />
+<span role="menuitem" on:keydown={() => {}} />
