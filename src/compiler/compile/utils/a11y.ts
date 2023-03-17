56ceeb3aa9e214f8ad2ca5cf81b5f3cd7072d905
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-03-17 13:30:12 -0400

    Implement interactive-supports-focus

--- src/compiler/compile/utils/a11y.ts
+++ src/compiler/compile/utils/a11y.ts
@@ -68,6 +68,24 @@ export function is_hidden_from_screen_reader(tag_name: string, attribute_map: Ma
 	return aria_hidden_value === true || aria_hidden_value === 'true';
 }
 
+export function has_disabled_attribute(attribute_map: Map<string, Attribute>) {
+	const disabled_attr = attribute_map.get('disabled');
+	const disabled_attr_value = disabled_attr && disabled_attr.get_static_value();
+	if (disabled_attr_value) {
+		return true;
+	}
+
+	const aria_disabled_attr = attribute_map.get('aria-disabled');
+	if (aria_disabled_attr) {
+		const aria_disabled_attr_value = aria_disabled_attr.get_static_value();
+		if (aria_disabled_attr_value === true) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
 const non_interactive_element_role_schemas: ARIARoleRelationConcept[] = [];
 
 elementRoles.entries().forEach(([schema, roles]) => {
