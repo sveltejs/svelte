7e9e78b37c0c57a02c015196984e18cc47827345
Cavit Ertuğrul Sırt <100495175+ertucode@users.noreply.github.com>
2023-03-19 09:20:20 +0300

    docs: Tutorial body had <svelte:body /> even though <svelte:document /> was the subject (#8396)

--- site/content/tutorial/16-special-elements/06-svelte-document/app-a/App.svelte
+++ site/content/tutorial/16-special-elements/06-svelte-document/app-a/App.svelte
@@ -4,7 +4,7 @@
 	const handleSelectionChange = (e) => selection = document.getSelection();
 </script>
 
-<svelte:body />
+<svelte:document />
 
 <p>Select this text to fire events</p>
 <p>Selection: {selection}</p>
