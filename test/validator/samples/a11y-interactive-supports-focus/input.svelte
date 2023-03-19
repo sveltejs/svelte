050b9637f4291bb17e07b049bc13d40f0dc01cbf
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-03-19 00:43:46 -0400

    Add new tests based on the fixes

--- test/validator/samples/a11y-interactive-supports-focus/input.svelte
+++ test/validator/samples/a11y-interactive-supports-focus/input.svelte
@@ -10,3 +10,23 @@
 <!-- INVALID -->
 <div role="button" on:keypress={() => {}} />
 <span role="menuitem" on:keydown={() => {}} />
+<div role="button" on:keyup={() => {}} />
+<span role="menuitem" on:click={() => {}} on:keypress={() => {}} />
+<div role="button" on:contextmenu={() => {}} />
+<span role="menuitem" on:dblclick={() => {}} />
+<div role="button" on:drag={() => {}} />
+<span role="menuitem" on:dragend={() => {}} />
+<div role="button" on:dragenter={() => {}} />
+<span role="menuitem" on:dragexit={() => {}} />
+<div role="button" on:dragleave={() => {}} />
+<span role="menuitem" on:dragover={() => {}} />
+<div role="button" on:dragstart={() => {}} />
+<span role="menuitem" on:drop={() => {}} />
+<div role="button" on:mousedown={() => {}} />
+<span role="menuitem" on:mouseenter={() => {}} />
+<div role="button" on:mouseleave={() => {}} />
+<span role="menuitem" on:mousemove={() => {}} />
+<div role="button" on:mouseout={() => {}} />
+<span role="menuitem" on:mouseover={() => {}} />
+<div role="button" on:mouseup={() => {}} />
+
