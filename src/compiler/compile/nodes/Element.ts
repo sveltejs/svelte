fed93ab9e1da9b54852c17ebe1dc721e52bbb27b
Nguyen Tran <88808276+ngtr6788@users.noreply.github.com>
2023-03-20 08:18:54 -0400

    feat: add a11y `interactive-supports-focus` (#8392)
    #820

--- src/compiler/compile/nodes/Element.ts
+++ src/compiler/compile/nodes/Element.ts
@@ -25,7 +25,7 @@ import { Literal } from 'estree';
 import compiler_warnings from '../compiler_warnings';
 import compiler_errors from '../compiler_errors';
 import { ARIARoleDefinitionKey, roles, aria, ARIAPropertyDefinition, ARIAProperty } from 'aria-query';
-import { is_interactive_element, is_non_interactive_element, is_non_interactive_roles, is_presentation_role, is_interactive_roles, is_hidden_from_screen_reader, is_semantic_role_element, is_abstract_role } from '../utils/a11y';
+import { is_interactive_element, is_non_interactive_element, is_non_interactive_roles, is_presentation_role, is_interactive_roles, is_hidden_from_screen_reader, is_semantic_role_element, is_abstract_role, is_static_element, has_disabled_attribute } from '../utils/a11y';
 
 const aria_attributes = 'activedescendant atomic autocomplete busy checked colcount colindex colspan controls current describedby description details disabled dropeffect errormessage expanded flowto grabbed haspopup hidden invalid keyshortcuts label labelledby level live modal multiline multiselectable orientation owns placeholder posinset pressed readonly relevant required roledescription rowcount rowindex rowspan selected setsize sort valuemax valuemin valuenow valuetext'.split(' ');
 const aria_attribute_set = new Set(aria_attributes);
@@ -73,6 +73,33 @@ const a11y_labelable = new Set([
 	'progress',
 	'select',
 	'textarea'
+]);
+
+const a11y_interactive_handlers = new Set([
+	// Keyboard events
+	'keypress',
+	'keydown',
+	'keyup',
+
+	// Click events
+	'click',
+	'contextmenu',
+	'dblclick',
+	'drag',
+	'dragend',
+	'dragenter',
+	'dragexit',
+	'dragleave',
+	'dragover',
+	'dragstart',
+	'drop',
+	'mousedown',
+	'mouseenter',
+	'mouseleave',
+	'mousemove',
+	'mouseout',
+	'mouseover',
+	'mouseup'
 ]);
 
 const a11y_nested_implicit_semantics = new Map([
@@ -600,6 +627,21 @@ export default class Element extends Node {
 								if (has_missing_props) {
 									component.warn(attribute, compiler_warnings.a11y_role_has_required_aria_props(current_role, required_role_props));
 								}
+							}
+						}
+
+						// interactive-supports-focus
+						if (
+							!has_disabled_attribute(attribute_map) &&
+							!is_hidden_from_screen_reader(this.name, attribute_map) &&
+							!is_presentation_role(current_role) &&
+							is_interactive_roles(current_role) &&
+							is_static_element(this.name, attribute_map) &&
+							!attribute_map.get('tabindex')
+						) {
+							const has_interactive_handlers = handlers.some((handler) => a11y_interactive_handlers.has(handler.name));
+							if (has_interactive_handlers) {
+								component.warn(this, compiler_warnings.a11y_interactive_supports_focus(current_role));
 							}
 						}
 
---
+++ test/validator/samples/a11y-interactive-supports-focus/input.svelte
@@ -0,0 +1,32 @@
+<!-- VALID -->
+<div aria-hidden role="button" on:keypress={() => {}} />
+<div aria-disabled role="button" on:keypress={() => {}} />
+<div disabled role="button" on:keypress={() => {}} />
+<div role="presentation" on:keypress={() => {}} />
+<button on:click={() => {}} />
+<div role="menuitem" tabindex="0" on:click={() => {}} on:keypress={() => {}} />
+<div role="button" tabindex="-1" on:click={() => {}} on:keypress={() => {}} />
+
+<!-- INVALID -->
+<div role="button" on:keypress={() => {}} />
+<span role="menuitem" on:keydown={() => {}} />
+<div role="button" on:keyup={() => {}} />
+<span role="menuitem" on:click={() => {}} on:keypress={() => {}} />
+<div role="button" on:contextmenu={() => {}} />
+<span role="menuitem" on:dblclick={() => {}} />
+<div role="button" on:drag={() => {}} />
+<span role="menuitem" on:dragend={() => {}} />
+<div role="button" on:dragenter={() => {}} />
+<span role="menuitem" on:dragexit={() => {}} />
+<div role="button" on:dragleave={() => {}} />
+<span role="menuitem" on:dragover={() => {}} />
+<div role="button" on:dragstart={() => {}} />
+<span role="menuitem" on:drop={() => {}} />
+<div role="button" on:mousedown={() => {}} />
+<span role="menuitem" on:mouseenter={() => {}} />
+<div role="button" on:mouseleave={() => {}} />
+<span role="menuitem" on:mousemove={() => {}} />
+<div role="button" on:mouseout={() => {}} />
+<span role="menuitem" on:mouseover={() => {}} />
+<div role="button" on:mouseup={() => {}} />
+
