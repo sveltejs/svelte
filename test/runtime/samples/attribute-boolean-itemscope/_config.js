ee92c9aac6e34cd4539de7855a4394061f3a70b1
aMediocreDad <amediocredad@outlook.com>
2023-03-24 20:05:57 +0100

    test: Convert itemscope test to microdata test

--- test/runtime/samples/attribute-boolean-itemscope/_config.js
+++
@@ -1,4 +0,0 @@
-// There is no relationship between the attribute name and the document node https://developer.mozilla.org/en-US/docs/Web/HTML/Microdata
-export default {
-	html: '<div itemscope />'
-};
