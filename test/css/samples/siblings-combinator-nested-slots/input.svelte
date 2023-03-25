c4200dceb31dd8671de4568646e540565527bd5e
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-03-25 17:19:13 -0400

    Add tests for sibling selectors ignoring slots

---
+++ test/css/samples/siblings-combinator-nested-slots/input.svelte
@@ -0,0 +1,18 @@
+<slot>
+  <slot>
+    <slot>
+      <h1>test</h1>
+    </slot>
+  </slot>
+</slot>
+<slot>
+  <slot>
+    <span>Hello</span>
+  </slot>
+</slot>
+
+<style>
+  h1 + span {
+    color: red;
+  }
+</style>
