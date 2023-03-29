9793b418177488807f5fa0480448117388e78856
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2023-03-29 17:40:31 +0530

    feat(site-2): Local examples (#8431)
    * feat(docs): Local tutorial
    
    * Refactor some stuff
    
    * Better error handling
    
    * Fix search imports
    
    * Prerender tutorial
    
    * try prerendering hack
    
    * fix super stupid display hidden bug
    
    * Shorten the rendered URL
    
    * Shorten URL code even more
    
    * Prerender in svelte.config.js
    
    * Refactor
    
    * Fix ScreenToggle dark mode styles
    
    * Initial POC
    
    * Rvert to old hack
    
    * Fix svelte-document.jpg

--- sites/svelte.dev/src/routes/examples/+layout.js
+++
@@ -1,6 +0,0 @@
-import { PUBLIC_API_BASE } from '$env/static/public';
-
-export async function load({ fetch }) {
-	const examples = await fetch(`${PUBLIC_API_BASE}/docs/svelte/examples`).then((r) => r.json());
-	return { examples };
-}
