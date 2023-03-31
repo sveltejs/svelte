2ad602975050e9df6b401d3c0a89354616f05fb2
Puru Vijay <devpuruvj@gmail.com>
2023-03-31 19:04:14 +0530

    Don't render anything on docs if /docs

--- sites/svelte.dev/src/routes/+layout.svelte
+++ sites/svelte.dev/src/routes/+layout.svelte
@@ -14,40 +14,42 @@
 	{/if}
 </svelte:head>
 
-<Shell nav_visible={$page.url.pathname !== '/repl/embed'}>
-	<Nav logo="/svelte-logo.svg">
-		<svelte:fragment slot="nav-center">
-			{#if $page.url.pathname !== '/search'}
-				<li><Search /></li>
-			{/if}
-		</svelte:fragment>
+<div style:display={$page.url.pathname !== '/docs' ? 'contents' : 'none'}>
+	<Shell nav_visible={$page.url.pathname !== '/repl/embed'}>
+		<Nav logo="/svelte-logo.svg">
+			<svelte:fragment slot="nav-center">
+				{#if $page.url.pathname !== '/search'}
+					<li><Search /></li>
+				{/if}
+			</svelte:fragment>
 
-		<svelte:fragment slot="nav-right">
-			<NavItem href="/tutorial">Tutorial</NavItem>
-			<NavItem href="/docs/introduction">Docs</NavItem>
-			<NavItem href="/examples">Examples</NavItem>
-			<NavItem href="/repl">REPL</NavItem>
-			<NavItem href="/blog">Blog</NavItem>
-			<NavItem href="/faq">FAQ</NavItem>
+			<svelte:fragment slot="nav-right">
+				<NavItem href="/tutorial">Tutorial</NavItem>
+				<NavItem href="/docs/introduction">Docs</NavItem>
+				<NavItem href="/examples">Examples</NavItem>
+				<NavItem href="/repl">REPL</NavItem>
+				<NavItem href="/blog">Blog</NavItem>
+				<NavItem href="/faq">FAQ</NavItem>
 
-			<Separator />
+				<Separator />
 
-			<NavItem external="https://kit.svelte.dev">SvelteKit</NavItem>
+				<NavItem external="https://kit.svelte.dev">SvelteKit</NavItem>
 
-			<NavItem external="/chat" title="Discord Chat">
-				<span slot="small">Discord</span>
-				<Icon name="message-square" />
-			</NavItem>
+				<NavItem external="/chat" title="Discord Chat">
+					<span slot="small">Discord</span>
+					<Icon name="message-square" />
+				</NavItem>
 
-			<NavItem external="https://github.com/sveltejs/svelte" title="GitHub Repo">
-				<span slot="small">GitHub</span>
-				<Icon name="github" />
-			</NavItem>
-		</svelte:fragment>
-	</Nav>
+				<NavItem external="https://github.com/sveltejs/svelte" title="GitHub Repo">
+					<span slot="small">GitHub</span>
+					<Icon name="github" />
+				</NavItem>
+			</svelte:fragment>
+		</Nav>
 
-	<slot />
-</Shell>
+		<slot />
+	</Shell>
+</div>
 
 {#if browser}
 	<SearchBox />
