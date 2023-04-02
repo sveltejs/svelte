90988b663f64c885e187f94a0fdaf58f22807547
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-04-01 20:26:35 -0700

    fix a number of lint errors

--- src/compiler/compile/utils/get_object.ts
+++ src/compiler/compile/utils/get_object.ts
@@ -1,12 +1,12 @@
 import {
 	Node,
-	Identifier,
-} from "estree";
+	Identifier
+} from 'estree';
 
 export default function get_object(node: Node): Identifier {
 	while (true) {
-		if (node.type === "MemberExpression") node = node.object;
-		else if (node.type === "CallExpression") node = node.callee;
+		if (node.type === 'MemberExpression') node = node.object;
+		else if (node.type === 'CallExpression') node = node.callee;
 		else break;
 	}
 	return node as Identifier;
--- test/runtime/samples/binding-input-text-map/_config.js
+++ test/runtime/samples/binding-input-text-map/_config.js
@@ -12,14 +12,13 @@ export default {
 		assert.equal(input.value, '50');
 
 		component.map.get(component.id).x = 30;
-		component.map = component.map;
 		assert.equal(input.value, '30');
 
 		input.value = '42';
 		await input.dispatchEvent(new window.Event('input'));
 
 		assert.equal(input.value, '42');
-		assert.equal(component.map.get(component.id).x, '42')
+		assert.equal(component.map.get(component.id).x, '42');
 
 		component.map.set(3, { x: 7, y: 8 });
 		component.id = 3;
