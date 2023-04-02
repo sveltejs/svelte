6a1795906ea466f14cf40877257fbe3503828bcb
tanhauhau <lhtan93@gmail.com>
2023-04-02 15:39:38 +0800

    temp disable eslint rule

--- src/compiler/compile/utils/get_object.ts
+++ src/compiler/compile/utils/get_object.ts
@@ -4,6 +4,7 @@ import {
 } from 'estree';
 
 export default function get_object(node: Node): Identifier {
+	/* eslint-disable-next-line no-constant-condition */
 	while (true) {
 		if (node.type === 'MemberExpression') node = node.object;
 		else if (node.type === 'CallExpression') node = node.callee;
