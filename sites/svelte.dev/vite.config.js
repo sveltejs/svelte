1f21c5fd64ebff7a724463b2e5400642b59385c4
Puru Vijay <devpuruvj@gmail.com>
2023-04-20 23:17:32 +0530

    chore: Upgrade to Vite 4.3

--- sites/svelte.dev/vite.config.js
+++ sites/svelte.dev/vite.config.js
@@ -50,22 +50,6 @@ const config = {
 		fs: {
 			strict: false
 		}
-	},
-	// !HACK: Remove once vite 4.3 is out
-	worker: {
-		plugins: [
-			{
-				name: 'remove-manifest',
-				configResolved(c) {
-					const manifestPlugin = c.worker.plugins.findIndex((p) => p.name === 'vite:manifest');
-					c.worker.plugins.splice(manifestPlugin, 1);
-					const ssrManifestPlugin = c.worker.plugins.findIndex(
-						(p) => p.name === 'vite:ssr-manifest'
-					);
-					c.plugins.splice(ssrManifestPlugin, 1);
-				}
-			}
-		]
 	}
 };
 
