b56050b60a3d2c6766b602933d8c61248d061bf6
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-04-25 08:47:46 -0400

    Add more destructured var store tests

---
+++ test/runtime/samples/var-store-destructured-in-do-while/_config.js
@@ -0,0 +1,3 @@
+export default {
+  html: '<p>3</p><p>5</p><p>4</p>'
+};
---
+++ test/runtime/samples/var-store-destructured-in-if/main.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { writable } from "svelte/store";
+
+	var thing = [writable(3), writable(4), writable(5)];
+
+	if (true) {
+		var [a] = thing;
+	}
+
+	if (true) 
+		var [,c,b] = thing;
+
+	var x = $a;
+	var y = $b;
+	var z = $c;
+</script>
+
+<p>{x}</p>
+<p>{y}</p>
+<p>{z}</p>
