64cd799dac71d4116ff8027315ae5ede3f731008
Puru Vijay <devpuruvj@gmail.com>
2023-05-01 02:40:52 +0530

    Few fixes

--- sites/svelte.dev/src/routes/_components/Try.svelte
+++ sites/svelte.dev/src/routes/_components/Try.svelte
@@ -55,27 +55,21 @@
 </div>
 
 <style>
-	@media (prefers-color-scheme: light) {
-		.try-container {
-			--background-1: radial-gradient(circle at top right, rgb(230, 233, 236), rgb(244, 245, 247));
-			--background-2: var(--sk-back-4);
-		}
+	.try-container {
+		--background-2: var(--sk-back-4);
+	}
 
-		:global(body.dark .try-container) {
-			--background-1: #222;
-			--background-2: #444;
-		}
+	:global(body.dark .try-container) {
+		--background-2: #444;
 	}
 
 	@media (prefers-color-scheme: dark) {
 		.try-container {
-			--background-1: #222;
 			--background-2: #444;
 		}
 
 		:global(body.light .try-container) {
-			--background-1: radial-gradient(circle at top right, rgb(230, 233, 236), rgb(244, 245, 247));
-			--background-2: var(--sk-back-4);
+			--background-2: var(--sk-back-4) !important;
 		}
 	}
 
