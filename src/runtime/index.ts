0dd0c789768e89dedab42c597f3b6bf8d36b0099
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2023-05-08 22:25:29 -0600

    Convert src/runtime/index.ts to JavaScript

--- src/runtime/index.ts
+++ src/runtime/index.ts
@@ -1,22 +1,6 @@
 import './ambient';
+export { onMount, onDestroy, beforeUpdate, afterUpdate, setContext, getContext, getAllContexts, hasContext, tick, createEventDispatcher, SvelteComponentDev as SvelteComponent, SvelteComponentTyped } from './internal';
 
-export {
-	onMount,
-	onDestroy,
-	beforeUpdate,
-	afterUpdate,
-	setContext,
-	getContext,
-	getAllContexts,
-	hasContext,
-	tick,
-	createEventDispatcher,
-	SvelteComponentDev as SvelteComponent,
-	SvelteComponentTyped
-} from './internal';
-export type {
-	ComponentType,
-	ComponentConstructorOptions,
-	ComponentProps,
-	ComponentEvents
-} from './internal';
+
+
+
