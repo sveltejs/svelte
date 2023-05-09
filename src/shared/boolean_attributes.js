9fe8b9b677aa1886e223da85adb8a4a88dc52c05
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2023-05-08 22:42:00 -0600

    Lint and format src/shared/boolean_attributes.js

--- src/shared/boolean_attributes.js
+++ src/shared/boolean_attributes.js
@@ -1,29 +1,29 @@
 const _boolean_attributes = [
-    'allowfullscreen',
-    'allowpaymentrequest',
-    'async',
-    'autofocus',
-    'autoplay',
-    'checked',
-    'controls',
-    'default',
-    'defer',
-    'disabled',
-    'formnovalidate',
-    'hidden',
-    'inert',
-    'ismap',
-    'loop',
-    'multiple',
-    'muted',
-    'nomodule',
-    'novalidate',
-    'open',
-    'playsinline',
-    'readonly',
-    'required',
-    'reversed',
-    'selected'
+	'allowfullscreen',
+	'allowpaymentrequest',
+	'async',
+	'autofocus',
+	'autoplay',
+	'checked',
+	'controls',
+	'default',
+	'defer',
+	'disabled',
+	'formnovalidate',
+	'hidden',
+	'inert',
+	'ismap',
+	'loop',
+	'multiple',
+	'muted',
+	'nomodule',
+	'novalidate',
+	'open',
+	'playsinline',
+	'readonly',
+	'required',
+	'reversed',
+	'selected'
 ];
 /**
  * List of HTML boolean attributes (e.g. `<input disabled>`).
@@ -32,6 +32,4 @@ const _boolean_attributes = [
  */
 export const boolean_attributes = new Set([..._boolean_attributes]);
 
-
 /** @typedef {Class<_boolean_attributes>} BooleanAttributes */
-
