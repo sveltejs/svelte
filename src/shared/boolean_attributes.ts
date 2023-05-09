760d336a32d1c38e46f3055c6edcf581ee7f9b2a
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2023-05-08 22:41:57 -0600

    Convert src/shared/boolean_attributes.ts to JavaScript

--- src/shared/boolean_attributes.ts
+++ src/shared/boolean_attributes.ts
@@ -1,35 +1,37 @@
 const _boolean_attributes = [
-	'allowfullscreen',
-	'allowpaymentrequest',
-	'async',
-	'autofocus',
-	'autoplay',
-	'checked',
-	'controls',
-	'default',
-	'defer',
-	'disabled',
-	'formnovalidate',
-	'hidden',
-	'inert',
-	'ismap',
-	'loop',
-	'multiple',
-	'muted',
-	'nomodule',
-	'novalidate',
-	'open',
-	'playsinline',
-	'readonly',
-	'required',
-	'reversed',
-	'selected'
-] as const;
-
-export type BooleanAttributes = (typeof _boolean_attributes)[number];
-
+    'allowfullscreen',
+    'allowpaymentrequest',
+    'async',
+    'autofocus',
+    'autoplay',
+    'checked',
+    'controls',
+    'default',
+    'defer',
+    'disabled',
+    'formnovalidate',
+    'hidden',
+    'inert',
+    'ismap',
+    'loop',
+    'multiple',
+    'muted',
+    'nomodule',
+    'novalidate',
+    'open',
+    'playsinline',
+    'readonly',
+    'required',
+    'reversed',
+    'selected'
+];
 /**
  * List of HTML boolean attributes (e.g. `<input disabled>`).
  * Source: https://html.spec.whatwg.org/multipage/indices.html
+ * @type {Set<string>}
  */
-export const boolean_attributes: Set<string> = new Set([..._boolean_attributes]);
+export const boolean_attributes = new Set([..._boolean_attributes]);
+
+
+/** @typedef {Class<_boolean_attributes>} BooleanAttributes */
+
