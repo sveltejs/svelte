c4a9f74bdaabe4bf77374f1b1a83c276ef469e9d
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-09 10:02:59 +0200

    Lint and format ./src/compiler/parse/acorn.js

--- src/compiler/parse/acorn.js
+++ src/compiler/parse/acorn.js
@@ -4,23 +4,21 @@ import * as code_red from 'code-red';
  * @param {string} source
  * @returns {any}
  */
-export const parse = (source) => code_red.parse(source, {
-    sourceType: 'module',
-    ecmaVersion: 13,
-    locations: true
-});
+export const parse = (source) =>
+	code_red.parse(source, {
+		sourceType: 'module',
+		ecmaVersion: 13,
+		locations: true
+	});
 
 /**
  * @param {string} source
  * @param {number} index
  * @returns {any}
  */
-export const parse_expression_at = (source, index) => code_red.parseExpressionAt(source, index, {
-    sourceType: 'module',
-    ecmaVersion: 13,
-    locations: true
-});
-
-
-
-
+export const parse_expression_at = (source, index) =>
+	code_red.parseExpressionAt(source, index, {
+		sourceType: 'module',
+		ecmaVersion: 13,
+		locations: true
+	});
