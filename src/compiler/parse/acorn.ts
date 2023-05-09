f6fd5adc230b4d58eb65133a82bcb6141121adcd
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-09 10:02:52 +0200

    Convert ./src/compiler/parse/acorn.ts to JavaScript

--- src/compiler/parse/acorn.ts
+++ src/compiler/parse/acorn.ts
@@ -1,16 +1,26 @@
-import { Node } from 'acorn';
 import * as code_red from 'code-red';
 
-export const parse = (source: string): Node =>
-	code_red.parse(source, {
-		sourceType: 'module',
-		ecmaVersion: 13,
-		locations: true
-	});
+/**
+ * @param {string} source
+ * @returns {any}
+ */
+export const parse = (source) => code_red.parse(source, {
+    sourceType: 'module',
+    ecmaVersion: 13,
+    locations: true
+});
 
-export const parse_expression_at = (source: string, index: number): Node =>
-	code_red.parseExpressionAt(source, index, {
-		sourceType: 'module',
-		ecmaVersion: 13,
-		locations: true
-	});
+/**
+ * @param {string} source
+ * @param {number} index
+ * @returns {any}
+ */
+export const parse_expression_at = (source, index) => code_red.parseExpressionAt(source, index, {
+    sourceType: 'module',
+    ecmaVersion: 13,
+    locations: true
+});
+
+
+
+
