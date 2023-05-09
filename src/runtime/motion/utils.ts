a54c482adbec8cb10f8be2498890c9f7b47c8807
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2023-05-08 22:16:39 -0600

    Convert src/runtime/motion/utils.ts to JavaScript

--- src/runtime/motion/utils.ts
+++ src/runtime/motion/utils.ts
@@ -1,3 +1,10 @@
-export function is_date(obj: any): obj is Date {
-	return Object.prototype.toString.call(obj) === '[object Date]';
+/** @param {any} obj
+ * @returns {boolean}
+ */
+export function is_date(obj) {
+    return Object.prototype.toString.call(obj) === '[object Date]';
 }
+
+
+
+
