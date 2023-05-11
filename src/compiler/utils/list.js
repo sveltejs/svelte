e86fb16402da2519861d3fced483e60e2630453e
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-11 17:38:20 +0200

    bunch of easy ones

--- src/compiler/utils/list.js
+++ src/compiler/utils/list.js
@@ -1,4 +1,8 @@
-export default function list(items: string[], conjunction = 'or') {
+/**
+ * @param {string[]} items
+ * @param {string} [conjunction]
+ */
+export default function list(items, conjunction = 'or') {
 	if (items.length === 1) return items[0];
 	return `${items.slice(0, -1).join(', ')} ${conjunction} ${items[items.length - 1]}`;
 }
