837a8b7f602df3c4a1af24ad67e0d5af71e583d6
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-11 18:37:28 +0200

    Lint and format src/compiler/compile/render_ssr/handlers/AwaitBlock.js

--- src/compiler/compile/render_ssr/handlers/AwaitBlock.js
+++ src/compiler/compile/render_ssr/handlers/AwaitBlock.js
@@ -7,31 +7,29 @@ import { get_const_tags } from './shared/get_const_tags.js';
  * @param {import('../Renderer').RenderOptions} options
  */
 export default function (node, renderer, options) {
-    renderer.push();
-    renderer.render(node.pending.children, options);
+	renderer.push();
+	renderer.render(node.pending.children, options);
 
- /**
- * @type {undefined}
- */
-    const pending = renderer.pop();
-    renderer.push();
-    renderer.render(node.then.children, options);
+	/**
+	 * @type {undefined}
+	 */
+	const pending = renderer.pop();
+	renderer.push();
+	renderer.render(node.then.children, options);
 
- /**
- * @type {undefined}
- */
-    const then = renderer.pop();
-    renderer.add_expression(x `
+	/**
+	 * @type {undefined}
+	 */
+	const then = renderer.pop();
+	renderer.add_expression(x`
 		function(__value) {
 			if (@is_promise(__value)) {
 				__value.then(null, @noop);
 				return ${pending};
 			}
-			return (function(${node.then_node ? node.then_node : ''}) { ${get_const_tags(node.then.const_tags)}; return ${then}; }(__value));
+			return (function(${node.then_node ? node.then_node : ''}) { ${get_const_tags(
+		node.then.const_tags
+	)}; return ${then}; }(__value));
 		}(${node.expression.node})
 	`);
 }
-
-
-
-
