b67fda1be1cd5ecd232e4a15d6f9e645705a72f8
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-11 21:13:01 +0200

    Lint and format src/compiler/compile/render_ssr/handlers/DebugTag.js

--- src/compiler/compile/render_ssr/handlers/DebugTag.js
+++ src/compiler/compile/render_ssr/handlers/DebugTag.js
@@ -6,16 +6,13 @@ import { x, p } from 'code-red';
  * @param {import('../Renderer.js').RenderOptions} options
  */
 export default function (node, renderer, options) {
-    if (!options.dev)
-        return;
-    const filename = options.filename || null;
-    const { line, column } = options.locate(node.start + 1);
-    const obj = x `{
-		${node.expressions.map((e) => p `${e.node.name}`)}
+	if (!options.dev) return;
+	const filename = options.filename || null;
+	const { line, column } = options.locate(node.start + 1);
+	const obj = x`{
+		${node.expressions.map((e) => p`${e.node.name}`)}
 	}`;
-    renderer.add_expression(x `@debug(${filename ? x `"${filename}"` : x `null`}, ${line - 1}, ${column}, ${obj})`);
+	renderer.add_expression(
+		x`@debug(${filename ? x`"${filename}"` : x`null`}, ${line - 1}, ${column}, ${obj})`
+	);
 }
-
-
-
-
