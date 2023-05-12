acc0b54624d661cc3e6056f79d71257e815bfd8f
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:47:28 +0200

    Lint and format src/compiler/compile/nodes/Head.js

--- src/compiler/compile/nodes/Head.js
+++ src/compiler/compile/nodes/Head.js
@@ -6,38 +6,40 @@ import { regex_non_whitespace_character } from '../../utils/patterns.js';
 
 /** @extends Node */
 export default class Head extends Node {
+	/** @type {'Head'} */
+	type;
 
-    /** @type {'Head'} */
-    type;
+	/** @type {any[]} */
+	children; // TODO
 
-    /** @type {any[]} */
-    children; // TODO
+	/** @type {string} */
+	id;
 
-    /** @type {string} */
-    id;
-
- /**
-  * @param {import('../Component.js').default} component  *
-     * @param {import('./shared/Node.js').default} parent  *
-     * @param {import('./shared/TemplateScope.js').default} scope  *
-     * @param {import('../../interfaces.js').TemplateNode} info  undefined
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        this.cannot_use_innerhtml();
-        if (info.attributes.length) {
-            component.error(info.attributes[0], compiler_errors.invalid_attribute_head);
-            return;
-        }
-        this.children = map_children(component, parent, scope, info.children.filter(/** @param {any} child */ (child) => {
-            return child.type !== 'Text' || regex_non_whitespace_character.test(child.data);
-        }));
-        if (this.children.length > 0) {
-            this.id = `svelte-${hash(this.component.source.slice(this.start, this.end))}`;
-        }
-    }
+	/**
+	 * @param {import('../Component.js').default} component  *
+	 * @param {import('./shared/Node.js').default} parent  *
+	 * @param {import('./shared/TemplateScope.js').default} scope  *
+	 * @param {import('../../interfaces.js').TemplateNode} info  undefined
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		this.cannot_use_innerhtml();
+		if (info.attributes.length) {
+			component.error(info.attributes[0], compiler_errors.invalid_attribute_head);
+			return;
+		}
+		this.children = map_children(
+			component,
+			parent,
+			scope,
+			info.children.filter(
+				/** @param {any} child */ (child) => {
+					return child.type !== 'Text' || regex_non_whitespace_character.test(child.data);
+				}
+			)
+		);
+		if (this.children.length > 0) {
+			this.id = `svelte-${hash(this.component.source.slice(this.start, this.end))}`;
+		}
+	}
 }
-
-
-
-
