89d9ab942eeb94f3b7b5cb815b0cbb6c1e377936
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:44:11 +0200

    Lint and format src/compiler/compile/nodes/DebugTag.js

--- src/compiler/compile/nodes/DebugTag.js
+++ src/compiler/compile/nodes/DebugTag.js
@@ -3,27 +3,24 @@ import Expression from './shared/Expression.js';
 
 /** @extends Node */
 export default class DebugTag extends Node {
+	/** @type {'DebugTag'} */
+	type;
 
-    /** @type {'DebugTag'} */
-    type;
+	/** @type {import('./shared/Expression.js').default[]} */
+	expressions;
 
-    /** @type {import('./shared/Expression.js').default[]} */
-    expressions;
-
- /**
-  * @param {import('../Component.js').default} component  *
-     * @param {import('./interfaces.js').INode} parent  *
-     * @param {import('./shared/TemplateScope.js').default} scope  *
-     * @param {import('../../interfaces.js').TemplateNode} info  undefined
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        this.expressions = info.identifiers.map(/** @param {EsTreeNode} node */ (node) => {
-            return new Expression(component, parent, scope, node);
-        });
-    }
+	/**
+	 * @param {import('../Component.js').default} component  *
+	 * @param {import('./interfaces.js').INode} parent  *
+	 * @param {import('./shared/TemplateScope.js').default} scope  *
+	 * @param {import('../../interfaces.js').TemplateNode} info  undefined
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		this.expressions = info.identifiers.map(
+			/** @param {EsTreeNode} node */ (node) => {
+				return new Expression(component, parent, scope, node);
+			}
+		);
+	}
 }
-
-
-
-
