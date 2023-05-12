9c98b7ca1f10de5b996cdd6c9a1314324f020ca8
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 14:58:58 +0200

    Convert src/compiler/compile/render_dom/wrappers/shared/add_event_handlers.ts to JavaScript

--- src/compiler/compile/render_dom/wrappers/shared/add_event_handlers.ts
+++ src/compiler/compile/render_dom/wrappers/shared/add_event_handlers.ts
@@ -1,19 +1,21 @@
-import Block from '../../Block';
-import EventHandler from '../Element/EventHandler';
-import { Expression } from 'estree';
-
-export default function add_event_handlers(
-	block: Block,
-	target: string | Expression,
-	handlers: EventHandler[]
-) {
-	handlers.forEach((handler) => add_event_handler(block, target, handler));
+/**
+ * @param {import('../../Block.js').default} block
+ * @param {string | import('estree').Expression} target
+ * @param {import('../Element/EventHandler.js').default[]} handlers
+ */
+export default function add_event_handlers(block, target, handlers) {
+    handlers.forEach((handler) => add_event_handler(block, target, handler));
 }
 
-export function add_event_handler(
-	block: Block,
-	target: string | Expression,
-	handler: EventHandler
-) {
-	handler.render(block, target);
+/**
+ * @param {import('../../Block.js').default} block
+ * @param {string | import('estree').Expression} target
+ * @param {import('../Element/EventHandler.js').default} handler
+ */
+export function add_event_handler(block, target, handler) {
+    handler.render(block, target);
 }
+
+
+
+
