9c864b9e39facb9dc754a51640dff7be38260f95
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:50:19 +0200

    Lint and format src/compiler/compile/nodes/RawMustacheTag.js

--- src/compiler/compile/nodes/RawMustacheTag.js
+++ src/compiler/compile/nodes/RawMustacheTag.js
@@ -2,23 +2,18 @@ import Tag from './shared/Tag.js';
 
 /** @extends Tag */
 export default class RawMustacheTag extends Tag {
+	/** @type {'RawMustacheTag'} */
+	type;
 
-    /** @type {'RawMustacheTag'} */
-    type;
-
- /**
-  * @param {any} component
-     * @param {any} parent
-     * @param {any} scope
-     * @param {any} info
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        this.cannot_use_innerhtml();
-        this.not_static_content();
-    }
+	/**
+	 * @param {any} component
+	 * @param {any} parent
+	 * @param {any} scope
+	 * @param {any} info
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		this.cannot_use_innerhtml();
+		this.not_static_content();
+	}
 }
-
-
-
-
