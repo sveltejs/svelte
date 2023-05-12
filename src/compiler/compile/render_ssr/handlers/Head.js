45c9b21b243b1ad7a9c7b344a9e4f672ea3071b9
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 12:03:44 +0200

    Lint and format src/compiler/compile/render_ssr/handlers/Head.js

--- src/compiler/compile/render_ssr/handlers/Head.js
+++ src/compiler/compile/render_ssr/handlers/Head.js
@@ -6,24 +6,20 @@ import { x } from 'code-red';
  * @param {import('../Renderer.js').RenderOptions} options
  */
 export default function (node, renderer, options) {
-    const head_options = {
-        ...options,
-        head_id: node.id
-    };
-    renderer.push();
-    renderer.render(node.children, head_options);
-    const result = renderer.pop();
+	const head_options = {
+		...options,
+		head_id: node.id
+	};
+	renderer.push();
+	renderer.render(node.children, head_options);
+	const result = renderer.pop();
 
-    /** @type {import('estree').Node} */
-    let expression = result;
-    if (options.hydratable) {
-        const start_comment = `HEAD_${node.id}_START`;
-        const end_comment = `HEAD_${node.id}_END`;
-        expression = x `'<!-- ${start_comment} -->' + ${expression} + '<!-- ${end_comment} -->'`;
-    }
-    renderer.add_expression(x `$$result.head += ${expression}, ""`);
+	/** @type {import('estree').Node} */
+	let expression = result;
+	if (options.hydratable) {
+		const start_comment = `HEAD_${node.id}_START`;
+		const end_comment = `HEAD_${node.id}_END`;
+		expression = x`'<!-- ${start_comment} -->' + ${expression} + '<!-- ${end_comment} -->'`;
+	}
+	renderer.add_expression(x`$$result.head += ${expression}, ""`);
 }
-
-
-
-
