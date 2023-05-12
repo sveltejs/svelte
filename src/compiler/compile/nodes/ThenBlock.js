49d85957091a675f4a870813630f262aadcdea81
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:52:16 +0200

    Lint and format src/compiler/compile/nodes/ThenBlock.js

--- src/compiler/compile/nodes/ThenBlock.js
+++ src/compiler/compile/nodes/ThenBlock.js
@@ -3,39 +3,35 @@ import get_const_tags from './shared/get_const_tags.js';
 
 /** @extends AbstractBlock */
 export default class ThenBlock extends AbstractBlock {
+	/** @type {'ThenBlock'} */
+	type;
 
-    /** @type {'ThenBlock'} */
-    type;
+	/** @type {import('./shared/TemplateScope.js').default} */
+	scope;
 
-    /** @type {import('./shared/TemplateScope.js').default} */
-    scope;
+	/** @type {import('./ConstTag.js').default[]} */
+	const_tags;
 
-    /** @type {import('./ConstTag.js').default[]} */
-    const_tags;
-
- /**
-  * @param {import('../Component.js').default} component
-     * @param {import('./AwaitBlock.js').default} parent
-     * @param {import('./shared/TemplateScope.js').default} scope
-     * @param {import('../../interfaces.js').TemplateNode} info
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        this.scope = scope.child();
-        if (parent.then_node) {
-            parent.then_contexts.forEach(/** @param {any} context */ (context) => {
-                if (context.type !== 'DestructuredVariable')
-                    return;
-                this.scope.add(context.key.name, parent.expression.dependencies, this);
-            });
-        }
-        [this.const_tags, this.children] = get_const_tags(info.children, component, this, parent);
-        if (!info.skip) {
-            this.warn_if_empty_block();
-        }
-    }
+	/**
+	 * @param {import('../Component.js').default} component
+	 * @param {import('./AwaitBlock.js').default} parent
+	 * @param {import('./shared/TemplateScope.js').default} scope
+	 * @param {import('../../interfaces.js').TemplateNode} info
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		this.scope = scope.child();
+		if (parent.then_node) {
+			parent.then_contexts.forEach(
+				/** @param {any} context */ (context) => {
+					if (context.type !== 'DestructuredVariable') return;
+					this.scope.add(context.key.name, parent.expression.dependencies, this);
+				}
+			);
+		}
+		[this.const_tags, this.children] = get_const_tags(info.children, component, this, parent);
+		if (!info.skip) {
+			this.warn_if_empty_block();
+		}
+	}
 }
-
-
-
-
