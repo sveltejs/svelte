8216140e0f6aae2a4f8dadd267ea672ec3f537b7
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2023-05-12 12:59:42 +0530

    Update sites/svelte.dev/src/routes/_components/Demo.svelte
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- sites/svelte.dev/src/routes/_components/Demo.svelte
+++ sites/svelte.dev/src/routes/_components/Demo.svelte
@@ -105,7 +105,7 @@
 		height: 100%;
 		align-items: center;
 		justify-content: center;
-		border-right: 0.5px solid var(--sk-text-4);
+		border-right: 0.5px solid var(--sk-back-4);
 		background-color: var(--sk-back-4);
 		transition: 0.15s ease;
 		transition-property: transform, background-color, color;
