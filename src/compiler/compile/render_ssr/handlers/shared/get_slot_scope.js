6f7d19c880b7582c574829d3abca90e045c03f89
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 12:13:25 +0200

    Lint and format src/compiler/compile/render_ssr/handlers/shared/get_slot_scope.js

--- src/compiler/compile/render_ssr/handlers/shared/get_slot_scope.js
+++ src/compiler/compile/render_ssr/handlers/shared/get_slot_scope.js
@@ -3,24 +3,19 @@
  * @returns {import("C:/repos/svelte/svelte/node_modules/.pnpm/@types+estree@1.0.0/node_modules/@types/estree/index").ObjectPattern}
  */
 export function get_slot_scope(lets) {
-    if (lets.length === 0)
-        return null;
-    return {
-        type: 'ObjectPattern',
-        properties: lets.map((l) => {
-            return {
-                type: 'Property',
-                kind: 'init',
-                method: false,
-                shorthand: false,
-                computed: false,
-                key: l.name,
-                value: l.value || l.name
-            };
-        })
-    };
+	if (lets.length === 0) return null;
+	return {
+		type: 'ObjectPattern',
+		properties: lets.map((l) => {
+			return {
+				type: 'Property',
+				kind: 'init',
+				method: false,
+				shorthand: false,
+				computed: false,
+				key: l.name,
+				value: l.value || l.name
+			};
+		})
+	};
 }
-
-
-
-
