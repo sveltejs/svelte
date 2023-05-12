f176fd1a223bdf23c0187b84e3e7d162e6a4ec3c
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:39:03 +0200

    Lint and format src/compiler/compile/nodes/Body.js

--- src/compiler/compile/nodes/Body.js
+++ src/compiler/compile/nodes/Body.js
@@ -4,38 +4,33 @@ import Action from './Action.js';
 
 /** @extends Node */
 export default class Body extends Node {
+	/** @type {'Body'} */
+	type;
 
-    /** @type {'Body'} */
-    type;
+	/** @type {import('./EventHandler.js').default[]} */
+	handlers = [];
 
-    /** @type {import('./EventHandler.js').default[]} */
-    handlers = [];
+	/** @type {import('./Action.js').default[]} */
+	actions = [];
 
-    /** @type {import('./Action.js').default[]} */
-    actions = [];
-
- /**
-  * @param {import('../Component.js').default} component  *
-     * @param {import('./shared/Node.js').default} parent  *
-     * @param {import('./shared/TemplateScope.js').default} scope  *
-     * @param {import('../../interfaces.js').Element} info  undefined
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        info.attributes.forEach(/** @param {any} node */ (node) => {
-            if (node.type === 'EventHandler') {
-                this.handlers.push(new EventHandler(component, this, scope, node));
-            }
-            else if (node.type === 'Action') {
-                this.actions.push(new Action(component, this, scope, node));
-            }
-            else {
-                // TODO there shouldn't be anything else here...
-            }
-        });
-    }
+	/**
+	 * @param {import('../Component.js').default} component  *
+	 * @param {import('./shared/Node.js').default} parent  *
+	 * @param {import('./shared/TemplateScope.js').default} scope  *
+	 * @param {import('../../interfaces.js').Element} info  undefined
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		info.attributes.forEach(
+			/** @param {any} node */ (node) => {
+				if (node.type === 'EventHandler') {
+					this.handlers.push(new EventHandler(component, this, scope, node));
+				} else if (node.type === 'Action') {
+					this.actions.push(new Action(component, this, scope, node));
+				} else {
+					// TODO there shouldn't be anything else here...
+				}
+			}
+		);
+	}
 }
-
-
-
-
