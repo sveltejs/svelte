6582e56bb132f64586f3bc724249d8ab79301e16
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:49:42 +0200

    Lint and format src/compiler/compile/nodes/Options.js

--- src/compiler/compile/nodes/Options.js
+++ src/compiler/compile/nodes/Options.js
@@ -2,11 +2,6 @@ import Node from './shared/Node.js';
 
 /** @extends Node */
 export default class Options extends Node {
-
-    /** @type {'Options'} */
-    type;
+	/** @type {'Options'} */
+	type;
 }
-
-
-
-
