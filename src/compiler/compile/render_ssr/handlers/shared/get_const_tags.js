f57591cf0622c4e1a150663a9a9a7a17eac2edcf
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 12:12:37 +0200

    Lint and format src/compiler/compile/render_ssr/handlers/shared/get_const_tags.js

--- src/compiler/compile/render_ssr/handlers/shared/get_const_tags.js
+++ src/compiler/compile/render_ssr/handlers/shared/get_const_tags.js
@@ -1,21 +1,16 @@
 /** @param {ConstTag[]} const_tags */
 export function get_const_tags(const_tags) {
-    if (const_tags.length === 0)
-        return null;
-    return {
-        type: 'VariableDeclaration',
-        kind: 'let',
-        declarations: const_tags.map((const_tag) => {
-            const assignment = const_tag.node.expression;
-            return {
-                type: 'VariableDeclarator',
-                id: assignment.left,
-                init: assignment.right
-            };
-        })
-    };
+	if (const_tags.length === 0) return null;
+	return {
+		type: 'VariableDeclaration',
+		kind: 'let',
+		declarations: const_tags.map((const_tag) => {
+			const assignment = const_tag.node.expression;
+			return {
+				type: 'VariableDeclarator',
+				id: assignment.left,
+				init: assignment.right
+			};
+		})
+	};
 }
-
-
-
-
