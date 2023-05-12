2653a6518b6e8b243b7513ac5b554beb8631f0c2
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 12:09:19 +0200

    Lint and format src/compiler/compile/render_ssr/handlers/Text.js

--- src/compiler/compile/render_ssr/handlers/Text.js
+++ src/compiler/compile/render_ssr/handlers/Text.js
@@ -6,19 +6,17 @@ import { escape_html } from '../../utils/stringify.js';
  * @param {import('../Renderer.js').RenderOptions} _options
  */
 export default function (node, renderer, _options) {
-    let text = node.data;
-    if (node.use_space()) {
-        text = ' ';
-    }
-    else if (!node.parent ||
-        node.parent.type !== 'Element' ||
-        (( /** @type {import('../../nodes/Element.js').default} */(node.parent)).name !== 'script' && ( /** @type {import('../../nodes/Element.js').default} */(node.parent)).name !== 'style')) {
-        // unless this Text node is inside a <script> or <style> element, escape &,<,>
-        text = escape_html(text);
-    }
-    renderer.add_string(text);
+	let text = node.data;
+	if (node.use_space()) {
+		text = ' ';
+	} else if (
+		!node.parent ||
+		node.parent.type !== 'Element' ||
+		(/** @type {import('../../nodes/Element.js').default} */ (node.parent).name !== 'script' &&
+			/** @type {import('../../nodes/Element.js').default} */ (node.parent).name !== 'style')
+	) {
+		// unless this Text node is inside a <script> or <style> element, escape &,<,>
+		text = escape_html(text);
+	}
+	renderer.add_string(text);
 }
-
-
-
-
