3370a12e7364a54a81a0407e06f6328daa7d47b1
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:50:11 +0200

    Convert src/compiler/compile/nodes/RawMustacheTag.ts to JavaScript

--- src/compiler/compile/nodes/RawMustacheTag.ts
+++ src/compiler/compile/nodes/RawMustacheTag.ts
@@ -1,10 +1,24 @@
-import Tag from './shared/Tag';
+import Tag from './shared/Tag.js';
 
+/** @extends Tag */
 export default class RawMustacheTag extends Tag {
-	type: 'RawMustacheTag';
-	constructor(component, parent, scope, info) {
-		super(component, parent, scope, info);
-		this.cannot_use_innerhtml();
-		this.not_static_content();
-	}
+
+    /** @type {'RawMustacheTag'} */
+    type;
+
+ /**
+  * @param {any} component
+     * @param {any} parent
+     * @param {any} scope
+     * @param {any} info
+     */
+    constructor(component, parent, scope, info) {
+        super(component, parent, scope, info);
+        this.cannot_use_innerhtml();
+        this.not_static_content();
+    }
 }
+
+
+
+
