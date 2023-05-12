638d01ca5258aa1035d4681b8291087208d66251
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 12:09:08 +0200

    Lint and format src/compiler/compile/render_ssr/handlers/Tag.js

--- src/compiler/compile/render_ssr/handlers/Tag.js
+++ src/compiler/compile/render_ssr/handlers/Tag.js
@@ -6,12 +6,10 @@ import { x } from 'code-red';
  * @param {import('../Renderer.js').RenderOptions} _options
  */
 export default function (node, renderer, _options) {
-    const snippet = node.expression.node;
-    renderer.add_expression(node.parent && node.parent.type === 'Element' && node.parent.name === 'style'
-        ? snippet
-        : x `@escape(${snippet})`);
+	const snippet = node.expression.node;
+	renderer.add_expression(
+		node.parent && node.parent.type === 'Element' && node.parent.name === 'style'
+			? snippet
+			: x`@escape(${snippet})`
+	);
 }
-
-
-
-
