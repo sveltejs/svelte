37d2ae04311a2985ba708330a4b32600a5d2dbd6
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:51:29 +0200

    Lint and format src/compiler/compile/nodes/StyleDirective.js

--- src/compiler/compile/nodes/StyleDirective.js
+++ src/compiler/compile/nodes/StyleDirective.js
@@ -7,62 +7,60 @@ const valid_modifiers = new Set(['important']);
 
 /** @extends Node */
 export default class StyleDirective extends Node {
+	/** @type {'StyleDirective'} */
+	type;
 
-    /** @type {'StyleDirective'} */
-    type;
+	/** @type {string} */
+	name;
 
-    /** @type {string} */
-    name;
+	/** @type {Set<string>} */
+	modifiers;
 
-    /** @type {Set<string>} */
-    modifiers;
+	/** @type {import('./shared/Expression.js').default} */
+	expression;
 
-    /** @type {import('./shared/Expression.js').default} */
-    expression;
+	/** @type {boolean} */
+	should_cache;
 
-    /** @type {boolean} */
-    should_cache;
-
- /**
-  * @param {import('../Component.js').default} component
-     * @param {import('./shared/Node.js').default} parent
-     * @param {import('./shared/TemplateScope.js').default} scope
-     * @param {import('../../interfaces.js').TemplateNode} info
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        this.name = info.name;
-        this.modifiers = new Set(info.modifiers);
-        for (const modifier of this.modifiers) {
-            if (!valid_modifiers.has(modifier)) {
-                component.error(this, compiler_errors.invalid_style_directive_modifier(list([...valid_modifiers])));
-            }
-        }
-        // Convert the value array to an expression so it's easier to handle
-        // the StyleDirective going forward.
-        if (info.value === true || (info.value.length === 1 && info.value[0].type === 'MustacheTag')) {
-            const identifier = info.value === true
-                ? ( /** @type {any} */({
-                    type: 'Identifier',
-                    start: info.end - info.name.length,
-                    end: info.end,
-                    name: info.name
-                }))
-                : info.value[0].expression;
-            this.expression = new Expression(component, this, scope, identifier);
-            this.should_cache = false;
-        }
-        else {
-            const raw_expression = nodes_to_template_literal(info.value);
-            this.expression = new Expression(component, this, scope, raw_expression);
-            this.should_cache = raw_expression.expressions.length > 0;
-        }
-    }
-    get important() {
-        return this.modifiers.has('important');
-    }
+	/**
+	 * @param {import('../Component.js').default} component
+	 * @param {import('./shared/Node.js').default} parent
+	 * @param {import('./shared/TemplateScope.js').default} scope
+	 * @param {import('../../interfaces.js').TemplateNode} info
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		this.name = info.name;
+		this.modifiers = new Set(info.modifiers);
+		for (const modifier of this.modifiers) {
+			if (!valid_modifiers.has(modifier)) {
+				component.error(
+					this,
+					compiler_errors.invalid_style_directive_modifier(list([...valid_modifiers]))
+				);
+			}
+		}
+		// Convert the value array to an expression so it's easier to handle
+		// the StyleDirective going forward.
+		if (info.value === true || (info.value.length === 1 && info.value[0].type === 'MustacheTag')) {
+			const identifier =
+				info.value === true
+					? /** @type {any} */ ({
+							type: 'Identifier',
+							start: info.end - info.name.length,
+							end: info.end,
+							name: info.name
+					  })
+					: info.value[0].expression;
+			this.expression = new Expression(component, this, scope, identifier);
+			this.should_cache = false;
+		} else {
+			const raw_expression = nodes_to_template_literal(info.value);
+			this.expression = new Expression(component, this, scope, raw_expression);
+			this.should_cache = raw_expression.expressions.length > 0;
+		}
+	}
+	get important() {
+		return this.modifiers.has('important');
+	}
 }
-
-
-
-
