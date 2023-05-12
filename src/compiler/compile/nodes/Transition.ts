d7e2f3603edde2be1defb5bed21fa2f57da55ce3
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:52:47 +0200

    Convert src/compiler/compile/nodes/Transition.ts to JavaScript

--- src/compiler/compile/nodes/Transition.ts
+++ src/compiler/compile/nodes/Transition.ts
@@ -1,40 +1,49 @@
-import Node from './shared/Node';
-import Expression from './shared/Expression';
-import Component from '../Component';
-import TemplateScope from './shared/TemplateScope';
-import { TemplateNode } from '../../interfaces';
-import Element from './Element';
-import compiler_errors from '../compiler_errors';
+import Node from './shared/Node.js';
+import Expression from './shared/Expression.js';
+import compiler_errors from '../compiler_errors.js';
 
+/** @extends Node */
 export default class Transition extends Node {
-	type: 'Transition';
-	name: string;
-	directive: string;
-	expression: Expression;
-	is_local: boolean;
 
-	constructor(component: Component, parent: Element, scope: TemplateScope, info: TemplateNode) {
-		super(component, parent, scope, info);
+    /** @type {'Transition'} */
+    type;
 
-		component.warn_if_undefined(info.name, info, scope);
+    /** @type {string} */
+    name;
 
-		this.name = info.name;
-		component.add_reference(this as any, info.name.split('.')[0]);
+    /** @type {string} */
+    directive;
 
-		this.directive = info.intro && info.outro ? 'transition' : info.intro ? 'in' : 'out';
-		this.is_local = info.modifiers.includes('local');
+    /** @type {import('./shared/Expression.js').default} */
+    expression;
 
-		if ((info.intro && parent.intro) || (info.outro && parent.outro)) {
-			const parent_transition = parent.intro || parent.outro;
-			component.error(
-				info,
-				compiler_errors.duplicate_transition(this.directive, parent_transition.directive)
-			);
-			return;
-		}
+    /** @type {boolean} */
+    is_local;
 
-		this.expression = info.expression
-			? new Expression(component, this, scope, info.expression)
-			: null;
-	}
+ /**
+  * @param {import('../Component.js').default} component
+     * @param {import('./Element.js').default} parent
+     * @param {import('./shared/TemplateScope.js').default} scope
+     * @param {import('../../interfaces.js').TemplateNode} info
+     */
+    constructor(component, parent, scope, info) {
+        super(component, parent, scope, info);
+        component.warn_if_undefined(info.name, info, scope);
+        this.name = info.name;
+        component.add_reference(/** @type {any} */ (this), info.name.split('.')[0]);
+        this.directive = info.intro && info.outro ? 'transition' : info.intro ? 'in' : 'out';
+        this.is_local = info.modifiers.includes('local');
+        if ((info.intro && parent.intro) || (info.outro && parent.outro)) {
+            const parent_transition = parent.intro || parent.outro;
+            component.error(info, compiler_errors.duplicate_transition(this.directive, parent_transition.directive));
+            return;
+        }
+        this.expression = info.expression
+            ? new Expression(component, this, scope, info.expression)
+            : null;
+    }
 }
+
+
+
+
