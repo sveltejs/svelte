1b234cc5fb2c3f191007aaa841ec9b50c9d80175
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:41:00 +0200

    Lint and format src/compiler/compile/nodes/Comment.js

--- src/compiler/compile/nodes/Comment.js
+++ src/compiler/compile/nodes/Comment.js
@@ -2,29 +2,24 @@ import Node from './shared/Node.js';
 
 /** @extends Node */
 export default class Comment extends Node {
+	/** @type {'Comment'} */
+	type;
 
-    /** @type {'Comment'} */
-    type;
+	/** @type {string} */
+	data;
 
-    /** @type {string} */
-    data;
+	/** @type {string[]} */
+	ignores;
 
-    /** @type {string[]} */
-    ignores;
-
- /**
-  * @param {import('../Component.js').default} component  *
-     * @param {import('./shared/Node.js').default} parent  *
-     * @param {import('./shared/TemplateScope.js').default} scope  *
-     * @param {import('../../interfaces.js').TemplateNode} info  undefined
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        this.data = info.data;
-        this.ignores = info.ignores;
-    }
+	/**
+	 * @param {import('../Component.js').default} component  *
+	 * @param {import('./shared/Node.js').default} parent  *
+	 * @param {import('./shared/TemplateScope.js').default} scope  *
+	 * @param {import('../../interfaces.js').TemplateNode} info  undefined
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		this.data = info.data;
+		this.ignores = info.ignores;
+	}
 }
-
-
-
-
