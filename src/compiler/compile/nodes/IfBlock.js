8bd12c01623e1db281252adf0bd2010f678f5cd5
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:48:06 +0200

    Lint and format src/compiler/compile/nodes/IfBlock.js

--- src/compiler/compile/nodes/IfBlock.js
+++ src/compiler/compile/nodes/IfBlock.js
@@ -5,40 +5,35 @@ import get_const_tags from './shared/get_const_tags.js';
 
 /** @extends AbstractBlock */
 export default class IfBlock extends AbstractBlock {
+	/** @type {'IfBlock'} */
+	type;
 
-    /** @type {'IfBlock'} */
-    type;
+	/** @type {import('./shared/Expression.js').default} */
+	expression;
 
-    /** @type {import('./shared/Expression.js').default} */
-    expression;
+	/** @type {import('./ElseBlock.js').default} */
+	else;
 
-    /** @type {import('./ElseBlock.js').default} */
-    else;
+	/** @type {import('./shared/TemplateScope.js').default} */
+	scope;
 
-    /** @type {import('./shared/TemplateScope.js').default} */
-    scope;
+	/** @type {import('./ConstTag.js').default[]} */
+	const_tags;
 
-    /** @type {import('./ConstTag.js').default[]} */
-    const_tags;
-
- /**
-  * @param {import('../Component.js').default} component
-     * @param {import('./shared/Node.js').default} parent
-     * @param {import('./shared/TemplateScope.js').default} scope
-     * @param {import('../../interfaces.js').TemplateNode} info
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        this.scope = scope.child();
-        this.cannot_use_innerhtml();
-        this.not_static_content();
-        this.expression = new Expression(component, this, this.scope, info.expression);
-        [this.const_tags, this.children] = get_const_tags(info.children, component, this, this);
-        this.else = info.else ? new ElseBlock(component, this, scope, info.else) : null;
-        this.warn_if_empty_block();
-    }
+	/**
+	 * @param {import('../Component.js').default} component
+	 * @param {import('./shared/Node.js').default} parent
+	 * @param {import('./shared/TemplateScope.js').default} scope
+	 * @param {import('../../interfaces.js').TemplateNode} info
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		this.scope = scope.child();
+		this.cannot_use_innerhtml();
+		this.not_static_content();
+		this.expression = new Expression(component, this, this.scope, info.expression);
+		[this.const_tags, this.children] = get_const_tags(info.children, component, this, this);
+		this.else = info.else ? new ElseBlock(component, this, scope, info.else) : null;
+		this.warn_if_empty_block();
+	}
 }
-
-
-
-
