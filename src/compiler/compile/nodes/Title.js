0ed905ef2a2bbb93ed5f536a504a06e19a27f2ca
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:52:33 +0200

    Lint and format src/compiler/compile/nodes/Title.js

--- src/compiler/compile/nodes/Title.js
+++ src/compiler/compile/nodes/Title.js
@@ -4,41 +4,38 @@ import compiler_errors from '../compiler_errors.js';
 
 /** @extends Node */
 export default class Title extends Node {
+	/** @type {'Title'} */
+	type;
 
-    /** @type {'Title'} */
-    type;
+	/** @type {import('./shared/map_children.js').Children} */
+	children;
 
-    /** @type {import('./shared/map_children.js').Children} */
-    children;
+	/** @type {boolean} */
+	should_cache;
 
-    /** @type {boolean} */
-    should_cache;
-
- /**
-  * @param {import('../Component.js').default} component
-     * @param {import('./shared/Node.js').default} parent
-     * @param {import('./shared/TemplateScope.js').default} scope
-     * @param {import('../../interfaces.js').TemplateNode} info
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        this.children = map_children(component, parent, scope, info.children);
-        if (info.attributes.length > 0) {
-            component.error(info.attributes[0], compiler_errors.illegal_attribute_title);
-            return;
-        }
-        info.children.forEach(/** @param {any} child */ (child) => {
-            if (child.type !== 'Text' && child.type !== 'MustacheTag') {
-                return component.error(child, compiler_errors.illegal_structure_title);
-            }
-        });
-        this.should_cache =
-            info.children.length === 1
-                ? info.children[0].type !== 'Identifier' || scope.names.has(info.children[0].name)
-                : true;
-    }
+	/**
+	 * @param {import('../Component.js').default} component
+	 * @param {import('./shared/Node.js').default} parent
+	 * @param {import('./shared/TemplateScope.js').default} scope
+	 * @param {import('../../interfaces.js').TemplateNode} info
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		this.children = map_children(component, parent, scope, info.children);
+		if (info.attributes.length > 0) {
+			component.error(info.attributes[0], compiler_errors.illegal_attribute_title);
+			return;
+		}
+		info.children.forEach(
+			/** @param {any} child */ (child) => {
+				if (child.type !== 'Text' && child.type !== 'MustacheTag') {
+					return component.error(child, compiler_errors.illegal_structure_title);
+				}
+			}
+		);
+		this.should_cache =
+			info.children.length === 1
+				? info.children[0].type !== 'Identifier' || scope.names.has(info.children[0].name)
+				: true;
+	}
 }
-
-
-
-
