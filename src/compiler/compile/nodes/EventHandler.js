c4681c158d640775bd29126c139dc1c5ef46f710
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:46:35 +0200

    Lint and format src/compiler/compile/nodes/EventHandler.js

--- src/compiler/compile/nodes/EventHandler.js
+++ src/compiler/compile/nodes/EventHandler.js
@@ -5,84 +5,81 @@ const regex_contains_term_function_expression = /FunctionExpression/;
 
 /** @extends Node */
 export default class EventHandler extends Node {
+	/** @type {'EventHandler'} */
+	type;
 
-    /** @type {'EventHandler'} */
-    type;
+	/** @type {string} */
+	name;
 
-    /** @type {string} */
-    name;
+	/** @type {Set<string>} */
+	modifiers;
 
-    /** @type {Set<string>} */
-    modifiers;
+	/** @type {import('./shared/Expression.js').default} */
+	expression;
 
-    /** @type {import('./shared/Expression.js').default} */
-    expression;
+	/** @type {import('estree').Identifier} */
+	handler_name;
+	/** */
+	uses_context = false;
+	/** */
+	can_make_passive = false;
 
-    /** @type {import('estree').Identifier} */
-    handler_name;
-    /** */
-    uses_context = false;
-    /** */
-    can_make_passive = false;
+	/**
+	 * @param {import('../Component.js').default} component  *
+	 * @param {import('./shared/Node.js').default} parent  *
+	 * @param {import('./shared/TemplateScope.js').default} template_scope  *
+	 * @param {import('../../interfaces.js').TemplateNode} info  undefined
+	 */
+	constructor(component, parent, template_scope, info) {
+		super(component, parent, template_scope, info);
+		this.name = info.name;
+		this.modifiers = new Set(info.modifiers);
+		if (info.expression) {
+			this.expression = new Expression(component, this, template_scope, info.expression);
+			this.uses_context = this.expression.uses_context;
+			if (
+				regex_contains_term_function_expression.test(info.expression.type) &&
+				info.expression.params.length === 0
+			) {
+				// TODO make this detection more accurate — if `event.preventDefault` isn't called, and
+				// `event` is passed to another function, we can make it passive
+				this.can_make_passive = true;
+			} else if (info.expression.type === 'Identifier') {
+				let node = component.node_for_declaration.get(info.expression.name);
+				if (node) {
+					if (node.type === 'VariableDeclaration') {
+						// for `const handleClick = () => {...}`, we want the [arrow] function expression node
+						const declarator = node.declarations.find(
+							/** @param {any} d */
+							(d) => /** @type {import('estree').Identifier} */ (d.id).name === info.expression.name
+						);
+						node = declarator && declarator.init;
+					}
+					if (
+						node &&
+						(node.type === 'FunctionExpression' ||
+							node.type === 'FunctionDeclaration' ||
+							node.type === 'ArrowFunctionExpression') &&
+						node.params.length === 0
+					) {
+						this.can_make_passive = true;
+					}
+				}
+			}
+		} else {
+			this.handler_name = component.get_unique_name(`${sanitize(this.name)}_handler`);
+		}
+	}
 
- /**
-  * @param {import('../Component.js').default} component  *
-     * @param {import('./shared/Node.js').default} parent  *
-     * @param {import('./shared/TemplateScope.js').default} template_scope  *
-     * @param {import('../../interfaces.js').TemplateNode} info  undefined
-     */
-    constructor(component, parent, template_scope, info) {
-        super(component, parent, template_scope, info);
-        this.name = info.name;
-        this.modifiers = new Set(info.modifiers);
-        if (info.expression) {
-            this.expression = new Expression(component, this, template_scope, info.expression);
-            this.uses_context = this.expression.uses_context;
-            if (regex_contains_term_function_expression.test(info.expression.type) &&
-                info.expression.params.length === 0) {
-                // TODO make this detection more accurate — if `event.preventDefault` isn't called, and
-                // `event` is passed to another function, we can make it passive
-                this.can_make_passive = true;
-            }
-            else if (info.expression.type === 'Identifier') {
-                let node = component.node_for_declaration.get(info.expression.name);
-                if (node) {
-                    if (node.type === 'VariableDeclaration') {
-                        // for `const handleClick = () => {...}`, we want the [arrow] function expression node
-                        const declarator = node.declarations.find(
-
-                        /** @param {any} d */
-                        (d) => ( /** @type {import('estree').Identifier} */(d.id)).name === info.expression.name);
-                        node = declarator && declarator.init;
-                    }
-                    if (node &&
-                        (node.type === 'FunctionExpression' ||
-                            node.type === 'FunctionDeclaration' ||
-                            node.type === 'ArrowFunctionExpression') &&
-                        node.params.length === 0) {
-                        this.can_make_passive = true;
-                    }
-                }
-            }
-        }
-        else {
-            this.handler_name = component.get_unique_name(`${sanitize(this.name)}_handler`);
-        }
-    }
-
-    /** @returns {boolean} */
-    get reassigned() {
-        if (!this.expression) {
-            return false;
-        }
-        const node = this.expression.node;
-        if (regex_contains_term_function_expression.test(node.type)) {
-            return false;
-        }
-        return this.expression.dynamic_dependencies().length > 0;
-    }
+	/** @returns {boolean} */
+	get reassigned() {
+		if (!this.expression) {
+			return false;
+		}
+		const node = this.expression.node;
+		if (regex_contains_term_function_expression.test(node.type)) {
+			return false;
+		}
+		return this.expression.dynamic_dependencies().length > 0;
+	}
 }
-
-
-
-
