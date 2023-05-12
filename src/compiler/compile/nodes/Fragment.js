7f3d43eb35a6864362219a5e8ee56b636e3315e0
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:47:04 +0200

    Lint and format src/compiler/compile/nodes/Fragment.js

--- src/compiler/compile/nodes/Fragment.js
+++ src/compiler/compile/nodes/Fragment.js
@@ -4,31 +4,26 @@ import TemplateScope from './shared/TemplateScope.js';
 
 /** @extends Node */
 export default class Fragment extends Node {
+	/** @type {'Fragment'} */
+	type;
 
-    /** @type {'Fragment'} */
-    type;
+	/** @type {import('../render_dom/Block.js').default} */
+	block;
 
-    /** @type {import('../render_dom/Block.js').default} */
-    block;
+	/** @type {import('./interfaces.js').INode[]} */
+	children;
 
-    /** @type {import('./interfaces.js').INode[]} */
-    children;
+	/** @type {import('./shared/TemplateScope.js').default} */
+	scope;
 
-    /** @type {import('./shared/TemplateScope.js').default} */
-    scope;
-
- /**
-  * @param {import('../Component.js').default} component  *
-     * @param {import('../../interfaces.js').TemplateNode} info  undefined
-     */
-    constructor(component, info) {
-        const scope = new TemplateScope();
-        super(component, null, scope, info);
-        this.scope = scope;
-        this.children = map_children(component, this, scope, info.children);
-    }
+	/**
+	 * @param {import('../Component.js').default} component  *
+	 * @param {import('../../interfaces.js').TemplateNode} info  undefined
+	 */
+	constructor(component, info) {
+		const scope = new TemplateScope();
+		super(component, null, scope, info);
+		this.scope = scope;
+		this.children = map_children(component, this, scope, info.children);
+	}
 }
-
-
-
-
