65763cf657c5f52e11a7a91751327a23574d305e
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:40:42 +0200

    Lint and format src/compiler/compile/nodes/Class.js

--- src/compiler/compile/nodes/Class.js
+++ src/compiler/compile/nodes/Class.js
@@ -3,31 +3,26 @@ import Expression from './shared/Expression.js';
 
 /** @extends Node */
 export default class Class extends Node {
+	/** @type {'Class'} */
+	type;
 
-    /** @type {'Class'} */
-    type;
+	/** @type {string} */
+	name;
 
-    /** @type {string} */
-    name;
+	/** @type {import('./shared/Expression.js').default} */
+	expression;
 
-    /** @type {import('./shared/Expression.js').default} */
-    expression;
-
- /**
-  * @param {import('../Component.js').default} component  *
-     * @param {import('./shared/Node.js').default} parent  *
-     * @param {import('./shared/TemplateScope.js').default} scope  *
-     * @param {import('../../interfaces.js').TemplateNode} info  undefined
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        this.name = info.name;
-        this.expression = info.expression
-            ? new Expression(component, this, scope, info.expression)
-            : null;
-    }
+	/**
+	 * @param {import('../Component.js').default} component  *
+	 * @param {import('./shared/Node.js').default} parent  *
+	 * @param {import('./shared/TemplateScope.js').default} scope  *
+	 * @param {import('../../interfaces.js').TemplateNode} info  undefined
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		this.name = info.name;
+		this.expression = info.expression
+			? new Expression(component, this, scope, info.expression)
+			: null;
+	}
 }
-
-
-
-
