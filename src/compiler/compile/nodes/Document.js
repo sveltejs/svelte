d17043bff5a728d5cec8f066a1ed035b4a125fdb
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:44:37 +0200

    Lint and format src/compiler/compile/nodes/Document.js

--- src/compiler/compile/nodes/Document.js
+++ src/compiler/compile/nodes/Document.js
@@ -10,63 +10,70 @@ const valid_bindings = ['fullscreenElement', 'visibilityState'];
 
 /** @extends Node */
 export default class Document extends Node {
+	/** @type {'Document'} */
+	type;
 
-    /** @type {'Document'} */
-    type;
+	/** @type {import('./EventHandler.js').default[]} */
+	handlers = [];
 
-    /** @type {import('./EventHandler.js').default[]} */
-    handlers = [];
+	/** @type {import('./Binding.js').default[]} */
+	bindings = [];
 
-    /** @type {import('./Binding.js').default[]} */
-    bindings = [];
+	/** @type {import('./Action.js').default[]} */
+	actions = [];
 
-    /** @type {import('./Action.js').default[]} */
-    actions = [];
+	/**
+	 * @param {import('../Component.js').default} component  *
+	 * @param {import('./shared/Node.js').default} parent  *
+	 * @param {import('./shared/TemplateScope.js').default} scope  *
+	 * @param {import('../../interfaces.js').Element} info  undefined
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		info.attributes.forEach(
+			/** @param {any} node */ (node) => {
+				if (node.type === 'EventHandler') {
+					this.handlers.push(new EventHandler(component, this, scope, node));
+				} else if (node.type === 'Binding') {
+					if (!~valid_bindings.indexOf(node.name)) {
+						const match = fuzzymatch(node.name, valid_bindings);
+						if (match) {
+							return component.error(
+								node,
+								compiler_errors.invalid_binding_on(
+									node.name,
+									'<svelte:document>',
+									` (did you mean '${match}'?)`
+								)
+							);
+						} else {
+							return component.error(
+								node,
+								compiler_errors.invalid_binding_on(
+									node.name,
+									'<svelte:document>',
+									` — valid bindings are ${list(valid_bindings)}`
+								)
+							);
+						}
+					}
+					this.bindings.push(new Binding(component, this, scope, node));
+				} else if (node.type === 'Action') {
+					this.actions.push(new Action(component, this, scope, node));
+				} else {
+					// TODO there shouldn't be anything else here...
+				}
+			}
+		);
+		this.validate();
+	}
 
- /**
-  * @param {import('../Component.js').default} component  *
-     * @param {import('./shared/Node.js').default} parent  *
-     * @param {import('./shared/TemplateScope.js').default} scope  *
-     * @param {import('../../interfaces.js').Element} info  undefined
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        info.attributes.forEach(/** @param {any} node */ (node) => {
-            if (node.type === 'EventHandler') {
-                this.handlers.push(new EventHandler(component, this, scope, node));
-            }
-            else if (node.type === 'Binding') {
-                if (!~valid_bindings.indexOf(node.name)) {
-                    const match = fuzzymatch(node.name, valid_bindings);
-                    if (match) {
-                        return component.error(node, compiler_errors.invalid_binding_on(node.name, '<svelte:document>', ` (did you mean '${match}'?)`));
-                    }
-                    else {
-                        return component.error(node, compiler_errors.invalid_binding_on(node.name, '<svelte:document>', ` — valid bindings are ${list(valid_bindings)}`));
-                    }
-                }
-                this.bindings.push(new Binding(component, this, scope, node));
-            }
-            else if (node.type === 'Action') {
-                this.actions.push(new Action(component, this, scope, node));
-            }
-            else {
-                // TODO there shouldn't be anything else here...
-            }
-        });
-        this.validate();
-    }
-
-    /** @private */
-    validate() {
-        const handlers_map = new Set();
-        this.handlers.forEach(/** @param {any} handler */ (handler) => handlers_map.add(handler.name));
-        if (handlers_map.has('mouseenter') || handlers_map.has('mouseleave')) {
-            this.component.warn(this, compiler_warnings.avoid_mouse_events_on_document);
-        }
-    }
+	/** @private */
+	validate() {
+		const handlers_map = new Set();
+		this.handlers.forEach(/** @param {any} handler */ (handler) => handlers_map.add(handler.name));
+		if (handlers_map.has('mouseenter') || handlers_map.has('mouseleave')) {
+			this.component.warn(this, compiler_warnings.avoid_mouse_events_on_document);
+		}
+	}
 }
-
-
-
-
