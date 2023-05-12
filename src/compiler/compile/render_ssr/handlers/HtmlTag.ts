82dece16f4617fb0449575cbc08bbe5f98d4c603
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 12:04:08 +0200

    Convert src/compiler/compile/render_ssr/handlers/HtmlTag.ts to JavaScript

--- src/compiler/compile/render_ssr/handlers/HtmlTag.ts
+++ src/compiler/compile/render_ssr/handlers/HtmlTag.ts
@@ -1,9 +1,16 @@
-import Renderer, { RenderOptions } from '../Renderer';
-import RawMustacheTag from '../../nodes/RawMustacheTag';
-import { Expression } from 'estree';
-
-export default function (node: RawMustacheTag, renderer: Renderer, options: RenderOptions) {
-	if (options.hydratable) renderer.add_string('<!-- HTML_TAG_START -->');
-	renderer.add_expression(node.expression.node as Expression);
-	if (options.hydratable) renderer.add_string('<!-- HTML_TAG_END -->');
+/**
+ * @param {import('../../nodes/RawMustacheTag.js').default} node
+ * @param {import('../Renderer.js').default} renderer
+ * @param {import('../Renderer.js').RenderOptions} options
+ */
+export default function (node, renderer, options) {
+    if (options.hydratable)
+        renderer.add_string('<!-- HTML_TAG_START -->');
+    renderer.add_expression(/** @type {Expression} */ (node.expression.node));
+    if (options.hydratable)
+        renderer.add_string('<!-- HTML_TAG_END -->');
 }
+
+
+
+
