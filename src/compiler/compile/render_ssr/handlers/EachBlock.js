611637975a54435c63f798f0bb6e480f175307c7
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 12:01:00 +0200

    Lint and format src/compiler/compile/render_ssr/handlers/EachBlock.js

--- src/compiler/compile/render_ssr/handlers/EachBlock.js
+++ src/compiler/compile/render_ssr/handlers/EachBlock.js
@@ -7,26 +7,22 @@ import { get_const_tags } from './shared/get_const_tags.js';
  * @param {import('../Renderer.js').RenderOptions} options
  */
 export default function (node, renderer, options) {
-    const args = [node.context_node];
-    if (node.index)
-        args.push({ type: 'Identifier', name: node.index });
-    renderer.push();
-    renderer.render(node.children, options);
-    const result = renderer.pop();
-    const consequent = x `@each(${node.expression.node}, (${args}) => { ${get_const_tags(node.const_tags)}; return ${result} })`;
-    if (node.else) {
-        renderer.push();
-        renderer.render(node.else.children, options);
-        let alternate = renderer.pop();
-        if (node.else.const_tags.length > 0)
-            alternate = x `(() => { ${get_const_tags(node.else.const_tags)}; return ${alternate} })()`;
-        renderer.add_expression(x `${node.expression.node}.length ? ${consequent} : ${alternate}`);
-    }
-    else {
-        renderer.add_expression(consequent);
-    }
+	const args = [node.context_node];
+	if (node.index) args.push({ type: 'Identifier', name: node.index });
+	renderer.push();
+	renderer.render(node.children, options);
+	const result = renderer.pop();
+	const consequent = x`@each(${node.expression.node}, (${args}) => { ${get_const_tags(
+		node.const_tags
+	)}; return ${result} })`;
+	if (node.else) {
+		renderer.push();
+		renderer.render(node.else.children, options);
+		let alternate = renderer.pop();
+		if (node.else.const_tags.length > 0)
+			alternate = x`(() => { ${get_const_tags(node.else.const_tags)}; return ${alternate} })()`;
+		renderer.add_expression(x`${node.expression.node}.length ? ${consequent} : ${alternate}`);
+	} else {
+		renderer.add_expression(consequent);
+	}
 }
-
-
-
-
