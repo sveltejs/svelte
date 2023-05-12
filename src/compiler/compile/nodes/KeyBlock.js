0db621f90b66e606f56135b655f8c02a00e324e3
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:48:46 +0200

    Lint and format src/compiler/compile/nodes/KeyBlock.js

--- src/compiler/compile/nodes/KeyBlock.js
+++ src/compiler/compile/nodes/KeyBlock.js
@@ -4,29 +4,24 @@ import AbstractBlock from './shared/AbstractBlock.js';
 
 /** @extends AbstractBlock */
 export default class KeyBlock extends AbstractBlock {
+	/** @type {'KeyBlock'} */
+	type;
 
-    /** @type {'KeyBlock'} */
-    type;
+	/** @type {import('./shared/Expression.js').default} */
+	expression;
 
-    /** @type {import('./shared/Expression.js').default} */
-    expression;
-
- /**
-  * @param {import('../Component.js').default} component
-     * @param {import('./shared/Node.js').default} parent
-     * @param {import('./shared/TemplateScope.js').default} scope
-     * @param {import('../../interfaces.js').TemplateNode} info
-     */
-    constructor(component, parent, scope, info) {
-        super(component, parent, scope, info);
-        this.cannot_use_innerhtml();
-        this.not_static_content();
-        this.expression = new Expression(component, this, scope, info.expression);
-        this.children = map_children(component, this, scope, info.children);
-        this.warn_if_empty_block();
-    }
+	/**
+	 * @param {import('../Component.js').default} component
+	 * @param {import('./shared/Node.js').default} parent
+	 * @param {import('./shared/TemplateScope.js').default} scope
+	 * @param {import('../../interfaces.js').TemplateNode} info
+	 */
+	constructor(component, parent, scope, info) {
+		super(component, parent, scope, info);
+		this.cannot_use_innerhtml();
+		this.not_static_content();
+		this.expression = new Expression(component, this, scope, info.expression);
+		this.children = map_children(component, this, scope, info.children);
+		this.warn_if_empty_block();
+	}
 }
-
-
-
-
