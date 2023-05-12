0b9f0b595dc5b4e2c9d943d50d47036fd86967e1
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 15:05:32 +0200

    Convert src/compiler/compile/render_dom/wrappers/DebugTag.ts to JavaScript

--- src/compiler/compile/render_dom/wrappers/DebugTag.ts
+++ src/compiler/compile/render_dom/wrappers/DebugTag.ts
@@ -1,88 +1,90 @@
-import Renderer from '../Renderer';
-import Wrapper from './shared/Wrapper';
-import Block from '../Block';
-import DebugTag from '../../nodes/DebugTag';
-import add_to_set from '../../utils/add_to_set';
+import Wrapper from './shared/Wrapper.js';
+import add_to_set from '../../utils/add_to_set.js';
 import { b, p } from 'code-red';
-import { Identifier, DebuggerStatement } from 'estree';
 
+/** @extends Wrapper */
 export default class DebugTagWrapper extends Wrapper {
-	node: DebugTag;
 
-	constructor(
-		renderer: Renderer,
-		block: Block,
-		parent: Wrapper,
-		node: DebugTag,
-		_strip_whitespace: boolean,
-		_next_sibling: Wrapper
-	) {
-		super(renderer, block, parent, node);
-	}
+    /** @type {import('../../nodes/DebugTag.js').default} */
+    node;
 
-	render(block: Block, _parent_node: Identifier, _parent_nodes: Identifier) {
-		const { renderer } = this;
-		const { component } = renderer;
+ /**
+  * @param {import('../Renderer.js').default} renderer
+     * @param {import('../Block.js').default} block
+     * @param {import('./shared/Wrapper.js').default} parent
+     * @param {import('../../nodes/DebugTag.js').default} node
+     * @param {boolean} _strip_whitespace
+     * @param {import('./shared/Wrapper.js').default} _next_sibling
+     */
+    constructor(renderer, block, parent, node, _strip_whitespace, _next_sibling) {
+        super(renderer, block, parent, node);
+    }
 
-		if (!renderer.options.dev) return;
+ /**
+  * @param {import('../Block.js').default} block
+     * @param {import('estree').Identifier} _parent_node
+     * @param {import('estree').Identifier} _parent_nodes
+     */
+    render(block, _parent_node, _parent_nodes) {
+        const { renderer } = this;
+        const { component } = renderer;
+        if (!renderer.options.dev)
+            return;
+        const { var_lookup } = component;
+        const start = component.locate(this.node.start + 1);
+        const end = { line: start.line, column: start.column + 6 };
+        const loc = { start, end };
 
-		const { var_lookup } = component;
+        /** @type {import('estree').DebuggerStatement} */
+        const debug = {
+            type: 'DebuggerStatement',
+            loc
+        };
+        if (this.node.expressions.length === 0) {
+            // Debug all
+            block.chunks.create.push(debug);
+            block.chunks.update.push(debug);
+        }
+        else {
 
-		const start = component.locate(this.node.start + 1);
-		const end = { line: start.line, column: start.column + 6 };
+            /** @type {import('estree').Identifier} */
+            const log = {
+                type: 'Identifier',
+                name: 'log',
+                loc
+            };
 
-		const loc = { start, end };
-
-		const debug: DebuggerStatement = {
-			type: 'DebuggerStatement',
-			loc
-		};
-
-		if (this.node.expressions.length === 0) {
-			// Debug all
-			block.chunks.create.push(debug);
-			block.chunks.update.push(debug);
-		} else {
-			const log: Identifier = {
-				type: 'Identifier',
-				name: 'log',
-				loc
-			};
-
-			const dependencies: Set<string> = new Set();
-			this.node.expressions.forEach((expression) => {
-				add_to_set(dependencies, expression.dependencies);
-			});
-
-			const contextual_identifiers = this.node.expressions
-				.filter((e) => {
-					const variable = var_lookup.get((e.node as Identifier).name);
-					return !(variable && variable.hoistable);
-				})
-				.map((e) => (e.node as Identifier).name);
-
-			const logged_identifiers = this.node.expressions.map(
-				(e) => p`${(e.node as Identifier).name}`
-			);
-
-			const debug_statements = b`
-				${contextual_identifiers.map((name) => b`const ${name} = ${renderer.reference(name)};`)}
+            /** @type {Set<string>} */
+            const dependencies = new Set();
+            this.node.expressions.forEach((expression) => {
+                add_to_set(dependencies, expression.dependencies);
+            });
+            const contextual_identifiers = this.node.expressions
+                .filter((e) => {
+                const variable = var_lookup.get(( /** @type {import('estree').Identifier} */(e.node)).name);
+                return !(variable && variable.hoistable);
+            })
+                .map((e) => ( /** @type {import('estree').Identifier} */(e.node)).name);
+            const logged_identifiers = this.node.expressions.map((e) => p `${( /** @type {import('estree').Identifier} */(e.node)).name}`);
+            const debug_statements = b `
+				${contextual_identifiers.map((name) => b `const ${name} = ${renderer.reference(name)};`)}
 				@_console.${log}({ ${logged_identifiers} });
 				debugger;`;
-
-			if (dependencies.size) {
-				const condition = renderer.dirty(Array.from(dependencies));
-
-				block.chunks.update.push(b`
+            if (dependencies.size) {
+                const condition = renderer.dirty(Array.from(dependencies));
+                block.chunks.update.push(b `
 					if (${condition}) {
 						${debug_statements}
 					}
 				`);
-			}
-
-			block.chunks.create.push(b`{
+            }
+            block.chunks.create.push(b `{
 				${debug_statements}
 			}`);
-		}
-	}
+        }
+    }
 }
+
+
+
+
