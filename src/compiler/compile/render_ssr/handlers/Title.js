eec1af14a8f53b0bc2dd3d7b1802ea735b030b91
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 12:09:40 +0200

    Lint and format src/compiler/compile/render_ssr/handlers/Title.js

--- src/compiler/compile/render_ssr/handlers/Title.js
+++ src/compiler/compile/render_ssr/handlers/Title.js
@@ -6,14 +6,10 @@ import { x } from 'code-red';
  * @param {import('../Renderer.js').RenderOptions} options
  */
 export default function (node, renderer, options) {
-    renderer.push();
-    renderer.add_string('<title>');
-    renderer.render(node.children, options);
-    renderer.add_string('</title>');
-    const result = renderer.pop();
-    renderer.add_expression(x `$$result.title = ${result}, ""`);
+	renderer.push();
+	renderer.add_string('<title>');
+	renderer.render(node.children, options);
+	renderer.add_string('</title>');
+	const result = renderer.pop();
+	renderer.add_expression(x`$$result.title = ${result}, ""`);
 }
-
-
-
-
