67b7e9d27f76abff047eb87dec3856d7ba128b6b
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:27:29 +0200

    Lint and format src/compiler/compile/nodes/shared/is_contextual.js

--- src/compiler/compile/nodes/shared/is_contextual.js
+++ src/compiler/compile/nodes/shared/is_contextual.js
@@ -6,19 +6,12 @@ import { is_reserved_keyword } from '../../utils/reserved_keywords.js';
  * @param {string} name
  */
 export default function is_contextual(component, scope, name) {
-    if (is_reserved_keyword(name))
-        return true;
-    // if it's a name below root scope, it's contextual
-    if (!scope.is_top_level(name))
-        return true;
-    const variable = component.var_lookup.get(name);
-    // hoistables, module declarations, and imports are non-contextual
-    if (!variable || variable.hoistable)
-        return false;
-    // assume contextual
-    return true;
+	if (is_reserved_keyword(name)) return true;
+	// if it's a name below root scope, it's contextual
+	if (!scope.is_top_level(name)) return true;
+	const variable = component.var_lookup.get(name);
+	// hoistables, module declarations, and imports are non-contextual
+	if (!variable || variable.hoistable) return false;
+	// assume contextual
+	return true;
 }
-
-
-
-
