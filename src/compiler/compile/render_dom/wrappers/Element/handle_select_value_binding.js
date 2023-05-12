50ced4db8d495fabd5ba967303e6a8e66f95d2ff
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 14:33:46 +0200

    Lint and format src/compiler/compile/render_dom/wrappers/Element/handle_select_value_binding.js

--- src/compiler/compile/render_dom/wrappers/Element/handle_select_value_binding.js
+++ src/compiler/compile/render_dom/wrappers/Element/handle_select_value_binding.js
@@ -3,15 +3,11 @@
  * @param {Set<string>} dependencies
  */
 export default function handle_select_value_binding(attr, dependencies) {
-    const { parent } = attr;
-    if (parent.node.name === 'select') {
-        ( /** @type {import('./index.js').default} */(parent)).select_binding_dependencies = dependencies;
-        dependencies.forEach((prop) => {
-            parent.renderer.component.indirect_dependencies.set(prop, new Set());
-        });
-    }
+	const { parent } = attr;
+	if (parent.node.name === 'select') {
+		/** @type {import('./index.js').default} */ (parent).select_binding_dependencies = dependencies;
+		dependencies.forEach((prop) => {
+			parent.renderer.component.indirect_dependencies.set(prop, new Set());
+		});
+	}
 }
-
-
-
-
