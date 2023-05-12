e927272a21af13d422c1a48a7f61d90947690ceb
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-12 17:48:39 +0200

    Convert src/compiler/compile/nodes/KeyBlock.ts to JavaScript

--- src/compiler/compile/nodes/KeyBlock.ts
+++ src/compiler/compile/nodes/KeyBlock.ts
@@ -1,25 +1,32 @@
-import Expression from './shared/Expression';
-import map_children from './shared/map_children';
-import AbstractBlock from './shared/AbstractBlock';
-import Component from '../Component';
-import TemplateScope from './shared/TemplateScope';
-import { TemplateNode } from '../../interfaces';
-import Node from './shared/Node';
+import Expression from './shared/Expression.js';
+import map_children from './shared/map_children.js';
+import AbstractBlock from './shared/AbstractBlock.js';
 
+/** @extends AbstractBlock */
 export default class KeyBlock extends AbstractBlock {
-	type: 'KeyBlock';
 
-	expression: Expression;
+    /** @type {'KeyBlock'} */
+    type;
 
-	constructor(component: Component, parent: Node, scope: TemplateScope, info: TemplateNode) {
-		super(component, parent, scope, info);
-		this.cannot_use_innerhtml();
-		this.not_static_content();
+    /** @type {import('./shared/Expression.js').default} */
+    expression;
 
-		this.expression = new Expression(component, this, scope, info.expression);
-
-		this.children = map_children(component, this, scope, info.children);
-
-		this.warn_if_empty_block();
-	}
+ /**
+  * @param {import('../Component.js').default} component
+     * @param {import('./shared/Node.js').default} parent
+     * @param {import('./shared/TemplateScope.js').default} scope
+     * @param {import('../../interfaces.js').TemplateNode} info
+     */
+    constructor(component, parent, scope, info) {
+        super(component, parent, scope, info);
+        this.cannot_use_innerhtml();
+        this.not_static_content();
+        this.expression = new Expression(component, this, scope, info.expression);
+        this.children = map_children(component, this, scope, info.children);
+        this.warn_if_empty_block();
+    }
 }
+
+
+
+
