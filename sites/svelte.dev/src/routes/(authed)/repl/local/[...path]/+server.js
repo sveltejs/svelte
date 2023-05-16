543e93bcf41e0940fd676248d259a3e37ab861f9
Puru Vijay <devpuruvj@gmail.com>
2023-05-16 20:50:05 +0530

    fix(site-2): Use dynamic LOCAL_SVELTE_PATH

--- sites/svelte.dev/src/routes/(authed)/repl/local/[...path]/+server.js
+++ sites/svelte.dev/src/routes/(authed)/repl/local/[...path]/+server.js
@@ -1,8 +1,8 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
-import { LOCAL_SVELTE_PATH } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
-const local_svelte_path = LOCAL_SVELTE_PATH || '../../../svelte';
+const local_svelte_path = env.LOCAL_SVELTE_PATH || '../../../svelte';
 
 export function GET({ params: { path } }) {
 	if (import.meta.env.PROD || ('/' + path).includes('/.')) {
