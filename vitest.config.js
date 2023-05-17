1420814fcce6b4910d3a3fbb85ec985f724ca827
Simon Holthausen <simon.holthausen@vercel.com>
2023-05-17 12:20:08 +0200

    fix exclude

--- vitest.config.js
+++ vitest.config.js
@@ -14,6 +14,6 @@ export default defineConfig({
 	test: {
 		dir: 'vitest',
 		reporters: ['dot'],
-		exclude: [...configDefaults.exclude, 'vitest/**/samples']
+		exclude: [...configDefaults.exclude, '**/samples/**']
 	}
 });
