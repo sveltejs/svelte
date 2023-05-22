da472336003bd6ccfb75c12404d933a9508ded0f
gtmnayan <gtmnayan@gmail.com>
2023-05-22 19:10:11 +0545

    fix lint fr

--- test/compiler-errors/compiler-errors.test.js
+++ test/compiler-errors/compiler-errors.test.js
@@ -1,5 +1,5 @@
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import { assert, describe, expect, it } from 'vitest';
 import { compile } from '../../compiler.mjs';
 
