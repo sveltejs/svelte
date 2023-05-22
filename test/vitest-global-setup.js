06c7d6c607df089f312209b133dc8f7c58ed52ad
gtmnayan <gtmnayan@gmail.com>
2023-05-22 18:16:06 +0545

    document necessity for the hack

--- test/vitest-global-setup.js
+++ test/vitest-global-setup.js
@@ -2,6 +2,8 @@ import { mkdirSync, rmSync, writeFileSync } from 'node:fs';
 
 // There are a lot of tests in the runtime suite, which take up a lot of time.
 // Split them into groups so that they can run in parallel and finish faster.
+// Vitest parallelizes tests on a per-file basis, so this hack creates a bunch
+// of files that run a subset of the tests.
 function create_shard_files() {
 	let num_shards = +process.env.SVELTE_TEST_SUITE_SHARDS || 1;
 	num_shards = Math.max(1, num_shards);
