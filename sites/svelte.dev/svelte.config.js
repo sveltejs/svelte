cad3878eb4302c844550b365057569047d121964
Puru Vijay <47742487+PuruVJ@users.noreply.github.com>
2023-05-26 02:43:27 +0530

    Update sites/svelte.dev/svelte.config.js
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- sites/svelte.dev/svelte.config.js
+++ sites/svelte.dev/svelte.config.js
@@ -1,6 +1,5 @@
 // @ts-check
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
