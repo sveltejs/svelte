36f98aca655fa3249c1b1d66eeb51bc886621ce2
gtmnayan <gtmnayan@gmail.com>
2023-05-26 19:22:24 +0545

    document script

--- scripts/compile-test.js
+++ scripts/compile-test.js
@@ -1,3 +1,6 @@
+// Compile all Svelte files in a directory to JS and CSS files
+// Usage: node scripts/compile-test.js <directory>
+
 import { mkdirSync, readFileSync, writeFileSync } from 'fs';
 import path from 'path';
 import glob from 'tiny-glob/sync.js';
@@ -12,17 +15,26 @@ const options = [
 ];
 for (const file of glob('**/*.svelte', { cwd })) {
 	const contents = readFileSync(`${cwd}/${file}`, 'utf-8').replace(/\r/g, '');
-
+	let w;
 	for (const [name, opts] of options) {
 		const dir = `${cwd}/_output/${name}`;
 
-		const { js, css } = compile(contents, {
+		const { js, css, warnings } = compile(contents, {
 			...opts,
 			filename: file
 		});
 
+		if (warnings.length) {
+            w = warnings;
+        }
+
 		mkdirSync(dir, { recursive: true });
 		js.code && writeFileSync(`${dir}/${file.replace(/\.svelte$/, '.js')}`, js.code);
 		css.code && writeFileSync(`${dir}/${file.replace(/\.svelte$/, '.css')}`, css.code);
+	}
+
+	if (w) {
+		console.log(`Warnings for ${file}:`);
+		console.log(w);
 	}
 }
