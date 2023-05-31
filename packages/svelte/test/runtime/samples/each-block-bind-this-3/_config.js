cadd3d6f1be7039024a84da5515d0dff73295845
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-05-31 10:59:38 -0400

    Linting fixes

--- packages/svelte/test/runtime/samples/each-block-bind-this-3/_config.js
+++ packages/svelte/test/runtime/samples/each-block-bind-this-3/_config.js
@@ -3,7 +3,7 @@ export default {
     <p>10</p><p>20</p><p>30</p>
 	`,
 
-	async test({ assert, window, component, target }) {
+	async test({ assert, component, target }) {
 		const pArray = target.querySelectorAll('p');
 		assert.equal(component.bindings[10], pArray[0]);
 		assert.equal(component.bindings[20], pArray[1]);
--- packages/svelte/test/runtime/samples/each-block-bind-this-5/_config.js
+++ packages/svelte/test/runtime/samples/each-block-bind-this-5/_config.js
@@ -3,7 +3,7 @@ export default {
     <p>0</p><p>1</p><p>2</p><p>3</p><p>4</p><p>5</p><p>6</p><p>7</p><p>8</p><p>9</p>
 	`,
 
-	async test({ assert, window, component, target }) {
+	async test({ assert, component, target }) {
 		const pArray = target.querySelectorAll('p');
     for (let i = 0; i <= 9; i++) {
 		  assert.equal(component.bindings[i], pArray[i]);
