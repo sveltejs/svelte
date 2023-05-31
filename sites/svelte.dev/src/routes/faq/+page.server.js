88de4d805673398dbf8aca03d1a924743e102c79
Rich Harris <git@rich-harris.dev>
2023-05-31 09:30:18 -0400

    move FAQ to /docs/faq

--- sites/svelte.dev/src/routes/faq/+page.server.js
+++
@@ -1,7 +0,0 @@
-import { get_parsed_faq, get_faq_data } from '$lib/server/faq/index.js';
-
-export const prerender = true;
-
-export async function load() {
-	return { faq: get_parsed_faq(get_faq_data()) };
-}
