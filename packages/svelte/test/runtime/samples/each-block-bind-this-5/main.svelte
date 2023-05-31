e9de5f3f81a5efe91e593da3bd2db38d92f76cde
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-05-31 09:22:19 -0400

    Edit test 5

--- packages/svelte/test/runtime/samples/each-block-bind-this-5/main.svelte
+++ packages/svelte/test/runtime/samples/each-block-bind-this-5/main.svelte
@@ -1,13 +1,16 @@
 <script>
-	const set = new Set();
-  set.add(new Set([...'0123'].map(i => ([i]))));
-  set.add(new Set([...'456'].map(i => ([i]))));
-  set.add(new Set([...'789'].map(i => ([i]))));
+	const setFunc = () => {
+    const set = new Set();
+    set.add(new Set([...'0123'].map(i => ([i]))));
+    set.add(new Set([...'456'].map(i => ([i]))));
+    set.add(new Set([...'789'].map(i => ([i]))));
+    return set;
+  }
 
   export let bindings = [];
 </script>
 
-{#each set as newSet}
+{#each setFunc() as newSet}
   {#each newSet as piece}
     <p bind:this={bindings[piece[0]]}>{piece[0]}</p>
   {/each}
