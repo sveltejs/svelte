ef8db22df14aaf401424d74a8f21bc441b5ae536
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-06-01 13:08:07 -0700

    Update packages/svelte/test/version/version.test.js

--- packages/svelte/test/version/version.test.js
+++ packages/svelte/test/version/version.test.js
@@ -6,11 +6,11 @@ import { try_load_json } from '../helpers.js';
 describe('svelte/compiler VERSION', () => {
 	it('should be the exact version from package.json');
 	const pkg = try_load_json(
-		fileURLToPath(new URL(`../../../../packages/svelte/package.json`, import.meta.url))
+		fileURLToPath(new URL('../../../../packages/svelte/package.json', import.meta.url))
 	);
 	assert.equal(
 		VERSION,
 		pkg.version,
-		`VERSION export in src/shared/version.js does not equal version in package.json`
+		'VERSION export in src/shared/version.js does not equal version in package.json'
 	);
 });
