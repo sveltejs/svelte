64e798768881cb5dcc9b457b7d0161c214bc7414
gtmnayan <gtmnayan@gmail.com>
2023-06-11 21:41:14 +0545

    fix:backwards compatibility for types

--- packages/svelte/scripts/generate-dts.js
+++ packages/svelte/scripts/generate-dts.js
@@ -1,11 +1,26 @@
 import { createBundle } from 'dts-buddy';
 
+fs.mkdirSync('types/faux', { recursive: true });
+fs.readdirSync('src/runtime', { withFileTypes: true })
+	.filter((dirent) => dirent.isDirectory())
+	.forEach((dirent) =>
+		fs.writeFileSync(`types/faux/${dirent.name}.d.ts`, `import '../index.d.ts';`)
+	);
+
+fs.writeFileSync('types/faux/index.d.ts', `import '../index.d.ts';`);
+fs.writeFileSync('types/faux/compiler.d.ts', `import '../index.d.ts';`);
+
+// TODO: some way to mark these as deprecated
+fs.writeFileSync('types/faux/types/compiler/preprocess.d.ts', `import '../index.d.ts';`);
+fs.writeFileSync('types/faux/types/compiler/interfaces.d.ts', `import '../index.d.ts';`);
+
 await createBundle({
 	output: 'types/index.d.ts',
 	modules: {
 		svelte: 'src/runtime/public.d.ts',
 		'svelte/compiler': 'src/compiler/public.d.ts',
 		'svelte/types/compiler/preprocess': 'src/compiler/preprocess/public.d.ts',
+		'svelte/types/compiler/interfaces': 'src/compiler/interfaces.d.ts',
 		'svelte/action': 'src/runtime/action/public.d.ts',
 		'svelte/animate': 'src/runtime/animate/public.d.ts',
 		'svelte/easing': 'src/runtime/easing/index.js',
