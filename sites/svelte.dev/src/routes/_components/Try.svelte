9a9f93bca360ed4e9dd646193e4de66b69866b96
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-06-15 12:38:59 -0700

    Update sites/svelte.dev/src/routes/_components/Try.svelte

--- sites/svelte.dev/src/routes/_components/Try.svelte
+++ sites/svelte.dev/src/routes/_components/Try.svelte
@@ -9,7 +9,7 @@
 			<div class="copy">
 				<h2>see for yourself</h2>
 				<div>
-					Try locally, <a target="_blank" rel="noreferrer" href="https://sveltekit.new">on StackBlitz</a>,<br />
+					Try it locally, <a target="_blank" rel="noreferrer" href="https://sveltekit.new">on StackBlitz</a>,<br />
 					or in <a target="_blank" href="https://learn.svelte.dev">the online tutorial</a>.
 				</div>
 			</div>
