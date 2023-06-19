039c06071455ad5b39005f120400f29796b7ca2b
gtmnayan <gtmnayan@gmail.com>
2023-06-19 22:23:54 +0545

    whoops

--- packages/svelte/src/runtime/tag-version.js
+++ packages/svelte/src/runtime/tag-version.js
@@ -1,5 +1,5 @@
 import { VERSION } from '../shared/version';
 
-if (typeof window !== undefined)
+if (typeof window !== "undefined")
 	// @ts-ignore
 	(window.__svelte_versions || (window.__svelte_versions = [])).push(VERSION);
