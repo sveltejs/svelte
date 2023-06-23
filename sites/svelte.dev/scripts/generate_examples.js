75bf96f20559bc550224866e1ee9c181e1ffc600
Simon Holthausen <simon.holthausen@vercel.com>
2023-06-23 16:47:44 +0200

    windows fix

--- sites/svelte.dev/scripts/generate_examples.js
+++ sites/svelte.dev/scripts/generate_examples.js
@@ -1,9 +1,12 @@
 import { get_examples_data } from '../src/lib/server/examples/index.js';
 import fs from 'node:fs';
 
-const examples_data = get_examples_data(
-	new URL('../../../documentation/examples', import.meta.url).pathname
-);
+let examples_path = new URL('../../../documentation/examples', import.meta.url).pathname;
+if (process.platform === 'win32') {
+	examples_path = examples_path.slice(1); // remove leading slash
+}
+
+const examples_data = get_examples_data(examples_path);
 
 try {
 	fs.mkdirSync(new URL('../src/lib/generated/', import.meta.url), { recursive: true });
