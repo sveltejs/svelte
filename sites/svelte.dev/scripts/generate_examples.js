0b99e256bddea4d9e497d23f960e46d5fa6789ab
Simon Holthausen <simon.holthausen@vercel.com>
2023-06-23 16:51:24 +0200

    use fileURLToPath

--- sites/svelte.dev/scripts/generate_examples.js
+++ sites/svelte.dev/scripts/generate_examples.js
@@ -1,12 +1,10 @@
+import { fileURLToPath } from 'node:url';
 import { get_examples_data } from '../src/lib/server/examples/index.js';
 import fs from 'node:fs';
 
-let examples_path = new URL('../../../documentation/examples', import.meta.url).pathname;
-if (process.platform === 'win32') {
-	examples_path = examples_path.slice(1); // remove leading slash
-}
-
-const examples_data = get_examples_data(examples_path);
+const examples_data = get_examples_data(
+	fileURLToPath(new URL('../../../documentation/examples', import.meta.url))
+);
 
 try {
 	fs.mkdirSync(new URL('../src/lib/generated/', import.meta.url), { recursive: true });
