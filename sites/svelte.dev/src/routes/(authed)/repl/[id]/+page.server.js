6e430df1cbbb276457972867900362b5b6c45c36
Puru Vijay <devpuruvj@gmail.com>
2023-07-06 01:04:32 +0530

    Remote debugging

--- sites/svelte.dev/src/routes/(authed)/repl/[id]/+page.server.js
+++ sites/svelte.dev/src/routes/(authed)/repl/[id]/+page.server.js
@@ -3,11 +3,16 @@ import { error } from '@sveltejs/kit';
 export async function load({ fetch, params, url }) {
 	const res = await fetch(`/repl/${params.id}.json`);
 
+	console.log(1, res);
+
 	if (!res.ok) {
 		throw error(res.status);
 	}
 
+	console.log(2);
+
 	const gist = await res.json();
+	console.log(3, gist);
 
 	return {
 		gist,
