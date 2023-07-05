c21eee63d8f65e8f7c609aabaff3b7e9c55db38f
Puru Vijay <devpuruvj@gmail.com>
2023-07-06 01:20:52 +0530

    url

--- sites/svelte.dev/src/routes/(authed)/repl/[id]/+page.server.js
+++ sites/svelte.dev/src/routes/(authed)/repl/[id]/+page.server.js
@@ -3,7 +3,7 @@ import { error } from '@sveltejs/kit';
 export async function load({ fetch, params, url }) {
 	const res = await fetch(`/repl/${params.id}.json`);
 
-	console.log(1, (await res.text()).slice(4000));
+	console.log(1, res.url);
 
 	if (!res.ok) {
 		throw error(res.status);
