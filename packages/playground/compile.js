bd65818602daa5fc360a6a2e607f9d1715cde4a2
gtmnayan <50981692+gtm-nayan@users.noreply.github.com>
2023-07-10 08:46:16 +0545

    Update packages/playground/compile.js
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- packages/playground/compile.js
+++ packages/playground/compile.js
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from 'node:fs';
 import { compile } from '../svelte/src/compiler/index.js';
 
 const code = readFileSync('src/App.svelte', 'utf8');
