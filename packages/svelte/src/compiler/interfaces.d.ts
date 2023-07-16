40e0b5e3e9c8c682dda80a24309b8d111cd07d5a
Romain l'Ourson <rcrestey@gmail.com>
2023-07-16 22:27:49 +0200

    docs: 04-compiler (#26)
    * docs: 04-compiler
    
    * docs: 04-rest
    
    ---------
    
    Co-authored-by: Romain Crestey <romain.crestey@radiofrance.com>

--- packages/svelte/src/compiler/interfaces.d.ts
+++ packages/svelte/src/compiler/interfaces.d.ts
@@ -170,182 +170,188 @@ export type CssHashGetter = (args: {
 
 export interface CompileOptions {
 	/**
-	 * Sets the name of the resulting JavaScript class (though the compiler will rename it if it would otherwise conflict with other variables in scope).
-	 * It will normally be inferred from `filename`
+	 * définit le nom de la classe JavaScript généré (bien que le compilateur la renomera si son nom entre en conflit avec une autre variable).
+	 * Il est normalement inféré à partir l'option `filename`.
 	 *
 	 * @default 'Component'
 	 */
 	name?: string;
 
 	/**
-	 * Used for debugging hints and sourcemaps. Your bundler plugin will set it automatically.
+	 * utilisée pour les conseils de déboggage et les <span class="vo">[sourcemaps](/docs/web#sourcemap)</span>.
+	 * Le <span class="vo">[plugin](/docs/development#plugin)</span> du <span class="vo">[bundler](/docs/web#bundler-packager)</span> la définit automatiquement.
 	 *
 	 * @default null
 	 */
 	filename?: string;
 
 	/**
-	 * If `"dom"`, Svelte emits a JavaScript class for mounting to the DOM.
-	 * If `"ssr"`, Svelte emits an object with a `render` method suitable for server-side rendering.
-	 * If `false`, no JavaScript or CSS is returned; just metadata.
+	 * Si `"dom"`: Svelte émet une classe JavaScript pour monter le composant dans le <span class="vo">[DOM](/docs/web#dom)</span>.
+	 *
+	 * Si `"ssr"`: Svelte émet un objet avec une méthode `render`, appropriée pour les rendus côté serveur.
+	 *
+	 * Si `false`: aucun JavaScript ni CSS n'est émis, seules les <span class="vo">[metadata](/docs/web#metadata)</span> sont retournées.
 	 *
 	 * @default 'dom'
 	 */
 	generate?: 'dom' | 'ssr' | false;
 
 	/**
-	 * If `"throw"`, Svelte throws when a compilation error occurred.
-	 * If `"warn"`, Svelte will treat errors as warnings and add them to the warning report.
+	 * Si `"throw"`: Svelte lève une exception lorsqu'il rencontre une erreur de compilation.
+	 *
+	 * Si `"warn"`: Svelte traitera les erreurs comme des <span class="vo">[warnings](/docs/development#warning)</span> et les ajoutera au rapport de <span class="vo">[warnings](/docs/development#warning)</span>.
 	 *
 	 * @default 'throw'
 	 */
 	errorMode?: 'throw' | 'warn';
 
 	/**
-	 * If `"strict"`, Svelte returns a variables report with only variables that are not globals nor internals.
-	 * If `"full"`, Svelte returns a variables report with all detected variables.
-	 * If `false`, no variables report is returned.
+	 * Si `"strict"`: Svelte retourne un rapport de variables ne contenant que celles qui ne sont ni globales (_globals_) ni internes (_internals_).
+	 *
+	 * Si `"full"`: Svelte retourne un rapport de variables avec toutes les variables détectées.
+	 *
+	 * Si `false`: aucun rapport n'est retourné.
 	 *
 	 * @default 'strict'
 	 */
 	varsReport?: 'full' | 'strict' | false;
 
 	/**
-	 * An initial sourcemap that will be merged into the final output sourcemap.
-	 * This is usually the preprocessor sourcemap.
+	 * Une <span class="vo">[sourcemap](/docs/web#sourcemap)</span> initiale qui sera fusionnée dans la sourcemap finale.
+	 * C'est souvent la sourcemap du pré-processeur.
 	 *
 	 * @default null
 	 */
 	sourcemap?: object | string;
 
 	/**
-	 * If `true`, Svelte generate sourcemaps for components.
-	 * Use an object with `js` or `css` for more granular control of sourcemap generation.
+	 * Si `true`, Svelte génère des <span class="vo">[sourcemaps](/docs/web#sourcemap)</span> pour les composants.
+	 * Utilisez un objet avec `js` ou `css` pour un contrôle plus fin de la génération des sourcemaps. Par défaut, l'option est à `true`.
 	 *
 	 * @default true
 	 */
 	enableSourcemap?: EnableSourcemap;
 
 	/**
-	 * Used for your JavaScript sourcemap.
+	 * Nom de fichier utilisé pour les <span class="vo">[sourcemaps](/docs/web#sourcemap)</span> JavaScript.
 	 *
 	 * @default null
 	 */
 	outputFilename?: string;
 
 	/**
-	 * Used for your CSS sourcemap.
+	 * Nom de fichier utilisé pour les <span class="vo">[sourcemaps](/docs/web#sourcemap)</span> CSS.
 	 *
 	 * @default null
 	 */
 	cssOutputFilename?: string;
 
 	/**
-	 * The location of the `svelte` package.
-	 * Any imports from `svelte` or `svelte/[module]` will be modified accordingly.
+	 * L'emplacement de la librairie `svelte`.
+	 * Tous les imports à `svelte` ou `svelte/[module]` seront modifiés en conséquence.
 	 *
 	 * @default 'svelte'
 	 */
 	sveltePath?: string;
 
 	/**
-	 * If `true`, causes extra code to be added to components that will perform runtime checks and provide debugging information during development.
+	 * If `true`, entraîne l'ajout de code supplémentaire dans les composants pour effectuer des vérifications à <span class="vo">[runtime](/docs/development#runtime)</span> et fournir des informations de déboggage pendant les développements.
 	 *
 	 * @default false
 	 */
 	dev?: boolean;
 
 	/**
-	 * If `true`, getters and setters will be created for the component's props. If `false`, they will only be created for readonly exported values (i.e. those declared with `const`, `class` and `function`). If compiling with `customElement: true` this option defaults to `true`.
+	 * Si `true`, des <span class="vo">[getters et setters](/docs/development#getter-setter)</span> seront générés pour les <span class="vo">[props](/docs/sveltejs#props)</span> des composants. Si `false`, ils ne seront créés que pour les valeurs exportées en lecture seules (c'est-à-dire celles déclarées avec `const`, `class` et `function`). Activer l'option de compilation `customElement: true` changera la valeur par défaut de `accessors` à `true`.
 	 *
 	 * @default false
 	 */
 	accessors?: boolean;
 
 	/**
-	 * If `true`, tells the compiler that you promise not to mutate any objects.
-	 * This allows it to be less conservative about checking whether values have changed.
+	 * Si `true`, indique au compilateur que vous vous engagez à ne pas muter d'objets.
+	 * Cela permet au compilateur d'être moins conservatif lorsqu'il vérifie si une valeur a changé.
 	 *
 	 * @default false
 	 */
 	immutable?: boolean;
 
 	/**
-	 * If `true` when generating DOM code, enables the `hydrate: true` runtime option, which allows a component to upgrade existing DOM rather than creating new DOM from scratch.
-	 * When generating SSR code, this adds markers to `<head>` elements so that hydration knows which to replace.
+	 * Si `true`, lors de la génération du code <span class="vo">[DOM](/docs/web#dom)</span>, autorise l'option de <span class="vo">[runtime](/docs/development#runtime)</span> `hydrate: true`, qui permet à un composant de mettre à jour le DOM existant sans avoir à créer un nouveau noeud.
+	 * Lors de la génération de code côté serveur (<span class="vo">[SSR](/docs/web#server-side-rendering)</span>), cela ajoute des marqueurs à la section `<head>` pour identifier quels éléments hydrater.
 	 *
 	 * @default false
 	 */
 	hydratable?: boolean;
 
 	/**
-	 * If `true`, generates code that will work in IE9 and IE10, which don't support things like `element.dataset`.
+	 * Si `true`, génère du code compatible avec IE9 et IE10, navigateurs qui ne supportent pas par exemple : `element.dataset`.
 	 *
 	 * @default false
 	 */
 	legacy?: boolean;
 
 	/**
-	 * If `true`, tells the compiler to generate a custom element constructor instead of a regular Svelte component.
+	 * Si `true`, indique au compilateur de générer un constructeur de <span class="vo">[custom element](/docs/web#web-component)</span> à la place d'un composant Svelte traditionnel.
 	 *
 	 * @default false
 	 */
 	customElement?: boolean;
 
 	/**
-	 * A `string` that tells Svelte what tag name to register the custom element with.
-	 * It must be a lowercase alphanumeric string with at least one hyphen, e.g. `"my-element"`.
+	 * Une `string` qui indique à Svelte le nom à donner au <span class="vo">[custom element](/docs/web#web-component)</span>.
+	 * Ce doit être une chaîne composée de caractères alphanumériques avec au moins un tiret, par exemple `"mon-element"`.
 	 *
 	 * @default null
 	 */
 	tag?: string;
 
 	/**
-	 * - `'injected'` (formerly `true`), styles will be included in the JavaScript class and injected at runtime for the components actually rendered.
-	 * - `'external'` (formerly `false`), the CSS will be returned in the `css` field of the compilation result. Most Svelte bundler plugins will set this to `'external'` and use the CSS that is statically generated for better performance, as it will result in smaller JavaScript bundles and the output can be served as cacheable `.css` files.
-	 * - `'none'`, styles are completely avoided and no CSS output is generated.
+	 * - `'injected'` (anciennement `true`): le style sera inclus dans les classes JavaScript et injecté au <span class="vo">[runtime](/docs/development#runtime)</span> pour les composants réellement rendus.
+	 * - `'external'` (anciennement `false`): le style sera renvoyé dans la propriété `css` du résultat de la compilation. La plupart des <span class="vo">[plugins](/docs/development#plugin)</span> Svelte de <span class="vo">[bundler](/docs/web#bundler-packager)</span> définiront cette option à `'external'` et utiliseront le CSS généré statiquement. Cela permet d'atteindre de meilleures performances, puisque les <span class="vo">[bundles](/docs/web#bundler-packager)</span> JavaScript seront plus petits et le style généré sous forme de fichiers `.css` pourra être mis en cache.
+	 * - `'none'`: le style sera complètement ignoré et aucun CSS ne sera généré.
 	 */
 	css?: 'injected' | 'external' | 'none' | boolean;
 
 	/**
-	 * A `number` that tells Svelte to break the loop if it blocks the thread for more than `loopGuardTimeout` ms.
-	 * This is useful to prevent infinite loops.
-	 * **Only available when `dev: true`**.
+	 * Un `number` qui indique à Svelte d'arrêter une boucle si elle bloque le <span class="vo">[thread](/docs/development#thread)</span> durant plus de `loopGuardTimeout` ms. Utile pour éviter les boucles infinies.
+	 * **Uniquement disponible lorsque `dev: true`**
 	 *
 	 * @default 0
 	 */
 	loopGuardTimeout?: number;
 
 	/**
-	 * The namespace of the element; e.g., `"mathml"`, `"svg"`, `"foreign"`.
+	 * Le <span class="vo">[namespace](/docs/development#namespace)</span> de l'élément; par exemple, `"mathml"`, `"svg"`, `"foreign"`.
 	 *
 	 * @default 'html'
 	 */
 	namespace?: string;
 
 	/**
-	 * A function that takes a `{ hash, css, name, filename }` argument and returns the string that is used as a classname for scoped CSS.
-	 * It defaults to returning `svelte-${hash(css)}`.
+	 * Une fonction qui prend comme arguments `{ hash, css, name, filename }` et retourne un nom de classe utilisé pour le css <span class="vo">[scopé](/docs/development#scope)</span>.
+	 * La fonction par défaut retourne `svelte-${hash(css)}`.
 	 *
 	 * @default undefined
 	 */
 	cssHash?: CssHashGetter;
 
 	/**
-	 * If `true`, your HTML comments will be preserved during server-side rendering. By default, they are stripped out.
+	 * Si `true`, les commentaires HTML seront conservés au cours du rendu côté serveur.
+	 * Par défault, ils sont supprimés.
 	 *
 	 * @default false
 	 */
 	preserveComments?: boolean;
 
 	/**
-	 *  If `true`, whitespace inside and between elements is kept as you typed it, rather than removed or collapsed to a single space where possible.
+	 *  Si `true`, les caractères blancs (espaces, tabulations, ...) seront gardés tels quels, plutôt que supprimés ou fusionnés lorsque c'est possible.
 	 *
 	 * @default false
 	 */
 	preserveWhitespace?: boolean;
 	/**
-	 *  If `true`, exposes the Svelte major version on the global `window` object in the browser.
+	 *  Si `true`, expose la version majeure de Svelte dans l'objet global `window` du navigateur.
 	 *
 	 * @default true
 	 */
@@ -370,21 +376,21 @@ export interface AppendTarget {
 
 export interface Var {
 	name: string;
-	/** the `bar` in `export { foo as bar }` or `export let bar` */
+	/** le `bar` dans `export { foo as bar }` ou `export let bar` */
 	export_name?: string;
-	/** true if assigned a boolean default value (`export let foo = true`) */
+	/** `true` si une valeur par défaut booléenne lui est assignée (`export let foo = true`) */
 	is_boolean?: boolean;
 	injected?: boolean;
 	module?: boolean;
 	mutated?: boolean;
 	reassigned?: boolean;
-	referenced?: boolean; // referenced from template scope
-	referenced_from_script?: boolean; // referenced from script
+	referenced?: boolean; // référencé dans le contexte du <span class="vo">[template](/docs/development#template)</span>
+	referenced_from_script?: boolean; // référencé dans le script
 	writable?: boolean;
 
-	// used internally, but not exposed
+	// utilisé en interne, mais pas exposé
 	global?: boolean;
-	internal?: boolean; // event handlers, bindings
+	internal?: boolean; // gestionnaires d'évènement, <span class="vo">[bindings](/docs/sveltejs#bindings)</span>
 	initialised?: boolean;
 	hoistable?: boolean;
 	subscribable?: boolean;
@@ -397,30 +403,31 @@ export interface CssResult {
 	map: SourceMap;
 }
 
-/** The returned shape of `compile` from `svelte/compiler` */
+/** La forme de ce que renvoie `compile` du `svelte/compiler` */
 export interface CompileResult {
-	/** The resulting JavaScript code from compling the component */
+	/** Le code JavaScript généré après compilation du composant */
 	js: {
-		/** Code as a string */
+		/** Code en tant que `string` */
 		code: string;
-		/** A source map */
+		/** Une <span class="vo">[sourcemap](/docs/web#sourcemap)</span> */
 		map: any;
 	};
-	/** The resulting CSS code from compling the component */
+	/** Le code CSS généré après compilation du composant */
 	css: CssResult;
-	/** The abstract syntax tree representing the structure of the component */
+	/** L'arbre <span class="vo">[AST](/docs/development#ast)</span> représentant la structure du composant */
 	ast: Ast;
 	/**
-	 * An array of warning objects that were generated during compilation. Each warning has several properties:
-	 * - code is a string identifying the category of warning
-	 * - message describes the issue in human-readable terms
-	 * - start and end, if the warning relates to a specific location, are objects with line, column and character properties
-	 * - frame, if applicable, is a string highlighting the offending code with line numbers
+	 * Un tableau de <span class="vo">[warnings](/docs/development#warning)</span> générés pendant la compilation. Chaque <span class="vo">[warning](/docs/development#warning)</span> possède les attributs suivants :
+	 * - `code` : une `string` identifiant la catégorie du <span class="vo">[warning](/docs/development#warning)</span>
+	 * - `message` : décrit le problème de manière intelligible
+	 * - `start` et `end` : si le <span class="vo">[warning](/docs/development#warning)</span> est relevé à un endroit particulier, ce sont des objets avec les propriétés `line`, `column` et `character`
+	 * - `frame` : si pertinent, correspond à du texte précisant l'emplacement du code concerné, avec le numéro de ligne
 	 * */
 	warnings: Warning[];
-	/** An array of the component's declarations used by tooling in the ecosystem (like our ESLint plugin) to infer more information */
+
+	/** Un tableau reprenant les objets déclarés dans le composant et utilisés par l'écosystème (comme le <span class="vo">[plugin](/docs/development#plugin)</span> Svelte ESLint) pour inféré plus d'informations */
 	vars: Var[];
-	/** An object used by the Svelte developer team for diagnosing the compiler. Avoid relying on it to stay the same! */
+	/** Objet utilisé par l'équipe de développement de Svelte pour diagnostiquer le compilateur. Évitez de vous en servir car il pourrait changer à tout moment ! */
 	stats: {
 		timings: {
 			total: number;
