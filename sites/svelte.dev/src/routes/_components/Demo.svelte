abf3370518f5a13df54532ceb12168a949f10e87
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-08-19 09:44:41 -0700

    site: remove unused font files

--- sites/svelte.dev/src/routes/_components/Demo.svelte
+++ sites/svelte.dev/src/routes/_components/Demo.svelte
@@ -31,10 +31,6 @@
 	let selected = examples[0];
 </script>
 
-<svelte:head>
-	<link rel="prefetch" href="/fonts/overpass/overpass-latin-400.woff2" />
-</svelte:head>
-
 <Section --background="var(--sk-back-2)">
 	<h3>build with ease</h3>
 
