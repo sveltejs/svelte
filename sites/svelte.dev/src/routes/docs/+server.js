90da4f2feebd99c25e9f7618a7158de68c7185bb
gtmnayan <50981692+gtm-nayan@users.noreply.github.com>
2023-08-19 08:12:46 +0545

    Update sites/svelte.dev/src/routes/docs/+server.js
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- sites/svelte.dev/src/routes/docs/+server.js
+++ sites/svelte.dev/src/routes/docs/+server.js
@@ -1,7 +1,7 @@
 // @ts-expect-error custom suffix doesn't have types
 import js from 'minified-raw:./redirect.js';
 
-// prerenderer will choke otherwise
+// avoid outputting a file named "docs" that would conflict with prerendered "docs" directory
 export const prerender = false;
 
 export function GET() {
