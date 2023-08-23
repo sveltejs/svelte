b31abb4072323d20eaa75fdfaed251bb31ab09f8
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-08-22 20:23:59 -0700

    remove config

--- sites/svelte.dev/src/routes/(authed)/+layout.server.js
+++ sites/svelte.dev/src/routes/(authed)/+layout.server.js
@@ -1,11 +1,5 @@
 import * as session from '$lib/db/session';
 
-/** @type {import('@sveltejs/adapter-vercel').Config} */
-export const config = {
-	regions: ['iad1'], // our supabase instance is in us-east-1 (iad1)
-	runtime: 'edge'
-};
-
 export async function load({ request }) {
 	return {
 		user: session.from_cookie(request.headers.get('cookie'))
