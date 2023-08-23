52f0e9c0e4b6568af77913a38eb287671ff7d3ba
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-08-22 21:04:45 -0700

    deploy only repl as serverless function

--- sites/svelte.dev/src/routes/(authed)/+layout.server.js
+++ sites/svelte.dev/src/routes/(authed)/+layout.server.js
@@ -1,5 +1,10 @@
 import * as session from '$lib/db/session';
 
+/** @type {import('@sveltejs/adapter-vercel').Config} */
+export const config = {
+  runtime: 'nodejs18.x'
+};
+
 export async function load({ request }) {
 	return {
 		user: session.from_cookie(request.headers.get('cookie'))
