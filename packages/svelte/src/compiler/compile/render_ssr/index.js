d5839ef1949b67f9a4a875daac869bc5cfd3458e
gtmnayan <50981692+gtm-nayan@users.noreply.github.com>
2023-08-28 22:38:27 +0545

    fix: head duplication when binding is present (#9124)
    fixes #7879
    fixes #4982

--- packages/svelte/src/compiler/compile/render_ssr/index.js
+++ packages/svelte/src/compiler/compile/render_ssr/index.js
@@ -144,9 +144,13 @@ export default function ssr(component, options) {
 		? b`
 			let $$settled;
 			let $$rendered;
+			let #previous_head = $$result.head;
 
 			do {
 				$$settled = true;
+				// $$result.head is mutated by the literal expression
+				// need to reset it if we're looping back to prevent duplication
+				$$result.head = #previous_head;
 
 				${reactive_declarations}
 
---
+++ packages/svelte/test/server-side-rendering/samples/head-no-duplicates-with-binding/main.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Foo from './Foo.svelte';
+	let bar;
+</script>
+
+<svelte:head>
+	<link rel="canonical" href="/test" />
+	<meta name="description" content="test" />
+</svelte:head>
+
+<Foo bind:bar />
