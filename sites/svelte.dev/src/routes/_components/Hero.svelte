754fa717f86afbae4a118d87249ae1ea8c48932a
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-08-29 15:43:07 -0700

    contrast fix

--- sites/svelte.dev/src/routes/_components/Hero.svelte
+++ sites/svelte.dev/src/routes/_components/Hero.svelte
@@ -96,7 +96,6 @@
 		border-radius: var(--sk-border-radius);
 		box-shadow: 0px 6px 14px rgba(0, 0, 0, 0.08);
 		color: #fff;
-		color: color-mix(in hwb, hsl(var(--sk-theme-1-hsl)) 10%, var(--sk-back-1) 95%);
 		transition: 0.5s var(--quint-out);
 		transition-property: box-shadow, color;
 	}
