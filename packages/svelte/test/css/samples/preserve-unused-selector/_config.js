ebd3e43c89e6e0338fa286a5c57ceeb4dca94b2c
Maytham <44939173+Maytha8@users.noreply.github.com>
2023-09-10 16:14:44 +0800

    chore: add test for preserveUnusedSelector compile option

---
+++ packages/svelte/test/css/samples/preserve-unused-selector/_config.js
@@ -0,0 +1,30 @@
+export default {
+	compileOptions: {
+		preserveUnusedSelectors: true
+	},
+	warnings: [
+		{
+			filename: 'SvelteComponent.svelte',
+			code: 'css-unused-selector',
+			message: 'Unused CSS selector ".bar"',
+			start: {
+				line: 8,
+				column: 1,
+				character: 60
+			},
+			end: {
+				line: 8,
+				column: 5,
+				character: 64
+			},
+			pos: 60,
+			frame: `
+			 6:   }
+			 7:
+			 8:   .bar {
+			      ^
+			 9:     color: blue;
+			10:   }`
+		}
+	]
+};
