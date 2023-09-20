115ea1ff4afc57766f06bb8d9444ec59d00bc0e1
gtmnayan <50981692+gtm-nayan@users.noreply.github.com>
2023-09-20 13:33:47 +0545

    fix: do not add module declared variables as dependencies (#9122)
    closes #5943

--- packages/svelte/src/compiler/compile/render_dom/wrappers/shared/is_dynamic.js
+++ packages/svelte/src/compiler/compile/render_dom/wrappers/shared/is_dynamic.js
@@ -3,8 +3,11 @@ import { is_reserved_keyword } from '../../../utils/reserved_keywords.js';
 /** @param {import('../../../../interfaces.js').Var} variable */
 export default function is_dynamic(variable) {
 	if (variable) {
-		if (variable.mutated || variable.reassigned) return true; // dynamic internal state
-		if (!variable.module && variable.writable && variable.export_name) return true; // writable props
+		// Only variables declared in the instance script tags should be considered dynamic
+		const is_declared_in_reactive_context = !variable.module && !variable.global;
+
+		if (is_declared_in_reactive_context && (variable.mutated || variable.reassigned)) return true; // dynamic internal state
+		if (is_declared_in_reactive_context && variable.writable && variable.export_name) return true; // writable props
 		if (is_reserved_keyword(variable.name)) return true;
 	}
 	return false;
--- packages/svelte/test/js/samples/reactive-class-optimized/input.svelte
+++ packages/svelte/test/js/samples/reactive-class-optimized/input.svelte
@@ -1,6 +1,6 @@
 <script context="module">
 	let nonReactiveModuleVar = Math.random();
-	let reactiveModuleVar = Math.random();
+	let reassignedModuleVar = Math.random();
 </script>
 
 <script>
@@ -9,22 +9,22 @@
 	nonReactiveGlobal = Math.random();
 	const reactiveConst = {x: Math.random()};
 
-	$: reactiveDeclaration = reactiveModuleVar * 2;
+	$: reactiveDeclaration = reassignedModuleVar * 2;
 
-	reactiveModuleVar += 1;
+	reassignedModuleVar += 1;
 	if (Math.random()) {
 		reactiveConst.x += 1;
 	}
 </script>
 
 <!--These should all get updaters because they have at least one reactive dependency-->
-<div class:update1={reactiveModuleVar}></div>
 <div class:update2={reactiveConst.x}></div>
 <div class:update3={nonReactiveGlobal && reactiveConst.x}></div>
 <div class:update4={$reactiveStoreVal}></div>
 <div class:update5={$reactiveDeclaration}></div>
 
 <!--These shouldn't get updates because they're purely non-reactive-->
+<div class:update1={reassignedModuleVar}></div>
 <div class:static1={nonReactiveModuleVar}></div>
 <div class:static2={nonReactiveGlobal}></div>
 <div class:static3={nonReactiveModuleVar && nonReactiveGlobal}></div>
