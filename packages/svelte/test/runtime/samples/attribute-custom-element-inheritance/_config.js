41ac289c2a407058661bce401440d9bbe18f6365
Simon Holthausen <simon.holthausen@vercel.com>
2023-10-18 13:10:38 +0200

    add ce inheritance test

---
+++ packages/svelte/test/runtime/samples/attribute-custom-element-inheritance/_config.js
@@ -0,0 +1,7 @@
+export default {
+	skip_if_ssr: true,
+	skip_if_hydrate: true,
+	html: `
+		<my-custom-element>Hello World!</my-custom-element>
+	`
+};
---
+++ packages/svelte/test/runtime/samples/attribute-custom-element-inheritance/main.svelte
@@ -0,0 +1,33 @@
+<script>
+	class MyCustomElement extends HTMLElement {
+		constructor() {
+			super();
+			this._obj = null;
+			this._text = null;
+		}
+
+		set text(text) {
+			this._text = text;
+			this.render();
+		}
+
+		set camelCase(obj) {
+			this._obj = obj;
+			this.render();
+		}
+
+		connectedCallback() {
+			this.render();
+		}
+
+		render() {
+			this.innerHTML = 'Hello ' + this._obj.text + this._text;
+		}
+	}
+
+	class Extended extends MyCustomElement {}
+
+	window.customElements.define('my-custom-element', Extended);
+</script>
+
+<my-custom-element camelCase={{ text: 'World' }} text="!" />
