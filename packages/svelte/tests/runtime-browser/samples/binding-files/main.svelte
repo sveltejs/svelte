0c10b3c0196e3a73a88901b1fc41c78bd8f6410a
Tee Ming <chewteeming01@gmail.com>
2023-11-15 19:02:11 +0800

    allow setting files

---
+++ packages/svelte/tests/runtime-browser/samples/binding-files/main.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { onMount } from "svelte";
+
+  let files;
+
+  onMount(() => {
+		let list = new DataTransfer();
+		let file = new File(["content"], "filename.jpg");
+		list.items.add(file);
+		files = list.files;
+	})
+</script>
+
+<input type="file" bind:files />
+
+<button onclick={() => files = new DataTransfer().files}>Reset</button>
