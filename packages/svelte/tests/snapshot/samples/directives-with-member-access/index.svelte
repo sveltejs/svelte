6f3dc04c824e7ae6e5a2b0524a743d25a8baa496
Paolo Ricciuti <ricciutipaolo@gmail.com>
2023-11-15 12:53:23 +0100

    fix: allow member access on directives (#9462)
    fixes #9445

---
+++ packages/svelte/tests/snapshot/samples/directives-with-member-access/index.svelte
@@ -0,0 +1,40 @@
+<script>
+	const one = ()=>{};
+	const nested = {one, "with-string": one};
+	const evenmore = {nested};
+</script>
+
+<div use:one />
+<div use:nested.one />
+<div use:evenmore.nested.one />
+
+<div transition:one />
+<div transition:nested.one />
+<div transition:evenmore.nested.one />
+
+<div animate:one />
+<div animate:nested.one />
+<div animate:evenmore.nested.one />
+
+<div in:one />
+<div in:nested.one />
+<div in:evenmore.nested.one />
+
+<div out:one />
+<div out:nested.one />
+<div out:evenmore.nested.one />
+
+<div use:nested.with-string />
+<div use:evenmore.nested.with-string />
+
+<div transition:nested.with-string />
+<div transition:evenmore.nested.with-string />
+
+<div animate:nested.with-string />
+<div animate:evenmore.nested.with-string />
+
+<div in:nested.with-string />
+<div in:evenmore.nested.with-string />
+
+<div out:nested.with-string />
+<div out:evenmore.nested.with-string />
