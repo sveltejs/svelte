44783968cfdd1acfb9730209f85704c43c0bc907
Romain Crestey <rcrestey@gmail.com>
2023-11-16 18:51:23 +0100

    fix: discord invite

--- sites/svelte.dev/src/routes/chat/+server.js
+++ sites/svelte.dev/src/routes/chat/+server.js
@@ -1,6 +1,6 @@
 export function GET() {
 	return new Response(undefined, {
 		status: 302,
-		headers: { Location: 'https://discord.gg/D6Dzc5m3' }
+		headers: { Location: 'https://discord.gg/cJZUCUxMYZ' }
 	});
 }
