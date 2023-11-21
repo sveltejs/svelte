1003acccc4a7446dc84d287038fe9c8d0bc12f40
Simon H <5968653+dummdidumm@users.noreply.github.com>
2023-11-21 16:52:54 +0100

    feat: add Snippet type (#9584)
    * feat: add Snippet type
    
    related to #9447
    
    * one more test

---
+++ packages/svelte/tests/types/snippet.ts
@@ -0,0 +1,40 @@
+import type { Snippet } from 'svelte';
+
+const return_type: ReturnType<Snippet> = null as any;
+
+// @ts-expect-error
+const a: Snippet<{ text: string }> = () => {};
+// @ts-expect-error
+const b: Snippet<boolean> = (a, b) => {
+	return return_type;
+};
+// @ts-expect-error
+const c: Snippet<boolean> = (a: string) => {
+	return return_type;
+};
+// @ts-expect-error
+const d: Snippet<boolean> = (a: string, b: number) => {
+	return return_type;
+};
+// @ts-expect-error
+const e: Snippet = (a: string) => {
+	return return_type;
+};
+const f: Snippet = (a) => {
+	// @ts-expect-error
+	a?.x;
+	return return_type;
+};
+const g: Snippet<boolean> = (a) => {
+	// @ts-expect-error
+	a === '';
+	a === true;
+	return return_type;
+};
+const h: Snippet<{ a: true }> = (a) => {
+	a.a === true;
+	return return_type;
+};
+const i: Snippet = () => {
+	return return_type;
+};
