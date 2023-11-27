51a9c6e765a323c2963a5184eaa9450d5d93fb38
dominikg <dominik.goepel@gmx.de>
2023-11-27 14:26:15 +0100

    chore: update playgrounds/demo to exclude svelte from being optimized and generate an App.svelte so first run doesn't throw

---
+++ playgrounds/demo/scripts/App.template.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	function openInEditor(){
+		fetch('./__open-in-editor?file=src/App.svelte');
+	}
+</script>
+<h1>Demo App</h1>
+<button class="open-in-editor" on:click={openInEditor}>edit App.svelte</button>
