b45cd60198b2f09488faf14b07d898c9d0515727
Rich Harris <rich.harris@vercel.com>
2023-11-28 11:07:10 -0500

    tests

---
+++ packages/svelte/tests/runtime-runes/samples/magic-deep/main.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { magic } from 'svelte';
+
+	const object = magic({
+		foo: {
+			bar: {
+				baz: 1
+			}
+		}
+	});
+</script>
+
+<button onclick={() => object.foo.bar.baz += 1}>
+	{object.foo.bar.baz}
+</button>
+
+<button onclick={() => object.foo.bar = { baz: object.foo.bar.baz * 2 }}>
+	double
+</button>
---
+++ packages/svelte/tests/runtime-runes/samples/magic/main.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { magic } from 'svelte';
+
+	const object = magic({ count: 0 });
+</script>
+
+<button onclick={() => object.count += 1}>
+	clicks: {object.count}
+</button>
