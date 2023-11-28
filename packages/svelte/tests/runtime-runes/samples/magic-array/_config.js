575d0fcfe78381aa7d8aad738925b7c447602c75
Rich Harris <rich.harris@vercel.com>
2023-11-28 11:28:12 -0500

    skipped, partially passing array test

---
+++ packages/svelte/tests/runtime-runes/samples/magic-array/_config.js
@@ -0,0 +1,61 @@
+import { test } from '../../test';
+
+export default test({
+	skip: true, // TODO `array.reverse()` doesn't trigger each block update
+
+	html: `
+		<button>1 + 2 + 3 = 6</button>
+		<button>clear</button>
+		<button>reverse</button>
+		<span>1</span>
+		<span>2</span>
+		<span>3</span>
+		<strong>array[1]: 2</strong>
+	`,
+
+	async test({ assert, target }) {
+		const [add, clear, reverse] = target.querySelectorAll('button');
+
+		await add?.click();
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+				<button>1 + 2 + 3 + 4 = 10</button>
+				<button>clear</button>
+				<button>reverse</button>
+				<span>1</span>
+				<span>2</span>
+				<span>3</span>
+				<span>4</span>
+				<strong>array[1]: 2</strong>
+			`
+		);
+
+		await reverse?.click();
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+				<button>4 + 3 + 2 + 1 = 10</button>
+				<button>clear</button>
+				<button>reverse</button>
+				<span>4</span>
+				<span>3</span>
+				<span>2</span>
+				<span>1</span>
+				<strong>array[1]: 3</strong>
+			`
+		);
+
+		await clear?.click();
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+				<button>4 = 4</button>
+				<button>clear</button>
+				<button>reverse</button>
+				<span>4</span>
+				<strong>array[1]: undefined</strong>
+			`
+		);
+	}
+});
