cb1ea1f2dbfedde5db4468e6f79abcde2dc83438
Simon H <5968653+dummdidumm@users.noreply.github.com>
2023-11-30 11:15:38 +0100

    fix: ensure `$$slots` exists in runes mode (#9718)
    fixes #9683

---
+++ .changeset/flat-melons-protect.md
@@ -0,0 +1,5 @@
+---
+'svelte': patch
+---
+
+fix: ensure `$$slots` exists in runes mode
---
+++ packages/svelte/tests/runtime-runes/samples/slot/_config.js
@@ -0,0 +1,6 @@
+import { test } from '../../test';
+
+// Test that $$slots exists in runes mode
+export default test({
+	html: `<p>bar</p>`
+});
