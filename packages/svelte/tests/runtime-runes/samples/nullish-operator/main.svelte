28ac98e3f04d4bced13f1b34eb40cf9c6a075ae4
Dominic Gannaway <dg@domgan.com>
2023-11-30 13:29:10 +0000

    fix test

--- packages/svelte/tests/runtime-runes/samples/nullish-operator/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/nullish-operator/main.svelte
@@ -3,26 +3,26 @@
 	let b1 = $state();
 
 	const {log} = $props();
-	
+
 	$effect(() => {
 		log.push('a1: ', a1);
 	});
 	$effect(() => {
 		log.push('b1: ', b1);
 	});
-	
+
 	a();
 	queueMicrotask(a);
-	
+
 	b();
 	queueMicrotask(b);
-	
+
 	function a() {
-		a1 ??= {};
+		a1 ??= true;
 	}
-	
+
 	function b() {
-		b1 ?? (b1 = {});
+		b1 ?? (b1 = true);
 	}
 </script>
 
