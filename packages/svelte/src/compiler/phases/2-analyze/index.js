eb86455da840e7275e5023c19ea3fd8e29435c00
Rich Harris <rich.harris@vercel.com>
2023-12-06 12:31:00 -0500

    chore: remove unused check

--- packages/svelte/src/compiler/phases/2-analyze/index.js
+++ packages/svelte/src/compiler/phases/2-analyze/index.js
@@ -409,10 +409,10 @@ export function analyze_component(root, options) {
 		analysis.reactive_statements = order_reactive_statements(analysis.reactive_statements);
 	}
 
-	// warn on any nonstate declarations that are a) reassigned and mutated and b) referenced in the template
+	// warn on any nonstate declarations that are a) reassigned and b) referenced in the template
 	for (const scope of [module.scope, instance.scope]) {
 		outer: for (const [name, binding] of scope.declarations) {
-			if (binding.kind === 'normal' && binding.reassigned && binding.mutated) {
+			if (binding.kind === 'normal' && binding.reassigned) {
 				for (const { path } of binding.references) {
 					if (path[0].type !== 'Fragment') continue;
 					for (let i = 1; i < path.length; i += 1) {
