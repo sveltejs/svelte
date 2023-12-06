ab7d5085d2f093f3c5f50e154088c542a80d860a
Rich Harris <rich.harris@vercel.com>
2023-12-06 18:35:45 -0500

    chore: reuse proxy between objects

---
+++ packages/svelte/tests/runtime-runes/samples/proxy-shared/main.svelte
@@ -0,0 +1,14 @@
+<script>
+	let obj = { count: 0 };
+
+	let a = $state(obj);
+	let b = $state(obj);
+</script>
+
+<button onclick={() => a.count += 1}>
+	{a.count}
+</button>
+
+<button onclick={() => b.count += 1}>
+	{b.count}
+</button>
