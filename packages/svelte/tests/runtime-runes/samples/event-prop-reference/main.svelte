24a30ebb62a458bf272cbb39f3f97b8f682a1403
Dominic Gannaway <dg@domgan.com>
2023-12-07 16:56:00 +0000

    fix: handle event hoisting props referencing

---
+++ packages/svelte/tests/runtime-runes/samples/event-prop-reference/main.svelte
@@ -0,0 +1,13 @@
+<script>
+	import { log } from './log.js';
+
+	let { item } = $props();
+
+	function onclick() {
+		log.push(item?.name);
+	}
+</script>
+
+<button {onclick}>
+	{item?.name}
+</button>
