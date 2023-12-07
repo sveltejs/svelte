540a31c2abde50bf38183af7b9433d0c5bbe1b0d
Dominic Gannaway <dg@domgan.com>
2023-12-07 10:39:24 +0000

    fix: ensure computed props are wrapped in derived

---
+++ packages/svelte/tests/runtime-runes/samples/props-derived-2/Item.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { log } from './log.js';
+
+	const {active} = $props();
+	$effect.pre(() => {
+		log.push('active changed', active)
+	});
+</script>
+
+<p>Item is {active ? 'active' : 'inactive'}</p>
---
+++ packages/svelte/tests/runtime-runes/samples/props-derived-2/main.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Item from './Item.svelte';
+	let activeIndex = $state(0);
+</script>
+
+<input bind:value={activeIndex} type="number" min="0" max={4} />
+<Item active={activeIndex == 0} />
+<Item active={activeIndex == 1} />
+<Item active={activeIndex == 2} />
+<Item active={activeIndex == 3} />
+<Item active={activeIndex == 4} />
