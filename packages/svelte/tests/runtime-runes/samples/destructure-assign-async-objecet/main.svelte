6221f8f9b917c011b0a05debbdac5641adc61e18
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-12-19 19:52:33 -0500

    Add another test for array and object destructuring

---
+++ packages/svelte/tests/runtime-runes/samples/destructure-assign-async-objecet/main.svelte
@@ -0,0 +1,22 @@
+<script>
+	let name = $state("tom");
+	let email = $state("tom@mail.com");
+	let number = $state(555);
+
+	export const fetch_user = () => {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve({ name: "jerry", contacts: [777, "jerry@mail.com"] })
+			}, 100)
+		})
+	}
+
+	const update = async () => {
+		({ name, contacts: [number, email] } = await fetch_user());
+	}
+</script>
+
+<p>Name: {name}</p>
+<p>Email: {email}</p>
+<p>Number: {number}</p>
+<button on:click={update}>Update me</button>
