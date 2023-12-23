0f8b9220b4c532dd537e953adf5152b751e3ba72
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-12-23 01:58:31 -0500

    Format

--- packages/svelte/tests/runtime-runes/samples/destructure-assignment-async-4/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/destructure-assignment-async-4/main.svelte
@@ -4,13 +4,13 @@
     let c = $state(9);
     let d = $state(12);
 
-		let n = 18;
+    let n = 18;
 
     let z = $state(null);
 
     const update = async () => {
-				({ a = (console.log(11), await Promise.resolve(12)) } = {});
-				({ b } = { b: ++n });
+        ({ a = (console.log(11), await Promise.resolve(12)) } = {});
+        ({ b } = { b: ++n });
         [c] = [15];
         ({ d } = { d: ((a, b) => a + b)(1, await Promise.resolve(6)) });
     }
