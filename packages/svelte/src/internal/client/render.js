3756ce5a4fdc66dc67964828b5283d13ed0b96fe
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2023-12-22 22:55:20 -0500

    fix: Forgot to save

--- packages/svelte/src/internal/client/render.js
+++ packages/svelte/src/internal/client/render.js
@@ -2846,13 +2846,13 @@ export function sanitize_slots(props) {
  * @param {(() => any)[]} args
  * @returns {void}
  */
-export function snippet_effect(get_snippet, node, args) {
+export function snippet_effect(get_snippet, node, ...args) {
 	const block = create_snippet_block();
 	render_effect(() => {
 		// Only rerender when the snippet function itself changes,
 		// not when an eagerly-read prop inside the snippet function changes
 		const snippet = get_snippet();
-		untrack(() => snippet(node, args));
+		untrack(() => snippet(node, ...args));
 		return () => {
 			if (block.d !== null) {
 				remove(block.d);
