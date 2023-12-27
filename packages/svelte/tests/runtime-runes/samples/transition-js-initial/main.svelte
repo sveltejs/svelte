8a013c4cc637f672aa841bd60bc5a571c56cf989
Holger Dal Mogensen <61235930+sockmaster27@users.noreply.github.com>
2023-12-27 17:31:28 +0100

    fix: transition parameters are not reactive (#9836)
    * test: add tests of transitions in new runtime
    
    * fix: move evaluation of props
    
    * format
    
    * add changeset

---
+++ packages/svelte/tests/runtime-runes/samples/transition-js-initial/main.svelte
@@ -0,0 +1,16 @@
+<script>
+	const { visible = false } = $props();
+
+	function foo(node, params) {
+		return {
+			duration: 100,
+			tick: t => {
+				node.foo = t;
+			}
+		};
+	}
+</script>
+
+{#if visible}
+	<div transition:foo></div>
+{/if}
