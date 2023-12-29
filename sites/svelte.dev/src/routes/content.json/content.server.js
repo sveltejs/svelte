fee31c3894794a96f51c57b7d55f822386c201ea
Puru <awesomepuruvj@gmail.com>
2023-12-29 10:18:18 +0530

    Take in all sections

--- sites/svelte.dev/src/routes/content.json/content.server.js
+++ sites/svelte.dev/src/routes/content.json/content.server.js
@@ -48,22 +48,12 @@ export async function content() {
 		const intro = sections?.shift()?.trim();
 		const rank = +metadata.rank;
 
-		// Push the main page into it
 		blocks.push({
 			breadcrumbs: [...breadcrumbs, removeMarkdown(metadata.title ?? '')],
 			href: get_href([slug]),
 			content: await plaintext(intro),
 			rank
 		});
-
-		if (intro) {
-			blocks.push({
-				breadcrumbs: [...breadcrumbs, removeMarkdown(metadata.title ?? '')],
-				href: get_href([slug]),
-				content: await plaintext(intro),
-				rank
-			});
-		}
 
 		for (const section of sections) {
 			const lines = section.split('\n');
