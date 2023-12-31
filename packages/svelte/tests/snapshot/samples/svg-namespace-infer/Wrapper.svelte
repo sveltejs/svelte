4c68f639161814a64d16f2b37a39766390e7e20d
navorite <navorite@gmail.com>
2023-12-31 15:56:20 +0200

    test

---
+++ packages/svelte/tests/snapshot/samples/svg-namespace-infer/Wrapper.svelte
@@ -0,0 +1,17 @@
+<script>
+  let { a, arr } = $props();
+</script>
+
+{#if a}
+  {@const tag = "test"}
+  <text x="0" y="26">A {tag}</text>
+{:else}
+  {@html "<text x=0 y=25>temp</text>"}
+  <text x="0" y="26">not A</text>
+{/if}
+
+{#each arr as item, idx}
+  <text x={idx * 10} y={42}>{item}</text>
+{/each}
+
+<!-- test comment -->
