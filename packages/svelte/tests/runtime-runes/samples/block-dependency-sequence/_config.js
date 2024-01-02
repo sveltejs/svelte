8c877721e6dcefe1682ea44ada4a1825dbff4fba
Dominic Gannaway <dg@domgan.com>
2024-01-02 13:29:43 +0000

    lint

--- packages/svelte/tests/runtime-runes/samples/block-dependency-sequence/_config.js
+++ packages/svelte/tests/runtime-runes/samples/block-dependency-sequence/_config.js
@@ -4,14 +4,23 @@ import { test } from '../../test';
 export default test({
 	async test({ assert, target, component }) {
 		const [b1, b2] = target.querySelectorAll('button');
-		assert.htmlEqual(target.innerHTML, '<div>5</div><div>5</div><div>3</div><button>set null</button><button>set object</button');
+		assert.htmlEqual(
+			target.innerHTML,
+			'<div>5</div><div>5</div><div>3</div><button>set null</button><button>set object</button'
+		);
 		flushSync(() => {
 			b2.click();
 		});
-		assert.htmlEqual(target.innerHTML, '<div>5</div><div>5</div><div>3</div><button>set null</button><button>set object</button');
+		assert.htmlEqual(
+			target.innerHTML,
+			'<div>5</div><div>5</div><div>3</div><button>set null</button><button>set object</button'
+		);
 		flushSync(() => {
 			b1.click();
 		});
-		assert.htmlEqual(target.innerHTML, '<div>5</div><div></div><div>3</div><button>set null</button><button>set object</button');
+		assert.htmlEqual(
+			target.innerHTML,
+			'<div>5</div><div></div><div>3</div><button>set null</button><button>set object</button'
+		);
 	}
 });
