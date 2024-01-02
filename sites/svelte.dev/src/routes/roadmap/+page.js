9649e9766d4bfcb252614111417ba0bd88bce2a1
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-01-02 14:01:04 +0100

    Update sites/svelte.dev/src/routes/roadmap/+page.js

--- sites/svelte.dev/src/routes/roadmap/+page.js
+++ sites/svelte.dev/src/routes/roadmap/+page.js
@@ -2,7 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 export function load() {
 	redirect(
-    		307,
-    		'https://docs.google.com/document/d/1IA9Z5rcIm_KRxvh_L42d2NDdYRHZ72MfszhyJrsmf5A'
-    	);
+		307,
+		'https://docs.google.com/document/d/1IA9Z5rcIm_KRxvh_L42d2NDdYRHZ72MfszhyJrsmf5A'
+	);
 }
