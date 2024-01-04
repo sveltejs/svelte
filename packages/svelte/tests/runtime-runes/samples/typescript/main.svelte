8dc4f2bf24febc2e9448a0d849bb5c1cda32d3cb
Dominic Gannaway <dg@domgan.com>
2024-01-04 14:53:13 +0000

    revert

---
+++ packages/svelte/tests/runtime-runes/samples/typescript/main.svelte
@@ -0,0 +1,23 @@
+<script context="module" lang="ts">
+	interface Hello { message: 'hello' }
+	type Goodbye = { message: 'goodbye' };
+
+	export type { Hello };
+</script>
+
+<script>
+	import type { Foo } from './types';
+	import { type Bar, type Baz } from './types';
+
+	let count = $state(0);
+	const person = {
+		message: 'goodbye'
+	} satisfies Goodbye;
+</script>
+
+<button
+	on:click={(e: MouseEvent) => {
+		const next: number = count + 1;
+		count = next! as number;
+	}}
+>clicks: {count}</button>
