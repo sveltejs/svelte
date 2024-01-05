d2c506351a060098a7050d6c3566b303e83800cb
surajgoraya <SurajGoraya@outlook.com>
2024-01-05 17:42:33 -0500

    fix: allow boolean shorthand for `popover` prop

--- packages/svelte/elements.d.ts
+++ packages/svelte/elements.d.ts
@@ -731,7 +731,7 @@ export interface HTMLAttributes<T extends EventTarget> extends AriaAttributes, D
 	title?: string | undefined | null;
 	translate?: 'yes' | 'no' | '' | undefined | null;
 	inert?: boolean | undefined | null;
-	popover?: 'auto' | 'manual' | '' | undefined | null;
+	popover?: boolean | 'auto' | 'manual' | '' | undefined | null;
 
 	// Unknown
 	radiogroup?: string | undefined | null; // <command>, <menuitem>
