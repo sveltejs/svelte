674615effb8d5c927bd57eedf9c176a98c0daada
Dominic Gannaway <dg@domgan.com>
2024-01-09 22:18:54 +0000

    fix: further animation transition improvements

--- packages/svelte/src/internal/client/each.js
+++ packages/svelte/src/internal/client/each.js
@@ -735,6 +735,7 @@ export function destroy_each_item_block(
 	controlled = false
 ) {
 	const transitions = block.s;
+	const dom = block.d;
 
 	if (apply_transitions && transitions !== null) {
 		// We might have pending key transitions, if so remove them first
@@ -750,10 +751,13 @@ export function destroy_each_item_block(
 			if (transition_block !== null) {
 				transition_block.push(block);
 			}
+			if (dom !== null) {
+				// @ts-ignore
+				dom.__animate = false;
+			}
 			return;
 		}
 	}
-	const dom = block.d;
 	if (!controlled && dom !== null) {
 		remove(dom);
 	}
