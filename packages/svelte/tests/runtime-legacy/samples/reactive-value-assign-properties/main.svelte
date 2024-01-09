ac4eae4963a1cb226f34da17f6341aade1d27ad5
Simon Holthausen <simon.holthausen@vercel.com>
2024-01-09 15:18:48 +0100

    fix: legacy reactive dependencies tweak
    take into account member expressions when determining legacy reactive dependencies
    fixes #9954

---
+++ packages/svelte/tests/runtime-legacy/samples/reactive-value-assign-properties/main.svelte
@@ -0,0 +1,12 @@
+<script>
+	let button = { title: '', label: '' };
+	let title = '';
+	let label = '';
+
+	title = label = '1'; // to add dependencies/generate update block
+
+	$: button.title = title;
+	$: button.label = label;
+</script>
+
+{button.title} {button.label}
