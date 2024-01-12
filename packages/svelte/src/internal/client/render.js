0eca0ace9457d226409e582f9e275cb8a4ddb0a1
Dominic Gannaway <trueadm@users.noreply.github.com>
2024-01-12 13:59:55 +0000

    fix: improve event handling compatibility with delegation (#10168)
    * fix: improve event handling compatibility with delegation
    
    * fix
    
    * lint
    
    * add test

--- packages/svelte/src/internal/client/render.js
+++ packages/svelte/src/internal/client/render.js
@@ -319,7 +319,15 @@ export function event(event_name, dom, handler, capture, passive) {
 		capture,
 		passive
 	};
-	const target_handler = handler;
+	/**
+	 * @this {EventTarget}
+	 */
+	function target_handler(/** @type {Event} */ event) {
+		handle_event_propagation(dom, event);
+		if (!event.cancelBubble) {
+			return handler.call(this, event);
+		}
+	}
 	dom.addEventListener(event_name, target_handler, options);
 	// @ts-ignore
 	if (dom === document.body || dom === window || dom === document) {
