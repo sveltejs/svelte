c41fff0daea640e2ab0033a0fbcafc4985f3e3f3
Tobias Messner <tobias@zusor.io>
2024-01-13 20:06:35 +0100

    fix(repl): update broken embed repl to work like regular one, remove obsolete ReplWidget.svelte

--- sites/svelte.dev/src/routes/(authed)/repl/embed/+page.js
+++
@@ -1,8 +0,0 @@
-export function load({ url }) {
-	const query = url.searchParams;
-	return {
-		version: query.get('version') || '3',
-		gist: query.get('gist'),
-		example: query.get('example')
-	};
-}
