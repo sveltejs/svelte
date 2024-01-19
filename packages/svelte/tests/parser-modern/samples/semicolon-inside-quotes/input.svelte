b845535327c3cec5d8e422726434e828b17b36e9
n0n3br <rogeriol@gmail.com>
2024-01-19 13:45:32 -0300

    add double quotes

--- packages/svelte/tests/parser-modern/samples/semicolon-inside-quotes/input.svelte
+++ packages/svelte/tests/parser-modern/samples/semicolon-inside-quotes/input.svelte
@@ -1,6 +1,8 @@
+<h1>
+	Semicolon inside quotes
+</h1>
 <style>
-	@import url('https://fonts.googleapis.com/css2?family=Cairo:wght@400;700&display=swap');
-
+	@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
 	h1 {
 		font-weight: bold;
 	}
