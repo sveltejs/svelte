83628c901d93bdb417fbbbb696d7fb9fa9fcfc40
Dominic Gannaway <trueadm@users.noreply.github.com>
2024-01-19 20:30:11 +0000

    fix main (#10237)
    * fix main
    
    * fix main

--- packages/svelte/tests/compiler-errors/samples/unmatched-closing-tag-autoclose/_config.js
+++ packages/svelte/tests/compiler-errors/samples/unmatched-closing-tag-autoclose/_config.js
@@ -4,7 +4,7 @@ export default test({
 	error: {
 		code: 'invalid-closing-tag-after-autoclose',
 		message:
-			'</p> attempted to close element that was already automatically closed by <pre> (cannot nest <pre> inside <p>',
+			'</p> attempted to close element that was already automatically closed by <pre> (cannot nest <pre> inside <p>)',
 		position: [24, 24]
 	}
 });
