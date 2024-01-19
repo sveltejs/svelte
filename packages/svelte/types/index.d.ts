25f8e128620136c1563ebea0dc27e22b26502793
Dominic Gannaway <dg@domgan.com>
2024-01-19 22:41:18 +0000

    update types

--- packages/svelte/types/index.d.ts
+++ packages/svelte/types/index.d.ts
@@ -2461,6 +2461,32 @@ declare namespace $state {
  */
 declare function $derived<T>(expression: T): T;
 
+declare namespace $derived {
+	/**
+	 * Sometimes you need to create complex derivations which don't fit inside a short expression.
+	 * In this case, you can resort to `$derived.fn` which accepts a function as its argument and returns its value.
+	 *
+	 * Example:
+	 * ```ts
+	 * $derived.fn(() => {
+	 *   let tmp = count;
+	 *	 if (count > 10) {
+	 *	   tmp += 100;
+	 *	 }
+	 *	 return tmp * 2;
+	 * });
+	 * ```
+	 *
+	 * `$derived.fn` passes the previous derived value as a single argument to the derived function. This can be useful for when
+	 * you might want to compare the latest derived value with the previous derived value. Furthermore, you might want to pluck out
+	 * specific properties of derived state to use in the new derived state given a certain condition – which can be useful when dealing
+	 * with more complex state machines.
+	 *
+	 * https://svelte-5-preview.vercel.app/docs/runes#$derived-fn
+	 */
+	export function fn<T>(fn: (prev: T) => T): void;
+}
+
 /**
  * Runs code when a component is mounted to the DOM, and then whenever its dependencies change, i.e. `$state` or `$derived` values.
  * The timing of the execution is after the DOM has been updated.
