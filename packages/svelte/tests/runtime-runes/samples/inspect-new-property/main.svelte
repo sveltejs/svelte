14bf4b4b2cf9a13eb72da26458b35c69553baaa9
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-01-23 11:29:09 +0100

    fix: ensure proxy is updated before notifying listeners (#10267)
    fixes #10264
    fixes #10265

---
+++ packages/svelte/tests/runtime-runes/samples/inspect-new-property/main.svelte
@@ -0,0 +1,15 @@
+<script>
+	let obj = $state({});
+	let array = $state([]);
+
+	$inspect(obj);
+	$inspect(array);
+</script>
+
+<button
+	onclick={() => {
+		obj.x = "hello";
+		array[0] = "hello";
+	}}>
+	add prop
+</button>
