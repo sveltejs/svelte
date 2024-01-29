6a4b302abda7c8231e16e827d36a1a66d70be0cb
Simon Holthausen <simon.holthausen@vercel.com>
2024-01-29 14:55:55 +0100

    I meant skip lol

--- packages/svelte/tests/runtime-legacy/samples/dynamic-element-svg-implicit-namespace/_config.js
+++ packages/svelte/tests/runtime-legacy/samples/dynamic-element-svg-implicit-namespace/_config.js
@@ -4,7 +4,7 @@ export default test({
 	// This is skipped for now, because it's not clear how to make this work on client-side initial run:
 	// The anchor isn't connected to its parent at the time we can do a runtime check for the namespace, and we
 	// need the parent for this check. (this didn't work in Svelte 4 either)
-	solo: true,
+	skip: true,
 	html: '<svg><path></path></svg>',
 
 	test({ assert, target }) {
