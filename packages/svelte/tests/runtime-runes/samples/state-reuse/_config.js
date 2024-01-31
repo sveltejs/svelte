5fdda34c626ce385b4a9a51ef01d8d8a7ce57d02
Rich Harris <rich.harris@vercel.com>
2024-01-31 17:56:59 -0500

    update test

--- packages/svelte/tests/runtime-runes/samples/state-reuse/_config.js
+++ packages/svelte/tests/runtime-runes/samples/state-reuse/_config.js
@@ -1,28 +1,10 @@
 import { test } from '../../test';
 
-/**
- * @type {any[]}
- */
-let log;
-/**
- * @type {typeof console.warn}}
- */
-let original_log;
-
 export default test({
 	compileOptions: {
 		dev: true
 	},
-	before_test() {
-		log = [];
-		original_log = console.warn;
-		console.warn = (...v) => {
-			log.push(...v);
-		};
-	},
-	after_test() {
-		console.warn = original_log;
-	},
+
 	html: `<button>state1.value: a state2.value: a</button>`,
 
 	async test({ assert, target }) {
@@ -30,8 +12,5 @@ export default test({
 
 		await btn?.click();
 		assert.htmlEqual(target.innerHTML, `<button>state1.value: b state2.value: b</button>`);
-		assert.deepEqual(log, [
-			'Object passed to $state was frozen after being passed to $state before. Consider using $state.frozen instead, or unfreeze the object.'
-		]);
 	}
 });
