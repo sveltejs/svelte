bf803761300c5d6c0983a2e04bc60f7294fda09a
Andreas Söderlund <ciscoheat@gmail.com>
2024-02-01 10:43:06 +0100

    Reproduced issue with failing test.

---
+++ packages/svelte/tests/runtime-runes/samples/bind-group-nested-data/main.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { writable } from 'svelte/store';
+
+  let menu = ['Cookies and cream', 'Mint choc chip', 'Raspberry ripple'];
+  let order = writable({ flavours: ['Mint choc chip'], scoops: 1 });
+</script>
+
+<form method="POST">
+  <h2>Size</h2>
+
+  <label>
+    <input type="radio" bind:group={$order.scoops} name="scoops" value={1} />
+    One scoop
+  </label>
+
+  <label>
+    <input type="radio" bind:group={$order.scoops} name="scoops" value={2} />
+    Two scoops
+  </label>
+
+  <label>
+    <input type="radio" bind:group={$order.scoops} name="scoops" value={3} />
+    Three scoops
+  </label>
+
+  <h2>Flavours</h2>
+
+  {#each menu as flavour}
+    <label>
+      <input
+        type="checkbox"
+        bind:group={$order.flavours}
+        name="flavours"
+        value={flavour}
+      />
+      {flavour}
+    </label>
+  {/each}
+</form>
+
+<div>
+	<h2>Current flavours</h2>
+	<span id="output">{$order.flavours.join('+')}</span>
+</div>
