b696081922cee42d7d8389ddeec4a9db5dd0434d
Daniel Imfeld <daniel@imfeld.dev>
2024-02-04 11:52:59 -1000

    Allow let: directive on slot element

---
+++ .changeset/friendly-candles-relate.md
@@ -0,0 +1,5 @@
+---
+"svelte": patch
+---
+
+Allow let: directive on slots, for Svelte 4 compatibility
---
+++ packages/svelte/tests/validator/samples/slot-let-directive/input.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Nested from './Nested.svelte';
+</script>
+
+<Nested>
+	<slot slot="abc" name="abc" let:value {value} />
+</Nested>
