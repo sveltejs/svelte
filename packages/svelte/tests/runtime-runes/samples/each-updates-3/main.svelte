01933d526b51fc2996fbdb933954aeee28486662
Dominic Gannaway <dg@domgan.com>
2024-02-07 14:02:32 +0000

    fix: improve indexed each array reconcilation

---
+++ packages/svelte/tests/runtime-runes/samples/each-updates-3/main.svelte
@@ -0,0 +1,36 @@
+<script>
+	let entries = $state([
+			{
+					id: 'a',
+					subitems: ['a'],
+			},
+			{
+					id: 'b',
+					subitems: ['b'],
+			},
+			{
+					id: 'c',
+					subitems: ['c'],
+			},
+			{
+					id: 'd',
+					subitems: ['d'],
+			},
+	]);
+
+	function onDeleteEntry(entry) {
+			entries = entries.filter((innerEntry) => innerEntry.id !== entry.id);
+	}
+	</script>
+
+	<ul>
+			{#each entries as entry}
+					<li>
+							<button on:click={() => onDeleteEntry(entry)}>Delete</button>
+							{entry.id}
+							{#each entry.subitems as subitem}
+									{subitem}
+							{/each}
+					</li>
+			{/each}
+	</ul>
