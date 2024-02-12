2f82b3d4c36677c34b3bfedea3d7e41dece94877
Dominic Gannaway <dg@domgan.com>
2024-02-12 13:43:45 +0000

    revert

--- packages/svelte/tests/runtime-runes/samples/state-readonly/_config.js
+++ packages/svelte/tests/runtime-runes/samples/state-readonly/_config.js
@@ -1,20 +1,13 @@
 import { test } from '../../test';
 
 export default test({
-	html: '<button>set</button><button>delete</button><p>a,b,c</p><p>{"a":1,"b":2,"c":3}</p>',
+	compileOptions: {
+		dev: true
+	},
 	async test({ assert, target }) {
-		const [btn, bt2] = target.querySelectorAll('button');
+		const btn = target.querySelector('button');
 
 		await btn?.click();
-		assert.htmlEqual(
-			target.innerHTML,
-			`<button>set</button><button>delete</button><p>a,b,c</p><p>{"a":1,"b":2,"c":3}</p>`
-		);
-
-		await bt2?.click();
-		assert.htmlEqual(
-			target.innerHTML,
-			`<button>set</button><button>delete</button><p>a,c</p><p>{"a":1,"c":3}</p>`
-		);
+		assert.htmlEqual(target.innerHTML, `<button></button>\n[object Object]`);
 	}
 });
