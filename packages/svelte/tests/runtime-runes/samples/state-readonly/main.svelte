fb1fb1734cbb442ef044209775603d84e2ecf4b4
Dominic Gannaway <dg@domgan.com>
2024-02-12 13:44:11 +0000

    revert

--- packages/svelte/tests/runtime-runes/samples/state-readonly/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/state-readonly/main.svelte
@@ -1,8 +1,12 @@
 <script>
-	let numbers = $state({ a: 1, b: 2, c: 3 });
+	import Component from './Component.svelte';
+
+	let state = $state();
 </script>
 
-<button onclick={() => { numbers.b = 2; }}>set</button>
-<button onclick={() => { delete numbers.b; }}>delete</button>
-<p>{Object.keys(numbers)}</p>
-<p>{JSON.stringify(numbers)}</p>
+<button onclick={() => {
+	state = {}
+}}></button>
+{#if state}
+	<Component state={state} />
+{/if}
