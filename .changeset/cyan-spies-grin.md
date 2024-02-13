85aa07428dff653ed57483f657ad1451fcdad466
Dominic Gannaway <dg@domgan.com>
2024-02-13 14:42:33 +0000

    fix: improve global transition handling of effect cleardown

---
+++ .changeset/cyan-spies-grin.md
@@ -0,0 +1,5 @@
+---
+"svelte": patch
+---
+
+fix: improve global transition handling of effect cleardown
---
+++ packages/svelte/tests/runtime-runes/samples/dynamic-component-transition/_config.js
@@ -0,0 +1,21 @@
+import { test } from '../../test';
+import { flushSync } from 'svelte';
+
+export default test({
+	async test({ assert, target, raf }) {
+		const btn = target.querySelector('button');
+
+		raf.tick(0);
+
+		flushSync(() => {
+			btn?.click();
+		});
+
+		assert.htmlEqual(target.innerHTML, `<h1>Outside</h1><button>Hide</button>`);
+
+		debugger;
+		raf.tick(100);
+
+		assert.htmlEqual(target.innerHTML, `<h1>Outside</h1>`);
+	}
+});
