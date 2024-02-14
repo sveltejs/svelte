c9e17eb60184f4db7a0e25c6e19fd437d6348780
Simon Holthausen <simon.holthausen@vercel.com>
2024-02-14 17:45:05 +0100

    skip test and note that it's skipped in Svelte 4, too

--- packages/svelte/tests/sourcemaps/samples/binding-shorthand/_config.js
+++ packages/svelte/tests/sourcemaps/samples/binding-shorthand/_config.js
@@ -1,7 +1,7 @@
 import { test } from '../../test';
 
 export default test({
-	skip: true, // TODO: no source maps for binding in template
+	skip: true, // No source map for binding in template because there's no loc property for it; skipped in Svelte 4, too
 	client: [
 		'potato',
 		{ str: 'potato', idxOriginal: 1, idxGenerated: 3 },
