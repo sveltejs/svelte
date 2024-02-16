d5645576e60b985a3eb9c2fc551d830c397684eb
Rich Harris <rich.harris@vercel.com>
2024-02-15 20:28:52 -0500

    tests

---
+++ packages/svelte/tests/css/samples/nested-css/input.svelte
@@ -0,0 +1,74 @@
+<div class="a">
+	<div class="a"></div>
+
+	<div class="b">
+		<div class="c"></div>
+	</div>
+
+	<div class="d"></div>
+</div>
+
+<div class="container">
+	<div class="a"></div>
+</div>
+
+<style>
+	.a {
+		color: green;
+
+		/* implicit & */
+		.b, .unused {
+			color: green;
+
+			.c {
+				color: green;
+			}
+
+			.unused {
+				color: red;
+
+				.c {
+					color: red;
+				}
+			}
+		}
+
+		.d {
+			.unused {
+				color: red;
+			}
+		}
+
+		/* explicit & */
+		& .b {
+			color: green;
+
+			.c {
+				& & {
+					color: red;
+				}
+			}
+		}
+
+		& & {
+			color: green;
+		}
+
+		/* silly but valid */
+		&& {
+			color: rebeccapurple;
+		}
+
+		.container & {
+			color: green;
+		}
+
+		&.b {
+			color: red;
+		}
+
+		.unused {
+			color: red;
+		}
+	}
+</style>
