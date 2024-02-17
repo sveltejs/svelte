23d9d12047ab8eab3a02a70696255abcd869481f
Rich Harris <rich.harris@vercel.com>
2024-02-17 11:55:35 -0500

    update test

--- packages/svelte/tests/validator/samples/global-event-reference/input.svelte
+++ packages/svelte/tests/validator/samples/global-event-reference/input.svelte
@@ -5,8 +5,5 @@
 <button {onclick}></button>
 <button onclick={onclick}></button>
 
-<button {onTypeScriptWillCatchThis}></button>
-<button onTypeScriptWillCatchThis={onTypeScriptWillCatchThis}></button>
-
 <button {onkeydown}></button>
 <button onkeydown={onkeydown}></button>
