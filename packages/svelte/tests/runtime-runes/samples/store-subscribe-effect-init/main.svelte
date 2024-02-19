41cdbafc40caf21083a40bf86aeadaefab3771b1
Dominic Gannaway <trueadm@users.noreply.github.com>
2024-02-19 12:24:35 +0000

    fix: improve effect over-fire on store subscription init (#10535)

---
+++ packages/svelte/tests/runtime-runes/samples/store-subscribe-effect-init/main.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { untrack } from 'svelte';
+	import { writable } from 'svelte/store';
+	import { log } from './log';
+
+	const storeCount = writable(0)
+	let count = $state(0);
+
+	$effect(() => {
+		$storeCount;
+		untrack(() => {
+			count++;
+			log.push(count);
+		});
+	});
+</script>
+
+<button on:click={() => $storeCount++}>increment</button>
