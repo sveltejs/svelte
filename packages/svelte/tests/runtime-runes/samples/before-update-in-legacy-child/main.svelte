39a7ae864762cf2c2cdbe24f5f32b89d0c2df364
Rich Harris <rich.harris@vercel.com>
2024-02-20 12:31:15 -0500

    fix: remove memory leak

---
+++ packages/svelte/tests/runtime-runes/samples/before-update-in-legacy-child/main.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Child from './Child.svelte';
+	let object = $state({ count: 0 })
+</script>
+
+<button onclick={() => object.count += 1}>
+	clicks: {object.count}
+</button>
+
+<Child {object} />
