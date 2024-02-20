76d213938c7d5423527f7d6a4f015f477a627f82
Dominic Gannaway <dg@domgan.com>
2024-02-20 13:49:36 +0000

    fix: improve state store mutation compiler output

---
+++ packages/svelte/tests/runtime-runes/samples/state-store-props/Component.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { writable } from 'svelte/store';
+
+	let { data } = $props();
+	let form = writable(data.form);
+
+	function addTag() {
+		$form.data.tags['third'] = 3;
+	}
+</script>
+
+<pre>{JSON.stringify($form, null, 2)}</pre>
+
+<button on:click={addTag}>add</button>
---
+++ packages/svelte/tests/runtime-runes/samples/state-store-props/main.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Page from './Component.svelte';
+
+	let data = $state({ form: { data: { tags: { first: 1, second: 2 }}}});
+</script>
+<Page {data} />
