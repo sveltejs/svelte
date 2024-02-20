6104318e5d1e85699e5dd718d2894fb95c268afe
Rich Harris <rich.harris@vercel.com>
2024-02-20 11:22:46 -0500

    update tests

--- packages/svelte/tests/compiler-errors/samples/export-derived-state/_config.js
+++ packages/svelte/tests/compiler-errors/samples/export-derived-state/_config.js
@@ -3,7 +3,8 @@ import { test } from '../../test';
 export default test({
 	error: {
 		code: 'invalid-derived-export',
-		message: 'Cannot export derived state from a module',
+		message:
+			'Cannot export derived state from a module. To expose the current derived value, export a function returning its value',
 		position: [24, 66]
 	}
 });
