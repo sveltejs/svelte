1645057b06baa30c944190a6a16980e8870df264
Simon Holthausen <simon.holthausen@vercel.com>
2024-02-20 13:28:54 +0100

    fix: don't execute scripts inside `@html` when instantiated on the client
    In Svelte 4, scripts inside `@html` were not executed when it was created client-side. This is because `innerHTML = ..` which was used under the hood does not execute scripts due to security reasons. This adjusts the code so the same is true for Svelte 5.

---
+++ packages/svelte/tests/runtime-browser/samples/html-tag-script/_config.js
@@ -0,0 +1,9 @@
+import { test } from '../../assert';
+
+export default test({
+	// Test that @html does not execute scripts when instantiated in the client.
+	// Needs to be in this test suite because JSDOM does not quite get this right.
+	html: `<div></div><script>document.body.innerHTML = 'this should not be executed'</script>`,
+	skip_if_ssr: 'permanent',
+	skip_if_hydrate: 'permanent'
+});
