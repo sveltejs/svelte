c10337cfb24e6cc4a2395bffc7d19c9de570335b
Dominic Gannaway <trueadm@users.noreply.github.com>
2024-02-22 16:09:40 +0000

    fix: improve proxy effect dependency tracking (#10605)

---
+++ packages/svelte/tests/runtime-runes/samples/state-proxy-version/main.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { setContext } from 'svelte';
+
+	import Item from './Item.svelte'
+
+	let items = $state({});
+
+	setContext('container', {
+		register: (id) => items[id] = true,
+		unregister: (id) => delete items[id]
+	});
+</script>
+
+<Item />
