f658ca0d548cc0e4eb0e33f1c4a7586f3dd8ccec
Dominic Gannaway <trueadm@users.noreply.github.com>
2024-02-23 18:28:56 +0000

    feat: add reactive Date object to svelte/reactivity (#10622)
    * feat: add reactive Date object to svelte/reactivity
    
    * add type safety, fix revealed typos
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>

---
+++ packages/svelte/src/reactivity/index.js
@@ -0,0 +1,103 @@
+import { source } from '../internal/client/reactivity/sources';
+import { get, set } from '../internal/client/runtime';
+
+/** @type {Array<keyof Date>} */
+const read = [
+	'getDate',
+	'getDay',
+	'getFullYear',
+	'getHours',
+	'getMilliseconds',
+	'getMinutes',
+	'getMonth',
+	'getSeconds',
+	'getTime',
+	'getTimezoneOffset',
+	'getUTCDate',
+	'getUTCDay',
+	'getUTCFullYear',
+	'getUTCHours',
+	'getUTCMilliseconds',
+	'getUTCMinutes',
+	'getUTCMonth',
+	'getUTCSeconds',
+	// @ts-expect-error this is deprecated
+	'getYear',
+	'toDateString',
+	'toISOString',
+	'toJSON',
+	'toLocaleDateString',
+	'toLocaleString',
+	'toLocaleTimeString',
+	'toString',
+	'toTimeString',
+	'toUTCString'
+];
+
+/** @type {Array<keyof Date>} */
+const write = [
+	'setDate',
+	'setFullYear',
+	'setHours',
+	'setMilliseconds',
+	'setMinutes',
+	'setMonth',
+	'setSeconds',
+	'setTime',
+	'setUTCDate',
+	'setUTCFullYear',
+	'setUTCHours',
+	'setUTCMilliseconds',
+	'setUTCMinutes',
+	'setUTCMonth',
+	'setUTCSeconds',
+	// @ts-expect-error this is deprecated
+	'setYear'
+];
+
+class ReactiveDate extends Date {
+	#raw_time = source(super.getTime());
+	static #inited = false;
+
+	// We init as part of the first instance so that we can treeshake this class
+	#init() {
+		if (!ReactiveDate.#inited) {
+			ReactiveDate.#inited = true;
+			const proto = ReactiveDate.prototype;
+			const date_proto = Date.prototype;
+
+			for (const method of read) {
+				// @ts-ignore
+				proto[method] = function () {
+					get(this.#raw_time);
+					// @ts-ignore
+					return date_proto[method].call(this);
+				};
+			}
+
+			for (const method of write) {
+				// @ts-ignore
+				proto[method] = function (/** @type {any} */ ...args) {
+					// @ts-ignore
+					const v = date_proto[method].apply(this, args);
+					const time = date_proto.getTime.call(this);
+					if (time !== this.#raw_time.v) {
+						set(this.#raw_time, time);
+					}
+					return v;
+				};
+			}
+		}
+	}
+
+	/**
+	 * @param {any[]} values
+	 */
+	constructor(...values) {
+		// @ts-ignore
+		super(...values);
+		this.#init();
+	}
+}
+
+export { ReactiveDate as Date };
---
+++ packages/svelte/tests/runtime-runes/samples/date/main.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { Date } from 'svelte/reactivity';
+
+	let date = new Date('2024/02/23 15:00:00');
+</script>
+
+<div>getSeconds: {date.getSeconds()}</div>
+<div>getMinutes: {date.getMinutes()}</div>
+<div>getHours: {date.getHours()}</div>
+<div>getTime: {date.getTime()}</div>
+<div>toDateString: {date.toDateString()}</div>
+
+<button onclick={() => {
+	date.setSeconds(date.getSeconds() + 1);
+}}>1 second</button>
+<button onclick={() => {
+	date.setMinutes(date.getMinutes() + 1);
+}}>1 minute</button>
+<button onclick={() => {
+	date.setHours(date.getHours() + 1);
+}}>1 hour</button>
