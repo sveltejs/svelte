a13fcabfdfa97ca66ae9997f89ea644c138aedb6
Dominic Gannaway <dg@domgan.com>
2024-02-28 17:05:56 +0000

    fix: improve derived rune destructuring support

---
+++ packages/svelte/tests/runtime-runes/samples/destructure-derived-object/main.svelte
@@ -0,0 +1,23 @@
+
+<script>
+  function get_values() {
+    let a = $state(0);
+    let b = $state(0);
+    let c = $state(0);
+
+    return {
+       get a() { return a },
+       get b() { return b },
+       get c() { return c },
+			 increment() {
+				a++;
+				b++;
+				c++;
+			 }
+    };
+  }
+
+  const { a, b, c, increment } = $derived(get_values());
+</script>
+
+<button onclick={increment}>{a} {b} {c}</button>
