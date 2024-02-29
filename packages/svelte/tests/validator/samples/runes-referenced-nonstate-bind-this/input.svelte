74f8e261c6da4f2c882ecf76bd34c8e7cce2656b
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-02-29 17:50:46 +0100

    fix: prevent false positive non-state warnings for `bind:this` (#10674)
    `bind:this` doesn't need to be a state reference if it will never change
    fixes #10435

---
+++ packages/svelte/tests/validator/samples/runes-referenced-nonstate-bind-this/input.svelte
@@ -0,0 +1,11 @@
+<script>
+	let no_need;
+	let does_need1;
+	let does_need2 = $state();
+</script>
+
+<div bind:this={no_need}></div>
+{#if true}
+	<div bind:this={does_need1}></div>
+	<div bind:this={does_need2}></div>
+{/if}
