b8afb9b6e89314353d239aad6a1d5f14c1e5fb31
Rich Harris <rich.harris@vercel.com>
2024-03-04 18:11:00 -0700

    move reactivity types into separate .d.ts file

--- packages/svelte/src/internal/client/types.d.ts
+++ packages/svelte/src/internal/client/types.d.ts
@@ -1,9 +1,4 @@
 import {
-	DERIVED,
-	EFFECT,
-	RENDER_EFFECT,
-	SOURCE,
-	PRE_EFFECT,
 	ROOT_BLOCK,
 	EACH_BLOCK,
 	EACH_ITEM_BLOCK,
@@ -16,16 +11,7 @@ import {
 	SNIPPET_BLOCK,
 	STATE_SYMBOL
 } from './constants.js';
-
-// Put all internal types in this file. Once we convert to JSDoc, we can make this a d.ts file
-
-export type SignalFlags =
-	| typeof SOURCE
-	| typeof DERIVED
-	| typeof EFFECT
-	| typeof PRE_EFFECT
-	| typeof RENDER_EFFECT;
-export type EffectType = typeof EFFECT | typeof PRE_EFFECT | typeof RENDER_EFFECT;
+import type { ComputationSignal, EffectSignal, Signal, SourceSignal } from './reactivity/types.js';
 
 type EventCallback = (event: Event) => boolean;
 export type EventCallbackMap = Record<string, EventCallback | EventCallback[]>;
@@ -66,70 +52,6 @@ export type ComponentContext = {
 		m: Array<() => any>;
 	};
 };
-
-// We keep two shapes rather than a single monomorphic shape to improve the memory usage.
-// Source signals don't need the same shape as they simply don't do as much as computations
-// (effects and derived signals). Thus we can improve the memory profile at the slight cost
-// of some runtime performance.
-
-export type SourceSignal<V = unknown> = {
-	/** consumers: Signals that read from the current signal */
-	c: null | ComputationSignal[];
-	/** equals: For value equality */
-	e: null | EqualsFunctions;
-	/** flags: The types that the signal represent, as a bitwise value */
-	f: SignalFlags;
-	/** value: The latest value for this signal */
-	v: V;
-	// write version
-	w: number;
-};
-
-export type SourceSignalDebug = {
-	/** This is DEV only */
-	inspect: Set<Function>;
-};
-
-export type ComputationSignal<V = unknown> = {
-	/** block: The block associated with this effect/computed */
-	b: null | Block;
-	/** consumers: Signals that read from the current signal */
-	c: null | ComputationSignal[];
-	/** context: The associated component if this signal is an effect/computed */
-	x: null | ComponentContext;
-	/** dependencies: Signals that this signal reads from */
-	d: null | Signal<V>[];
-	/** destroy: Thing(s) that need destroying */
-	y: null | (() => void) | Array<() => void>;
-	/** equals: For value equality */
-	e: null | EqualsFunctions;
-	/** The types that the signal represent, as a bitwise value */
-	f: SignalFlags;
-	/** init: The function that we invoke for effects and computeds */
-	i:
-		| null
-		| (() => V)
-		| (() => void | (() => void))
-		| ((b: Block, s: Signal) => void | (() => void));
-	/** references: Anything that a signal owns */
-	r: null | ComputationSignal[];
-	/** value: The latest value for this signal, doubles as the teardown for effects */
-	v: V;
-	/** level: the depth from the root signal, used for ordering render/pre-effects topologically **/
-	l: number;
-	/** write version: used for unowned signals to track if their depdendencies are dirty or not **/
-	w: number;
-};
-
-export type Signal<V = unknown> = SourceSignal<V> | ComputationSignal<V>;
-
-export type SignalDebug<V = unknown> = SourceSignalDebug & Signal<V>;
-
-export type EffectSignal = ComputationSignal<null | (() => void)>;
-
-export type MaybeSignal<T = unknown> = T | Signal<T>;
-
-export type UnwrappedSignal<T> = T extends Signal<infer U> ? U : T;
 
 export type EqualsFunctions<T = any> = (a: T, v: T) => boolean;
 
@@ -421,3 +343,5 @@ export interface ProxyMetadata<T = Record<string | symbol, any>> {
 export type ProxyStateObject<T = Record<string | symbol, any>> = T & {
 	[STATE_SYMBOL]: ProxyMetadata;
 };
+
+export * from './reactivity/types';
