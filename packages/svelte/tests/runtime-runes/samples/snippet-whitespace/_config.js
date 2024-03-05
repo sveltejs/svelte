5d3385c56f9c95ad45dce1fbb4ed4e1a77ae5411
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-03-05 01:05:18 +0100

    fix: don't collapse whitespace within text nodes (#10691)
    fixes #9892

--- packages/svelte/tests/runtime-runes/samples/snippet-whitespace/_config.js
+++ packages/svelte/tests/runtime-runes/samples/snippet-whitespace/_config.js
@@ -4,5 +4,11 @@ export default test({
 	compileOptions: {
 		dev: true // Render in dev mode to check that the validation error is not thrown
 	},
-	html: `A\nB\nC\nD`
+	withoutNormalizeHtml: 'only-strip-comments',
+	html: `A B C D <pre>Testing
+123          ;
+    456</pre>`,
+	ssrHtml: `A B C D <pre>Testing
+123          ;
+    456</pre>`
 });
--- packages/svelte/tests/runtime-runes/samples/snippet-whitespace/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/snippet-whitespace/main.svelte
@@ -1,5 +1,15 @@
+<script>
+	import Pre from "./pre.svelte";
+
+</script>
 A
 {#snippet snip()}C{/snippet}
 B
 {@render snip()}
 D
+
+<Pre>
+    Testing
+123          ;
+    456
+</Pre>
