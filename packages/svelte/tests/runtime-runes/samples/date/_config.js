cc22492e7a3ce1aeb4c54fd7feeaab05c1f72050
Papaya <47937166+a137264832@users.noreply.github.com>
2024-03-07 17:46:00 +0800

    fix: use toDateString occur error when use pnpm run test in China (#10719)

--- packages/svelte/tests/runtime-runes/samples/date/_config.js
+++ packages/svelte/tests/runtime-runes/samples/date/_config.js
@@ -2,7 +2,7 @@ import { flushSync } from '../../../../src/main/main-client';
 import { test } from '../../test';
 
 export default test({
-	html: `<div>getSeconds: 0</div><div>getMinutes: 0</div><div>getHours: 15</div><div>getTime: 1708700400000</div><div>toDateString: Fri Feb 23 2024</div><button>1 second</button><button>1 minute</button><button>1 hour</button>`,
+	html: `<div>getSeconds: 0</div><div>getMinutes: 0</div><div>getHours: 15</div><div>getTime: 1708700400000</div><div>toUTCString: Fri, 23 Feb 2024 15:00:00 GMT</div><button>1 second</button><button>1 minute</button><button>1 hour</button>`,
 
 	test({ assert, target }) {
 		const [btn, btn2, btn3] = target.querySelectorAll('button');
@@ -13,7 +13,7 @@ export default test({
 
 		assert.htmlEqual(
 			target.innerHTML,
-			`<div>getSeconds: 1</div><div>getMinutes: 0</div><div>getHours: 15</div><div>getTime: 1708700401000</div><div>toDateString: Fri Feb 23 2024</div><button>1 second</button><button>1 minute</button><button>1 hour</button>`
+			`<div>getSeconds: 1</div><div>getMinutes: 0</div><div>getHours: 15</div><div>getTime: 1708700401000</div><div>toUTCString: Fri, 23 Feb 2024 15:00:01 GMT</div><button>1 second</button><button>1 minute</button><button>1 hour</button>`
 		);
 
 		flushSync(() => {
@@ -22,7 +22,7 @@ export default test({
 
 		assert.htmlEqual(
 			target.innerHTML,
-			`<div>getSeconds: 1</div><div>getMinutes: 1</div><div>getHours: 15</div><div>getTime: 1708700461000</div><div>toDateString: Fri Feb 23 2024</div><button>1 second</button><button>1 minute</button><button>1 hour</button>`
+			`<div>getSeconds: 1</div><div>getMinutes: 1</div><div>getHours: 15</div><div>getTime: 1708700461000</div><div>toUTCString: Fri, 23 Feb 2024 15:01:01 GMT</div><button>1 second</button><button>1 minute</button><button>1 hour</button>`
 		);
 
 		flushSync(() => {
@@ -31,7 +31,7 @@ export default test({
 
 		assert.htmlEqual(
 			target.innerHTML,
-			`<div>getSeconds: 1</div><div>getMinutes: 1</div><div>getHours: 16</div><div>getTime: 1708704061000</div><div>toDateString: Fri Feb 23 2024</div><button>1 second</button><button>1 minute</button><button>1 hour</button>`
+			`<div>getSeconds: 1</div><div>getMinutes: 1</div><div>getHours: 16</div><div>getTime: 1708704061000</div><div>toUTCString: Fri, 23 Feb 2024 16:01:01 GMT</div><button>1 second</button><button>1 minute</button><button>1 hour</button>`
 		);
 	}
 });
