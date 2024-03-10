73bf0c5a39d6e0c10caab075ed56147ecd411351
Rich Harris <rich.harris@vercel.com>
2024-03-10 17:31:37 -0400

    tidy up

--- packages/svelte/tests/runtime-legacy/samples/component-transition/_config.js
+++ packages/svelte/tests/runtime-legacy/samples/component-transition/_config.js
@@ -3,25 +3,18 @@ import { ok, test } from '../../test';
 
 export default test({
 	async test({ assert, target, raf }) {
-		const button = target.querySelector('#button');
+		const button = /** @type {HTMLButtonElement} */ (target.querySelector('#button'));
 		const container = target.querySelector('#container');
 		ok(button);
 		ok(container);
 
 		// Multiple click on button
-		// @ts-ignore
 		button.click();
-		// @ts-ignore
 		button.click();
-		// @ts-ignore
 		button.click();
-		// @ts-ignore
 		button.click();
-		// @ts-ignore
 		button.click();
-		// @ts-ignore
 		button.click();
-		// @ts-ignore
 		button.click();
 
 		raf.tick(0);
