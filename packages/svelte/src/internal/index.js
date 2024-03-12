ce69df84f117c667ac6aa27409ae371b55fc40bf
Rich Harris <rich.harris@vercel.com>
2024-03-12 12:13:07 -0400

    move template code into its own module

--- packages/svelte/src/internal/index.js
+++ packages/svelte/src/internal/index.js
@@ -31,6 +31,7 @@ export * from './client/dom/blocks/snippet.js';
 export * from './client/dom/blocks/svelte-component.js';
 export * from './client/dom/blocks/svelte-element.js';
 export * from './client/dom/blocks/svelte-head.js';
+export * from './client/dom/template.js';
 export * from './client/reactivity/deriveds.js';
 export * from './client/reactivity/effects.js';
 export * from './client/reactivity/sources.js';
