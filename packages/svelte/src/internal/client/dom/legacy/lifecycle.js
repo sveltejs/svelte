b3946e05d63740335465b26fde21920e833ebf1d
Dominic Gannaway <dg@domgan.com>
2024-03-13 09:39:47 +0000

    simplify pre_effect logic

--- packages/svelte/src/internal/client/dom/legacy/lifecycle.js
+++ packages/svelte/src/internal/client/dom/legacy/lifecycle.js
@@ -1,6 +1,6 @@
 import { run } from '../../../common.js';
 import { pre_effect, user_effect } from '../../reactivity/effects.js';
-import { current_component_context, deep_read_state, get, untrack } from '../../runtime.js';
+import { current_component_context, deep_read_state, flush_local_render_effects, get, untrack } from '../../runtime.js';
 
 /**
  * Legacy-mode only: Call `onMount` callbacks and set up `beforeUpdate`/`afterUpdate` effects
@@ -16,6 +16,7 @@ export function init() {
 		pre_effect(() => {
 			observe_all(context);
 			callbacks.b.forEach(run);
+			flush_local_render_effects();
 		});
 	}
 
