1e653ef4d8cccf57dff1e43cbfacce0415cafe57
Rich Harris <richard.a.harris@gmail.com>
2024-03-13 11:36:45 -0400

    fix: make `set.has(...)` granular for existing properties (#10793)
    * fix: make set.has(...) granular
    
    * changeset
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>

--- packages/svelte/src/reactivity/set.test.ts
+++ packages/svelte/src/reactivity/set.test.ts
@@ -30,7 +30,50 @@ test('set.values()', () => {
 		set.clear();
 	});
 
-	assert.deepEqual(log, [5, true, [1, 2, 3, 4, 5], 4, false, [1, 2, 4, 5], 0, false, []]);
+	assert.deepEqual(log, [5, true, [1, 2, 3, 4, 5], 4, false, [1, 2, 4, 5], 0, [], false]); // TODO update when we fix effect ordering bug
+
+	cleanup();
+});
+
+test('set.has(...)', () => {
+	const set = new ReactiveSet([1, 2, 3]);
+
+	const log: any = [];
+
+	const cleanup = user_root_effect(() => {
+		pre_effect(() => {
+			log.push('has 1', set.has(1));
+		});
+
+		pre_effect(() => {
+			log.push('has 2', set.has(2));
+		});
+
+		pre_effect(() => {
+			log.push('has 3', set.has(3));
+		});
+	});
+
+	flushSync(() => {
+		set.delete(2);
+	});
+
+	flushSync(() => {
+		set.add(2);
+	});
+
+	assert.deepEqual(log, [
+		'has 1',
+		true,
+		'has 2',
+		true,
+		'has 3',
+		true,
+		'has 2',
+		false,
+		'has 2',
+		true
+	]);
 
 	cleanup();
 });
