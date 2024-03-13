0758095a9dfb8ba675b36daad48b465885e1f635
Simon Holthausen <simon.holthausen@vercel.com>
2024-03-13 11:40:16 +0100

    fix: invalidate store when mutated inside each block
    fixes #10771

---
+++ .changeset/khaki-ligers-sing.md
@@ -0,0 +1,5 @@
+---
+"svelte": patch
+---
+
+fix: invalidate store when mutated inside each block
---
+++ packages/svelte/tests/runtime-legacy/samples/binding-store-each/_config.js
@@ -0,0 +1,29 @@
+import { ok, test } from '../../test';
+
+export default test({
+	skip_if_ssr: 'permanent',
+	html: `
+		<input type="checkbox">
+		<input type="checkbox">
+		<input type="checkbox">
+		0
+	`,
+
+	async test({ assert, target, window }) {
+		const input = target.querySelector('input');
+		ok(input);
+
+		input.checked = true;
+		await input.dispatchEvent(new window.Event('change', { bubbles: true }));
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+			<input type="checkbox">
+			<input type="checkbox">
+			<input type="checkbox">
+			1
+		`
+		);
+	}
+});
