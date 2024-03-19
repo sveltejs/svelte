117082b0395f7b066b7396a04a3201e50638c667
Dominic Gannaway <trueadm@users.noreply.github.com>
2024-03-19 16:28:22 +0000

    fix: improve handling of unowned derived signals (#10842)

---
+++ packages/svelte/tests/runtime-runes/samples/derived-unowned-3/main.svelte
@@ -0,0 +1,49 @@
+<script>
+  import { untrack } from 'svelte';
+
+  class Model {
+    data = $state();
+
+    constructor(data) {
+      this.data = data;
+    }
+
+    name = $derived(this.data?.name);
+    source = $derived(this.data?.source);
+
+    toggle() {
+      this.data.name = this.data.name === 'zeeba' ? 'neighba' : 'zeeba';
+    }
+  }
+
+	let model = $state(new Model({ name: 'zeeba', source: 'initial' }));
+
+  let setModel = (source) => {
+		let next = new Model({ name: 'zeeba', source });
+		model = next;
+  }
+
+  let needsSet = $state(false);
+
+  $effect(() => {
+    if(needsSet) {
+      setModel('effect');
+      untrack(() => {
+        needsSet = false;
+      });
+    }
+  });
+
+  let setWithEffect = () => {
+    needsSet = true;
+  };
+
+  let toggle = () => {
+    model.toggle();
+  }
+</script>
+
+<button onclick={setWithEffect}>Activate</button>
+<button onclick={toggle}>Toggle</button>
+{model.name}
+{model.data.name}
