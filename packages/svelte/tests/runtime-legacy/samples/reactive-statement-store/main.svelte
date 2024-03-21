17f9baa2285a681fbc09f289c05b71e5fb49bd07
Simon Holthausen <simon.holthausen@vercel.com>
2024-03-21 21:59:26 +0100

    chore: failing test for out-of-order `$:` execution
    pre effects, which also contain `$:` statements, are more controlled in legacy mode and should only run once per tick, in their defined order. Flushing them out of order can result in bugs. Related to #10795 and #10787

---
+++ packages/svelte/tests/runtime-legacy/samples/reactive-statement-store/main.svelte
@@ -0,0 +1,16 @@
+<script>
+	let x = 1;
+	let y = true;
+	$: array = y ? [1, 2] : [1];
+	$: count = array.length === 2 && x ? 1 : 0;
+	$: sum = count + array.length;
+</script>
+
+<button
+	on:click={() => {
+		// order is important here: x must be updated before y
+		// in order to test that $: still runs in the correct order
+		x = 2;
+		y = false;
+	}}>{sum}</button
+>
