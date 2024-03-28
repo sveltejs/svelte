756ed0f3e19ff5e3e33363c3cd85a5fe6afebc46
Rich Harris <rich.harris@vercel.com>
2024-03-28 10:27:45 -0400

    failing test

--- packages/svelte/tests/runtime-legacy/samples/reactive-assignment-prevent-loop/main.svelte
+++ packages/svelte/tests/runtime-legacy/samples/reactive-assignment-prevent-loop/main.svelte
@@ -1,9 +1,22 @@
 <script>
+	import { log } from './log.js';
+
 	let count1 = 0;
 	let count2 = 0;
-	function increaseCount1() { count1++ }
-	$: if(count2 < 10) { increaseCount1() }
-	$: if(count1 < 10) { count2++ }
+
+	function increaseCount1() {
+		count1++;
+	}
+
+	$: if (count2 < 10) {
+		log.push(1);
+		increaseCount1();
+	}
+
+	$: if (count1 < 10) {
+		log.push(2);
+		count2++;
+	}
 </script>
 
 <button on:click={() => count1++}>{count1} / {count2}</button>
