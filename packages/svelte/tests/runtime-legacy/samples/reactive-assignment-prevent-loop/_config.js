12c2ba582fe31fc152f793b2f945bcd51d31d2e0
Rich Harris <rich.harris@vercel.com>
2024-03-28 10:45:09 -0400

    fix test

--- packages/svelte/tests/runtime-legacy/samples/reactive-assignment-prevent-loop/_config.js
+++ packages/svelte/tests/runtime-legacy/samples/reactive-assignment-prevent-loop/_config.js
@@ -10,12 +10,12 @@ export default test({
 	},
 
 	test({ assert, target }) {
-		assert.equal(log, [2, 1]);
+		assert.deepEqual(log, [2, 1]);
 
 		const button = target.querySelector('button');
 
 		flushSync(() => button?.click());
-		assert.equal(log, [2, 1, 2, 1]);
+		assert.deepEqual(log, [2, 1, 2, 1]);
 
 		assert.htmlEqual(target.innerHTML, '<button>3 / 2</button>');
 	}
