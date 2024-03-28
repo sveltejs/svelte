566991b78582c4f7840b0af46b06aabbb58f88bf
Rich Harris <rich.harris@vercel.com>
2024-03-28 12:27:57 -0400

    tweaks

--- packages/svelte/src/internal/client/dom/elements/actions.js
+++ packages/svelte/src/internal/client/dom/elements/actions.js
@@ -1,31 +1,32 @@
 import { effect } from '../../reactivity/effects.js';
-import { source } from '../../reactivity/sources.js';
-import { deep_read_state, get, untrack, update } from '../../runtime.js';
+import { set, source } from '../../reactivity/sources.js';
+import { deep_read_state, get, untrack } from '../../runtime.js';
 
 /**
  * @template P
  * @param {Element} dom
  * @param {(dom: Element, value?: P) => import('#client').ActionPayload<P>} action
- * @param {() => P} [value_fn]
+ * @param {() => P} [get_value]
  * @returns {void}
  */
-export function action(dom, action, value_fn) {
+export function action(dom, action, get_value) {
 	/** @type {undefined | import('#client').ActionPayload<P>} */
 	var payload = undefined;
 	var version = source(0);
-	/**
-	 * @type {P}
-	 */
+	var inited = false;
+
+	/** @type {P} */
 	var value;
-	var init = false;
 
 	// The value needs to be calculated independent of the action body, as we only want to evaluate
 	// the payload of the action once – not when the value changes.
 	effect(() => {
 		get(version);
-		if (value_fn) {
-			value = value_fn();
+
+		if (get_value) {
+			value = get_value();
 		}
+
 		if (payload !== undefined) {
 			var update = payload.update;
 			if (typeof update === 'function') {
@@ -38,7 +39,7 @@ export function action(dom, action, value_fn) {
 	// TODO we could take advantage of this and enable https://github.com/sveltejs/svelte/issues/6942
 	effect(() => {
 		untrack(() => {
-			if (value_fn) {
+			if (get_value) {
 				if (payload === undefined) {
 					payload = action(dom, value) || {};
 					if (payload?.update) {
@@ -47,12 +48,9 @@ export function action(dom, action, value_fn) {
 						// together with actions and mutation, it wouldn't notice the change without a deep read.
 						effect(() => {
 							deep_read_state(value);
-							if (init) {
-								untrack(() => {
-									update(version, 1);
-								});
-							}
-							init = true;
+
+							if (inited) set(version, version.v + 1);
+							inited = true;
 						});
 					}
 				}
@@ -60,16 +58,7 @@ export function action(dom, action, value_fn) {
 				payload = action(dom);
 			}
 		});
-	});
 
-	effect(() => {
-		if (payload !== undefined) {
-			var destroy = payload.destroy;
-			if (typeof destroy === 'function') {
-				return () => {
-					/** @type {Function} */ (destroy)();
-				};
-			}
-		}
+		return payload?.destroy;
 	});
 }
