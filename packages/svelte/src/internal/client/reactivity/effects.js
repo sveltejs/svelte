09703aee9ba84bce8d33c924b2407a053aa3ef0c
Dominic Gannaway <dg@domgan.com>
2024-03-30 20:31:35 +0000

    add TODOs

--- packages/svelte/src/internal/client/reactivity/effects.js
+++ packages/svelte/src/internal/client/reactivity/effects.js
@@ -323,6 +323,8 @@ export function pause_effects(effects, callback = noop) {
 		pause_children(effects[i], transitions, true);
 	}
 
+	// TODO: would be good to avoid this closure in the case where we have no
+	// transitions at all. It would make it far more JIT friendly in the hot cases.
 	out(transitions, () => {
 		for (var i = 0; i < length; i++) {
 			destroy_effect(effects[i]);
@@ -370,6 +372,8 @@ function pause_children(effect, transitions, local) {
 		var sibling = child.next;
 		var transparent = (child.f & IS_ELSEIF) !== 0 || (child.f & BRANCH_EFFECT) !== 0;
 		// TODO we don't need to call pause_children recursively with a linked list in place
+		// it's slightly more involved though as we have to account for `transparent` changing
+		// through the tree.
 		pause_children(child, transitions, transparent ? local : false);
 		child = sibling;
 	}
@@ -404,6 +408,8 @@ function resume_children(effect, local) {
 		var sibling = child.next;
 		var transparent = (child.f & IS_ELSEIF) !== 0 || (child.f & BRANCH_EFFECT) !== 0;
 		// TODO we don't need to call resume_children recursively with a linked list in place
+		// it's slightly more involved though as we have to account for `transparent` changing
+		// through the tree.
 		resume_children(child, transparent ? local : false);
 		child = sibling;
 	}
