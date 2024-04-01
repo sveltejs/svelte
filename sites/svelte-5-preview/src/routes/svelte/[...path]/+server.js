4f3fae7c6d2a3e142658acd066e4632ef8e63d9f
Rich Harris <rich.harris@vercel.com>
2024-04-01 18:03:14 -0400

    chore: compiler subpackage (#10988)
    * move compiler.cjs to compiler/index.js
    
    * changeset
    
    * prettier
    
    * ugh

--- sites/svelte-5-preview/src/routes/svelte/[...path]/+server.js
+++ sites/svelte-5-preview/src/routes/svelte/[...path]/+server.js
@@ -1,4 +1,4 @@
-import compiler_cjs from '../../../../../../packages/svelte/compiler.cjs?url';
+import compiler_cjs from '../../../../../../packages/svelte/compiler/index.js?url';
 import package_json from '../../../../../../packages/svelte/package.json?url';
 import { read } from '$app/server';
 
@@ -14,14 +14,14 @@ export const prerender = true;
 
 export function entries() {
 	const entries = Object.keys(files).map((path) => ({ path: path.replace(prefix, '') }));
-	entries.push({ path: 'compiler.cjs' }, { path: 'package.json' });
+	entries.push({ path: 'compiler/index.js' }, { path: 'package.json' });
 	return entries;
 }
 
 // service worker requests files under this path to load the compiler and runtime
 export async function GET({ params }) {
 	let url = '';
-	if (params.path === 'compiler.cjs') {
+	if (params.path === 'compiler/index.js') {
 		url = compiler_cjs;
 	} else if (params.path === 'package.json') {
 		url = package_json;
