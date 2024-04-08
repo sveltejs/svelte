5c25d41794cec04fc5c9c29094072c3b39ddc781
Simon Holthausen <simon.holthausen@vercel.com>
2024-04-08 22:31:40 +0200

    finalize

--- packages/svelte/src/compiler/phases/2-analyze/css/css-warn.js
+++ packages/svelte/src/compiler/phases/2-analyze/css/css-warn.js
@@ -1,5 +1,6 @@
 import { walk } from 'zimmerframe';
 import { warn } from '../../../warnings.js';
+import { is_keyframes_node } from '../../css.js';
 
 /**
  * @param {import('#compiler').Css.StyleSheet} stylesheet
@@ -11,15 +12,18 @@ export function warn_unused(stylesheet, warnings) {
 
 /** @type {import('zimmerframe').Visitors<import('#compiler').Css.Node, { warnings: import('../../types.js').RawWarning[], stylesheet: import('#compiler').Css.StyleSheet }>} */
 const visitors = {
+	Atrule(node, context) {
+		if (!is_keyframes_node(node)) {
+			context.next();
+		}
+	},
+	PseudoClassSelector(node, context) {
+		if (node.name === 'is' || node.name === 'where') {
+			context.next();
+		}
+	},
 	ComplexSelector(node, context) {
 		if (!node.metadata.used) {
-			for (let i = context.path.length - 1; i >= 0; i--) {
-				const parent = context.path[i];
-				if (parent.type === 'RelativeSelector' && parent.metadata.is_global) {
-					return; // no need to recurse; everything below is global, too
-				}
-			}
-
 			const content = context.state.stylesheet.content;
 			const text = content.styles.substring(node.start - content.start, node.end - content.start);
 			warn(context.state.warnings, node, context.path, 'css-unused-selector', text);
--- packages/svelte/tests/css/samples/unused-selector-trailing/_config.js
+++ packages/svelte/tests/css/samples/unused-selector-trailing/_config.js
@@ -5,15 +5,15 @@ export default test({
 		{
 			code: 'css-unused-selector',
 			end: {
-				character: 472,
-				column: 19,
-				line: 22
+				character: 32,
+				column: 3,
+				line: 5
 			},
-			message: 'Unused CSS selector ":global(.x) + .bar"',
+			message: 'Unused CSS selector "h2"',
 			start: {
-				character: 454,
+				character: 30,
 				column: 1,
-				line: 22
+				line: 5
 			}
 		}
 	]
