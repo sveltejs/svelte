1391bedc4bd502490d64e7cf25e721914b5c1a7d
Simon Holthausen <simon.holthausen@vercel.com>
2024-04-09 15:34:29 +0200

    fix: ensure top level snippets are defined when binding to component prop
    ...by hoisting top level snippets out of the binding loop in ssr mode
    fixes #11086

---
+++ packages/svelte/tests/snapshot/samples/bind-component-snippet/_expected/server/index.svelte.js
@@ -0,0 +1,43 @@
+// index.svelte (Svelte VERSION)
+// Note: compiler output will change before 5.0 is released!
+import * as $ from "svelte/internal/server";
+import TextInput from './Child.svelte';
+
+export default function Bind_component_snippet($$payload, $$props) {
+	$.push(true);
+
+	let value = '';
+	const _snippet = snippet;
+
+	function snippet($$payload) {
+		$$payload.out += `Something`;
+	}
+
+	let $$settled = true;
+	let $$inner_payload;
+
+	function $$render_inner($$payload) {
+		$$payload.out += `<!--[-->`;
+
+		TextInput($$payload, {
+			get value() {
+				return value;
+			},
+			set value($$value) {
+				value = $$value;
+				$$settled = false;
+			}
+		});
+
+		$$payload.out += `<!--]--> value: ${$.escape(value)}`;
+	};
+
+	do {
+		$$settled = true;
+		$$inner_payload = $.copy_payload($$payload);
+		$$render_inner($$inner_payload);
+	} while (!$$settled);
+
+	$.assign_payload($$payload, $$inner_payload);
+	$.pop();
+}
---
+++ packages/svelte/tests/snapshot/samples/bind-component-snippet/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import TextInput from './Child.svelte';
+
+	let value = $state('');
+	const _snippet = snippet;
+</script>
+
+{#snippet snippet()}
+	Something
+{/snippet}
+
+<TextInput bind:value />
+value: {value}
