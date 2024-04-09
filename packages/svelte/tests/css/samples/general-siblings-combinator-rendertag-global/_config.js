ed9bab9200acb3801694f4a1d6fdf30829fdc32d
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-04-09 11:56:54 +0200

    fix: better sibling selector handling (#11096)
    Keep sibling selectors when dealing with slots/render tags/`svelte:element` tags
    fixes #9274

---
+++ packages/svelte/tests/css/samples/general-siblings-combinator-rendertag-global/_config.js
@@ -0,0 +1,13 @@
+import { test } from '../../test';
+
+export default test({
+	warnings: [
+		// TODO
+		// {
+		// 	code: 'css-unused-selector',
+		// 	message: 'Unused CSS selector ".a ~ .b"',
+		// 	start: { character: 111, column: 1, line: 10 },
+		// 	end: { character: 118, column: 8, line: 10 }
+		// },
+	]
+});
