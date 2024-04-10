0270cd5bd87b5940463743f795406e78e4df3476
Dominic Gannaway <dg@domgan.com>
2024-04-10 12:44:36 +0100

    tweak

--- playgrounds/demo/src/entry-client.ts
+++ playgrounds/demo/src/entry-client.ts
@@ -2,7 +2,7 @@ import { mount, hydrate, unmount } from 'svelte';
 import App from './App.svelte';
 
 const root = document.getElementById('root')!;
-const render = root.firstChild === null ? mount : hydrate;
+const render = root.firstChild?.nextSibling ? hydrate : mount;
 
 const component = render(App, {
 	target: document.getElementById('root')!
