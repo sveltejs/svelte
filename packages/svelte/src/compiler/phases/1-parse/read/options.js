49ffecc5581eb2294c5cead0df2d3b5315f08c90
Jeremiasz Major <jrh.mjr@gmail.com>
2024-04-10 13:58:36 +0200

    revert "remove eslint workaround that is no longer needed"
    turns out it's needed after all

--- packages/svelte/src/compiler/phases/1-parse/read/options.js
+++ packages/svelte/src/compiler/phases/1-parse/read/options.js
@@ -38,6 +38,7 @@ export default function read_options(node) {
 			}
 			case 'tag': {
 				error(attribute, 'tag-option-deprecated');
+				break; // eslint doesn't know this is unnecessary
 			}
 			case 'customElement': {
 				/** @type {import('#compiler').SvelteOptions['customElement']} */
