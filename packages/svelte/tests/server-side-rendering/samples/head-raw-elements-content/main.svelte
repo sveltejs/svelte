ef11f96713a4837e13a02e27ddd0b03a65cce185
tanhauhau <lhtan93@gmail.com>
2024-04-11 23:33:18 +0800

    fix: missing classes after dynamic expressions in class attributes

---
+++ packages/svelte/tests/server-side-rendering/samples/head-raw-elements-content/main.svelte
@@ -0,0 +1,11 @@
+<script>
+	const dynamic_value = 'bar';
+</script>
+<div class="{dynamic_value} baz">bar</div>
+<div class="foo {dynamic_value} baz">bar</div>
+
+<style>
+	.baz {
+		color: red;
+	}
+</style>
