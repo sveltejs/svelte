fab34ab529a22b9c06bb6f79d58d9c239f91c442
Rich Harris <rich.harris@vercel.com>
2024-04-15 15:09:49 -0400

    adjust test to reflect new semantics

--- packages/svelte/tests/runtime-runes/samples/non-local-mutation-inherited-owner/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/non-local-mutation-inherited-owner/main.svelte
@@ -2,8 +2,7 @@
 	import Counter from './Counter.svelte';
 	import { global } from './state.svelte.js';
 
-	let object = $state({ count: 0 });
-	global.object = object;
+	global.object = { count: 0 };
 </script>
 
 <Counter />
