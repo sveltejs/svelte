2fa095b61eeacfddcfd2da973958930ea1e2b4b7
Dominic Gannaway <trueadm@users.noreply.github.com>
2024-04-15 14:30:54 +0100

    fix: improve spreading of attributes (#11177)
    * fix: improve spreading of attributes
    
    * twak

---
+++ packages/svelte/tests/runtime-runes/samples/spread-props-2/main.svelte
@@ -0,0 +1,11 @@
+<script>
+	let isRed = $state(true)
+
+  const attributes = $derived({
+    'data-red': isRed ? true : undefined
+  });
+</script>
+
+<button onclick={() => isRed = !isRed }>Toggle</button>
+
+<h1 {...attributes}>Style: {isRed ? 'red' : 'not red'}</h1>
