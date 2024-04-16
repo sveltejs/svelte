3c53da40ecfd66f9af3161bc445afa0e6a30c008
Dominic Gannaway <dg@domgan.com>
2024-04-16 21:09:29 +0100

    fix: remove memory leak from retaining old DOM elements

--- packages/svelte/src/internal/client/dom/blocks/html.js
+++ packages/svelte/src/internal/client/dom/blocks/html.js
@@ -1,8 +1,9 @@
 import { derived } from '../../reactivity/deriveds.js';
 import { render_effect } from '../../reactivity/effects.js';
-import { get } from '../../runtime.js';
+import { current_effect, get } from '../../runtime.js';
 import { hydrate_nodes, hydrating } from '../hydration.js';
 import { create_fragment_from_html, remove } from '../reconciler.js';
+import { push_template_node } from '../template.js';
 
 /**
  * @param {Element | Text | Comment} anchor
@@ -11,10 +12,12 @@ import { create_fragment_from_html, remove } from '../reconciler.js';
  * @returns {void}
  */
 export function html(anchor, get_value, svg) {
+	var effect = anchor.parentNode !== current_effect?.dom ? current_effect : null;
 	let value = derived(get_value);
 
 	render_effect(() => {
-		var dom = html_to_dom(anchor, get(value), svg);
+		var dom = html_to_dom(anchor, effect, get(value), svg);
+		effect = null;
 
 		if (dom) {
 			return () => remove(dom);
@@ -27,11 +30,12 @@ export function html(anchor, get_value, svg) {
  * inserts it before the target anchor and returns the new nodes.
  * @template V
  * @param {Element | Text | Comment} target
+ * @param {import('#client').Effect | null} effect
  * @param {V} value
  * @param {boolean} svg
  * @returns {Element | Comment | (Element | Comment | Text)[]}
  */
-function html_to_dom(target, value, svg) {
+function html_to_dom(target, effect, value, svg) {
 	if (hydrating) return hydrate_nodes;
 
 	var html = value + '';
@@ -49,6 +53,9 @@ function html_to_dom(target, value, svg) {
 	if (node.childNodes.length === 1) {
 		var child = /** @type {Text | Element | Comment} */ (node.firstChild);
 		target.before(child);
+		if (effect !== null) {
+			push_template_node(effect, child);
+		}
 		return child;
 	}
 
@@ -60,6 +67,10 @@ function html_to_dom(target, value, svg) {
 		}
 	} else {
 		target.before(node);
+	}
+
+	if (effect !== null) {
+		push_template_node(effect, nodes);
 	}
 
 	return nodes;
