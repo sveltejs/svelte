744213540a374858de28ce19366db155f953ac65
Dominic Gannaway <dg@domgan.com>
2024-04-16 14:03:00 +0100

    revise

--- packages/svelte/src/compiler/phases/3-transform/client/visitors/template.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/template.js
@@ -918,9 +918,13 @@ function serialize_bind_this(bind_this, context, node) {
 	/** @type {import('estree').Expression[]} */
 	const args = [node, b.arrow([b.id('$$value'), ...ids], update), b.arrow([...ids], bind_this_id)];
 	// If we're mutating a property, then it might already be non-existent.
-	// We can use an optional member accessor to avoid any runtime issues.
-	if (bind_this_id.type === 'MemberExpression') {
-		bind_this_id.optional = true;
+	// If we make all the object nodes optional, then it avoids any runtime exceptions.
+	/** @type {import('estree').Expression | import('estree').Super} */
+	let bind_node = bind_this_id;
+
+	while (bind_node?.type === 'MemberExpression') {
+		bind_node.optional = true;
+		bind_node = bind_node.object;
 	}
 	if (each_ids.size) {
 		args.push(b.thunk(b.array(Array.from(each_ids.values()).map((id) => id[1]))));
