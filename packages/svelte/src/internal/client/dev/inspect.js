a12c6bbcc8f161e4f2394429d6c244522a9ea0f4
Rich Harris <rich.harris@vercel.com>
2024-04-18 11:39:58 -0400

    better error

--- packages/svelte/src/internal/client/dev/inspect.js
+++ packages/svelte/src/internal/client/dev/inspect.js
@@ -1,6 +1,6 @@
 import { snapshot } from '../proxy.js';
-import { user_pre_effect } from '../reactivity/effects.js';
-import { deep_read } from '../runtime.js';
+import { render_effect } from '../reactivity/effects.js';
+import { current_effect, deep_read } from '../runtime.js';
 import { array_prototype, get_prototype_of, object_prototype } from '../utils.js';
 
 /** @type {Function | null} */
@@ -16,18 +16,27 @@ export let inspect_captured_signals = [];
 
 /**
  * @param {() => any[]} get_value
- * @param {Function} [inspect]
+ * @param {Function} [inspector]
  */
 // eslint-disable-next-line no-console
-export function inspect(get_value, inspect = console.log) {
+export function inspect(get_value, inspector = console.log) {
+	if (!current_effect) {
+		throw new Error(
+			'$inspect can only be used inside an effect (e.g. during component initialisation)'
+		);
+	}
+
 	let initial = true;
 
-	user_pre_effect(() => {
-		const fn = () => {
-			const value = deep_snapshot(get_value());
-			inspect(initial ? 'init' : 'update', ...value);
-		};
+	// we assign the function directly to signals, rather than just
+	// calling `inspector` directly inside the effect, so that
+	// we get useful stack traces
+	var fn = () => {
+		const value = deep_snapshot(get_value());
+		inspector(initial ? 'init' : 'update', ...value);
+	};
 
+	render_effect(() => {
 		inspect_fn = fn;
 		deep_read(get_value());
 		inspect_fn = null;
