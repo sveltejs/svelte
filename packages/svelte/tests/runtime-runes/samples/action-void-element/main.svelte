b9990153a3bd0355e6a2c258c295b2585730d948
Rich Harris <rich.harris@vercel.com>
2024-04-20 09:23:09 -0400

    fix: run render functions for dynamic void elements

---
+++ packages/svelte/tests/runtime-runes/samples/action-void-element/main.svelte
@@ -0,0 +1,9 @@
+<script>
+	/** @param {HTMLInputElement} node */
+	function action(node) {
+		node.value = 'set from action';
+	}
+</script>
+
+<input use:action>
+<svelte:element this={'input'} use:action />
