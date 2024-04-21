4540c267df14a591ece7db4e3fee8d7c6f98e4fe
Rich Harris <rich.harris@vercel.com>
2024-04-21 19:00:38 -0400

    simplify test

--- packages/svelte/tests/runtime-legacy/samples/binding-focused/_config.js
+++ packages/svelte/tests/runtime-legacy/samples/binding-focused/_config.js
@@ -1,70 +1,19 @@
-import { tick } from 'svelte';
+import { flushSync } from 'svelte';
 
-import { ok, test } from '../../test';
+import { test } from '../../test';
 
 export default test({
-	html: `
-		<div>false</div>
-		<div>false</div>
-		<input type="number">
-		<input type="number">
-		<button>click</button>
-	`,
-
-	ssrHtml: `
-		<div></div>
-		<div></div>
-		<input type="number">
-		<input type="number">
-		<button>click</button>
-	`,
-
 	async test({ assert, component, target, window }) {
-		const [d1, d2] = target.querySelectorAll('div');
 		const [in1, in2] = target.querySelectorAll('input');
-		const button = target.querySelector('button');
-		ok(in1);
-		ok(in2);
-		ok(button);
-		ok(d1);
-		ok(d2);
-		assert.equal(d1.textContent, 'false');
-		assert.equal(d2.textContent, 'false');
-		const event1 = new window.MouseEvent('click', { bubbles: true });
-		in1.value = '1';
-		await in1.dispatchEvent(event1);
-		await tick();
+
+		flushSync(() => in1.focus());
 		assert.equal(window.document.activeElement, in1);
 		assert.equal(component.a, true);
 		assert.equal(component.b, false);
-		assert.equal(d1.textContent, 'true');
-		assert.equal(d2.textContent, 'false');
 
-		in2.value = '1';
-		const event2 = new window.MouseEvent('click', { bubbles: true });
-		await in2.dispatchEvent(event2);
-		await tick();
+		flushSync(() => in2.focus());
+		assert.equal(window.document.activeElement, in2);
 		assert.equal(component.a, false);
 		assert.equal(component.b, true);
-		assert.equal(d1.textContent, 'false');
-		assert.equal(d2.textContent, 'true');
-
-		const event3 = new window.MouseEvent('click', { bubbles: true });
-		await button.dispatchEvent(event3);
-		await tick();
-
-		assert.equal(d1.textContent, 'false');
-		assert.equal(d2.textContent, 'false');
-
-		assert.htmlEqual(
-			target.innerHTML,
-			`
-				<div>false</div>
-				<div>false</div>
-				<input type="number">
-				<input type="number">
-				<button>click</button>
-			`
-		);
 	}
 });
--- packages/svelte/tests/runtime-legacy/samples/binding-focused/main.svelte
+++ packages/svelte/tests/runtime-legacy/samples/binding-focused/main.svelte
@@ -2,8 +2,6 @@
 	export let a;
 	export let b;
 </script>
-<div>{a}</div>
-<div>{b}</div>
-<input type="number" bind:focused={a} onclick='{e => e.target.focus()}' />
-<input type="number" bind:focused={b} onclick='{e => e.target.focus()}' />
-<button onclick='{e => e.target.focus()}'>click</button>
+
+<input bind:focused={a} />
+<input bind:focused={b} />
