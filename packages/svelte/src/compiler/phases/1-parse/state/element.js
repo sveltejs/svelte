6f3554eeeeb47e711390bc8d3008ae06f01e06f2
Rich Harris <rich.harris@vercel.com>
2024-04-23 13:10:26 -0400

    unused

--- packages/svelte/src/compiler/phases/1-parse/state/element.js
+++ packages/svelte/src/compiler/phases/1-parse/state/element.js
@@ -1,5 +1,3 @@
-import { extract_svelte_ignore } from '../../../utils/extract_svelte_ignore.js';
-import fuzzymatch from '../utils/fuzzymatch.js';
 import { is_void } from '../utils/names.js';
 import read_expression from '../read/expression.js';
 import { read_script } from '../read/script.js';
