ec329a954dee894cab2f39e1c806853cf62084aa
paoloricciuti <ricciutipaolo@gmail.com>
2024-04-25 00:31:18 +0200

    chore: rename class in test

--- packages/svelte/tests/runtime-runes/samples/class-state-with-literal/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/class-state-with-literal/main.svelte
@@ -1,12 +1,12 @@
 <script>
-	class Counter {
+	class Toggle {
 		"aria-pressed" = $state(false);
 
 		toggle(){
 			this["aria-pressed"] = !this["aria-pressed"]
 		}
 	}
-	const toggle = new Counter();
+	const toggle = new Toggle();
 </script>
 
 <button on:click={() => toggle.toggle()}>{toggle["aria-pressed"]}</button>
