a4acf7e71f73d56dd06b79d1fe9ee25b1cec19b6
paoloricciuti <ricciutipaolo@gmail.com>
2024-05-04 19:54:14 +0200

    fix: fix test for ssrHtml, html and runtime

--- packages/svelte/tests/runtime-browser/samples/fine-grained-hydration-clean-attr/_config.js
+++ packages/svelte/tests/runtime-browser/samples/fine-grained-hydration-clean-attr/_config.js
@@ -1,12 +1,10 @@
 import { test } from '../../assert';
 
 export default test({
-	html: `<input type="checkbox" name="lang" value="keep" checked />`,
-	mode: ['server'],
-	test({ window, assert, mod }) {
-		assert.htmlEqual(
-			window.document.body.innerHTML,
-			`<input type="checkbox" name="lang" value="keep" />`
-		);
+	html: `<input type="checkbox" name="lang" value="keep" />`,
+	ssrHtml: `<input type="checkbox" name="lang" value="keep" checked />`,
+	test({ window, assert, target, mod }) {
+		const input = target.querySelector('input');
+		assert.equal(input?.checked, true);
 	}
 });
