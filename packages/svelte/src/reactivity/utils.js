17f28a36e5aee842a68c36b22968bef688b55ee5
godzylinux <fohoov@gmail.com>
2024-05-08 17:22:02 +0330

    removed map from utils because it wasnt used anymore

--- packages/svelte/src/reactivity/utils.js
+++ packages/svelte/src/reactivity/utils.js
@@ -2,36 +2,6 @@ import { source, set } from '../internal/client/reactivity/sources.js';
 import { get } from '../internal/client/runtime.js';
 
 /**
- * @template T
- * @template U
- * @param {Iterable<T>} iterable
- * @param {(value: T) => U} fn
- * @param {string} name
- * @returns {IterableIterator<U>}
- */
-export function map(iterable, fn, name) {
-	return {
-		[Symbol.iterator]: get_this,
-		next() {
-			for (const value of iterable) {
-				return { done: false, value: fn(value) };
-			}
-
-			return { done: true, value: undefined };
-		},
-		// @ts-expect-error
-		get [Symbol.toStringTag]() {
-			return name;
-		}
-	};
-}
-
-/** @this {any} */
-function get_this() {
-	return this;
-}
-
-/**
  * @template TEntityInstance
  * @template {(keyof TEntityInstance)[]} TMutationProperties
  * @typedef {Record<TMutationProperties[number], (value: TEntityInstance, property: TMutationProperties[number], ...params: unknown[])=>boolean>}  Interceptors - return false if you want to prevent reactivity for this call/get
