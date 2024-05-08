e47d0b9cfe2d16bd28c8db74e1a98b272401ac02
Rich Harris <rich.harris@vercel.com>
2024-05-08 12:29:24 -0400

    add __svelte_meta to prototype

--- packages/svelte/src/internal/client/dom/operations.js
+++ packages/svelte/src/internal/client/dom/operations.js
@@ -1,5 +1,6 @@
 import { hydrate_anchor, hydrate_nodes, hydrating } from './hydration.js';
 import { get_descriptor } from '../utils.js';
+import { DEV } from 'esm-env';
 
 // We cache the Node and Element prototype methods, so that we can avoid doing
 // expensive prototype chain lookups.
@@ -69,6 +70,11 @@ export function init_operations() {
 	element_prototype.__className = '';
 	// @ts-expect-error
 	element_prototype.__attributes = null;
+
+	if (DEV) {
+		// @ts-expect-error
+		element_prototype.__svelte_meta = null;
+	}
 
 	first_child_get = /** @type {(this: Node) => ChildNode | null} */ (
 		// @ts-ignore
