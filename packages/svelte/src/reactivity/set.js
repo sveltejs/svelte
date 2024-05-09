4ca0c9bdea3efb8afd57cf2ca28ed8a78643e4f4
godzylinux <fohoov@gmail.com>
2024-05-09 18:27:57 +0330

    again another take on this matter, but this version is not optimized at all

--- packages/svelte/src/reactivity/set.js
+++ packages/svelte/src/reactivity/set.js
@@ -2,15 +2,31 @@ import { make_reactive } from './utils.js';
 
 export const ReactiveSet = make_reactive(Set, {
 	mutation_properties: ['add', 'clear', 'delete'],
+	read_properties: ['has', 'size'],
 	interceptors: {
-		add: (value, property, ...params) => {
-			return !value.has(params[0]);
+		add: (notify_read_methods, value, property, ...params) => {
+			if (value.has(params[0])) {
+				return false;
+			}
+			notify_read_methods(['has'], params[0]);
+			notify_read_methods(['size']);
+			return true;
 		},
-		clear: (value, property, ...params) => {
-			return value.size !== 0;
+		clear: (notify_read_methods, value, property, ...params) => {
+			if (value.size == 0) {
+				return false;
+			}
+			notify_read_methods(['has'], params[0]);
+			notify_read_methods(['size']);
+			return true;
 		},
-		delete: (value, property, ...params) => {
-			return value.has(params[0]);
+		delete: (notify_read_methods, value, property, ...params) => {
+			if (!value.has(params[0])) {
+				return false;
+			}
+			notify_read_methods(['has'], params[0]);
+			notify_read_methods(['size']);
+			return true;
 		}
 	}
 });
