98f07915b348771f765b9b6d07a276b37839bea8
Rich Harris <rich.harris@vercel.com>
2024-05-10 22:58:08 -0400

    add tests

---
+++ packages/svelte/tests/validator/samples/unknown-code/input.svelte
@@ -0,0 +1,9 @@
+<svelte:options runes={true} />
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<div>
+	<img src="this-is-fine.jpg">
+</div>
+
+<!-- svelte-ignore a11y-misplaced-scope -->
+<div scope></div>
