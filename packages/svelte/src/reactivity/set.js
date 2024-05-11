68dfe7914a5f7d00699d28be9f8e5d3c14acc294
godzylinux <fohoov@gmail.com>
2024-05-11 10:18:06 +0330

    removed size from read properties because it can depend on version signal and doesn't require its own signal

--- packages/svelte/src/reactivity/set.js
+++ packages/svelte/src/reactivity/set.js
@@ -2,14 +2,13 @@ import { make_reactive, NOTIFY_WITH_ALL_PARAMS } from './utils.js';
 
 export const ReactiveSet = make_reactive(Set, {
 	write_properties: ['add', 'clear', 'delete'],
-	read_properties: ['has', 'size'],
+	read_properties: ['has'],
 	interceptors: {
 		add: (notify_read_methods, value, property, ...params) => {
 			if (value.has(params[0])) {
 				return false;
 			}
 			notify_read_methods(['has'], params[0]);
-			notify_read_methods(['size']);
 			return true;
 		},
 		clear: (notify_read_methods, value, property, ...params) => {
@@ -17,7 +16,6 @@ export const ReactiveSet = make_reactive(Set, {
 				return false;
 			}
 			notify_read_methods(['has'], NOTIFY_WITH_ALL_PARAMS);
-			notify_read_methods(['size']);
 			return true;
 		},
 		delete: (notify_read_methods, value, property, ...params) => {
@@ -25,7 +23,6 @@ export const ReactiveSet = make_reactive(Set, {
 				return false;
 			}
 			notify_read_methods(['has'], params[0]);
-			notify_read_methods(['size']);
 			return true;
 		}
 	}
