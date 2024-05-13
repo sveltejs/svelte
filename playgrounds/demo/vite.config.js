6f88ce509080d05993f758769958f6cbc8ca8a16
Rich Harris <rich.harris@vercel.com>
2024-05-12 22:16:30 -0400

    chore: improve local playground

--- playgrounds/demo/vite.config.js
+++ playgrounds/demo/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import inspect from 'vite-plugin-inspect';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [inspect(), svelte()],
 	optimizeDeps: {
 		// svelte is a local workspace package, optimizing it would require dev server restarts with --force for every change
 		exclude: ['svelte']
