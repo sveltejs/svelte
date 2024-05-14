2bc39b1de2d160368c2ad8663c8876efb9736ff0
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-05-14 11:24:43 +0200

    chore: better ignore code handling (#11606)
    Instead of hacking an ignores array onto each node (and possibly degrading perf a bit because the object shape is mutated) we keep track of ignores in a stack. The new approach also avoids the indirection the old one had to do because the new approach looks upwards (checking if parent is a fragment) instead of iterating the children (checking for comments in them).
    As a bonus unknown code warnings are now in order (line-column-wise) with the other warnings. Also fixes #11482 because text nodes of all shapes are ok

--- packages/svelte/src/compiler/state.js
+++ packages/svelte/src/compiler/state.js
@@ -10,6 +10,21 @@ export let filename;
 
 export let locator = getLocator('', { offsetLine: 1 });
 
+/** @type {Set<string>[]} */
+export let ignore_stack = [];
+
+/**
+ * @param {string[]} ignores
+ */
+export function push_ignore(ignores) {
+	const next = new Set([...(ignore_stack.at(-1) || []), ...ignores]);
+	ignore_stack.push(next);
+}
+
+export function pop_ignore() {
+	ignore_stack.pop();
+}
+
 /**
  * @param {{
  *   source: string;
@@ -20,4 +35,5 @@ export function reset(options) {
 	filename = options.filename;
 	locator = getLocator(options.source, { offsetLine: 1 });
 	warnings = [];
+	ignore_stack = [];
 }
