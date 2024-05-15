40aea0c7bba42690ab98a1be028d9863286bcfff
adiguba <frederic.martini@gmail.com>
2024-05-15 07:52:25 +0200

    nullish value must be ignored

--- packages/svelte/src/internal/client/dom/elements/bindings/window.js
+++ packages/svelte/src/internal/client/dom/elements/bindings/window.js
@@ -32,8 +32,8 @@ export function bind_window_scroll(type, get_value, update) {
 	};
 
 	render_effect(() => {
-		latest_value = get_value() || 0;
-		if (!scrolling) {
+		latest_value = get_value();
+		if (!scrolling && latest_value!=null) {
 			scrolling = true;
 			clearTimeout(timeout);
 			if (is_scrolling_x) {
