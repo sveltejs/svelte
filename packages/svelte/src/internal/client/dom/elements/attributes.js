a5fd28a14bcdce10d8b9fcee1d323d5b8788cbc6
Dominic Gannaway <trueadm@users.noreply.github.com>
2024-05-20 22:05:39 +0100

    fix: inline pointer events now correctly work in Chrome (#11695)
    * fix: inline pointer events now correctly work in Chrome
    
    * fix more

--- packages/svelte/src/internal/client/dom/elements/attributes.js
+++ packages/svelte/src/internal/client/dom/elements/attributes.js
@@ -2,7 +2,7 @@ import { DEV } from 'esm-env';
 import { hydrating } from '../hydration.js';
 import { get_descriptors, get_prototype_of, map_get, map_set } from '../../utils.js';
 import { AttributeAliases, DelegatedEvents, namespace_svg } from '../../../../constants.js';
-import { delegate } from './events.js';
+import { create_event, delegate } from './events.js';
 import { autofocus } from './misc.js';
 import { effect, effect_root } from '../../reactivity/effects.js';
 import * as w from '../../warnings.js';
@@ -151,9 +151,13 @@ export function set_attributes(element, prev, next, lowercase_attributes, css_ha
 				if (!delegated) {
 					// we use `addEventListener` here because these events are not delegated
 					if (!prev) {
-						events.push([key, value, () => element.addEventListener(event_name, value, opts)]);
+						events.push([
+							key,
+							value,
+							() => (next[key] = create_event(event_name, element, value, opts))
+						]);
 					} else {
-						element.addEventListener(event_name, value, opts);
+						next[key] = create_event(event_name, element, value, opts);
 					}
 				} else {
 					// @ts-ignore
