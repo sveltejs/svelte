09addad9ae9ecb5141a473c4a33baa9195b35dc0
Dominic Gannaway <trueadm@users.noreply.github.com>
2024-05-21 15:23:14 +0100

    fix: improve handling of unowned derived signal (#11712)

---
+++ packages/svelte/tests/runtime-runes/samples/derived-unowned-8/main.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+	let visible = $state(true);
+
+	function toggleVisibility() {
+		visible = !visible;
+	}
+
+	let unchangedState = $state("unchanged state");
+	let derived = $derived.by(() => {
+		console.log("recalculating");
+		return unchangedState;
+	});
+</script>
+
+<button onclick={toggleVisibility}>Toggle Visibility</button>
+{#if visible}
+	<p>{derived}</p>
+{/if}
