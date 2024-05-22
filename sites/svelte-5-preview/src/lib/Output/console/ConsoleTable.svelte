d4718e0755eac1e505292d0fb7cfd8682eca3048
Rich Harris <rich.harris@vercel.com>
2024-05-22 17:08:02 -0400

    playground: better console (#11719)
    * remove unused code
    
    * better console
    
    * simplify
    
    * simplify
    
    * fix
    
    * fix
    
    * fix
    
    * fix console.table
    
    * more table tweaks
    
    * improve table styles
    
    * tweaks
    
    * bump svelte-json-tree
    
    * tweak group styles
    
    * level -> command
    
    * remove stack from assertions
    
    * nicer style for duplicates
    
    * styles
    
    * tidy/simplify
    
    * tweaks
    
    * tweaks
    
    * tweaks
    
    * fixes
    
    * better stack traces (albeit not sourcemapped yet)

--- sites/svelte-5-preview/src/lib/Output/console/ConsoleTable.svelte
+++ sites/svelte-5-preview/src/lib/Output/console/ConsoleTable.svelte
@@ -7,28 +7,54 @@
 	/** @type {any} */
 	export let columns;
 
-	const INDEX_KEY = '(index)';
-	const VALUE_KEY = 'Value';
-
-	$: keys = Object.keys(data);
-	$: columns_to_render = columns || get_columns_to_render(data, keys);
+	$: table = create_table(data, columns);
 
 	/**
 	 * @param {any} data
-	 * @param {string[]} keys
+	 * @param {string[]} [custom_columns]
 	 */
-	function get_columns_to_render(data, keys) {
-		const columns = new Set([INDEX_KEY]);
-		for (const key of keys) {
-			const value = data[key];
-			if (typeof value === 'object') {
-				Object.keys(value).forEach((key) => columns.add(key));
-			} else {
-				columns.add(VALUE_KEY);
+	function create_table(data, custom_columns) {
+		let has_non_object = false;
+		const columns = new Set();
+
+		if (custom_columns) {
+			custom_columns.forEach((column) => columns.add(column));
+		} else {
+			for (const key in data) {
+				const value = data[key];
+				if (typeof value === 'object') {
+					Object.keys(value).forEach((key) => columns.add(key));
+				} else {
+					has_non_object = true;
+				}
 			}
 		}
 
-		return [...columns];
+		const is_array = Array.isArray(data);
+
+		const rows = Object.keys(data).map((key) => {
+			const value = data[key];
+			const values = [];
+
+			for (const column of columns) {
+				values.push(typeof value === 'object' && column in value ? value[column] : '');
+			}
+
+			if (has_non_object) {
+				values.push(typeof value !== 'object' ? value : '');
+			}
+
+			return {
+				key: is_array ? parseInt(key) : key,
+				values
+			};
+		});
+
+		if (has_non_object) {
+			columns.add('Value');
+		}
+
+		return { columns, rows };
 	}
 </script>
 
@@ -36,24 +62,33 @@
 	<table>
 		<thead>
 			<tr>
-				{#each columns_to_render as column}
+				<th>(index)</th>
+
+				{#each table.columns as column}
 					<th>{column}</th>
 				{/each}
 			</tr>
 		</thead>
+
 		<tbody>
-			{#each keys as key}
+			{#each table.rows as row}
 				<tr>
-					{#each columns_to_render as column}
-						{#if column === INDEX_KEY}
-							<td>{key}</td>
-						{:else if column === VALUE_KEY}
-							<td><JSONNode value={data[key]} /></td>
-						{:else if column in data[key]}
-							<td><JSONNode value={data[key][column]} /></td>
+					<td>
+						{#if typeof row.key === 'string'}
+							{row.key}
 						{:else}
-							<td></td>
+							<JSONNode value={row.key} />
 						{/if}
+					</td>
+
+					{#each row.values as value}
+						<td>
+							{#if typeof value === 'string'}
+								{value}
+							{:else}
+								<JSONNode {value} />
+							{/if}
+						</td>
 					{/each}
 				</tr>
 			{/each}
@@ -63,32 +98,48 @@
 
 <style>
 	.table {
+		--json-tree-font-size: 1.2rem;
+		--json-tree-font-family: var(--sk-font-mono);
 		margin: 8px;
 		overflow: auto;
 		max-height: 200px;
+		border: 1px solid var(--sk-back-4);
+		border-radius: 2px;
+		overscroll-behavior: none;
 	}
+
 	table {
-		font-size: 12px;
-		font-family: var(--sk-font-mono);
 		border-collapse: collapse;
 		line-height: 1;
-		border: 1px solid #aaa;
 	}
+
 	th {
-		background: #f3f3f3;
-		padding: 4px 8px;
-		border: 1px solid #aaa;
+		background: var(--sk-back-3);
+		padding: 0.5rem 1rem;
+		border: none;
 		position: sticky;
+		font-weight: normal;
+		font-size: 1.4rem;
 		top: 0;
 	}
+
 	td {
-		padding: 2px 8px;
+		font-family: var(--sk-font-mono);
+		font-size: 1.2rem;
+		padding: 0.5rem 1rem;
+		border-bottom: none;
 	}
+
+	tr {
+		background: var(--sk-back-1);
+	}
+
 	tr:nth-child(2n) {
-		background: #f2f7fd;
+		background: var(--sk-back-3);
 	}
+
 	th,
 	td {
-		border-right: 1px solid #aaa;
+		border-right: 1px solid var(--sk-back-3);
 	}
 </style>
