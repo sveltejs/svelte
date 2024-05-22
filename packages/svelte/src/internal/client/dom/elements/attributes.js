c21f019a4b5e87fab98a111d1ece4ed26f1b4984
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-05-22 12:13:30 +0200

    chore: speedup hydration around input and select values (#11717)
    * chore: speedup hydration around input and select values
    
    * use idle tasks to do the work
    
    ---------
    
    Co-authored-by: Dominic Gannaway <dg@domgan.com>

--- packages/svelte/src/internal/client/dom/elements/attributes.js
+++ packages/svelte/src/internal/client/dom/elements/attributes.js
@@ -3,10 +3,11 @@ import { hydrating } from '../hydration.js';
 import { get_descriptors, get_prototype_of, map_get, map_set } from '../../utils.js';
 import { AttributeAliases, DelegatedEvents, namespace_svg } from '../../../../constants.js';
 import { create_event, delegate } from './events.js';
-import { autofocus } from './misc.js';
+import { add_form_reset_listener, autofocus } from './misc.js';
 import { effect, effect_root } from '../../reactivity/effects.js';
 import * as w from '../../warnings.js';
 import { LOADING_ATTR_SYMBOL } from '../../constants.js';
+import { queue_idle_task } from '../task.js';
 
 /**
  * The value/checked attribute in the template actually corresponds to the defaultValue property, so we need
@@ -16,12 +17,23 @@ import { LOADING_ATTR_SYMBOL } from '../../constants.js';
  */
 export function remove_input_attr_defaults(dom) {
 	if (hydrating) {
-		// using getAttribute instead of dom.value allows us to have
-		// null instead of "on" if the user didn't set a value
-		const value = dom.getAttribute('value');
-		set_attribute(dom, 'value', null);
-		set_attribute(dom, 'checked', null);
-		if (value) dom.value = value;
+		let already_removed = false;
+		// We try and remove the default attributes later, rather than sync during hydration.
+		// Doing it sync during hydration has a negative impact on performance, but deferring the
+		// work in an idle task alleviates this greatly. If a form reset event comes in before
+		// the idle callback, then we ensure the input defaults are cleared just before.
+		const remove_defaults = () => {
+			if (already_removed) return;
+			already_removed = true;
+			const value = dom.getAttribute('value');
+			set_attribute(dom, 'value', null);
+			set_attribute(dom, 'checked', null);
+			if (value) dom.value = value;
+		};
+		// @ts-expect-error
+		dom.__on_r = remove_defaults;
+		queue_idle_task(remove_defaults);
+		add_form_reset_listener();
 	}
 }
 
