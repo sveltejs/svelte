8459098c0507d3e633c289bb391a4946f1fd9989
Rich Harris <rich.harris@vercel.com>
2024-05-23 15:31:07 -0400

    chore: simplify effect.dom stuff (#11752)

--- packages/svelte/src/internal/client/dom/blocks/html.js
+++ packages/svelte/src/internal/client/dom/blocks/html.js
@@ -4,7 +4,6 @@ import { current_effect, get } from '../../runtime.js';
 import { is_array } from '../../utils.js';
 import { hydrate_nodes, hydrating } from '../hydration.js';
 import { create_fragment_from_html, remove } from '../reconciler.js';
-import { push_template_node } from '../template.js';
 
 /**
  * @param {import('#client').Effect} effect
@@ -38,7 +37,7 @@ export function html(anchor, get_value, svg, mathml) {
 	let value = derived(get_value);
 
 	render_effect(() => {
-		var dom = html_to_dom(anchor, parent_effect, get(value), svg, mathml);
+		var dom = html_to_dom(anchor, get(value), svg, mathml);
 
 		if (dom) {
 			return () => {
@@ -56,13 +55,12 @@ export function html(anchor, get_value, svg, mathml) {
  * inserts it before the target anchor and returns the new nodes.
  * @template V
  * @param {Element | Text | Comment} target
- * @param {import('#client').Effect | null} effect
  * @param {V} value
  * @param {boolean} svg
  * @param {boolean} mathml
  * @returns {Element | Comment | (Element | Comment | Text)[]}
  */
-function html_to_dom(target, effect, value, svg, mathml) {
+function html_to_dom(target, value, svg, mathml) {
 	if (hydrating) return hydrate_nodes;
 
 	var html = value + '';
@@ -81,9 +79,6 @@ function html_to_dom(target, effect, value, svg, mathml) {
 	if (node.childNodes.length === 1) {
 		var child = /** @type {Text | Element | Comment} */ (node.firstChild);
 		target.before(child);
-		if (effect !== null) {
-			push_template_node(child, effect);
-		}
 		return child;
 	}
 
@@ -95,10 +90,6 @@ function html_to_dom(target, effect, value, svg, mathml) {
 		}
 	} else {
 		target.before(node);
-	}
-
-	if (effect !== null) {
-		push_template_node(nodes, effect);
 	}
 
 	return nodes;
