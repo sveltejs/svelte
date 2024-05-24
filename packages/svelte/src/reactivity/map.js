6e9bbeaad489f06e8532cc4fa239b02e2ef86624
godzylinux <fohoov@gmail.com>
2024-05-24 14:10:19 +0330

    renamed variables and created config so interceptors can be extended easily

--- packages/svelte/src/reactivity/map.js
+++ packages/svelte/src/reactivity/map.js
@@ -4,29 +4,29 @@ export const ReactiveMap = make_reactive(Map, {
 	write_properties: ['clear', 'delete', 'set'],
 	read_properties: ['get', 'keys', 'has'],
 	interceptors: {
-		set: (notify_read_properties, value, property, ...params) => {
-			if (value.get(params[0]) === params[1]) {
+		set: (options, ...params) => {
+			if (options.value.get(params[0]) === params[1]) {
 				return false;
 			}
-			if (!value.has(params[0])) {
-				notify_read_properties(['keys']);
+			if (!options.value.has(params[0])) {
+				options.notify_read_properties(['keys']);
 			}
-			notify_read_properties(['get', 'has'], params[0]);
+			options.notify_read_properties(['get', 'has'], params[0]);
 			return true;
 		},
-		clear: (notify_read_properties, value, property, ...params) => {
-			if (value.size === 0) {
+		clear: (options, ...params) => {
+			if (options.value.size === 0) {
 				return false;
 			}
-			notify_read_properties(['get', 'keys', 'has'], NOTIFY_WITH_ALL_REGISTERED_PARAMS);
+			options.notify_read_properties(['get', 'keys', 'has'], NOTIFY_WITH_ALL_REGISTERED_PARAMS);
 			return true;
 		},
-		delete: (notify_read_properties, value, property, ...params) => {
-			if (!value.has(params[0])) {
+		delete: (options, ...params) => {
+			if (!options.value.has(params[0])) {
 				return false;
 			}
-			notify_read_properties(['get', 'has'], params[0]);
-			notify_read_properties(['keys']);
+			options.notify_read_properties(['get', 'has'], params[0]);
+			options.notify_read_properties(['keys']);
 			return true;
 		}
 	}
--- packages/svelte/src/reactivity/set.js
+++ packages/svelte/src/reactivity/set.js
@@ -4,25 +4,28 @@ export const ReactiveSet = make_reactive(Set, {
 	write_properties: ['add', 'clear', 'delete'],
 	read_properties: ['has'],
 	interceptors: {
-		add: (notify_read_properties, value, property, ...params) => {
-			if (value.has(params[0])) {
+		add: (options, ...params) => {
+			if (options.value.has(params[0])) {
 				return false;
 			}
-			notify_read_properties(['has'], params[0]);
+			options.notify_read_properties(['has'], params[0]);
 			return true;
 		},
-		clear: (notify_read_properties, value, property, ...params) => {
-			if (value.size == 0) {
+		clear: (options, ...params) => {
+			if (options.value.size == 0) {
 				return false;
 			}
-			notify_read_properties(['has'], NOTIFY_WITH_ALL_REGISTERED_PARAMS);
+			// get_registered_params('has').forEach((param) => {
+			// 	notify_read_properties(['has'], param);
+			// });
+			options.notify_read_properties(['has'], NOTIFY_WITH_ALL_REGISTERED_PARAMS);
 			return true;
 		},
-		delete: (notify_read_properties, value, property, ...params) => {
-			if (!value.has(params[0])) {
+		delete: (options, ...params) => {
+			if (!options.value.has(params[0])) {
 				return false;
 			}
-			notify_read_properties(['has'], params[0]);
+			options.notify_read_properties(['has'], params[0]);
 			return true;
 		}
 	}
