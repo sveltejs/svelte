5f09c897ba2508ae05ba6419b3953b03d26dd162
godzywp <53280503+FoHoOV@users.noreply.github.com>
2024-05-27 09:40:53 +0330

    improved test

--- packages/svelte/tests/runtime-browser/samples/attribute-spreading-with-events/_config.js
+++ packages/svelte/tests/runtime-browser/samples/attribute-spreading-with-events/_config.js
@@ -4,11 +4,11 @@ import { tick } from 'svelte';
 export default test({
 	async test({ assert, target }) {
 		await tick();
-		/** @type {HTMLDivElement} */
-		const el = target.querySelector('div');
+
+		const el = /** @type {HTMLDivElement} */ (target.querySelector('div'));
 		const animation_end = new window.AnimationEvent('animationend');
 
-		await el.dispatchEvent(animation_end);
+		el.dispatchEvent(animation_end);
 
 		await tick();
 
