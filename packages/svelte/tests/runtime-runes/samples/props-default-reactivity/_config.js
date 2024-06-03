5eb8641ac723a76111424d9805a3d29ba59d9fa9
Paolo Ricciuti <ricciutipaolo@gmail.com>
2024-06-03 12:06:41 +0200

    fix: keep default values of props a proxy after reassignment (#11860)
    * fix: keep default values of props a proxy after reassignment
    
    * fix: make this work for non bindable props too
    
    * chore: more comprehensive test
    
    * chore: cast away
    
    * chore: better variable name and check
    
    * chore: fix lint

--- packages/svelte/tests/runtime-runes/samples/props-default-reactivity/_config.js
+++ packages/svelte/tests/runtime-runes/samples/props-default-reactivity/_config.js
@@ -5,10 +5,12 @@ export default test({
 	html: `
 		<button>mutate: 0</button>
 		<button>reassign: 0</button>
+		<button>mutate: 0</button>
+		<button>reassign: 0</button>
 	`,
 
 	async test({ assert, target }) {
-		const [btn1, btn2] = target.querySelectorAll('button');
+		const [btn1, btn2, btn3, btn4] = target.querySelectorAll('button');
 
 		flushSync(() => {
 			btn1?.click();
@@ -19,6 +21,8 @@ export default test({
 			`
 				<button>mutate: 1</button>
 				<button>reassign: 1</button>
+				<button>mutate: 0</button>
+				<button>reassign: 0</button>
 			`
 		);
 
@@ -31,6 +35,64 @@ export default test({
 			`
 				<button>mutate: 2</button>
 				<button>reassign: 2</button>
+				<button>mutate: 0</button>
+				<button>reassign: 0</button>
+			`
+		);
+
+		flushSync(() => {
+			btn1?.click();
+		});
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+				<button>mutate: 3</button>
+				<button>reassign: 3</button>
+				<button>mutate: 0</button>
+				<button>reassign: 0</button>
+			`
+		);
+
+		flushSync(() => {
+			btn3?.click();
+		});
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+				<button>mutate: 3</button>
+				<button>reassign: 3</button>
+				<button>mutate: 1</button>
+				<button>reassign: 1</button>
+			`
+		);
+
+		flushSync(() => {
+			btn4?.click();
+		});
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+				<button>mutate: 3</button>
+				<button>reassign: 3</button>
+				<button>mutate: 2</button>
+				<button>reassign: 2</button>
+			`
+		);
+
+		flushSync(() => {
+			btn3?.click();
+		});
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+				<button>mutate: 3</button>
+				<button>reassign: 3</button>
+				<button>mutate: 3</button>
+				<button>reassign: 3</button>
 			`
 		);
 	}
