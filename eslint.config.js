843a9ae6d5e3b3843d3d9180364da65422d31439
Rich Harris <rich.harris@vercel.com>
2024-06-03 18:05:11 -0400

    again, typescript already has this covered, we don't need it

--- eslint.config.js
+++ eslint.config.js
@@ -14,7 +14,6 @@ export default [
 			lube
 		},
 		rules: {
-			'@typescript-eslint/no-misused-promises': 'error',
 			'@typescript-eslint/prefer-promise-reject-errors': 'error',
 			'@typescript-eslint/require-await': 'error',
 			'no-console': 'error',
