e7831e001d71752d4dfd374ecbcce665c50300d2
Simon Holthausen <simon.holthausen@vercel.com>
2024-06-05 21:18:35 +0200

    fix: correctly backport `svelte:element` to old AST
    Both `<svelte:element this="div">` and `<svelte:element this={"div"}>` were backported as `tag: "div"` for the old AST. That's wrong because the latter should result in `tag: { type: 'Literal', .. }`. Fixing this makes all the tests in prettier-plugin-svelte pass with Svelte 5.
    
    Also cleaned up a bit of code in the parser.

--- packages/svelte/tests/parser-legacy/samples/dynamic-element-string/input.svelte
+++ packages/svelte/tests/parser-legacy/samples/dynamic-element-string/input.svelte
@@ -1,2 +1,7 @@
+<svelte:element this="div"></svelte:element>
+<!-- prettier-ignore -->
+<svelte:element this='div'></svelte:element>
 <svelte:element this={"div"}></svelte:element>
+<!-- prettier-ignore -->
+<svelte:element this={'div'}></svelte:element>
 <svelte:element this={"div"} class="foo"></svelte:element>
