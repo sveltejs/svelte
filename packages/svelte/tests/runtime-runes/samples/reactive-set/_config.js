2b0dffa81036fe8d9c1bde119527b744c14756ec
Rich Harris <hello@rich-harris.dev>
2024-06-08 08:43:38 -0400

    Revert "fix: lazily create sources for Set (#11946)"
    This reverts commit 48fa6587c9766ef2a5e5892ec1e45afddc1d83ef.

--- packages/svelte/tests/runtime-runes/samples/reactive-set/_config.js
+++ packages/svelte/tests/runtime-runes/samples/reactive-set/_config.js
@@ -1,28 +1,50 @@
 import { flushSync } from '../../../../src/index-client';
-import { ok, test } from '../../test';
+import { test } from '../../test';
 
 export default test({
-	html: `<button>delete initial</button><button>add</button><button>delete</button><button>clear</button><div id="output"><p>1</p><div>0</div></div>`,
+	html: `<button>add</button><button>delete</button><button>clear</button>`,
 
 	test({ assert, target }) {
-		const [btn, btn2, btn3, btn4] = target.querySelectorAll('button');
-		const output = target.querySelector('#output');
-		ok(output);
+		const [btn, btn2, btn3] = target.querySelectorAll('button');
 
-		flushSync(() => btn?.click());
-		assert.htmlEqual(output.innerHTML, `<p>0</p>`);
+		flushSync(() => {
+			btn?.click();
+		});
 
-		flushSync(() => btn2?.click());
-		assert.htmlEqual(output.innerHTML, `<p>1</p><div>1</div>`);
+		assert.htmlEqual(
+			target.innerHTML,
+			`<button>add</button><button>delete</button><button>clear</button><div>1</div>`
+		);
 
-		flushSync(() => btn2?.click());
-		flushSync(() => btn2?.click());
-		assert.htmlEqual(output.innerHTML, `<p>3</p><div>1</div><div>2</div><div>3</div>`);
+		flushSync(() => {
+			btn?.click();
+		});
 
-		flushSync(() => btn3?.click());
-		assert.htmlEqual(output.innerHTML, `<p>2</p><div>1</div><div>2</div>`);
+		flushSync(() => {
+			btn?.click();
+		});
 
-		flushSync(() => btn4?.click());
-		assert.htmlEqual(output.innerHTML, `<p>0</p>`);
+		assert.htmlEqual(
+			target.innerHTML,
+			`<button>add</button><button>delete</button><button>clear</button><div>1</div><div>2</div><div>3</div>`
+		);
+
+		flushSync(() => {
+			btn2?.click();
+		});
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`<button>add</button><button>delete</button><button>clear</button><div>1</div><div>2</div>`
+		);
+
+		flushSync(() => {
+			btn3?.click();
+		});
+
+		assert.htmlEqual(
+			target.innerHTML,
+			`<button>add</button><button>delete</button><button>clear</button>`
+		);
 	}
 });
