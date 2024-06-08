388f21018363cefb4789b25ac55546b114e06278
Rich Harris <rich.harris@vercel.com>
2024-06-08 08:42:11 -0400

    feat: simpler hydration of CSS custom property wrappers (#11948)
    * simplify
    
    * DRY out
    
    * simplify
    
    * changeset
    
    * rename
    
    * simplify
    
    * we don't actually need the function, we can flatten it. more efficient
    
    * tidy up

--- packages/svelte/src/internal/client/dom/blocks/css-props.js
+++ packages/svelte/src/internal/client/dom/blocks/css-props.js
@@ -1,64 +1,35 @@
-import { namespace_svg } from '../../../../constants.js';
-import { hydrate_anchor, hydrate_start, hydrating } from '../hydration.js';
-import { empty } from '../operations.js';
+import { hydrating, set_hydrate_nodes } from '../hydration.js';
 import { render_effect } from '../../reactivity/effects.js';
 
 /**
- * @param {Element | Text | Comment} anchor
- * @param {boolean} is_html
- * @param {() => Record<string, string>} props
- * @param {(anchor: Element | Text | Comment) => any} component
+ * @param {HTMLDivElement | SVGGElement} element
+ * @param {() => Record<string, string>} get_styles
  * @returns {void}
  */
-export function css_props(anchor, is_html, props, component) {
-	/** @type {HTMLElement | SVGElement} */
-	let element;
-
-	/** @type {Text | Comment} */
-	let component_anchor;
-
+export function css_props(element, get_styles) {
 	if (hydrating) {
-		// Hydration: css props element is surrounded by a ssr comment ...
-		element = /** @type {HTMLElement | SVGElement} */ (hydrate_start);
-		// ... and the child(ren) of the css props element is also surround by a ssr comment
-		component_anchor = /** @type {Comment} */ (
-			hydrate_anchor(/** @type {Comment} */ (element.firstChild))
+		set_hydrate_nodes(
+			/** @type {import('#client').TemplateNode[]} */ ([...element.childNodes]).slice(0, -1)
 		);
-	} else {
-		if (is_html) {
-			element = document.createElement('div');
-			element.style.display = 'contents';
-		} else {
-			element = document.createElementNS(namespace_svg, 'g');
-		}
-
-		anchor.before(element);
-		component_anchor = element.appendChild(empty());
 	}
 
-	component(component_anchor);
-
 	render_effect(() => {
-		/** @type {Record<string, string>} */
-		let current_props = {};
-
 		render_effect(() => {
-			const next_props = props();
+			var styles = get_styles();
 
-			for (const key in current_props) {
-				if (!(key in next_props)) {
+			for (var key in styles) {
+				var value = styles[key];
+
+				if (value) {
+					element.style.setProperty(key, value);
+				} else {
 					element.style.removeProperty(key);
 				}
 			}
-
-			for (const key in next_props) {
-				element.style.setProperty(key, next_props[key]);
-			}
-
-			current_props = next_props;
 		});
 
 		return () => {
+			// TODO use `teardown` instead of creating a nested effect, post-https://github.com/sveltejs/svelte/pull/11936
 			element.remove();
 		};
 	});
