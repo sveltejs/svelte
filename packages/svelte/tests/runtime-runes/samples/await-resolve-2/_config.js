0b7484cbfb0a7660f2f3ce678e505c4844436331
Dominic Gannaway <dg@domgan.com>
2024-06-11 20:09:36 +0100

    update test

--- packages/svelte/tests/runtime-runes/samples/await-resolve-2/_config.js
+++ packages/svelte/tests/runtime-runes/samples/await-resolve-2/_config.js
@@ -2,7 +2,7 @@ import { test } from '../../test';
 
 export default test({
 	async test({ assert, target, logs }) {
-		const [b1, b2, b3] = target.querySelectorAll('button');
+		const [b1, b2, b3, b4] = target.querySelectorAll('button');
 		b1.click();
 		await Promise.resolve();
 		b2.click();
@@ -13,6 +13,9 @@ export default test({
 		await Promise.resolve();
 		await Promise.resolve();
 		await Promise.resolve();
-		assert.deepEqual(logs, ['pending', 'a', 'b', 'c']);
+		b4.click();
+		await Promise.resolve();
+		await Promise.resolve();
+		assert.deepEqual(logs, ['pending', 'a', 'b', 'c', 'pending']);
 	}
 });
--- packages/svelte/tests/runtime-runes/samples/await-resolve-2/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/await-resolve-2/main.svelte
@@ -1,7 +1,8 @@
 <script>
-	const promise_a = Promise.resolve('a')
-	const promise_b = Promise.resolve('b')
-	const promise_c = Promise.resolve('c')
+	const promise_a = Promise.resolve('a');
+	const promise_b = Promise.resolve('b');
+	const promise_c = Promise.resolve('c');
+	const promise_d = new Promise(() => {});
 
 	let current_promise = $state(promise_a);
 </script>
@@ -17,3 +18,4 @@
 <button onclick={()=>{current_promise = promise_a}}>Show Promise A</button>
 <button onclick={()=>{current_promise = promise_b}}>Show Promise B</button>
 <button onclick={()=>{current_promise = promise_c}}>Show Promise C</button>
+<button onclick={()=>{current_promise = promise_d}}>Show Promise D</button>
