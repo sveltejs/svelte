a30c935eb7f509001a2a402822de023fa3a1f403
Simon Holthausen <simon.holthausen@vercel.com>
2024-06-17 13:06:21 +0200

    fix: handle `is` attribute on elements with spread
    The `is` attribute is special and always needs to be part of the static template string, or else it wouldn't be taken into account on initialization
    fixes #12052

---
+++ packages/svelte/tests/runtime-runes/samples/element-is-attribute/main.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" context="module">
+	if (!customElements.get('x-button')) {
+		class XButton extends HTMLButtonElement {
+			connectedCallback() {
+				this.addEventListener('click', () => console.log('works'));
+			}
+		}
+
+		customElements.define('x-button', XButton, { extends: 'button' });
+	}
+</script>
+
+<script lang="ts">
+	let { ...props } = $props();
+</script>
+
+<button is="x-button">click me</button>
+<button {...props} is="x-button">click me</button>
