e9d49c895067d834ed86b4b296a17316fb80267c
Dominic Gannaway <dg@domgan.com>
2024-06-18 12:48:02 +0100

    fix: improve compiler attribute validation logic

--- packages/svelte/tests/runtime-runes/samples/event-on-3/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/event-on-3/main.svelte
@@ -1,23 +1,5 @@
 <script>
-	import { on } from 'svelte/events';
-
-	let count = $state(0);
-
-	function increment(e) {
-		e.stopPropagation();
-		count += 1;
-	}
-
-	let sectionEl
-	$effect(() => {
-		return on(sectionEl, 'click', () => {
-			console.log('logged from addEventListener');
-		});
-	});
+	function f() {}
 </script>
 
-<section bind:this={sectionEl} onclick={() => console.log('logged from onclick')}>
-	<button onclick={increment}>
-		clicks: {count}
-	</button>
-</section>
+<input onchange={f}>{f}
