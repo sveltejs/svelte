0703693bc5a498d4f395aec27b2b4a14cb6c63ad
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-06-27 10:20:09 +0330

    simplified fine-grainability by using deriveds

--- packages/svelte/src/reactivity/date.js
+++ packages/svelte/src/reactivity/date.js
@@ -1,3 +1,7 @@
+/**
+ * @import { Source } from "#client"
+ */
+import { derived } from '../internal/client/index.js';
 import { source, set } from '../internal/client/reactivity/sources.js';
 import { get } from '../internal/client/runtime.js';
 
@@ -8,16 +12,14 @@ import { get } from '../internal/client/runtime.js';
 var inited = false;
 export class ReactiveDate extends Date {
 	/**
-	 * @type {Date}
+	 * @type {Source<number>}
 	 */
-	#modified_date_to_compare;
+	#raw_time = source(super.getTime());
 
 	/**
-	 * @type {Map<DateMethodNames, import("#client").Source<boolean>>}
+	 * @type {Map<DateMethodNames, Source<unknown>>}
 	 */
-	#signals = new Map();
-
-	#version = source(false);
+	#deriveds = new Map();
 
 	/**
 	 * @param {any[]} params
@@ -25,8 +27,6 @@ export class ReactiveDate extends Date {
 	constructor(...params) {
 		// @ts-ignore
 		super(...params);
-		// @ts-ignore
-		this.#modified_date_to_compare = new Date(...params);
 		this.#init();
 	}
 
@@ -36,7 +36,7 @@ export class ReactiveDate extends Date {
 		}
 
 		inited = true;
-		var proto = ReactiveDate.prototype;
+		var reactive_date_proto = ReactiveDate.prototype;
 		var date_proto = Date.prototype;
 
 		/**
@@ -45,7 +45,6 @@ export class ReactiveDate extends Date {
 		var versioned_read_props = [
 			'getTimezoneOffset',
 			'getTime',
-			'toDateString',
 			'toISOString',
 			'toLocaleString',
 			'toJSON',
@@ -67,24 +66,29 @@ export class ReactiveDate extends Date {
 
 		for (const method of fine_grained_read_props) {
 			// @ts-ignore
-			proto[method] = function (...args) {
+			reactive_date_proto[method] = function (...args) {
 				// methods like getMinutes and getUTCMinutes can use the same signal
-				var merged_method_name = strip_local_and_utc(fine_grained_read_props, method);
-				var sig = this.#signals.get(merged_method_name);
+				// we strip getUTCMinutes to getMinutes so that they always result into the same signal
+				var stripped_method_name = strip_local_and_utc(fine_grained_read_props, method);
+				var sig = this.#deriveds.get(stripped_method_name);
 				if (!sig) {
-					sig = source(false);
-					this.#signals.set(merged_method_name, sig);
+					sig = derived(() => {
+						get(this.#raw_time);
+						// @ts-ignore
+						return date_proto[stripped_method_name].apply(this, args);
+					});
+					this.#deriveds.set(stripped_method_name, sig);
 				}
-				get(sig);
+				var result = get(sig);
 				// @ts-ignore
-				return date_proto[method].apply(this, args);
+				return method === stripped_method_name ? result : date_proto[method].apply(this, args);
 			};
 		}
 
 		for (const method of versioned_read_props) {
 			// @ts-ignore
-			proto[method] = function (...args) {
-				get(this.#version);
+			reactive_date_proto[method] = function (...args) {
+				get(this.#raw_time);
 				// @ts-ignore
 				return date_proto[method].apply(this, args);
 			};
@@ -92,103 +96,16 @@ export class ReactiveDate extends Date {
 
 		for (const method of write_props) {
 			// @ts-ignore
-			proto[method] = function (...args) {
+			reactive_date_proto[method] = function (...args) {
 				// @ts-ignore
-				var v = date_proto[method].apply(this, args);
-				this.#notify_datetime_changes();
-				// @ts-ignore
-				this.#modified_date_to_compare[method](...args);
-				return v;
+				var result = date_proto[method].apply(this, args);
+				var new_time = date_proto.getTime.call(this);
+				if (this.#raw_time.v !== new_time) {
+					set(this.#raw_time, date_proto.getTime.call(this));
+				}
+				return result;
 			};
 		}
-	}
-
-	/**
-	 * we have to have another date instance to compare it with, because setting `X` might or might not affect `Y`
-	 * for instance calling `date.setMonth(55)` will also change the `date.getYear()`
-	 * but calling `date.setMonth(1)` (assuming its not 12) will not increase the year.
-	 * we could check all of these edge-cases but I think that might become complicated very soon and introduce more bugs
-	 * also there is the possibility of these behaviors to change as well,
-	 * so using another date instance and applying the change is a better idea I guess
-	 */
-	#notify_datetime_changes() {
-		if (this.#modified_date_to_compare.getTime() == super.getTime()) {
-			return;
-		}
-
-		var is_date_changed = false;
-		var is_time_changed = false;
-
-		if (super.getFullYear() !== this.#modified_date_to_compare.getFullYear()) {
-			this.#increment_signal('getFullYear');
-			is_date_changed = true;
-		}
-
-		// @ts-expect-error
-		if (super.getYear && super.getYear() !== this.#modified_date_to_compare.getYear()) {
-			// @ts-expect-error
-			this.#increment_signal('getYear');
-			is_date_changed = true;
-		}
-
-		if (super.getMonth() !== this.#modified_date_to_compare.getMonth()) {
-			this.#increment_signal('getMonth');
-			is_date_changed = true;
-		}
-
-		if (super.getDate() !== this.#modified_date_to_compare.getDate()) {
-			this.#increment_signal('getDate');
-			is_date_changed = true;
-		}
-
-		if (super.getDay() !== this.#modified_date_to_compare.getDay()) {
-			this.#increment_signal('getDay');
-			is_date_changed = true;
-		}
-
-		if (super.getHours() !== this.#modified_date_to_compare.getHours()) {
-			this.#increment_signal('getHours');
-			is_time_changed = true;
-		}
-
-		if (super.getMinutes() !== this.#modified_date_to_compare.getMinutes()) {
-			this.#increment_signal('getMinutes');
-			is_time_changed = true;
-		}
-
-		if (super.getSeconds() !== this.#modified_date_to_compare.getSeconds()) {
-			this.#increment_signal('getSeconds');
-			is_time_changed = true;
-		}
-
-		if (super.getMilliseconds() !== this.#modified_date_to_compare.getMilliseconds()) {
-			this.#increment_signal('getMilliseconds');
-			is_time_changed = true;
-		}
-
-		if (is_time_changed) {
-			this.#increment_signal('toTimeString');
-		}
-
-		if (is_date_changed) {
-			this.#increment_signal('toDateString');
-		}
-
-		if (is_time_changed || is_date_changed) {
-			set(this.#version, !this.#version.v);
-		}
-	}
-
-	/**
-	 * @param  {DateMethodNames} method_name
-	 */
-	#increment_signal(method_name) {
-		var signal = this.#signals.get(method_name);
-		if (!signal) {
-			return;
-		}
-		// not using a number intentionally because its enough (it will increment the internal signal's version)
-		set(signal, !signal.v);
 	}
 }
 
