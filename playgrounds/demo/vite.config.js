fa4de2a8f7fb8fb5f20dfad8e30b5b253813af19
Rich Harris <rich.harris@vercel.com>
2024-07-01 13:32:30 -0400

    chore: make it easier to toggle HMR in playground

--- playgrounds/demo/vite.config.js
+++ playgrounds/demo/vite.config.js
@@ -2,11 +2,22 @@ import { defineConfig } from 'vite';
 import inspect from 'vite-plugin-inspect';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
+const hmr = true;
+
 export default defineConfig({
 	build: {
 		minify: false
 	},
-	plugins: [inspect(), svelte()],
+
+	plugins: [
+		inspect(),
+		svelte({
+			compilerOptions: {
+				hmr
+			}
+		})
+	],
+
 	optimizeDeps: {
 		// svelte is a local workspace package, optimizing it would require dev server restarts with --force for every change
 		exclude: ['svelte']
