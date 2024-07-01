04f1208b43c21137e1138daa08dbcc5f456310cf
Dominic Gannaway <dg@domgan.com>
2024-07-01 17:25:13 +0100

    add runtime errors

--- packages/svelte/src/reactivity/index-client.js
+++ packages/svelte/src/reactivity/index-client.js
@@ -5,27 +5,27 @@ export { SvelteURL, SvelteURLSearchParams } from './url.js';
 
 /** @deprecated Use `SvelteDate` instead */
 function DeprecatedDate() {
-	throw new Error('Date is deprecated, using SvelteDate instead.');
+	throw new Error('Date is deprecated, use SvelteDate instead.');
 }
 
 /** @deprecated Use `SvelteSet` instead */
 function DeprecatedSet() {
-	throw new Error('Set is deprecated, using SvelteSet instead.');
+	throw new Error('Set is deprecated, use SvelteSet instead.');
 }
 
 /** @deprecated Use `SvelteMap` instead */
 function DeprecatedMap() {
-	throw new Error('Map is deprecated, using DeprecatedMap instead.');
+	throw new Error('Map is deprecated, use DeprecatedMap instead.');
 }
 
 /** @deprecated Use `SvelteURL` instead */
 function DeprecatedURL() {
-	throw new Error('URL is deprecated, using DeprecatedURL instead.');
+	throw new Error('URL is deprecated, use DeprecatedURL instead.');
 }
 
 /** @deprecated Use `SvelteURLSearchParams` instead */
 function DeprecatedURLSearchParams() {
-	throw new Error('URLSearchParams is deprecated, using SvelteURLSearchParams instead.');
+	throw new Error('URLSearchParams is deprecated, use SvelteURLSearchParams instead.');
 }
 
 // Deprecated
