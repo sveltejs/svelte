54d89a9078d2ea9eb2955f377b3bc1f902e5002b
Dominic Gannaway <dg@domgan.com>
2024-07-04 11:26:25 +0100

    fix: bail out of event hoisting when referencing store subscriptions

---
+++ packages/svelte/tests/runtime-runes/samples/event-store-no-hoisting/main.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { writable } from 'svelte/store';
+
+	let store = $state();
+
+	function setStore() {
+		store = writable(0, () => {
+			console.log('start');
+			return () => console.log('stop');
+		});
+	}
+</script>
+
+<button onclick={setStore}>set new store</button>
+<button onclick={() => $store++}>incr</button>
+
+<pre>{$store}</pre>
