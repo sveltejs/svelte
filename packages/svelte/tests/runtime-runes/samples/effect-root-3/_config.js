b985eea817c1b1cddf10d9ad9b6c0d3b2e0f4b8c
Dominic Gannaway <dg@domgan.com>
2024-07-05 14:46:49 +0100

    add test

---
+++ packages/svelte/tests/runtime-runes/samples/effect-root-3/_config.js
@@ -0,0 +1,28 @@
+import { flushSync } from 'svelte';
+import { test } from '../../test';
+
+export default test({
+	async test({ assert, target, logs }) {
+		const [b1, b2, b3] = target.querySelectorAll('button');
+
+		flushSync(() => {
+			b1.click();
+			b2.click();
+		});
+
+		assert.deepEqual(logs, [0, 1]);
+
+		flushSync(() => {
+			b3.click();
+		});
+
+		assert.deepEqual(logs, [0, 1, 'cleanup 1', 'cleanup 2']);
+
+		flushSync(() => {
+			b1.click();
+			b2.click();
+		});
+
+		assert.deepEqual(logs, [0, 1, 'cleanup 1', 'cleanup 2']);
+	}
+});
