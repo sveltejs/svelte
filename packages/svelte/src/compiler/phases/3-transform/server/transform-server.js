243c4b78b163140c81095483bb1121b1c988baf3
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-07-08 16:54:21 +0200

    fix: ensure `$effect.root` is ignored on the server (#12332)
    Ignore the contents of the effect root, just return a noop where necessary
    
    fixes #12322

--- packages/svelte/src/compiler/phases/3-transform/server/transform-server.js
+++ packages/svelte/src/compiler/phases/3-transform/server/transform-server.js
@@ -412,11 +412,8 @@ const global_visitors = {
 		}
 
 		if (rune === '$effect.root') {
-			const args = /** @type {import('estree').Expression[]} */ (
-				node.arguments.map((arg) => context.visit(arg))
-			);
-			// Just call the function directly
-			return b.call(args[0]);
+			// ignore $effect.root() calls, just return a noop which mimics the cleanup function
+			return b.arrow([], b.block([]));
 		}
 
 		if (rune === '$state.snapshot') {
