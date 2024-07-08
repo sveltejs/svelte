e8c3729fc94ae948983368a50c4745fc04627496
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-07-08 18:27:31 +0200

    fix: make `$state` component exports settable (#12345)
    * fix: make `$state` component exports settable
    
    fixes #11983
    
    * failing test
    
    * fix
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>

--- packages/svelte/tests/runtime-runes/samples/exports-4/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/exports-4/main.svelte
@@ -4,4 +4,4 @@
 </script>
 
 <Sub bind:this={sub} />
-<button on:click={() => sub.increment()}>{sub?.count} / {sub?.doubled}</button>
+<button on:click={() => sub.count++}>{sub?.count} / {sub?.doubled}</button>
