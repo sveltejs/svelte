f0cd5093996eca64d67cf7b8183b52f1de6beb20
Rich Harris <rich.harris@vercel.com>
2024-07-09 17:10:59 -0400

    reinstate optimisation, but without the bug

--- packages/svelte/src/internal/client/dom/css.js
+++ packages/svelte/src/internal/client/dom/css.js
@@ -1,10 +1,19 @@
+import { DEV } from 'esm-env';
 import { queue_micro_task } from './task.js';
+
+var seen = new Set();
 
 /**
  * @param {Node} anchor
  * @param {{ hash: string, code: string }} css
  */
 export function append_styles(anchor, css) {
+	// in dev, always check the DOM, so that styles can be replaced with HMR
+	if (!DEV) {
+		if (seen.has(css)) return;
+		seen.add(css);
+	}
+
 	// Use `queue_micro_task` to ensure `anchor` is in the DOM, otherwise getRootNode() will yield wrong results
 	queue_micro_task(() => {
 		var root = anchor.getRootNode();
