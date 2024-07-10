cfd2a6324493dab346aac1fd8c04d848d31bfb53
Rich Harris <rich.harris@vercel.com>
2024-07-09 22:39:33 -0400

    fix: avoid sending blog post text twice in data payload

--- sites/svelte.dev/src/lib/server/docs/types.d.ts
+++ sites/svelte.dev/src/lib/server/docs/types.d.ts
@@ -6,7 +6,6 @@ export interface Section {
 	// Currently, we are only going with 2 level headings, so this will be undefined. In future, we may want to support 3 levels, in which case this will be a list of sections
 	sections?: Section[];
 	breadcrumbs: string[];
-	text: string;
 }
 
 export type Category = {
