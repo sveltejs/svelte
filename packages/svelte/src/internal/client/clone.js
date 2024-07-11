39275bc340b665aa984826c28c2a6cd6e82df034
Rich Harris <rich.harris@vercel.com>
2024-07-11 14:53:37 -0400

    tweak

--- packages/svelte/src/internal/client/clone.js
+++ packages/svelte/src/internal/client/clone.js
@@ -1,11 +1,4 @@
-import { STATE_SYMBOL } from './constants.js';
-import {
-	define_property,
-	get_descriptors,
-	get_prototype_of,
-	is_array,
-	object_prototype
-} from './utils.js';
+import { get_prototype_of, is_array, object_prototype } from './utils.js';
 
 /**
  * @template T
@@ -25,39 +18,32 @@ export function snapshot(value) {
  * @returns {Record<string | symbol, any>}
  */
 function clone(value, cloned) {
-	if (typeof value === 'object' && value != null) {
+	if (typeof value === 'object' && value !== null) {
 		const unwrapped = cloned.get(value);
-		if (unwrapped !== undefined) {
-			return unwrapped;
-		}
+		if (unwrapped !== undefined) return unwrapped;
 
 		if (is_array(value)) {
 			/** @type {Record<string | symbol, any>} */
-			const array = [];
-			cloned.set(value, array);
+			const copy = [];
+			cloned.set(value, copy);
+
 			for (const element of value) {
-				array.push(clone(element, cloned));
+				copy.push(clone(element, cloned));
 			}
-			return array;
-		} else if (get_prototype_of(value) === object_prototype) {
+
+			return copy;
+		}
+
+		if (get_prototype_of(value) === object_prototype) {
 			/** @type {Record<string | symbol, any>} */
-			const obj = {};
-			const keys = Reflect.ownKeys(value);
-			const descriptors = get_descriptors(value);
-			cloned.set(value, obj);
+			const copy = {};
+			cloned.set(value, copy);
 
-			for (const key of keys) {
-				if (key === STATE_SYMBOL) continue;
-				if (descriptors[key].get) {
-					define_property(obj, key, descriptors[key]);
-				} else {
-					/** @type {T} */
-					const property = value[key];
-					obj[key] = clone(property, cloned);
-				}
+			for (var key in value) {
+				copy[key] = clone(value[key], cloned);
 			}
 
-			return obj;
+			return copy;
 		}
 
 		if (typeof value.toJSON === 'function') {
