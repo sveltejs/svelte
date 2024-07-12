88ee4906a601b414972263cd6732d4f6c7a2b889
Simon Holthausen <simon.holthausen@vercel.com>
2024-07-12 17:38:58 +0200

    fix

--- packages/svelte/src/compiler/phases/2-analyze/index.js
+++ packages/svelte/src/compiler/phases/2-analyze/index.js
@@ -442,6 +442,7 @@ export function analyze_component(root, source, options) {
 				has_props_rune: false,
 				component_slots: new Set(),
 				expression: null,
+				render_tag: null,
 				private_derived_state: [],
 				function_depth: scope.function_depth
 			};
@@ -513,6 +514,7 @@ export function analyze_component(root, source, options) {
 				reactive_statements: analysis.reactive_statements,
 				component_slots: new Set(),
 				expression: null,
+				render_tag: null,
 				private_derived_state: [],
 				function_depth: scope.function_depth
 			};
