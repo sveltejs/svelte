40d119158c584b52cdcc501409c0939690509da2
Rich Harris <rich.harris@vercel.com>
2024-07-12 10:57:33 -0400

    test hydration

---
+++ packages/svelte/tests/hydration/samples/snippet-raw-hydrate/main.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { createRawSnippet } from 'svelte';
+
+	const snippet = createRawSnippet({
+		mount() {
+			const p = document.createElement('p');
+			p.textContent = 'mounted';
+			return p;
+		},
+		hydrate(p) {
+			p.textContent = 'hydrated';
+		},
+		render() {
+			return `<p>rendered</p>`;
+		}
+	});
+</script>
+
+{@render snippet()}
---
+++ packages/svelte/tests/hydration/samples/snippet-raw-mount/main.svelte
@@ -0,0 +1,16 @@
+<script>
+	import { createRawSnippet } from 'svelte';
+
+	const snippet = createRawSnippet({
+		mount() {
+			const p = document.createElement('p');
+			p.textContent = 'mounted';
+			return p;
+		},
+		render() {
+			return `<p>rendered</p>`;
+		}
+	});
+</script>
+
+{@render snippet()}
