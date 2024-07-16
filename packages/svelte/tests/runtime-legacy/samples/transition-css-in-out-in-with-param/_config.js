fb5ab8241b446ab3580446bd4132d369b8a2268b
Rich Harris <rich.harris@vercel.com>
2024-07-16 09:18:00 -0400

    fix test

--- packages/svelte/tests/runtime-legacy/samples/transition-css-in-out-in-with-param/_config.js
+++ packages/svelte/tests/runtime-legacy/samples/transition-css-in-out-in-with-param/_config.js
@@ -1,22 +1,24 @@
 import { ok, test } from '../../test';
 
 export default test({
-	skip: true,
 	test({ assert, component, target, raf }) {
 		component.visible = true;
 		const div = target.querySelector('div');
 		ok(div);
 
-		assert.equal(div.style.opacity, '0');
+		assert.equal(div.style.color, 'blue');
 
 		component.visible = false;
-		assert.equal(div.style.opacity, '1');
+		assert.equal(div.style.color, 'yellow');
 
 		// change param
 		raf.tick(1);
 		component.param = true;
 		component.visible = true;
 
-		assert.equal(div.style.opacity, '1');
+		assert.equal(div.style.color, 'red');
+
+		component.visible = false;
+		assert.equal(div.style.color, 'green');
 	}
 });
