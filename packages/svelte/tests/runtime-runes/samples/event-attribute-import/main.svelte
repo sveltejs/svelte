cb728f062eb79df92526225f66515772369e86da
Rich Harris <rich.harris@vercel.com>
2024-07-23 15:18:45 -0400

    we have .svelte.js files

--- packages/svelte/tests/runtime-runes/samples/event-attribute-import/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/event-attribute-import/main.svelte
@@ -1,7 +1,7 @@
 <script>
-	import {handler, log_a, log_b} from './event.svelte';
+	import { handler, log_a, log_b } from './event.svelte.js';
 </script>
 
 <button onclick={handler.value}>click</button>
-<button onclick={() => handler.value = log_b}>change</button>
-<button onclick={() => handler.value = log_a}>change back</button>
+<button onclick={() => (handler.value = log_b)}>change</button>
+<button onclick={() => (handler.value = log_a)}>change back</button>
