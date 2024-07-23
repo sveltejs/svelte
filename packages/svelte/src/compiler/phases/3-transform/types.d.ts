ef2dcd0e3da321d4b8698a3a6afa2ba52acc2dc1
Rich Harris <rich.harris@vercel.com>
2024-07-23 14:51:45 -0400

    comment

--- packages/svelte/src/compiler/phases/3-transform/types.d.ts
+++ packages/svelte/src/compiler/phases/3-transform/types.d.ts
@@ -1,5 +1,5 @@
 import type { Scope } from '../scope.js';
-import type { Binding, SvelteNode, ValidatedModuleCompileOptions } from '#compiler';
+import type { SvelteNode, ValidatedModuleCompileOptions } from '#compiler';
 import type { Analysis } from '../types.js';
 import type { Expression, Identifier } from 'estree';
 
@@ -8,5 +8,9 @@ export interface TransformState {
 	readonly options: ValidatedModuleCompileOptions;
 	readonly scope: Scope;
 	readonly scopes: Map<SvelteNode, Scope>;
+	/**
+	 * A map of `[name, node]` pairs, where `Identifier` nodes matching `name`
+	 * will be replaced with `node` (e.g. `x` -> `$.get(x)`)
+	 */
 	readonly getters: Record<string, Expression | ((id: Identifier) => Expression)>;
 }
