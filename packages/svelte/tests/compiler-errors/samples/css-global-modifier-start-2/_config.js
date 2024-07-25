03737b39eb83fa3bc203b0316a67a23729bb72d5
Rich Harris <rich.harris@vercel.com>
2024-07-25 12:39:08 -0400

    update tests

--- packages/svelte/tests/compiler-errors/samples/css-global-modifier-start-2/_config.js
+++ packages/svelte/tests/compiler-errors/samples/css-global-modifier-start-2/_config.js
@@ -3,7 +3,7 @@ import { test } from '../../test';
 export default test({
 	error: {
 		code: 'css_global_block_invalid_modifier_start',
-		message: 'A `:global` selector at the very beginning cannot be modified by other selectors',
+		message: 'A `:global` selector can only be modified if it is a descendant of other selectors',
 		position: [147, 148]
 	}
 });
