ae8c20b11f6b270bdfd4f73f3d884935860c62b8
Rich Harris <rich.harris@vercel.com>
2024-07-29 12:11:59 -0400

    tidy up

--- packages/svelte/src/compiler/phases/2-analyze/visitors/CallExpression.js
+++ packages/svelte/src/compiler/phases/2-analyze/visitors/CallExpression.js
@@ -1,6 +1,5 @@
 /** @import { CallExpression } from 'estree' */
 /** @import { SvelteNode } from '#compiler' */
-/** @import { Scope } from '../../scope' */
 /** @import { Context } from '../types' */
 import { get_rune } from '../../scope.js';
 import * as e from '../../../errors.js';
@@ -11,107 +10,106 @@ import { get_parent } from '../../../utils/ast.js';
  * @param {Context} context
  */
 export function CallExpression(node, context) {
+	const parent = /** @type {SvelteNode} */ (get_parent(context.path, -1));
+
 	const rune = get_rune(node, context.state.scope);
 
-	if (rune === '$bindable' && node.arguments.length > 1) {
-		e.rune_invalid_arguments_length(node, '$bindable', 'zero or one arguments');
-	} else if (rune === '$host') {
-		if (node.arguments.length > 0) {
-			e.rune_invalid_arguments(node, '$host');
-		} else if (context.state.ast_type === 'module' || !context.state.analysis.custom_element) {
-			e.host_invalid_placement(node);
-		}
-	}
+	switch (rune) {
+		case null:
+			break;
 
-	validate_call_expression(node, context.state.scope, context.path);
+		case '$bindable':
+			if (node.arguments.length > 1) {
+				e.rune_invalid_arguments_length(node, '$bindable', 'zero or one arguments');
+			}
+
+			if (parent.type !== 'AssignmentPattern' || context.path.at(-3)?.type !== 'ObjectPattern') {
+				e.bindable_invalid_location(node);
+			}
+
+			const declarator = context.path.at(-4);
+			if (
+				declarator?.type !== 'VariableDeclarator' ||
+				get_rune(declarator.init, context.state.scope) !== '$props'
+			) {
+				e.bindable_invalid_location(node);
+			}
+
+			break;
+
+		case '$host':
+			if (node.arguments.length > 0) {
+				e.rune_invalid_arguments(node, '$host');
+			} else if (context.state.ast_type === 'module' || !context.state.analysis.custom_element) {
+				e.host_invalid_placement(node);
+			}
+			break;
+
+		case '$props':
+			if (parent.type !== 'VariableDeclarator') {
+				e.props_invalid_placement(node);
+			}
+			break;
+
+		case '$state':
+		case '$state.frozen':
+		case '$derived':
+		case '$derived.by':
+			if (
+				parent.type !== 'VariableDeclarator' &&
+				!(parent.type === 'PropertyDefinition' && !parent.static && !parent.computed)
+			) {
+				e.state_invalid_placement(node, rune);
+			}
+			break;
+
+		case '$effect':
+		case '$effect.pre':
+			if (parent.type !== 'ExpressionStatement') {
+				e.effect_invalid_placement(node);
+			}
+
+			if (node.arguments.length !== 1) {
+				e.rune_invalid_arguments_length(node, rune, 'exactly one argument');
+			}
+			break;
+
+		case '$effect.tracking':
+			if (node.arguments.length !== 0) {
+				e.rune_invalid_arguments(node, rune);
+			}
+			break;
+
+		case '$effect.root':
+			if (node.arguments.length !== 1) {
+				e.rune_invalid_arguments_length(node, rune, 'exactly one argument');
+			}
+			break;
+
+		case '$inspect':
+			if (node.arguments.length < 1) {
+				e.rune_invalid_arguments_length(node, rune, 'one or more arguments');
+			}
+			break;
+
+		case '$inspect().with':
+			if (node.arguments.length !== 1) {
+				e.rune_invalid_arguments_length(node, rune, 'exactly one argument');
+			}
+			break;
+
+		case '$state.snapshot':
+			if (node.arguments.length !== 1) {
+				e.rune_invalid_arguments_length(node, rune, 'exactly one argument');
+			}
+			break;
+
+		case '$state.is':
+			if (node.arguments.length !== 2) {
+				e.rune_invalid_arguments_length(node, rune, 'exactly two arguments');
+			}
+			break;
+	}
 
 	context.next();
-}
-
-/**
- * @param {CallExpression} node
- * @param {Scope} scope
- * @param {SvelteNode[]} path
- * @returns
- */
-function validate_call_expression(node, scope, path) {
-	const rune = get_rune(node, scope);
-	if (rune === null) return;
-
-	const parent = /** @type {SvelteNode} */ (get_parent(path, -1));
-
-	if (rune === '$props') {
-		if (parent.type === 'VariableDeclarator') return;
-		e.props_invalid_placement(node);
-	}
-
-	if (rune === '$bindable') {
-		if (parent.type === 'AssignmentPattern' && path.at(-3)?.type === 'ObjectPattern') {
-			const declarator = path.at(-4);
-			if (
-				declarator?.type === 'VariableDeclarator' &&
-				get_rune(declarator.init, scope) === '$props'
-			) {
-				return;
-			}
-		}
-		e.bindable_invalid_location(node);
-	}
-
-	if (
-		rune === '$state' ||
-		rune === '$state.frozen' ||
-		rune === '$derived' ||
-		rune === '$derived.by'
-	) {
-		if (parent.type === 'VariableDeclarator') return;
-		if (parent.type === 'PropertyDefinition' && !parent.static && !parent.computed) return;
-		e.state_invalid_placement(node, rune);
-	}
-
-	if (rune === '$effect' || rune === '$effect.pre') {
-		if (parent.type !== 'ExpressionStatement') {
-			e.effect_invalid_placement(node);
-		}
-
-		if (node.arguments.length !== 1) {
-			e.rune_invalid_arguments_length(node, rune, 'exactly one argument');
-		}
-	}
-
-	if (rune === '$effect.tracking') {
-		if (node.arguments.length !== 0) {
-			e.rune_invalid_arguments(node, rune);
-		}
-	}
-
-	if (rune === '$effect.root') {
-		if (node.arguments.length !== 1) {
-			e.rune_invalid_arguments_length(node, rune, 'exactly one argument');
-		}
-	}
-
-	if (rune === '$inspect') {
-		if (node.arguments.length < 1) {
-			e.rune_invalid_arguments_length(node, rune, 'one or more arguments');
-		}
-	}
-
-	if (rune === '$inspect().with') {
-		if (node.arguments.length !== 1) {
-			e.rune_invalid_arguments_length(node, rune, 'exactly one argument');
-		}
-	}
-
-	if (rune === '$state.snapshot') {
-		if (node.arguments.length !== 1) {
-			e.rune_invalid_arguments_length(node, rune, 'exactly one argument');
-		}
-	}
-
-	if (rune === '$state.is') {
-		if (node.arguments.length !== 2) {
-			e.rune_invalid_arguments_length(node, rune, 'exactly two arguments');
-		}
-	}
 }
