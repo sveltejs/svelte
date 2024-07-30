5acb51bb2881c0539ba84a15e9de5506b908cb27
Rich Harris <rich.harris@vercel.com>
2024-07-30 08:49:25 -0400

    more

--- packages/svelte/src/compiler/phases/2-analyze/visitors/Identifier.js
+++ packages/svelte/src/compiler/phases/2-analyze/visitors/Identifier.js
@@ -9,13 +9,16 @@ import { Runes } from '../../constants.js';
  * @param {Context} context
  */
 export function Identifier(node, context) {
-	if (context.state.analysis.runes) {
-		let i = context.path.length;
-		let parent = /** @type {Expression} */ (context.path[--i]);
+	let i = context.path.length;
+	let parent = /** @type {Expression} */ (context.path[--i]);
 
+	if (!is_reference(node, parent)) {
+		return;
+	}
+
+	if (context.state.analysis.runes) {
 		if (
 			Runes.includes(/** @type {Runes[number]} */ (node.name)) &&
-			is_reference(node, parent) &&
 			context.state.scope.get(node.name) === null &&
 			context.state.scope.get(node.name.slice(1)) === null
 		) {
@@ -44,72 +47,69 @@ export function Identifier(node, context) {
 			}
 		}
 	} else {
-		const parent = /** @type {Node} */ (context.path.at(-1));
-		if (is_reference(node, parent)) {
-			if (node.name === '$$props') {
-				context.state.analysis.uses_props = true;
-			}
+		if (node.name === '$$props') {
+			context.state.analysis.uses_props = true;
+		}
 
-			if (node.name === '$$restProps') {
-				context.state.analysis.uses_rest_props = true;
-			}
+		if (node.name === '$$restProps') {
+			context.state.analysis.uses_rest_props = true;
+		}
 
-			if (node.name === '$$slots') {
-				context.state.analysis.uses_slots = true;
-			}
+		if (node.name === '$$slots') {
+			context.state.analysis.uses_slots = true;
+		}
 
-			let binding = context.state.scope.get(node.name);
+		let binding = context.state.scope.get(node.name);
 
-			if (binding?.kind === 'store_sub') {
-				// get the underlying store to mark it as reactive in case it's mutated
-				binding = context.state.scope.get(node.name.slice(1));
-			}
+		if (binding?.kind === 'store_sub') {
+			// get the underlying store to mark it as reactive in case it's mutated
+			binding = context.state.scope.get(node.name.slice(1));
+		}
 
-			if (
-				binding?.kind === 'normal' &&
-				((binding.scope === context.state.instance_scope &&
-					binding.declaration_kind !== 'function') ||
-					binding.declaration_kind === 'import')
-			) {
-				if (binding.declaration_kind === 'import') {
-					if (
-						binding.mutated &&
-						// TODO could be more fine-grained - not every mention in the template implies a state binding
-						(context.state.reactive_statement || context.state.ast_type === 'template') &&
-						parent.type === 'MemberExpression'
-					) {
-						binding.kind = 'legacy_reactive_import';
-					}
-				} else if (
+		if (
+			binding?.kind === 'normal' &&
+			((binding.scope === context.state.instance_scope &&
+				binding.declaration_kind !== 'function') ||
+				binding.declaration_kind === 'import')
+		) {
+			if (binding.declaration_kind === 'import') {
+				if (
 					binding.mutated &&
 					// TODO could be more fine-grained - not every mention in the template implies a state binding
-					(context.state.reactive_statement || context.state.ast_type === 'template')
+					(context.state.reactive_statement || context.state.ast_type === 'template') &&
+					parent.type === 'MemberExpression'
 				) {
-					binding.kind = 'state';
-				} else if (
-					context.state.reactive_statement &&
-					parent.type === 'AssignmentExpression' &&
-					parent.left === binding.node
-				) {
-					binding.kind = 'derived';
+					binding.kind = 'legacy_reactive_import';
 				}
-			} else if (binding?.kind === 'each' && binding.mutated) {
-				// Ensure that the array is marked as reactive even when only its entries are mutated
-				let i = context.path.length;
-				while (i--) {
-					const ancestor = context.path[i];
-					if (
-						ancestor.type === 'EachBlock' &&
-						context.state.analysis.template.scopes.get(ancestor)?.declarations.get(node.name) ===
-							binding
-					) {
-						for (const binding of ancestor.metadata.references) {
-							if (binding.kind === 'normal') {
-								binding.kind = 'state';
-							}
+			} else if (
+				binding.mutated &&
+				// TODO could be more fine-grained - not every mention in the template implies a state binding
+				(context.state.reactive_statement || context.state.ast_type === 'template')
+			) {
+				binding.kind = 'state';
+			} else if (
+				context.state.reactive_statement &&
+				parent.type === 'AssignmentExpression' &&
+				parent.left === binding.node
+			) {
+				binding.kind = 'derived';
+			}
+		} else if (binding?.kind === 'each' && binding.mutated) {
+			// Ensure that the array is marked as reactive even when only its entries are mutated
+			let i = context.path.length;
+			while (i--) {
+				const ancestor = context.path[i];
+				if (
+					ancestor.type === 'EachBlock' &&
+					context.state.analysis.template.scopes.get(ancestor)?.declarations.get(node.name) ===
+						binding
+				) {
+					for (const binding of ancestor.metadata.references) {
+						if (binding.kind === 'normal') {
+							binding.kind = 'state';
 						}
-						break;
 					}
+					break;
 				}
 			}
 		}
