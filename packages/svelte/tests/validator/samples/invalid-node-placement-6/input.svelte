f548bdf35462efeec342c1ecbb22cd144dfe9f68
Simon Holthausen <simon.holthausen@vercel.com>
2024-07-31 22:16:25 +0200

    fix: allow nested `<dt>`/`<dd>` elements if they are within a `<dl>` element
    This introduces a resets array, which means descendants that are forbidden are allowed again, if an element within the resets array is encountered between the tag and the forbidden descendant
    
    fixes #12676

---
+++ packages/svelte/tests/validator/samples/invalid-node-placement-6/input.svelte
@@ -0,0 +1,14 @@
+<dl>
+	<dt>valid</dt>
+	<dd>
+		<!-- dl resets the validation -->
+		<dl>
+			<dt>valid</dt>
+			<dd>valid</dd>
+		</dl>
+		<!-- other tags don't -->
+		<div>
+			<dt>invalid</dt>
+		</div>
+	</dd>
+</dl>
