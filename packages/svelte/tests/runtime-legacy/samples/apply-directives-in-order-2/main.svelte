01e7845180fb6da48f225078ed4dc61f441da592
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-07-31 19:28:28 +0200

    fix: always synchronously call `bind:this` (#12679)
    fixes #12673
    
    #12591 wrongfully applied the "wrap in effect if an action on this element" logic for `bind:this`

--- packages/svelte/tests/runtime-legacy/samples/apply-directives-in-order-2/main.svelte
+++ packages/svelte/tests/runtime-legacy/samples/apply-directives-in-order-2/main.svelte
@@ -1,29 +1,86 @@
 <script>
+	import { onMount } from 'svelte';
+
 	export let value = [];
 
-	function one(elem) { elem.addEventListener('input', () => { value.push('1'); }); }
-	function four(elem) { elem.addEventListener('input', () => { value.push('4'); }); }
-	function eight(elem) { elem.addEventListener('input', () => { value.push('8'); }); }
-	function twelve(elem) { elem.addEventListener('input', () => { value.push('12'); }); }
-	function fifteen(elem) { elem.addEventListener('input', () => { value.push('15'); }); }
-	function seventeen(elem) { elem.addEventListener('input', () => { value.push('17'); }); }
-
-	const foo = {
-		set two(v) { value.push('2'); },
-		set six(v) { value.push('6'); },
-		set nine(v) { value.push('9'); },
-		set eleven(v) { value.push('11'); },
-		set thirteen(v) { value.push('13'); },
-		set sixteen(v) { value.push('16'); },
+	function one(elem) {
+		elem.addEventListener('input', () => {
+			value.push('1');
+		});
+	}
+	function four(elem) {
+		elem.addEventListener('input', () => {
+			value.push('4');
+		});
+	}
+	function eight(elem) {
+		elem.addEventListener('input', () => {
+			value.push('8');
+		});
+	}
+	function twelve(elem) {
+		elem.addEventListener('input', () => {
+			value.push('12');
+		});
+	}
+	function fifteen(elem) {
+		elem.addEventListener('input', () => {
+			value.push('15');
+		});
+	}
+	function seventeen(elem) {
+		elem.addEventListener('input', () => {
+			value.push('17');
+		});
 	}
 
-	function three() { value.push('3'); }
-	function five() { value.push('5'); }
-	function seven() { value.push('7'); }
-	function ten() { value.push('10'); }
-	function fourteen() { value.push('14'); }
-	function eighteen() { value.push('18'); }
+	const foo = {
+		set two(v) {
+			value.push('2');
+		},
+		set six(v) {
+			value.push('6');
+		},
+		set nine(v) {
+			value.push('9');
+		},
+		set eleven(v) {
+			value.push('11');
+		},
+		set thirteen(v) {
+			value.push('13');
+		},
+		set sixteen(v) {
+			value.push('16');
+		}
+	};
 
+	function three() {
+		value.push('3');
+	}
+	function five() {
+		value.push('5');
+	}
+	function seven() {
+		value.push('7');
+	}
+	function ten() {
+		value.push('10');
+	}
+	function fourteen() {
+		value.push('14');
+	}
+	function eighteen() {
+		value.push('18');
+	}
+
+	let el;
+
+	onMount(() => {
+		// ensure that bind:this doesn't influence the order of directives
+		// and isn't affected itself by an action being on the element
+		value.push('bind:this ' + !!el);
+	});
 </script>
 
 <input use:one bind:value={foo.two} on:input={three} />
@@ -31,4 +88,4 @@
 <input on:input={seven} use:eight bind:value={foo.nine} />
 <input on:input={ten} bind:value={foo.eleven} use:twelve />
 <input bind:value={foo.thirteen} on:input={fourteen} use:fifteen />
-<input bind:value={foo.sixteen} use:seventeen on:input={eighteen} />
+<input bind:this={el} bind:value={foo.sixteen} use:seventeen on:input={eighteen} />
