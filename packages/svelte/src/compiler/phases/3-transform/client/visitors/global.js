3ac58938e515b5fc8e23d48c46a2f0aecf40c55b
Rich Harris <rich.harris@vercel.com>
2024-07-31 23:05:21 -0400

    more

--- packages/svelte/src/compiler/phases/3-transform/client/visitors/global.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/global.js
@@ -1,21 +1,31 @@
 /** @import { Visitors } from '../types' */
 import { ArrowFunctionExpression } from './ArrowFunctionExpression.js';
 import { AssignmentExpression } from './AssignmentExpression.js';
+import { Comment } from './Comment.js';
+import { ConstTag } from './ConstTag.js';
+import { DebugTag } from './DebugTag.js';
 import { Fragment } from './Fragment.js';
 import { FunctionDeclaration } from './FunctionDeclaration.js';
 import { FunctionExpression } from './FunctionExpression.js';
+import { HtmlTag } from './HtmlTag.js';
 import { Identifier } from './Identifier.js';
 import { MemberExpression } from './MemberExpression.js';
+import { RenderTag } from './RenderTag.js';
 import { UpdateExpression } from './UpdateExpression.js';
 
 /** @type {Visitors} */
 export const global_visitors = {
 	ArrowFunctionExpression,
 	AssignmentExpression,
+	Comment,
+	ConstTag,
+	DebugTag,
 	Fragment,
 	FunctionDeclaration,
 	FunctionExpression,
+	HtmlTag,
 	Identifier,
 	MemberExpression,
+	RenderTag,
 	UpdateExpression
 };
