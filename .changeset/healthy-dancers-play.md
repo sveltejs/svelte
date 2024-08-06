1942f87ed9e2d51f5a86a982d1492e55182ba10b
Dominic Gannaway <trueadm@users.noreply.github.com>
2024-08-06 16:49:01 +0100

    fix: improve prop binding warning validation for stores (#12745)
    * fix: improve prop binding warning validation for stores
    
    * ts
    
    * address feedback
    
    * add comment
    
    * failing test
    
    * fix/simplify
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>

---
+++ .changeset/healthy-dancers-play.md
@@ -0,0 +1,5 @@
+---
+'svelte': patch
+---
+
+fix: improve prop binding warning validation for stores
--- packages/svelte/src/compiler/phases/3-transform/client/visitors/shared/utils.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/shared/utils.js
@@ -204,28 +204,30 @@ export function build_bind_this(expression, value, { state, visit }) {
  * @param {BindDirective} binding
  * @param {MemberExpression} expression
  */
-export function build_validate_binding(state, binding, expression) {
-	const string = state.analysis.source.slice(binding.start, binding.end);
-
-	const get_object = b.thunk(/** @type {Expression} */ (expression.object));
-	const get_property = b.thunk(
-		/** @type {Expression} */ (
-			expression.computed
-				? expression.property
-				: b.literal(/** @type {Identifier} */ (expression.property).name)
-		)
-	);
+export function validate_binding(state, binding, expression) {
+	// If we are referencing a $store.foo then we don't need to add validation
+	const left = object(binding.expression);
+	const left_binding = left && state.scope.get(left.name);
+	if (left_binding?.kind === 'store_sub') return;
 
 	const loc = locator(binding.start);
 
-	return b.stmt(
-		b.call(
-			'$.validate_binding',
-			b.literal(string),
-			get_object,
-			get_property,
-			loc && b.literal(loc.line),
-			loc && b.literal(loc.column)
+	state.init.push(
+		b.stmt(
+			b.call(
+				'$.validate_binding',
+				b.literal(state.analysis.source.slice(binding.start, binding.end)),
+				b.thunk(/** @type {Expression} */ (expression.object)),
+				b.thunk(
+					/** @type {Expression} */ (
+						expression.computed
+							? expression.property
+							: b.literal(/** @type {Identifier} */ (expression.property).name)
+					)
+				),
+				loc && b.literal(loc.line),
+				loc && b.literal(loc.column)
+			)
 		)
 	);
 }
