c4683eaa5e9219b82b6d666749cccb8db87faf92
Rich Harris <rich.harris@vercel.com>
2024-08-08 16:50:14 -0400

    add assign transformer

--- packages/svelte/src/compiler/phases/3-transform/client/visitors/AssignmentExpression.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/AssignmentExpression.js
@@ -7,5 +7,15 @@ import { build_setter } from '../utils.js';
  * @param {Context} context
  */
 export function AssignmentExpression(node, context) {
+	if (node.left.type === 'Identifier') {
+		if (Object.hasOwn(context.state.transformers, node.left.name)) {
+			const transformer = context.state.transformers[node.left.name]?.assign;
+
+			if (transformer) {
+				return transformer(node, context.visit);
+			}
+		}
+	}
+
 	return build_setter(node, context, context.next);
 }
--- packages/svelte/src/compiler/phases/3-transform/client/visitors/shared/declarations.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/shared/declarations.js
@@ -1,6 +1,6 @@
-/** @import { Identifier } from 'estree' */
+/** @import { BinaryOperator, Expression, Identifier } from 'estree' */
 /** @import { ComponentContext, Context } from '../../types' */
-import { is_state_source } from '../../utils.js';
+import { build_proxy_reassignment, is_state_source, should_proxy_or_freeze } from '../../utils.js';
 import * as b from '../../../../../utils/builders.js';
 
 /**
@@ -24,6 +24,34 @@ export function add_state_transformers(context) {
 		) {
 			context.state.transformers[name] = {
 				read: get_value,
+				assign: (node, visit) => {
+					let left = /** @type {Identifier} */ (node.left);
+					let value = /** @type {Expression} */ (visit(node.right));
+
+					if (node.operator !== '=') {
+						value = b.binary(
+							/** @type {BinaryOperator} */ (node.operator.slice(0, -1)),
+							/** @type {Expression} */ (visit(left)),
+							value
+						);
+					}
+
+					if (context.state.analysis.runes && should_proxy_or_freeze(value, context.state.scope)) {
+						if (binding.kind === 'frozen_state') {
+							value = b.call('$.freeze', value);
+						} else {
+							value = build_proxy_reassignment(value, left.name);
+						}
+					}
+
+					let call = b.call('$.set', left, value);
+
+					if (context.state.scope.get(`$${left.name}`)?.kind === 'store_sub') {
+						call = b.call('$.store_unsub', call, b.literal(`$${left.name}`), b.id('$$stores'));
+					}
+
+					return call;
+				},
 				update: (node) => {
 					return b.call(
 						node.prefix ? '$.update_pre' : '$.update',
