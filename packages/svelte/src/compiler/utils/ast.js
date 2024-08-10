d6e26c0953f0dc899e6bd4267f5bded05483d295
ottomated <31470743+ottomated@users.noreply.github.com>
2024-08-10 04:08:06 -0700

    chore: speed up exclude_from_object (#12783)
    * speed up exclude_from_object
    
    * changeset
    
    * convert prop names to string at compile time
    
    * faster still
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>

--- packages/svelte/src/compiler/utils/ast.js
+++ packages/svelte/src/compiler/utils/ast.js
@@ -276,15 +276,19 @@ function _extract_paths(assignments = [], param, expression, update_expression, 
 					const rest_expression = (object) => {
 						/** @type {ESTree.Expression[]} */
 						const props = [];
+
 						for (const p of param.properties) {
 							if (p.type === 'Property' && p.key.type !== 'PrivateIdentifier') {
 								if (p.key.type === 'Identifier' && !p.computed) {
 									props.push(b.literal(p.key.name));
+								} else if (p.key.type === 'Literal') {
+									props.push(b.literal(String(p.key.value)));
 								} else {
-									props.push(p.key);
+									props.push(b.call('String', p.key));
 								}
 							}
 						}
+
 						return b.call('$.exclude_from_object', expression(object), b.array(props));
 					};
 
