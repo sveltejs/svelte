232cf4a5995bc792cd9b676ea6236ee4e5cee358
Dominic Gannaway <dg@domgan.com>
2024-08-10 13:51:55 +0100

    feedback

--- packages/svelte/src/internal/client/dom/css.js
+++ packages/svelte/src/internal/client/dom/css.js
@@ -1,10 +1,20 @@
+import { DEV } from 'esm-env';
 import { queue_micro_task } from './task.js';
+
+var seen = new Set();
 
 /**
  * @param {Node} anchor
  * @param {{ hash: string, code: string }} css
+ * @param {boolean} is_custom_element
  */
-export function append_styles(anchor, css) {
+export function append_styles(anchor, css, is_custom_element) {
+	// in dev, always check the DOM, so that styles can be replaced with HMR
+	if (!DEV && !is_custom_element) {
+		if (seen.has(css)) return;
+		seen.add(css);
+	}
+
 	// Use `queue_micro_task` to ensure `anchor` is in the DOM, otherwise getRootNode() will yield wrong results
 	queue_micro_task(() => {
 		var root = anchor.getRootNode();
