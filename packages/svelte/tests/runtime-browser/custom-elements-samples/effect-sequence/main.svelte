994430f3d9e26dfeb5a529aa0f9c00f5f4e451c8
Rich Harris <rich.harris@vercel.com>
2024-08-10 12:51:01 -0400

    prettier

--- packages/svelte/tests/runtime-browser/custom-elements-samples/effect-sequence/main.svelte
+++ packages/svelte/tests/runtime-browser/custom-elements-samples/effect-sequence/main.svelte
@@ -1,8 +1,7 @@
 <svelte:options customElement="child-element" />
 
 <script>
-  $effect(() => {
-    $host().dispatchEvent(new CustomEvent('change', { bubbles: true }));
-  })
+	$effect(() => {
+		$host().dispatchEvent(new CustomEvent('change', { bubbles: true }));
+	});
 </script>
-
