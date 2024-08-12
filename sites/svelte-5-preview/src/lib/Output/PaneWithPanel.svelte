c11522caad671cf2ad2ba9543fed820c3b1adb3b
Ottomated <otto@ottomated.net>
2024-08-12 15:22:41 -0700

    fix jank

--- sites/svelte-5-preview/src/lib/Output/PaneWithPanel.svelte
+++ sites/svelte-5-preview/src/lib/Output/PaneWithPanel.svelte
@@ -19,6 +19,7 @@
 
 	const toggle = () => {
 		// The spring might be out of date, so snap it before animating the toggle
+		driver.set(pos, {hard: true})
 
 		if (expanded) {
 			expanded_pos = pos;
@@ -39,6 +40,7 @@
 	priority="max"
 	on:change={(ev) => {
 		pos = Number(ev.detail.substring(0, ev.detail.length - 1));
+		if (pos < 80) expanded_pos = pos;
 	}}
 >
 	<section slot="a">
