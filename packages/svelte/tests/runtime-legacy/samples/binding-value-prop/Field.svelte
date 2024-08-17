65b05e253293b67182d1671e679a2adb90140644
Rich Harris <rich.harris@vercel.com>
2024-08-17 11:07:09 -0400

    oops

--- packages/svelte/tests/runtime-legacy/samples/binding-value-prop/Field.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	export let value;
-</script>
-
-<input type="text" bind:value />
--- packages/svelte/tests/runtime-legacy/samples/binding-value-prop/main.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import Field from './Field.svelte';
-	import { writable } from 'svelte/store';
-
-	const value = writable('aaa');
-
-</script>
-
-<Field bind:value={$value} /> {$value}
