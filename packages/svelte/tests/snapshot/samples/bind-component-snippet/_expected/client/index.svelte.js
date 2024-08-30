b9a762d5e02706045d99db697749af42c2e415c8
paoloricciuti <ricciutipaolo@gmail.com>
2024-08-30 17:59:54 +0200

    fix: hoist imports on top

--- packages/svelte/tests/snapshot/samples/bind-component-snippet/_expected/client/index.svelte.js
+++ packages/svelte/tests/snapshot/samples/bind-component-snippet/_expected/client/index.svelte.js
@@ -1,6 +1,6 @@
 import "svelte/internal/disclose-version";
-import * as $ from "svelte/internal/client";
 import TextInput from './Child.svelte';
+import * as $ from "svelte/internal/client";
 
 var root = $.template(`<!> `, 1);
 
--- packages/svelte/tests/snapshot/samples/imports-in-modules/_expected/client/index.svelte.js
+++ packages/svelte/tests/snapshot/samples/imports-in-modules/_expected/client/index.svelte.js
@@ -1,6 +1,6 @@
 import "svelte/internal/disclose-version";
-import * as $ from "svelte/internal/client";
 import { random } from './module.svelte';
+import * as $ from "svelte/internal/client";
 
 export default function Imports_in_modules($$anchor) {
 	
