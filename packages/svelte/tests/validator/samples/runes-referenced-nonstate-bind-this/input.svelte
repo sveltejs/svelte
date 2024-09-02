197b998a3834075caa94a35ee4adf012b98641bb
Jeremiasz Major <jrh.mjr@gmail.com>
2024-09-02 15:48:31 +0200

    add failing test

--- packages/svelte/tests/validator/samples/runes-referenced-nonstate-bind-this/input.svelte
+++ packages/svelte/tests/validator/samples/runes-referenced-nonstate-bind-this/input.svelte
@@ -1,11 +1,24 @@
 <script>
-	let no_need;
+	let no_need_no_conditional;
+
+	let no_need_no_read1;
+	let no_need_no_read2;
+
 	let does_need1;
 	let does_need2 = $state();
 </script>
 
-<div bind:this={no_need}></div>
+{typeof no_need_no_conditional}
+{typeof does_need1}
+{typeof does_need2}
+
+<div bind:this={no_need_no_conditional}></div>
+
 {#if true}
+	<div bind:this={no_need_no_read1}></div>
+	<div bind:this={no_need_no_read2}></div>
 	<div bind:this={does_need1}></div>
 	<div bind:this={does_need2}></div>
 {/if}
+
+<button onclick={() => console.log(no_need_no_read2)}>foo</button>
