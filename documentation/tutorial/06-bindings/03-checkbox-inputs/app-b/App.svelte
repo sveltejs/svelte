921f0efc0d8fa9be054e973a322634e1d8df3147
Orlando Jimenez <orlandoesparza6@gmail.com>
2024-09-03 11:13:05 -0700

    Update App.svelte: Variable change to make tutorial more comprehensible

--- documentation/tutorial/06-bindings/03-checkbox-inputs/app-b/App.svelte
+++ documentation/tutorial/06-bindings/03-checkbox-inputs/app-b/App.svelte
@@ -1,16 +1,16 @@
 <script>
-	let yes = false;
+	let isChecked = false;
 </script>
 
 <label>
-	<input type="checkbox" bind:checked={yes} />
+	<input type="checkbox" bind:checked={isChecked} />
 	Yes! Send me regular email spam
 </label>
 
-{#if yes}
+{#if isChecked}
 	<p>Thank you. We will bombard your inbox and sell your personal details.</p>
 {:else}
 	<p>You must opt-in to continue. If you're not paying, you're the product.</p>
 {/if}
 
-<button disabled={!yes}> Subscribe </button>
+<button disabled={!isChecked}> Subscribe </button>
