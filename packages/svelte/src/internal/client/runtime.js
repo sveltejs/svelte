722611ea3fad892a98094bd6d8941e8c4ddcc060
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-09-10 14:58:25 +0200

    Update packages/svelte/src/internal/client/runtime.js

--- packages/svelte/src/internal/client/runtime.js
+++ packages/svelte/src/internal/client/runtime.js
@@ -198,7 +198,7 @@ export function check_dirtiness(reaction) {
 
 				// If we are working with an unowned signal as part of an effect (due to !current_skip_reaction)
 				// and the version hasn't changed, we still need to check that this reaction
-				// if linked to the dependency source – otherwise future updates will not be caught.
+				// is linked to the dependency source – otherwise future updates will not be caught.
 				if (
 					is_unowned &&
 					current_effect !== null &&
