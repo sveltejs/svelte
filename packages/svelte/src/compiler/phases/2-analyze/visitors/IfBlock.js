804b7140961e6e8d5c060930481b65c8078aa5d3
Simon H <5968653+dummdidumm@users.noreply.github.com>
2024-09-12 23:07:41 +0200

    fix: correctly validate nested if block tags (#13218)
    Fixes #13210

--- packages/svelte/src/compiler/phases/2-analyze/visitors/IfBlock.js
+++ packages/svelte/src/compiler/phases/2-analyze/visitors/IfBlock.js
@@ -12,15 +12,7 @@ export function IfBlock(node, context) {
 	validate_block_not_empty(node.alternate, context);
 
 	if (context.state.analysis.runes) {
-		const parent = context.path.at(-1);
-		const expected =
-			context.path.at(-2)?.type === 'IfBlock' &&
-			parent?.type === 'Fragment' &&
-			parent.nodes.length === 1
-				? ':'
-				: '#';
-
-		validate_opening_tag(node, context.state, expected);
+		validate_opening_tag(node, context.state, node.elseif ? ':' : '#');
 	}
 
 	mark_subtree_dynamic(context.path);
