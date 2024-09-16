c3d45908fcde1e9ff8a612276f1811652237074c
Simon Holthausen <simon.holthausen@vercel.com>
2024-09-16 10:12:55 +0200

    Add overloads to correct place, put it into dts for now because of https://github.com/microsoft/TypeScript/issues/59980

--- packages/svelte/tests/types/events.ts
+++ packages/svelte/tests/types/events.ts
@@ -4,19 +4,15 @@ import { on } from 'svelte/events';
 
 on(document.body, 'click', (e) => e.button);
 
+on(window, 'click', (e) => e.button);
+
+on(document, 'click', (e) => e.button);
+
 on(
 	document.body,
 	'clidck',
 	(e) =>
 		// @ts-expect-error
-		e.button
-);
-
-on(
-	window,
-	'click',
-	(e) =>
-		// @ts-expect-error ideally we'd know this is a MouseEvent here, too, but for keeping the types sane, we currently don't
 		e.button
 );
 
