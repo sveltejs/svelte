e49017dd593206b5b72f8b780309c2ea10f00efc
Rich Harris <rich.harris@vercel.com>
2024-09-17 11:24:12 -0400

    lint

--- packages/svelte/tests/runtime-browser/samples/mount-in-iframe/_config.js
+++ packages/svelte/tests/runtime-browser/samples/mount-in-iframe/_config.js
@@ -4,12 +4,17 @@ import { test } from '../../assert';
 export default test({
 	async test({ target, assert }) {
 		const button = target.querySelector('button');
-		const h1 = () => target.querySelector('iframe').contentWindow.document.querySelector('h1');
+		const h1 = () =>
+			/** @type {HTMLHeadingElement} */ (
+				/** @type {Window} */ (
+					target.querySelector('iframe')?.contentWindow
+				).document.querySelector('h1')
+			);
 
 		assert.equal(h1().textContent, 'count: 0');
 		assert.equal(getComputedStyle(h1()).color, 'rgb(255, 0, 0)');
 
-		flushSync(() => button.click());
+		flushSync(() => button?.click());
 
 		assert.equal(h1().textContent, 'count: 1');
 		assert.equal(getComputedStyle(h1()).color, 'rgb(255, 0, 0)');
