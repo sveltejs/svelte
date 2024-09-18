9423767c292693534ffe4920d2b7ddc274f265c3
Rich Harris <rich.harris@vercel.com>
2024-09-17 22:16:14 -0400

    revert

--- packages/svelte/src/internal/client/constants.js
+++ packages/svelte/src/internal/client/constants.js
@@ -9,15 +9,15 @@ export const DISCONNECTED = 1 << 8;
 export const CLEAN = 1 << 9;
 export const DIRTY = 1 << 10;
 export const MAYBE_DIRTY = 1 << 11;
-export const INERT = 1 << 13;
-export const DESTROYED = 1 << 14;
-export const EFFECT_RAN = 1 << 15;
+export const INERT = 1 << 12;
+export const DESTROYED = 1 << 13;
+export const EFFECT_RAN = 1 << 14;
 /** 'Transparent' effects do not create a transition boundary */
-export const EFFECT_TRANSPARENT = 1 << 16;
+export const EFFECT_TRANSPARENT = 1 << 15;
 /** Svelte 4 legacy mode props need to be handled with deriveds and be recognized elsewhere, hence the dedicated flag */
-export const LEGACY_DERIVED_PROP = 1 << 17;
-export const INSPECT_EFFECT = 1 << 18;
-export const HEAD_EFFECT = 1 << 19;
+export const LEGACY_DERIVED_PROP = 1 << 16;
+export const INSPECT_EFFECT = 1 << 17;
+export const HEAD_EFFECT = 1 << 18;
 
 export const STATE_SYMBOL = Symbol('$state');
 export const STATE_SYMBOL_METADATA = Symbol('$state metadata');
