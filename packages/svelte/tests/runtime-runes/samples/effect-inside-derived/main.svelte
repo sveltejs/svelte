0188a1ff5fbf43bfc783adab632c96386df69261
Rich Harris <rich.harris@vercel.com>
2024-09-18 13:25:48 -0400

    test

---
+++ packages/svelte/tests/runtime-runes/samples/effect-inside-derived/main.svelte
@@ -0,0 +1,19 @@
+<script>
+	let count = $state(0);
+
+	const { value } = $derived.by(() => {
+		let value = $state(0);
+
+		$effect(() => {
+			value = count;
+		});
+
+		return {
+			get value() {
+				return value;
+			}
+		};
+	});
+</script>
+
+<button onclick={() => (count += 1)}>clicks: {value}</button>
