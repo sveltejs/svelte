00f710d06a5dda974c8c17d58f976e803e2635a2
paoloricciuti <ricciutipaolo@gmail.com>
2024-09-21 18:54:28 +0200

    chore: add test

---
+++ packages/svelte/tests/migrate/samples/effects-with-alias-run/output.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { run as run_1 } from 'svelte/legacy';
+
+	let count = 0;
+	let run = true;
+	run_1(() => {
+		console.log(count);
+	});
+	run_1(() => {
+		if (count > 10 && run) {
+			alert('too high')
+		}
+	});
+	run_1(() => {
+		console.log('foo');
+		if (x) return;
+		console.log('bar');
+	});
+	run_1(() => {
+		$count = 1;
+	});
+</script>
