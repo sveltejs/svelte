b51bcdd4cc838e9df8a59ac767479505464b2d8d
Dominic Gannaway <dg@domgan.com>
2024-09-24 20:10:45 +0100

    fix: ensure use directives execute in the correct sequence

--- packages/svelte/tests/runtime-runes/samples/action-state-arg/_config.js
+++ packages/svelte/tests/runtime-runes/samples/action-state-arg/_config.js
@@ -1,7 +1,24 @@
 import { test } from '../../test';
+import { tick } from 'svelte';
 
 export default test({
-	async test({ assert, logs }) {
-		assert.deepEqual(logs, ['1', '2', '3', '4', '5', '6']);
+	html: `<button>mutate</button><button>reassign</button><div>0</div>`,
+
+	async test({ assert, target }) {
+		const [btn1, btn2] = target.querySelectorAll('button');
+
+		btn1.click();
+		await tick();
+		assert.htmlEqual(
+			target.innerHTML,
+			`<button>mutate</button><button>reassign</button><div>1</div>`
+		);
+
+		btn2.click();
+		await tick();
+		assert.htmlEqual(
+			target.innerHTML,
+			`<button>mutate</button><button>reassign</button><div>2</div>`
+		);
 	}
 });
