b71cad2ca14d1fce8ef41a91bafc4e82856640a1
Dominic Gannaway <dg@domgan.com>
2024-09-24 15:30:43 +0100

    add test

---
+++ packages/svelte/tests/runtime-runes/samples/transition-each/main.svelte
@@ -0,0 +1,30 @@
+<script>
+	function foo(node, params) {
+		return {
+			duration: 100,
+			tick: (t, u) => {
+				node.foo = t;
+			}
+		};
+	}
+
+	let list = $state([]);
+	let id = 0;
+
+	function push() {
+		list.push({ id: id++ })
+	}
+
+	function removeFirst() {
+		list.splice(0, 1);
+	}
+</script>
+
+<button onclick={push}>Push</button>
+<button onclick={removeFirst}>Remove</button>
+
+<ul>
+	{#each list as item (item.id)}
+		<li out:foo>{item.id}</li>
+	{/each}
+</ul>
