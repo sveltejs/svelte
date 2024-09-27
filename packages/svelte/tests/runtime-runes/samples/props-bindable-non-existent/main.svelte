351b3e7aac4096e56b645d73803a04c8a8c55b2a
Oscar Dominguez <dominguez.celada@gmail.com>
2024-09-27 21:53:32 +0200

    fix: warn instead of error when binding a non-existent prop
    Co-authored-by: Paolo Ricciuti <ricciutipaolo@gmail.com>

---
+++ packages/svelte/tests/runtime-runes/samples/props-bindable-non-existent/main.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Input from './Input.svelte';
+    import InputEditable from './InputEditable.svelte';
+
+		const components = [
+			Input,
+			InputEditable
+		]
+
+		const results = $state([{}, {}]);
+</script>
+
+{#each components as Component, i}
+	<Component bind:content={results[i].content} bind:value={results[i].value} />
+{/each}
