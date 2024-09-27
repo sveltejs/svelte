d4230049da0c6f52c9c2d2c3e8ecef6c949af09e
Paolo Ricciuti <ricciutipaolo@gmail.com>
2024-09-27 12:49:28 +0200

    fix: avoid migrating slots in custom elements (#13406)
    They'll be preserved as-is in some future Svelte version

---
+++ packages/svelte/tests/migrate/samples/slots-custom-element/input.svelte
@@ -0,0 +1,23 @@
+<svelte:options customElement="my-element" />
+
+<script>
+	// to show that it doesn't bail out from the whole migration
+	let count = 0;
+</script>
+
+<button on:click={()=>count++}><slot /></button>
+
+{count}
+
+{#if foo}
+	<slot name="foo" {foo} />
+{/if}
+
+{#if $$slots.bar}
+	{$$slots}
+	<slot name="bar" />
+{/if}
+
+{#if $$slots.default}foo{/if}
+
+<slot name="dashed-name" />
---
+++ packages/svelte/tests/migrate/samples/slots-custom-element/output.svelte
@@ -0,0 +1,23 @@
+<svelte:options customElement="my-element" />
+
+<script>
+	// to show that it doesn't bail out from the whole migration
+	let count = $state(0);
+</script>
+
+<button onclick={()=>count++}><slot /></button>
+
+{count}
+
+{#if foo}
+	<slot name="foo" {foo} />
+{/if}
+
+{#if bar}
+	{$$slots}
+	<slot name="bar" />
+{/if}
+
+{#if children}foo{/if}
+
+<slot name="dashed-name" />
