de65e1c7f48442c32aa0050aaeb57a67914e2a1c
paoloricciuti <ricciutipaolo@gmail.com>
2024-10-01 13:08:26 +0200

    chore: fix lint

--- packages/svelte/src/compiler/phases/3-transform/client/transform-client.js
+++ packages/svelte/src/compiler/phases/3-transform/client/transform-client.js
@@ -277,10 +277,7 @@ export function client_component(analysis, options) {
 		}
 
 		if (binding?.kind === 'prop' || binding?.kind === 'bindable_prop') {
-			return [
-				getter,
-				b.set(alias ?? name, [b.stmt(b.call(name, b.id('$$value')))])
-			];
+			return [getter, b.set(alias ?? name, [b.stmt(b.call(name, b.id('$$value')))])];
 		}
 
 		if (binding?.kind === 'state' || binding?.kind === 'raw_state') {
