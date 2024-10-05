83e77ff8e03cee4872040a6d0e1c2229ed51285a
paoloricciuti <ricciutipaolo@gmail.com>
2024-10-05 14:04:24 +0200

    chore: simplify migration comment test

--- packages/svelte/tests/migrate/samples/svelte-self-skip-filename/input.svelte
+++ packages/svelte/tests/migrate/samples/svelte-self-skip-filename/input.svelte
@@ -1,15 +1,3 @@
 {#if false}
 	<svelte:self />
-	<svelte:self with_attributes/>
-	<svelte:self count={count+1}/>
-	<svelte:self>
-		child
-	</svelte:self>
-	<svelte:self count={count+1}>
-		child
-	</svelte:self>
-	<svelte:self count={$$props.count}     >
-		child
-	</svelte:self>
-	<svelte:self></svelte:self>
 {/if}
--- packages/svelte/tests/migrate/samples/svelte-self-skip-filename/output.svelte
+++ packages/svelte/tests/migrate/samples/svelte-self-skip-filename/output.svelte
@@ -1,27 +1,4 @@
-<script>
-	/** @type {Record<string, any>} */
-	let { ...props } = $props();
-</script>
-
 {#if false}
-	<!-- @migration-task: migrate this by hand or call the migrate function with the filename of this file -->
+	<!-- @migration-task: svelte:self is deprecated, import this Svelte file into itself instead -->
 	<svelte:self />
-	<!-- @migration-task: migrate this by hand or call the migrate function with the filename of this file -->
-	<svelte:self with_attributes/>
-	<!-- @migration-task: migrate this by hand or call the migrate function with the filename of this file -->
-	<svelte:self count={count+1}/>
-	<!-- @migration-task: migrate this by hand or call the migrate function with the filename of this file -->
-	<svelte:self>
-		child
-	</svelte:self>
-	<!-- @migration-task: migrate this by hand or call the migrate function with the filename of this file -->
-	<svelte:self count={count+1}>
-		child
-	</svelte:self>
-	<!-- @migration-task: migrate this by hand or call the migrate function with the filename of this file -->
-	<svelte:self count={props.count}     >
-		child
-	</svelte:self>
-	<!-- @migration-task: migrate this by hand or call the migrate function with the filename of this file -->
-	<svelte:self></svelte:self>
 {/if}
