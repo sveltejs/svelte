d412d69548e0a695b40c59b051dc61a864fbc02c
Dominic Gannaway <dg@domgan.com>
2024-10-17 15:28:20 +0100

    revise

--- packages/svelte/src/internal/client/dev/inspect.js
+++ packages/svelte/src/internal/client/dev/inspect.js
@@ -18,11 +18,13 @@ export function inspect(get_value, inspector = console.log) {
 
 		// Capturing the value might result in an exception due to the inspect effect being
 		// sync and thus operating on stale data. In the case we encounter an exception we
-		// can bail-out of reporting the value
+		// can bail-out of reporting the value. Instead we simply console.error the error
+		// so at least it's known that an error occured, but we don't stop execution
 		try {
 			value = get_value();
-		} catch {
-			// NO-OP
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error(error);
 		}
 
 		if (value !== UNINITIALIZED) {
