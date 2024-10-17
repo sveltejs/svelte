440017d4425fe2904d3ddd5499e2eb6b62be2d28
Paolo Ricciuti <ricciutipaolo@gmail.com>
2024-10-17 11:28:54 +0200

    fix: better migration for leading and trailing comments (#13630)

---
+++ packages/svelte/tests/migrate/samples/reactive-statements-reorder-with-comments/output.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { run } from 'svelte/legacy';
+
+	let count = $state(0);
+
+
+
+	// triple
+	//  update triple
+	let triple = $derived(count * 3)
+	//  trailing comment
+	//  in triple;
+
+	function increment() {
+		count += 1;
+	}
+	// this comment should remain attached to this declaration after migration
+	let double = $derived(count * 2); // this too
+	run(() => {
+		console.log({ count, double });
+	});
+	
+</script>
+
+<button onclick={increment}>
+	clicks: {count}
+</button>
