17893b4e2e8b3792808aa5f50160e57a64275443
Dominic Gannaway <dg@domgan.com>
2024-10-17 15:31:12 +0100

    lint

--- packages/svelte/tests/runtime-legacy/samples/autofocus-2/_config.js
+++ packages/svelte/tests/runtime-legacy/samples/autofocus-2/_config.js
@@ -2,9 +2,6 @@ import { test } from '../../test';
 
 export default test({
 	async test({ assert, target, window }) {
-		assert.equal(
-			target.querySelector('input'),
-			window.document.activeElement
-		);
+		assert.equal(target.querySelector('input'), window.document.activeElement);
 	}
 });
