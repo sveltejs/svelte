969e6aa750a4dccf3566fd4b12545f83e2492c72
Paolo Ricciuti <ricciutipaolo@gmail.com>
2024-10-18 03:05:01 +0200

    feat: add `migration-task` for impossible to migrate slots (#13658)
    * feat: add `migration-task` for impossible to migrate slots
    
    * Update packages/svelte/src/compiler/migrate/index.js
    
    Co-authored-by: Paolo Ricciuti <ricciutipaolo@gmail.com>
    
    * Update packages/svelte/tests/migrate/samples/slot-non-identifier/output.svelte
    
    Co-authored-by: Paolo Ricciuti <ricciutipaolo@gmail.com>
    
    * Update packages/svelte/tests/migrate/samples/slot-non-identifier/output.svelte
    
    Co-authored-by: Paolo Ricciuti <ricciutipaolo@gmail.com>
    
    * Update packages/svelte/tests/migrate/samples/slot-non-identifier/output.svelte
    
    Co-authored-by: Paolo Ricciuti <ricciutipaolo@gmail.com>
    
    * Update packages/svelte/tests/migrate/samples/slot-non-identifier/output.svelte
    
    Co-authored-by: Paolo Ricciuti <ricciutipaolo@gmail.com>
    
    ---------
    
    Co-authored-by: Dominic Gannaway <trueadm@users.noreply.github.com>

---
+++ packages/svelte/tests/migrate/samples/slot-non-identifier/input.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Comp from "./Component.svelte";
+</script>
+
+<Comp>
+	<div slot="cool:stuff">
+		cool
+	</div>
+</Comp>
+
+<Comp>
+	<div slot="cool stuff">
+		cool
+	</div>
+</Comp>
+
+<Comp>
+	<div slot="stuff">
+		cool
+	</div>
+</Comp>
+
+<Comp>
+	<svelte:fragment slot="cool:stuff">
+		cool
+	</svelte:fragment>
+</Comp>
+
+<Comp>
+	<svelte:fragment slot="cool stuff">
+		cool
+	</svelte:fragment>
+</Comp>
+
+<Comp>
+	<svelte:fragment slot="stuff">
+		cool
+	</svelte:fragment>
+</Comp>
