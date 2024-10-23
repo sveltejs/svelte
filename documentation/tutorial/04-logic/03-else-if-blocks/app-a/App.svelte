12782103ad14c61acb737a42ef3f674534548f77
Conduitry <git@chor.date>
2024-10-23 08:44:51 -0400

    chore: remove obsolete site content

--- documentation/tutorial/04-logic/03-else-if-blocks/app-a/App.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	let x = 7;
-</script>
-
-{#if x > 10}
-	<p>{x} is greater than 10</p>
-{:else}
-	<p>{x} is between 0 and 10</p>
-{/if}
--- documentation/tutorial/07-lifecycle/02-ondestroy/app-a/utils.js
+++
@@ -1,9 +0,0 @@
-import { onDestroy } from 'svelte';
-
-export function onInterval(callback, milliseconds) {
-	const interval = setInterval(callback, milliseconds);
-
-	onDestroy(() => {
-		// Fix the memory leak here
-	});
-}
