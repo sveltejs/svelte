4774d101df07a0a27614b2f105e7500734374a66
paoloricciuti <ricciutipaolo@gmail.com>
2024-10-24 00:16:02 +0200

    chore: better test

--- packages/svelte/tests/migrate/samples/is-not-where-has/input.svelte
+++ packages/svelte/tests/migrate/samples/is-not-where-has/input.svelte
@@ -1,3 +1,24 @@
+<script>
+	function is(){}
+	function where(){}
+	function not(){}
+	function has(){}
+
+	// looks like css but it's not in style tag
+	const x = {
+		div:is(42),
+		span:where(42),
+		form:not(42),
+		input:has(42),
+	}
+</script>
+
+what if i'm talking about `:has()` in my blog?
+
+```css
+	:has(.is_cool)
+```
+
 <style lang="postcss">
 	div:has(span){}
 	div > :not(span){}
