37d01274ed308f08d96efc2611601f500e4d74ca
Dominic Gannaway <dg@domgan.com>
2024-10-25 14:12:25 +0100

    add test

---
+++ packages/svelte/tests/runtime-runes/samples/await-no-catch-error/main.svelte
@@ -0,0 +1,12 @@
+<script>
+	const promise = new Promise((res, rej) => {
+		rej(new Error('Test'));
+	})
+	let toggle = $state(false);
+</script>
+
+<button onclick={() => toggle = !toggle}>toggle</button>
+
+{#if toggle}
+	{#await promise}{/await}
+{/if}
