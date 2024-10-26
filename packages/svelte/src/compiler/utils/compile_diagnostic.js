d1f4beba6f738fa70d36cfcffe826745bd775b6f
Paolo Ricciuti <ricciutipaolo@gmail.com>
2024-10-26 08:53:34 +0200

    chore: add comment

--- packages/svelte/src/compiler/utils/compile_diagnostic.js
+++ packages/svelte/src/compiler/utils/compile_diagnostic.js
@@ -51,6 +51,7 @@ function get_code_frame(source, line, column) {
 /** @implements {ICompileDiagnostic} */
 export class CompileDiagnostic {
 	name = 'CompileDiagnostic';
+	// adding an empty stack so that vite will show the file and frame during build
 	stack = '';
 
 	/**
