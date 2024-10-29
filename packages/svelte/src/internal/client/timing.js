329492377c2b000b26dc7dee2e4ff7c978ae8129
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-10-29 16:28:36 -0700

    fix: don't access requestAnimationFrame eagerly

--- packages/svelte/src/internal/client/timing.js
+++ packages/svelte/src/internal/client/timing.js
@@ -3,13 +3,11 @@ import { noop } from '../shared/utils.js';
 
 import { BROWSER } from 'esm-env';
 
-const request_animation_frame = BROWSER ? requestAnimationFrame : noop;
-
 const now = BROWSER ? () => performance.now() : () => Date.now();
 
 /** @type {Raf} */
 export const raf = {
-	tick: /** @param {any} _ */ (_) => request_animation_frame(_),
+	tick: /** @param {any} _ */ (_) => (BROWSER ? requestAnimationFrame : noop)(_),
 	now: () => now(),
 	tasks: new Set()
 };
