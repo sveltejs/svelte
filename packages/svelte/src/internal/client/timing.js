217e40998a9ce56f3dc926fb4e985b4547564f40
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-10-30 08:35:26 -0700

    chore: add changeset and comment (#14040)
    for #14026

--- packages/svelte/src/internal/client/timing.js
+++ packages/svelte/src/internal/client/timing.js
@@ -7,6 +7,9 @@ const now = BROWSER ? () => performance.now() : () => Date.now();
 
 /** @type {Raf} */
 export const raf = {
+	// don't access requestAnimationFrame eagerly outside method
+	// this allows basic testing of user code without JSDOM
+	// bunder will eval and remove ternary when the user's app is built
 	tick: /** @param {any} _ */ (_) => (BROWSER ? requestAnimationFrame : noop)(_),
 	now: () => now(),
 	tasks: new Set()
