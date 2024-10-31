d93ad3ba1e8c28c2facaec49e19d2ac1ed577e9f
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2024-10-31 14:55:49 +0330

    feat: add `ComponentExports` utility type (#13441)
    Safer/clearer/backwards/forward-compatibale way to reference them
    
    ---------
    
    Co-authored-by: Simon Holthausen <simon.holthausen@vercel.com>

--- packages/svelte/types/index.d.ts
+++ packages/svelte/types/index.d.ts
@@ -235,6 +235,26 @@ declare module 'svelte' {
 				: never;
 
 	/**
+	 * Convenience type to get the properties that given component exports.
+	 *
+	 * Example: Typing the `bind:this` for a component named `MyComponent`
+	 * ```
+	 * <script lang="ts">
+	 * 	import MyComponent from '$lib/component';
+	 * 	let component: ComponentExports<typeof MyComponent> | undefined = undefined;
+	 * <script>
+	 *
+	 * <MyComponent bind:this={component} />
+	 * ```
+	 */
+	export type ComponentExports<TComponent extends Component<any, any> | typeof SvelteComponent<any>> =
+		TComponent extends typeof SvelteComponent<any>
+			? InstanceType<TComponent>
+			: TComponent extends Component<any, infer TExports>
+				? TExports
+				: never;
+
+	/**
 	 * @deprecated This type is obsolete when working with the new `Component` type.
 	 *
 	 * @description
