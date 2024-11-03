8de5605b6a5ffa436d6fb9be5991345477e09f73
Paolo Ricciuti <ricciutipaolo@gmail.com>
2024-11-03 14:36:11 +0100

    fix: prevent migrated snippet from shadow snippet prop (#14127)

---
+++ packages/svelte/tests/migrate/samples/shadowed-forwarded-slot/output.svelte
@@ -0,0 +1,33 @@
+<script>
+	/**
+	 * @typedef {Object} Props
+	 * @property {import('svelte').Snippet} [label]
+	 */
+
+	/** @type {Props} */
+	let { label } = $props();
+	const label_render = $derived(label);
+
+</script>
+
+{#if label}
+	{@render label?.()}
+{/if}
+<MyInput>
+	{#snippet label()}
+		{@render label_render?.()}
+	{/snippet}
+</MyInput>
+<MyInput>
+	{#snippet label()}
+		<div >
+			<MyComponent>
+				{#snippet label()}
+						<div >
+						{@render label_render?.()}
+					</div>
+					{/snippet}
+			</MyComponent>
+		</div>
+	{/snippet}
+</MyInput>
