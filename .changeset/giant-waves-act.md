ea0d80e1950b5c62547c69a26c823282e8d88ad3
Paolo Ricciuti <ricciutipaolo@gmail.com>
2024-11-07 15:33:02 +0100

    fix: consider variables with synthetic store sub as state (#14195)
    Fixes #14183

---
+++ .changeset/giant-waves-act.md
@@ -0,0 +1,5 @@
+---
+'svelte': patch
+---
+
+fix: consider variables with synthetic store sub as state
---
+++ packages/svelte/tests/runtime-legacy/samples/variable-assigned-store-state/Test.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let store;
+
+	let currentStore;
+	function update(){
+		currentStore = store
+	}
+</script>
+
+<button on:click={update}></button>
+<p>{$currentStore}</p>
