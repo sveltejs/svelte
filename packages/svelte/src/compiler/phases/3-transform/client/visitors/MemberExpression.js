0c584a0320873a9b501f3ec933936d733a451209
Dominic Gannaway <dg@domgan.com>
2024-11-13 15:33:34 +0000

    fix

--- packages/svelte/src/compiler/phases/3-transform/client/visitors/MemberExpression.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/MemberExpression.js
@@ -28,7 +28,7 @@ export function MemberExpression(node, context) {
 		parent?.type !== 'AssignmentExpression' &&
 		parent?.type !== 'UpdateExpression' &&
 		parent?.type !== 'Component' &&
-		parent?.type !== 'ChainExpression'
+		(parent?.type !== 'MemberExpression' || !parent?.optional)
 	) {
 		return trace(
 			node,
