8987391cdc2489bb8fe9918d93d1d78f0e4325d7
Rich Harris <rich.harris@vercel.com>
2024-11-13 22:34:40 -0500

    fix test

--- packages/svelte/tests/snapshot/samples/inline-module-vars/_expected/client/index.svelte.js
+++ packages/svelte/tests/snapshot/samples/inline-module-vars/_expected/client/index.svelte.js
@@ -7,7 +7,7 @@ const __DECLARED_ASSET_2__ = "__VITE_ASSET__2AM7_y_e__ 1440w, __VITE_ASSET__2AM7
 const __DECLARED_ASSET_3__ = "__VITE_ASSET__2AM7_y_g__";
 const a = 1;
 const b = 2;
-var root = $.template(`<picture><source srcset="${$.escape(__DECLARED_ASSET_0__, true)}" type="image/avif"> <source srcset="${$.escape(__DECLARED_ASSET_1__, true)}" type="image/webp"> <source srcset="${$.escape(__DECLARED_ASSET_2__, true)}" type="image/png"> <img src="${$.escape(__DECLARED_ASSET_3__, true)}" alt="production test" width="${1440}" height="${1440}"></picture> <p>${`${$.escape(a ?? "")} + ${$.escape(b ?? "")} = ${$.escape(a + b ?? "")}`}</p>`, 1);
+var root = $.template(`<picture><source srcset="${$.escape(__DECLARED_ASSET_0__, true)}" type="image/avif"> <source srcset="${$.escape(__DECLARED_ASSET_1__, true)}" type="image/webp"> <source srcset="${$.escape(__DECLARED_ASSET_2__, true)}" type="image/png"> <img src="${$.escape(__DECLARED_ASSET_3__, true)}" alt="production test" width="${1440}" height="${1440}"></picture> <p>${$.escape(a ?? "")} + ${$.escape(b ?? "")} = ${$.escape(a + b ?? "")}</p>`, 1);
 
 export default function Inline_module_vars($$anchor) {
 	var fragment = root();
