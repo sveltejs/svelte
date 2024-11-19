e9d38b52ce61efd2a27d99d890a93f38562226e8
paoloricciuti <ricciutipaolo@gmail.com>
2024-11-19 08:10:33 +0100

    fix: regenerate types

--- packages/svelte/types/index.d.ts
+++ packages/svelte/types/index.d.ts
@@ -1737,6 +1737,26 @@ declare module 'svelte/server' {
 					options: { props: Omit<Props, '$$slots' | '$$events'>; context?: Map<any, any> }
 				]
 	): RenderOutput;
+
+	/**
+	 * Only available on the server and when compiling with the `server` option.
+	 * Takes a component and returns an object with `body` and `head` properties on it, which you can use to populate the HTML when server-rendering your app.
+	 * Unlike `render` it doesn't render any hydration marker.
+	 */
+	export function renderStaticHTML<
+		Comp extends SvelteComponent<any> | Component<any>,
+		Props extends ComponentProps<Comp> = ComponentProps<Comp>
+	>(
+		...args: {} extends Props
+			? [
+					component: Comp extends SvelteComponent<any> ? ComponentType<Comp> : Comp,
+					options?: { props?: Omit<Props, '$$slots' | '$$events'>; context?: Map<any, any> }
+				]
+			: [
+					component: Comp extends SvelteComponent<any> ? ComponentType<Comp> : Comp,
+					options: { props: Omit<Props, '$$slots' | '$$events'>; context?: Map<any, any> }
+				]
+	): RenderOutput;
 	interface RenderOutput {
 		/** HTML that goes into the `<head>` */
 		head: string;
