fbbb7d92b1a8ec46ee98ce86d6d51fdf60534555
Rich Harris <rich.harris@vercel.com>
2024-11-26 18:58:08 -0500

    push once

--- packages/svelte/src/compiler/phases/3-transform/server/visitors/SvelteBoundary.js
+++ packages/svelte/src/compiler/phases/3-transform/server/visitors/SvelteBoundary.js
@@ -1,12 +1,7 @@
 /** @import { BlockStatement } from 'estree' */
 /** @import { AST } from '#compiler' */
 /** @import { ComponentContext } from '../types' */
-
-import {
-	BLOCK_CLOSE,
-	BLOCK_OPEN,
-	EMPTY_COMMENT
-} from '../../../../../internal/server/hydration.js';
+import { BLOCK_CLOSE, BLOCK_OPEN } from '../../../../../internal/server/hydration.js';
 import * as b from '../../../../utils/builders.js';
 
 /**
@@ -14,7 +9,9 @@ import * as b from '../../../../utils/builders.js';
  * @param {ComponentContext} context
  */
 export function SvelteBoundary(node, context) {
-	context.state.template.push(b.literal(BLOCK_OPEN));
-	context.state.template.push(/** @type {BlockStatement} */ (context.visit(node.fragment)));
-	context.state.template.push(b.literal(BLOCK_CLOSE));
+	context.state.template.push(
+		b.literal(BLOCK_OPEN),
+		/** @type {BlockStatement} */ (context.visit(node.fragment)),
+		b.literal(BLOCK_CLOSE)
+	);
 }
