sveltejs/svelte

commit 372884cf99420dd9d343a4ad78f6aa540a4df289

fix: use `internal_set` in `await` block (#13642)

packages/svelte/src/internal/client/dom/blocks/await.js
@@ -1,19 +1,19 @@
 /** @import { Effect, Source, TemplateNode } from '#client' */
-import { is_promise, noop } from '../../../shared/utils.js';
+import { DEV } from 'esm-env';
+import { is_promise } from '../../../shared/utils.js';
+import { block, branch, pause_effect, resume_effect } from '../../reactivity/effects.js';
+import { internal_set, mutable_source, source } from '../../reactivity/sources.js';
 import {
 	component_context,
 	flush_sync,
 	is_runes,
-	set_component_context,
 	set_active_effect,
 	set_active_reaction,
+	set_component_context,
 	set_dev_current_component_function
 } from '../../runtime.js';
-import { block, branch, pause_effect, resume_effect } from '../../reactivity/effects.js';
-import { DEV } from 'esm-env';
-import { queue_micro_task } from '../task.js';
 import { hydrate_next, hydrate_node, hydrating } from '../hydration.js';
-import { mutable_source, set, source } from '../../reactivity/sources.js';
+import { queue_micro_task } from '../task.js';
 
 const PENDING = 0;
 const THEN = 1;
@@ -120,12 +120,16 @@ export function await_block(node, get_input, pending_fn, then_fn, catch_fn) {
 			promise.then(
 				(value) => {
 					if (promise !== input) return;
-					set(input_source, value);
+					// we technically could use `set` here since it's on the next microtick
+					// but let's use internal_set for consistency and just to be safe
+					internal_set(input_source, value);
 					update(THEN, true);
 				},
 				(error) => {
 					if (promise !== input) return;
-					set(error_source, error);
+					// we technically could use `set` here since it's on the next microtick
+					// but let's use internal_set for consistency and just to be safe
+					internal_set(error_source, error);
 					update(CATCH, true);
 				}
 			);
@@ -142,7 +146,7 @@ export function await_block(node, get_input, pending_fn, then_fn, catch_fn) {
 				});
 			}
 		} else {
-			set(input_source, input);
+			internal_set(input_source, input);
 			update(THEN, false);
 		}
 

packages/svelte/tests/runtime-runes/samples/await-non-promise/_config.js
@@ -1,0 +1,9 @@
+import { flushSync } from 'svelte';
+import { test } from '../../test';
+
+export default test({
+	compileOptions: {
+		dev: true
+	},
+	test() {}
+});

packages/svelte/tests/runtime-runes/samples/await-non-promise/main.svelte
@@ -1,0 +1,9 @@
+<script>
+	let count = $state(43);
+</script>
+
+{#await count}
+	loading
+{:then count}
+	{count}
+{/await}

commit 142f60044ec81915550e9fe6f2ec02fa84cc286b

fix: show `:then` block for `null` value
fixes #14439

This bug was introduced in #13642 because setting the input to `null` means the equality check ("is the input different") fails if you set the value to `null`

packages/svelte/src/internal/client/dom/blocks/await.js
@@ -14,6 +14,7 @@ import {
 } from '../../runtime.js';
 import { hydrate_next, hydrate_node, hydrating } from '../hydration.js';
 import { queue_micro_task } from '../task.js';
+import { UNINITIALIZED } from '../../../../constants.js';
 
 const PENDING = 0;
 const THEN = 1;
@@ -40,7 +41,7 @@ export function await_block(node, get_input, pending_fn, then_fn, catch_fn) {
 	/** @type {any} */
 	var component_function = DEV ? component_context?.function : null;
 
-	/** @type {V | Promise<V> | null} */
+	/** @type {V | Promise<V> | typeof UNINITIALIZED} */
 	var input;
 
 	/** @type {Effect | null} */
@@ -156,8 +157,8 @@ export function await_block(node, get_input, pending_fn, then_fn, catch_fn) {
 			update(THEN, false);
 		}
 
-		// Set the input to null, in order to disable the promise callbacks
-		return () => (input = null);
+		// Set the input to something else, in order to disable the promise callbacks
+		return () => (input = UNINITIALIZED);
 	});
 
 	if (hydrating) {

packages/svelte/tests/runtime-runes/samples/await-non-promise/_config.js
@@ -1,9 +1,28 @@
 import { flushSync } from 'svelte';
-import { test } from '../../test';
+import { ok, test } from '../../test';
 
 export default test({
+	solo: true,
 	compileOptions: {
 		dev: true
 	},
-	test() {}
+	test({ assert, target }) {
+		const [btn1, btn2] = target.querySelectorAll('button');
+		const p = target.querySelector('p');
+		ok(p);
+
+		assert.htmlEqual(p.outerHTML, `<p>0</p>`);
+
+		btn1.click();
+		flushSync();
+		assert.htmlEqual(p.outerHTML, `<p>1</p>`);
+
+		btn2.click();
+		flushSync();
+		assert.htmlEqual(p.outerHTML, `<p></p>`);
+
+		btn1.click();
+		flushSync();
+		assert.htmlEqual(p.outerHTML, `<p>1</p>`);
+	}
 });

packages/svelte/tests/runtime-runes/samples/await-non-promise/main.svelte
@@ -1,9 +1,14 @@
 <script>
-	let count = $state(43);
+	let count = $state(0);
 </script>
 
-{#await count}
-	loading
-{:then count}
-	{count}
-{/await}
+<button onclick={() => count += 1}>increment</button>
+<button onclick={() => count = null}>nullify</button>
+
+<p>
+	{#await count}
+		loading
+	{:then count}
+		{count}
+	{/await}
+</p>