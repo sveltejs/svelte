fb413ab5e050e380173d76c86fa6630472c60d04
Rich Harris <rich.harris@vercel.com>
2024-12-01 17:28:14 -0500

    chore: better SSR output for <svelte:element>

--- packages/svelte/src/compiler/phases/3-transform/server/visitors/SvelteElement.js
+++ packages/svelte/src/compiler/phases/3-transform/server/visitors/SvelteElement.js
@@ -14,13 +14,14 @@ import { build_template } from './shared/utils.js';
  */
 export function SvelteElement(node, context) {
 	let tag = /** @type {Expression} */ (context.visit(node.tag));
-	if (tag.type !== 'Identifier') {
-		const tag_id = context.state.scope.generate('$$tag');
-		context.state.init.push(b.const(tag_id, tag));
-		tag = b.id(tag_id);
-	}
 
 	if (dev) {
+		if (tag.type !== 'Identifier') {
+			const tag_id = context.state.scope.generate('$$tag');
+			context.state.init.push(b.const(tag_id, tag));
+			tag = b.id(tag_id);
+		}
+
 		if (node.fragment.nodes.length > 0) {
 			context.state.init.push(b.stmt(b.call('$.validate_void_dynamic_element', b.thunk(tag))));
 		}
