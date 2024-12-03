cd452f9b9a4dddfbde326f84a1cc294d5f74fda1
Rich Harris <rich.harris@vercel.com>
2024-12-03 15:41:47 -0500

    update test

--- packages/svelte/tests/runtime-runes/samples/proxy-nullish-coalescing-assignment-warning/_config.js
+++ packages/svelte/tests/runtime-runes/samples/proxy-nullish-coalescing-assignment-warning/_config.js
@@ -2,14 +2,23 @@ import { flushSync } from 'svelte';
 import { test } from '../../test';
 
 export default test({
+	compileOptions: {
+		dev: true
+	},
+
 	html: `<button>items: null</button>`,
 
 	test({ assert, target, warnings }) {
-		const [btn1, btn2] = target.querySelectorAll('button');
+		const btn = target.querySelector('button');
 
-		flushSync(() => btn1.click());
+		flushSync(() => btn?.click());
 		assert.htmlEqual(target.innerHTML, `<button>items: []</button>`);
 
-		assert.equal(warnings, ['TODO']);
+		flushSync(() => btn?.click());
+		assert.htmlEqual(target.innerHTML, `<button>items: [0]</button>`);
+
+		assert.deepEqual(warnings, [
+			'Assignment to `items` property (main.svelte:5:24) will evaluate to the right-hand side, not the value of `items` following the assignment. This may result in unexpected behaviour.'
+		]);
 	}
 });
