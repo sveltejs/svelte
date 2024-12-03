2011cb402ec0b79a6d97fa72952d1208e0347bfe
Rich Harris <rich.harris@vercel.com>
2024-12-03 12:04:36 -0500

    use active reaction at time of instance creation

--- packages/svelte/src/reactivity/date.js
+++ packages/svelte/src/reactivity/date.js
@@ -1,40 +1,17 @@
-/** @import { Source, Derived, Effect } from '#client' */
-import { DERIVED } from '../internal/client/constants.js';
+/** @import { Source } from '#client' */
 import { derived } from '../internal/client/index.js';
 import { source, set } from '../internal/client/reactivity/sources.js';
 import { active_reaction, get, set_active_reaction } from '../internal/client/runtime.js';
 
 var inited = false;
 
-/**
- * @template T
- * @param {() => T} fn
- * @returns {T}
- */
-function with_parent_effect(fn) {
-	var previous_reaction = active_reaction;
-	var parent_reaction = active_reaction;
-
-	while (parent_reaction !== null) {
-		if ((parent_reaction.f & DERIVED) === 0) {
-			break;
-		}
-		parent_reaction = /** @type {Derived | Effect} */ (parent_reaction).parent;
-	}
-
-	set_active_reaction(parent_reaction);
-	try {
-		return fn();
-	} finally {
-		set_active_reaction(previous_reaction);
-	}
-}
-
 export class SvelteDate extends Date {
 	#time = source(super.getTime());
 
 	/** @type {Map<keyof Date, Source<unknown>>} */
 	#deriveds = new Map();
+
+	#reaction = active_reaction;
 
 	/** @param {any[]} params */
 	constructor(...params) {
@@ -68,18 +45,20 @@ export class SvelteDate extends Date {
 					var d = this.#deriveds.get(method);
 
 					if (d === undefined) {
-						// Ensure we create the derived inside the nearest parent effect if
-						// we're inside a derived, otherwise the derived will be destroyed each
-						// time it re-runs
-						d = with_parent_effect(() =>
-							derived(() => {
-								get(this.#time);
-								// @ts-ignore
-								return date_proto[method].apply(this, args);
-							})
-						);
+						// lazily create the derived, but as though it were being
+						// created at the same time as the class instance
+						const reaction = active_reaction;
+						set_active_reaction(this.#reaction);
+
+						d = derived(() => {
+							get(this.#time);
+							// @ts-ignore
+							return date_proto[method].apply(this, args);
+						});
 
 						this.#deriveds.set(method, d);
+
+						set_active_reaction(reaction);
 					}
 
 					return get(d);
