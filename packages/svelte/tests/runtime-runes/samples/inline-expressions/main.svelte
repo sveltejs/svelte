d511104ee0f77272ffb81b7ff4a3a0528e0e0302
adiGuba <frederic.martini@gmail.com>
2024-12-04 14:23:24 +0100

    chore: make test more robust (#14550)

--- packages/svelte/tests/runtime-runes/samples/inline-expressions/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/inline-expressions/main.svelte
@@ -1,6 +1,6 @@
 <p>Without text expression: 7.36</p>
 <p>With text expression: {7.36}</p>
-<p>With text expression and function call: {(7.36).toLocaleString()}</p>
+<p>With text expression and function call: {(7.36).toString()}</p>
 <p>With text expression and property access: {"test".length}</p>
 <h1>Hello {('name').toUpperCase().toLowerCase()}!</h1>
 <p>{"test".length}</p>
