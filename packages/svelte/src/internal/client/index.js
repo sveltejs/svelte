0791ba9c6b78ad844f4bc59dc5a3e131de747d70
Simon Holthausen <simon.holthausen@vercel.com>
2024-12-04 17:24:02 +0100

    fix: handle static form values in combination with default values
    When the `value` or `checked` attribute of an input or the contents of a textarea were static, setting the `defaulValue/defaultChecked` property caused the latter to take precedence over the former. This is due to how we transform the code: If the value is static, we put it onto

--- packages/svelte/src/internal/client/index.js
+++ packages/svelte/src/internal/client/index.js
@@ -35,7 +35,9 @@ export {
 	handle_lazy_img,
 	set_value,
 	set_checked,
-	set_selected
+	set_selected,
+	set_default_checked,
+	set_default_value
 } from './dom/elements/attributes.js';
 export { set_class, set_svg_class, set_mathml_class, toggle_class } from './dom/elements/class.js';
 export { apply, event, delegate, replay_events } from './dom/elements/events.js';
