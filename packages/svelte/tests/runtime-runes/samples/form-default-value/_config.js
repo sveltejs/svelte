0e9678b95fdd35ccb9bfc2e6651be8f0aaf981fc
Simon Holthausen <simon.holthausen@vercel.com>
2024-12-05 11:38:26 +0100

    test

--- packages/svelte/tests/runtime-runes/samples/form-default-value/_config.js
+++ packages/svelte/tests/runtime-runes/samples/form-default-value/_config.js
@@ -37,8 +37,8 @@ export default test({
 		const after_reset = [];
 
 		const reset = /** @type {HTMLInputElement} */ (target.querySelector('input[type=reset]'));
-		const [test1, test2, test3, test4, test5, test12] = target.querySelectorAll('div');
-		const [test6, test7, test8, test9] = target.querySelectorAll('select');
+		const [test1, test2, test3, test4, test5, test6, test13] = target.querySelectorAll('div');
+		const [test7, test8, test9, test10] = target.querySelectorAll('select');
 		const [
 			test1_span,
 			test2_span,
@@ -48,7 +48,8 @@ export default test({
 			test6_span,
 			test7_span,
 			test8_span,
-			test9_span
+			test9_span,
+			test10_span
 		] = target.querySelectorAll('span');
 
 		{
@@ -74,8 +75,8 @@ export default test({
 		{
 			/** @type {NodeListOf<HTMLInputElement | HTMLTextAreaElement>} */
 			const inputs = test2.querySelectorAll('input, textarea');
-			check_inputs(inputs, 'value', 'y');
-			assert.htmlEqual(test2_span.innerHTML, 'y y y y');
+			check_inputs(inputs, 'value', 'x');
+			assert.htmlEqual(test2_span.innerHTML, 'x x x x');
 
 			for (const input of inputs) {
 				set_input(input, 'value', 'foo');
@@ -85,33 +86,41 @@ export default test({
 			assert.htmlEqual(test2_span.innerHTML, 'foo foo foo foo');
 
 			after_reset.push(() => {
+				console.log('-------------');
 				check_inputs(inputs, 'value', 'x');
 				assert.htmlEqual(test2_span.innerHTML, 'x x x x');
 			});
 		}
 
 		{
-			/** @type {NodeListOf<HTMLInputElement>} */
-			const inputs = test3.querySelectorAll('input');
-			check_inputs(inputs, 'checked', true);
-			assert.htmlEqual(test3_span.innerHTML, 'true true');
+			/** @type {NodeListOf<HTMLInputElement | HTMLTextAreaElement>} */
+			const inputs = test3.querySelectorAll('input, textarea');
+			check_inputs(inputs, 'value', 'y');
+			assert.htmlEqual(test3_span.innerHTML, 'y y y y');
 
 			for (const input of inputs) {
-				set_input(input, 'checked', false);
+				set_input(input, 'value', 'foo');
 			}
 			flushSync();
-			check_inputs(inputs, 'checked', false);
-			assert.htmlEqual(test3_span.innerHTML, 'false false');
+			check_inputs(inputs, 'value', 'foo');
+			assert.htmlEqual(test3_span.innerHTML, 'foo foo foo foo');
 
 			after_reset.push(() => {
-				check_inputs(inputs, 'checked', true);
-				assert.htmlEqual(test3_span.innerHTML, 'true true');
+				check_inputs(inputs, 'value', 'x');
+				assert.htmlEqual(test3_span.innerHTML, 'x x x x');
 			});
 		}
 
 		{
 			/** @type {NodeListOf<HTMLInputElement>} */
 			const inputs = test4.querySelectorAll('input');
+			check_inputs(inputs, 'checked', true);
+			assert.htmlEqual(test4_span.innerHTML, 'true true');
+
+			for (const input of inputs) {
+				set_input(input, 'checked', false);
+			}
+			flushSync();
 			check_inputs(inputs, 'checked', false);
 			assert.htmlEqual(test4_span.innerHTML, 'false false');
 
@@ -124,29 +133,24 @@ export default test({
 		{
 			/** @type {NodeListOf<HTMLInputElement>} */
 			const inputs = test5.querySelectorAll('input');
-			check_inputs(inputs, 'checked', true);
-			assert.htmlEqual(test5_span.innerHTML, 'true');
+			check_inputs(inputs, 'checked', false);
+			assert.htmlEqual(test5_span.innerHTML, 'false false');
 
 			after_reset.push(() => {
-				check_inputs(inputs, 'checked', false);
-				assert.htmlEqual(test5_span.innerHTML, 'false');
+				check_inputs(inputs, 'checked', true);
+				assert.htmlEqual(test5_span.innerHTML, 'true true');
 			});
 		}
 
 		{
-			/** @type {NodeListOf<HTMLOptionElement>} */
-			const options = test6.querySelectorAll('option');
-			check_inputs(options, 'selected', [false, true, false]);
-			assert.htmlEqual(test6_span.innerHTML, 'b');
-
-			select_option(options[2]);
-			flushSync();
-			check_inputs(options, 'selected', [false, false, true]);
-			assert.htmlEqual(test6_span.innerHTML, 'c');
+			/** @type {NodeListOf<HTMLInputElement>} */
+			const inputs = test6.querySelectorAll('input');
+			check_inputs(inputs, 'checked', true);
+			assert.htmlEqual(test6_span.innerHTML, 'true');
 
 			after_reset.push(() => {
-				check_inputs(options, 'selected', [false, true, false]);
-				assert.htmlEqual(test6_span.innerHTML, 'b');
+				check_inputs(inputs, 'checked', false);
+				assert.htmlEqual(test6_span.innerHTML, 'false');
 			});
 		}
 
@@ -170,13 +174,13 @@ export default test({
 		{
 			/** @type {NodeListOf<HTMLOptionElement>} */
 			const options = test8.querySelectorAll('option');
+			check_inputs(options, 'selected', [false, true, false]);
+			assert.htmlEqual(test8_span.innerHTML, 'b');
+
+			select_option(options[2]);
+			flushSync();
 			check_inputs(options, 'selected', [false, false, true]);
 			assert.htmlEqual(test8_span.innerHTML, 'c');
-
-			select_option(options[0]);
-			flushSync();
-			check_inputs(options, 'selected', [true, false, false]);
-			assert.htmlEqual(test8_span.innerHTML, 'a');
 
 			after_reset.push(() => {
 				check_inputs(options, 'selected', [false, true, false]);
@@ -202,8 +206,25 @@ export default test({
 		}
 
 		{
+			/** @type {NodeListOf<HTMLOptionElement>} */
+			const options = test10.querySelectorAll('option');
+			check_inputs(options, 'selected', [false, false, true]);
+			assert.htmlEqual(test10_span.innerHTML, 'c');
+
+			select_option(options[0]);
+			flushSync();
+			check_inputs(options, 'selected', [true, false, false]);
+			assert.htmlEqual(test10_span.innerHTML, 'a');
+
+			after_reset.push(() => {
+				check_inputs(options, 'selected', [false, true, false]);
+				assert.htmlEqual(test10_span.innerHTML, 'b');
+			});
+		}
+
+		{
 			/** @type {NodeListOf<HTMLInputElement | HTMLTextAreaElement>} */
-			const inputs = test12.querySelectorAll('input, textarea');
+			const inputs = test13.querySelectorAll('input, textarea');
 			assert.equal(inputs[0].value, 'x');
 			assert.equal(/** @type {HTMLInputElement} */ (inputs[1]).checked, true);
 			assert.equal(inputs[2].value, 'x');
--- packages/svelte/tests/runtime-runes/samples/form-default-value/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/form-default-value/main.svelte
@@ -7,14 +7,22 @@
 	let value6 = $state();
 	let value7 = $state();
 	let value8 = $state();
-	let value9 = $state('y');
-	let value10 = $state('y');
-	let value11 = $state('y');
-	let value12 = $state('y');
-	let value13 = $state('y');
-	let value14 = $state('y');
-	let value15 = $state('y');
-	let value16 = $state('y');
+	let value9 = $state(null);
+	let value10 = $state(null);
+	let value11 = $state(null);
+	let value12 = $state(null);
+	let value13 = $state(null);
+	let value14 = $state(null);
+	let value15 = $state(null);
+	let value16 = $state(null);
+	let value17 = $state('y');
+	let value18 = $state('y');
+	let value19 = $state('y');
+	let value20 = $state('y');
+	let value21 = $state('y');
+	let value22 = $state('y');
+	let value23 = $state('y');
+	let value24 = $state('y');
 
 	let checked1 = $state();
 	let checked2 = $state();
@@ -53,7 +61,7 @@
 		<textarea defaultValue="x" bind:value={value8}></textarea>
 	</div>
 
-	<!-- defaultValue=x, value=y -->
+	<!-- defaultValue=x, value=null -->
 	<div class="test-2">
 		<input {defaultValue} bind:value={value9} />
 		<input {defaultValue} value={value10} />
@@ -65,9 +73,21 @@
 		<textarea defaultValue="x" bind:value={value16}></textarea>
 	</div>
 
+	<!-- defaultValue=x, value=y -->
+	<div class="test-3">
+		<input {defaultValue} bind:value={value17} />
+		<input {defaultValue} value={value18} />
+		<input defaultValue="x" value={value19} />
+		<input defaultValue="x" bind:value={value20} />
+		<textarea {defaultValue} value={value21}></textarea>
+		<textarea {defaultValue} bind:value={value22}></textarea>
+		<textarea defaultValue="x" value={value23}></textarea>
+		<textarea defaultValue="x" bind:value={value24}></textarea>
+	</div>
+
 	<p>Input checked</p>
 	<!-- defaultChecked=true, checked=undefined -->
-	<div class="test-3">
+	<div class="test-4">
 		<input type="checkbox" {defaultChecked} checked={checked1} />
 		<input type="checkbox" {defaultChecked} bind:checked={checked2} />
 		<input type="checkbox" defaultChecked checked={checked3} />
@@ -75,7 +95,7 @@
 	</div>
 
 	<!-- defaultChecked=true, checked=false -->
-	<div class="test-4">
+	<div class="test-5">
 		<input type="checkbox" {defaultChecked} checked={checked5} />
 		<input type="checkbox" {defaultChecked} bind:checked={checked6} />
 		<input type="checkbox" defaultChecked checked={checked7} />
@@ -83,7 +103,7 @@
 	</div>
 
 	<!-- defaultChecked=false, checked=true -->
-	<div class="test-5">
+	<div class="test-6">
 		<input type="checkbox" defaultChecked={false} checked={checked9} />
 		<input type="checkbox" defaultChecked={false} bind:checked={checked10} />
 	</div>
@@ -151,13 +171,14 @@
 	Bound values:
 	<span class="test-1">{value1} {value3} {value6} {value8}</span>
 	<span class="test-2">{value9} {value12} {value14} {value16}</span>
-	<span class="test-3">{checked2} {checked4}</span>
-	<span class="test-4">{checked6} {checked8}</span>
-	<span class="test-5">{checked10}</span>
-	<span class="test-6">{selected1}</span>
-	<span class="test-7">{selected2}</span>
-	<span class="test-8">{selected3}</span>
-	<span class="test-9">{selected4}</span>
-	<span class="test-10">{selected5}</span>
-	<span class="test-11">{selected6}</span>
+	<span class="test-3">{value17} {value20} {value22} {value24}</span>
+	<span class="test-4">{checked2} {checked4}</span>
+	<span class="test-5">{checked6} {checked8}</span>
+	<span class="test-6">{checked10}</span>
+	<span class="test-7">{selected1}</span>
+	<span class="test-8">{selected2}</span>
+	<span class="test-9">{selected3}</span>
+	<span class="test-10">{selected4}</span>
+	<span class="test-11">{selected5}</span>
+	<span class="test-12">{selected6}</span>
 </p>
