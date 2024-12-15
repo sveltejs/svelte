0ccf5b26ef0bb6ed0f8a8f28803aeae385ae6859
Simon Holthausen <simon.holthausen@vercel.com>
2024-12-15 13:28:16 +0100

    fix

--- packages/svelte/src/compiler/phases/3-transform/server/visitors/shared/element.js
+++ packages/svelte/src/compiler/phases/3-transform/server/visitors/shared/element.js
@@ -86,6 +86,7 @@ export function build_element_attributes(node, context) {
 			} else if (attribute.name !== 'defaultValue' && attribute.name !== 'defaultChecked') {
 				if (attribute.name === 'class') {
 					class_index = attributes.length;
+
 					if (attribute.metadata.is_dynamic_class) {
 						attributes.push({
 							...attribute,
@@ -98,11 +99,14 @@ export function build_element_attributes(node, context) {
 								)
 							}
 						});
+					} else {
+						attributes.push(attribute);
 					}
 				} else {
 					if (attribute.name === 'style') {
 						style_index = attributes.length;
 					}
+
 					attributes.push(attribute);
 				}
 			}
