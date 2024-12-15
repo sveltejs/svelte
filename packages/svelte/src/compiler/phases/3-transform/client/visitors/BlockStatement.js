d92bd8a9d1cea84cb4285e4eee690f106e673fb9
Rich Harris <rich.harris@vercel.com>
2024-12-15 09:02:25 -0500

    handle async functions

--- packages/svelte/src/compiler/phases/3-transform/client/visitors/BlockStatement.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/BlockStatement.js
@@ -1,4 +1,4 @@
-/** @import { BlockStatement, Expression, Statement } from 'estree' */
+/** @import { ArrowFunctionExpression, BlockStatement, CallExpression, Expression, FunctionDeclaration, FunctionExpression, Statement } from 'estree' */
 /** @import { ComponentContext } from '../types' */
 import { add_state_transformers } from './shared/declarations.js';
 import * as b from '../../../../utils/builders.js';
@@ -12,15 +12,20 @@ export function BlockStatement(node, context) {
 	const tracing = context.state.scope.tracing;
 
 	if (tracing !== null) {
-		return b.block([
-			b.return(
-				b.call(
-					'$.trace',
-					/** @type {Expression} */ (context.visit(tracing)),
-					b.thunk(b.block(node.body.map((n) => /** @type {Statement} */ (context.visit(n)))))
-				)
-			)
-		]);
+		const parent =
+			/** @type {ArrowFunctionExpression | FunctionDeclaration | FunctionExpression} */ (
+				context.path.at(-1)
+			);
+
+		const is_async = parent.async;
+
+		const call = b.call(
+			'$.trace',
+			/** @type {Expression} */ (tracing),
+			b.thunk(b.block(node.body.map((n) => /** @type {Statement} */ (context.visit(n)))), is_async)
+		);
+
+		return b.block([b.return(is_async ? b.await(call) : call)]);
 	}
 
 	context.next();
