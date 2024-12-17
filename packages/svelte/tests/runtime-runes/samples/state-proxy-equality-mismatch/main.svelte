36a437c2f9a4ac4b10d5f6f1bead546fa7fcbd88
Rich Harris <rich.harris@vercel.com>
2024-12-17 13:04:16 -0500

    fix: compare array contents for equality mismatch detections, not the arrays themselves (#14738)

---
+++ packages/svelte/tests/runtime-runes/samples/state-proxy-equality-mismatch/main.svelte
@@ -0,0 +1,23 @@
+<script>
+	let primitive = 'foo';
+	let object = {};
+
+	let array = $state([primitive, object]);
+</script>
+
+<button onclick={() => array.includes(primitive)}>array.includes(primitive)</button>
+<button onclick={() => array.includes(object)}>array.includes(object)</button>
+
+<hr />
+
+<button onclick={() => array.indexOf(primitive)}>array.indexOf(primitive)</button>
+<button onclick={() => array.indexOf(object)}>array.indexOf(object)</button>
+
+<hr />
+
+<button onclick={() => array.lastIndexOf(primitive)}>array.lastIndexOf(primitive)</button>
+<button onclick={() => array.lastIndexOf(object)}>array.lastIndexOf(object)</button>
+
+<hr />
+
+<button onclick={() => (array.length = 0)}>clear</button>
