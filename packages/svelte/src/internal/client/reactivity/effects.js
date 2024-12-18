d2d1b201b518b92c650a7db2e32cdbfa21e18704
Dominic Gannaway <dg@domgan.com>
2024-12-18 16:57:36 +0000

    types + changeset

--- packages/svelte/src/internal/client/reactivity/effects.js
+++ packages/svelte/src/internal/client/reactivity/effects.js
@@ -381,11 +381,7 @@ export function execute_effect_teardown(effect) {
 		set_is_destroying_effect(true);
 		set_active_reaction(null);
 		try {
-			if (typeof teardown === 'function') {
-				teardown.call(null);
-			} else {
-				teardown.abort('effect destroyed');
-			}
+			teardown.call(null);
 		} finally {
 			set_is_destroying_effect(previously_destroying_effect);
 			set_active_reaction(previous_reaction);
