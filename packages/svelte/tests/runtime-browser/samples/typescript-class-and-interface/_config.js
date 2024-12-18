c79b909458b6265a18cc554dd2affd832b9797af
Taku Fukada <naninunenor@gmail.com>
2024-12-18 18:15:49 +0900

    fix: remove 'implements' from class declaration (#14749)
    * fix: remove implements from class declaration
    
    * add a smoke test

---
+++ packages/svelte/tests/runtime-browser/samples/typescript-class-and-interface/_config.js
@@ -0,0 +1,3 @@
+import { test } from '../../assert';
+
+export default test({});
---
+++ packages/svelte/tests/runtime-browser/samples/typescript-class-and-interface/main.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  // TypeScript syntax should not cause compilation failure
+  interface MyInterface {}
+  class MyClass implements MyInterface {}
+</script>
