10c01bc801bff6be9b125acb66b128e56d21ba41
Arthur (@Refzlund) <arthur@refzlund.com>
2024-12-21 16:08:07 +0100

    lint

--- packages/svelte/src/compiler/phases/2-analyze/visitors/RenderTag.js
+++ packages/svelte/src/compiler/phases/2-analyze/visitors/RenderTag.js
@@ -28,11 +28,11 @@ export function RenderTag(node, context) {
 	let resolved = callee.type === 'Identifier' && is_resolved_snippet(binding);
 
 	/** @type {AST.SnippetBlock | undefined} */
-	let snippet
+	let snippet;
 
 	if (binding?.initial?.type === 'SnippetBlock') {
 		// if this render tag unambiguously references a local snippet, our job is easy
-		snippet = binding.initial
+		snippet = binding.initial;
 		node.metadata.snippets.add(snippet);
 	}
 
@@ -53,20 +53,22 @@ export function RenderTag(node, context) {
 	) {
 		e.render_tag_invalid_call_expression(node);
 	}
-	
-	const parent = context.path.at(-2);
-	const is_animated = snippet?.body.nodes.some(n => is_animate_directive(n));
 
-	if(is_animated) {
+	const parent = context.path.at(-2);
+	const is_animated = snippet?.body.nodes.some((n) => is_animate_directive(n));
+
+	if (is_animated) {
 		if (parent?.type !== 'EachBlock') {
 			e.animation_invalid_placement(node);
-		}
-		else if (!parent.key) {
+		} else if (!parent.key) {
 			e.animation_missing_key(parent);
-		} else if (parent.body.nodes.filter((n) =>
-			n.type !== 'Comment' &&
-			n.type !== 'ConstTag' &&
-			(n.type !== 'Text' || n.data.trim() !== '')).length > 1
+		} else if (
+			parent.body.nodes.filter(
+				(n) =>
+					n.type !== 'Comment' &&
+					n.type !== 'ConstTag' &&
+					(n.type !== 'Text' || n.data.trim() !== '')
+			).length > 1
 		) {
 			e.animation_invalid_placement(node);
 		}
@@ -77,17 +79,17 @@ export function RenderTag(node, context) {
 	context.next({ ...context.state, render_tag: node });
 }
 
-/** 
+/**
  * @param {AST.Text | AST.Tag | AST.ElementLike | AST.Comment | AST.Block} child
  * @param {boolean} render_snippet
  * @returns {boolean}
-*/
+ */
 function is_animate_directive(child, render_snippet = false) {
 	if (child.type === 'RenderTag') {
-		if(render_snippet) return false // Prevent infinite recursion
+		if (render_snippet) return false; // Prevent infinite recursion
 		for (const snippet_block of child.metadata.snippets) {
-			if(snippet_block.body.nodes.includes(child)) break
-			return snippet_block.body.nodes.some(n => is_animate_directive(n, true));
+			if (snippet_block.body.nodes.includes(child)) break;
+			return snippet_block.body.nodes.some((n) => is_animate_directive(n, true));
 		}
 	}
 	if (child.type !== 'RegularElement' && child.type !== 'SvelteElement') return false;
