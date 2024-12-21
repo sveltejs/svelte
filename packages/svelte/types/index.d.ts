e55c763986b694e6f5282ca1f2aa83e544f95df4
Marek Dědič <developer@dedic.eu>
2024-12-21 17:37:30 +0100

    Fixed docs for svelte/store not being rendered

--- packages/svelte/types/index.d.ts
+++ packages/svelte/types/index.d.ts
@@ -2148,10 +2148,42 @@ declare module 'svelte/store' {
 		 */
 		update(this: void, updater: Updater<T>): void;
 	}
+
+	/**
+	 * Create a store from a function that returns state, and (to make a writable store), an
+	 * optional second function that sets state.
+	 *
+	 * ```ts
+	 * import { toStore } from 'svelte/store';
+	 *
+	 * let count = $state(0);
+	 *
+	 * const store = toStore(() => count, (v) => (count = v));
+	 * ```
+	 */
 	export function toStore<V>(get: () => V, set: (v: V) => void): Writable<V>;
 
 	export function toStore<V>(get: () => V): Readable<V>;
 
+	/**
+	 * Convert a store to an object with a reactive `current` property. If `store`
+	 * is a readable store, `current` will be a readonly property.
+	 *
+	 * ```ts
+	 * import { fromStore, get, writable } from 'svelte/store';
+	 *
+	 * const store = writable(0);
+	 *
+	 * const count = fromStore(store);
+	 *
+	 * count.current; // 0;
+	 * store.set(1);
+	 * count.current; // 1
+	 *
+	 * count.current += 1;
+	 * get(store); // 2
+	 * ```
+	 */
 	export function fromStore<V>(store: Writable<V>): {
 		current: V;
 	};
