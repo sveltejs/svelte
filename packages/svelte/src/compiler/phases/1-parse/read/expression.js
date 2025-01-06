c4e9faad52c09e143148a69c3643d36462175499
Paolo Ricciuti <ricciutipaolo@gmail.com>
2025-01-06 11:08:07 +0100

    fix: correctly parse `each` with loose parser (#14887)
    * fix: correctly parse `each` with loose parser
    
    * chore: fix lint
    
    * chore: remove unused imports
    
    * Apply suggestions from code review
    
    * chore: fix lint
    
    ---------
    
    Co-authored-by: Simon H <5968653+dummdidumm@users.noreply.github.com>

--- packages/svelte/src/compiler/phases/1-parse/read/expression.js
+++ packages/svelte/src/compiler/phases/1-parse/read/expression.js
@@ -8,9 +8,31 @@ import { find_matching_bracket } from '../utils/bracket.js';
 /**
  * @param {Parser} parser
  * @param {string} [opening_token]
+ * @returns {Expression | undefined}
+ */
+export function get_loose_identifier(parser, opening_token) {
+	// Find the next } and treat it as the end of the expression
+	const end = find_matching_bracket(parser.template, parser.index, opening_token ?? '{');
+	if (end) {
+		const start = parser.index;
+		parser.index = end;
+		// We don't know what the expression is and signal this by returning an empty identifier
+		return {
+			type: 'Identifier',
+			start,
+			end,
+			name: ''
+		};
+	}
+}
+
+/**
+ * @param {Parser} parser
+ * @param {string} [opening_token]
+ * @param {boolean} [disallow_loose]
  * @returns {Expression}
  */
-export default function read_expression(parser, opening_token) {
+export default function read_expression(parser, opening_token, disallow_loose) {
 	try {
 		const node = parse_expression_at(parser.template, parser.ts, parser.index);
 
@@ -41,19 +63,12 @@ export default function read_expression(parser, opening_token) {
 
 		return /** @type {Expression} */ (node);
 	} catch (err) {
-		if (parser.loose) {
-			// Find the next } and treat it as the end of the expression
-			const end = find_matching_bracket(parser.template, parser.index, opening_token ?? '{');
-			if (end) {
-				const start = parser.index;
-				parser.index = end;
-				// We don't know what the expression is and signal this by returning an empty identifier
-				return {
-					type: 'Identifier',
-					start,
-					end,
-					name: ''
-				};
+		// If we are in an each loop we need the error to be thrown in cases like
+		// `as { y = z }` so we still throw and handle the error there
+		if (parser.loose && !disallow_loose) {
+			const expression = get_loose_identifier(parser, opening_token);
+			if (expression) {
+				return expression;
 			}
 		}
 
