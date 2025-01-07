b40f677e69fd331bbe1d3ea03a6ddf20ad5d7e06
Rich Harris <rich.harris@vercel.com>
2025-01-06 19:20:23 -0500

    fix test

--- packages/svelte/tests/runtime-legacy/samples/pre-tag/_config.js
+++ packages/svelte/tests/runtime-legacy/samples/pre-tag/_config.js
@@ -31,9 +31,7 @@ function get_html(ssr) {
     </span>
     E
     F
-  </pre></div> <div id="pre-with-leading-newline"><pre>leading newline</pre> <pre>  leading newline and spaces</pre> <pre>
-leading newlines</pre></div> <div id="pre-without-leading-newline"><pre>without spaces</pre> <pre>  with spaces  </pre> <pre>${' '}
+  </pre></div> <div id="pre-with-leading-newline"><pre>leading newline</pre> <pre>  leading newline and spaces</pre> <pre>leading newlines</pre></div> <div id="pre-without-leading-newline"><pre>without spaces</pre> <pre>  with spaces  </pre> <pre>${' '}
 newline after leading space</pre></div> <pre id="pre-with-multiple-leading-newlines">
-
 multiple leading newlines</pre>${ssr ? '<!--]-->' : ''}`;
 }
