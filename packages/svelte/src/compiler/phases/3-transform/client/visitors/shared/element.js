984e1021fab60f625b5d42cc5569d2c12d514c14
Rich Harris <rich.harris@vercel.com>
2025-01-22 09:54:05 -0500

    reuse mechanism

--- packages/svelte/src/compiler/phases/3-transform/client/visitors/shared/element.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/shared/element.js
@@ -5,7 +5,7 @@ import { normalize_attribute } from '../../../../../../utils.js';
 import { is_ignored } from '../../../../../state.js';
 import { is_event_attribute } from '../../../../../utils/ast.js';
 import * as b from '../../../../../utils/builders.js';
-import { build_getter, create_derived } from '../../utils.js';
+import { build_getter } from '../../utils.js';
 import { build_template_chunk, get_expression_id } from './utils.js';
 
 /**
@@ -61,10 +61,9 @@ export function build_set_attributes(
 			let value = /** @type {Expression} */ (context.visit(attribute));
 
 			if (attribute.metadata.expression.has_call) {
-				const id = b.id(state.scope.generate('spread_with_call'));
-				state.init.push(b.const(id, create_derived(state, b.thunk(value))));
-				value = b.call('$.get', id);
+				value = get_expression_id(context.state, value);
 			}
+
 			values.push(b.spread(value));
 		}
 	}
