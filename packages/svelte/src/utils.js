144b643e03a75fd0dcf3a17799570d4742dec3fd
Trevor Suarez <rican7@gmail.com>
2025-01-22 23:03:34 -0700

    Improving support for more boolean attributes:
    Added:
     - `defer`
     - `disablepictureinpicture`
     - `disableremoteplayback`
    
    Improved:
     - `allowfullscreen`
     - `novalidate`

--- packages/svelte/src/utils.js
+++ packages/svelte/src/utils.js
@@ -170,7 +170,10 @@ const DOM_BOOLEAN_ATTRIBUTES = [
 	'reversed',
 	'seamless',
 	'selected',
-	'webkitdirectory'
+	'webkitdirectory',
+	'defer',
+	'disablepictureinpicture',
+	'disableremoteplayback'
 ];
 
 /**
@@ -197,7 +200,10 @@ const ATTRIBUTE_ALIASES = {
 	defaultvalue: 'defaultValue',
 	defaultchecked: 'defaultChecked',
 	srcobject: 'srcObject',
-	novalidate: 'noValidate'
+	novalidate: 'noValidate',
+	allowfullscreen: 'allowFullscreen',
+	disablepictureinpicture: 'disablePictureInPicture',
+	disableremoteplayback: 'disableRemotePlayback'
 };
 
 /**
@@ -219,7 +225,11 @@ const DOM_PROPERTIES = [
 	'volume',
 	'defaultValue',
 	'defaultChecked',
-	'srcObject'
+	'srcObject',
+	'noValidate',
+	'allowFullscreen',
+	'disablePictureInPicture',
+	'disableRemotePlayback'
 ];
 
 /**
