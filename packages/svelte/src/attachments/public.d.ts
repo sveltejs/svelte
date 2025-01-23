01cad3e05674afcc21ad991d57be2594ce4e033c
Jayden Carey <jaydenaus9@gmail.com>
2025-01-23 13:14:07 +0800

    revert branded type change

--- packages/svelte/src/attachments/public.d.ts
+++ packages/svelte/src/attachments/public.d.ts
@@ -1,9 +1,4 @@
-declare const brand: unique symbol;
-type Brand<B> = { [brand]: B };
-type Branded<T, B> = T & Brand<B>;
-type AttachmentsKeySymbol = Branded<symbol, 'svelte.attachments'>;
-
 /**
  * A unique symbol used for defining the attachments to be applied to an element or component.
  */
-export const AttachmentsKey: AttachmentsKeySymbol;
+export const AttachmentsKey: unique symbol;
