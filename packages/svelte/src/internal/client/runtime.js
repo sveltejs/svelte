f04f22bdb11aefa2facc762cddc64a5accedcc72
Dominic Gannaway <trueadm@users.noreply.github.com>
2025-01-30 17:45:54 +0000

    tune

--- packages/svelte/src/internal/client/runtime.js
+++ packages/svelte/src/internal/client/runtime.js
@@ -959,13 +959,11 @@ export function get(signal) {
 		var derived = /** @type {Derived} */ (signal);
 		var parent = derived.parent;
 
-		if (parent !== null) {
+		if (parent !== null && (parent.f & UNOWNED) === 0) {
 			// If the derived is owned by another derived then mark it as unowned
 			// as the derived value might have been referenced in a different context
 			// since and thus its parent might not be its true owner anymore
-			if ((parent.f & UNOWNED) === 0) {
-				derived.f ^= UNOWNED;
-			}
+			derived.f ^= UNOWNED;
 		}
 	}
 
