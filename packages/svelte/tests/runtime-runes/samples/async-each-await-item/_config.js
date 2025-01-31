952ea25ed126dc4210e2eb5693231bdc06a44ea8
Rich Harris <rich.harris@vercel.com>
2025-01-31 08:07:33 -0500

    failing test

--- packages/svelte/tests/runtime-runes/samples/async-each-await-item/_config.js
+++ packages/svelte/tests/runtime-runes/samples/async-each-await-item/_config.js
@@ -2,35 +2,40 @@ import { flushSync, tick } from 'svelte';
 import { deferred } from '../../../../src/internal/shared/utils.js';
 import { test } from '../../test';
 
-/** @type {ReturnType<typeof deferred>} */
-let d;
+/** @type {Array<ReturnType<typeof deferred>>} */
+let items = [];
 
 export default test({
 	html: `<p>pending</p>`,
 
 	get props() {
-		d = deferred();
+		items = [deferred(), deferred(), deferred()];
 
 		return {
-			promise: d.promise
+			items
 		};
 	},
 
 	async test({ assert, target, component }) {
-		d.resolve(['a', 'b', 'c']);
+		items[0].resolve('a');
+		items[1].resolve('b');
+		items[2].resolve('c');
 		await Promise.resolve();
 		await Promise.resolve();
 		await tick();
 		flushSync();
 		assert.htmlEqual(target.innerHTML, '<p>a</p><p>b</p><p>c</p>');
 
-		d = deferred();
-		component.promise = d.promise;
+		items = [deferred(), deferred(), deferred(), deferred()];
+		component.items = items;
 		await tick();
 		assert.htmlEqual(target.innerHTML, '<p>a</p><p>b</p><p>c</p>');
 
-		d.resolve(['d', 'e', 'f', 'g']);
+		items[0].resolve('b');
+		items[1].resolve('c');
+		items[2].resolve('d');
+		items[3].resolve('e');
 		await tick();
-		assert.htmlEqual(target.innerHTML, '<p>d</p><p>e</p><p>f</p><p>g</p>');
+		assert.htmlEqual(target.innerHTML, '<p>b</p><p>c</p><p>d</p><p>e</p>');
 	}
 });
---
+++ packages/svelte/tests/runtime-runes/samples/async-each-await-item/main.svelte
@@ -0,0 +1,13 @@
+<script>
+	let { items } = $props();
+</script>
+
+<svelte:boundary>
+	{#each items as deferred}
+		<p>{await deferred.promise}</p>
+	{/each}
+
+	{#snippet pending()}
+		<p>pending</p>
+	{/snippet}
+</svelte:boundary>
