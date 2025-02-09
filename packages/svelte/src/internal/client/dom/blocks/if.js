c9a8e22dbf6aef10a9aade3ca76434c53e808434
adiguba <frederic.martini@gmail.com>
2025-02-09 13:28:07 +0100

    rewrite else/if hydration

--- packages/svelte/src/internal/client/dom/blocks/if.js
+++ packages/svelte/src/internal/client/dom/blocks/if.js
@@ -9,16 +9,17 @@ import {
 	set_hydrating
 } from '../hydration.js';
 import { block, branch, pause_effect, resume_effect } from '../../reactivity/effects.js';
-import { HYDRATION_START_ELSE, UNINITIALIZED } from '../../../../constants.js';
+import { HYDRATION_START, HYDRATION_START_ELSE, UNINITIALIZED } from '../../../../constants.js';
 
 /**
  * @param {TemplateNode} node
- * @param {(branch: (fn: (anchor: Node) => void, flag?: boolean) => void) => void} fn
- * @param {boolean} [elseif] True if this is an `{:else if ...}` block rather than an `{#if ...}`, as that affects which transitions are considered 'local'
+ * @param {(branch: (fn: (anchor: Node, r_index?: number, h_index?: number) => void, flag?: boolean) => void) => void} fn
+ * @param {number} [root_index]
+ * @param {number} [hydrate_index]
  * @returns {void}
  */
-export function if_block(node, fn, elseif = false) {
-	if (hydrating) {
+export function if_block(node, fn, root_index = 0, hydrate_index) {
+	if (hydrating && root_index === 0) {
 		hydrate_next();
 	}
 
@@ -33,28 +34,41 @@ export function if_block(node, fn, elseif = false) {
 	/** @type {UNINITIALIZED | boolean | null} */
 	var condition = UNINITIALIZED;
 
-	var flags = elseif ? EFFECT_TRANSPARENT : 0;
+	var flags = root_index > 0 ? EFFECT_TRANSPARENT : 0;
 
 	var has_branch = false;
 
-	const set_branch = (/** @type {(anchor: Node) => void} */ fn, flag = true) => {
+	const set_branch = (
+		/** @type {(anchor: Node, r_index?: number, h_index?: number) => void} */ fn,
+		flag = true
+	) => {
 		has_branch = true;
 		update_branch(flag, fn);
 	};
 
 	const update_branch = (
 		/** @type {boolean | null} */ new_condition,
-		/** @type {null | ((anchor: Node) => void)} */ fn
+		/** @type {null | ((anchor: Node, r_index?: number, h_index?: number) => void)} */ fn
 	) => {
 		if (condition === (condition = new_condition)) return;
 
 		/** Whether or not there was a hydration mismatch. Needs to be a `let` or else it isn't treeshaken out */
 		let mismatch = false;
 
-		if (hydrating) {
-			const is_else = /** @type {Comment} */ (anchor).data === HYDRATION_START_ELSE;
+		if (hydrating && hydrate_index != -1) {
+			if (hydrate_index === undefined) {
+				const data = /** @type {Comment} */ (anchor).data;
+				if (data === HYDRATION_START) {
+					hydrate_index = 0;
+				} else if (data === HYDRATION_START_ELSE) {
+					hydrate_index = Infinity;
+				} else {
+					hydrate_index = parseInt(data.substring(1));
+				}
+			}
+			const is_else = hydrate_index > root_index;
 
-			if (!!condition === is_else) {
+			if (!!condition === is_else || isNaN(hydrate_index)) {
 				// Hydration mismatch: remove everything inside the anchor and start fresh.
 				// This could happen with `{#if browser}...{/if}`, for example
 				anchor = remove_nodes();
@@ -62,6 +76,7 @@ export function if_block(node, fn, elseif = false) {
 				set_hydrate_node(anchor);
 				set_hydrating(false);
 				mismatch = true;
+				hydrate_index = -1; // ignore hydration in next else if
 			}
 		}
 
@@ -81,7 +96,7 @@ export function if_block(node, fn, elseif = false) {
 			if (alternate_effect) {
 				resume_effect(alternate_effect);
 			} else if (fn) {
-				alternate_effect = branch(() => fn(anchor));
+				alternate_effect = branch(() => fn(anchor, root_index + 1, hydrate_index));
 			}
 
 			if (consequent_effect) {
