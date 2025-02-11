a3e49b611094559a2c4f3830dec2cc75680140ab
Simon H <5968653+dummdidumm@users.noreply.github.com>
2025-02-11 23:07:17 +0100

    fix: recurse into `$derived` for ownership validation (#15166)
    - `$derived` can contain `$state` declarations so we cannot ignore them, so this reverts #14533
    - instead, we add equality checks to not do this expensive work unnecessarily
    - this also adds a render effect similar to the class ownership addition when it detects a getter on a POJO during ownership addition
    
    fixes #15164

---
+++ packages/svelte/tests/runtime-runes/samples/non-local-mutation-with-binding-8/CounterBinding.svelte
@@ -0,0 +1,7 @@
+<script>
+	let { linked3 = $bindable(), linked4 = $bindable() } = $props();
+</script>
+
+<p>Binding</p>
+<button onclick={() => linked3.count++}>Increment Linked 1 ({linked3.count})</button>
+<button onclick={() => linked4.count++}>Increment Linked 2 ({linked4.count})</button>
