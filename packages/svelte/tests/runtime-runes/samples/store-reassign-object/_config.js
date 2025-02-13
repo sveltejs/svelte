8b326b0ea77a2614869212c05e2832d531025010
tomoam <29677552+tomoam@users.noreply.github.com>
2025-02-13 19:09:59 +0900

    format

--- packages/svelte/tests/runtime-runes/samples/store-reassign-object/_config.js
+++ packages/svelte/tests/runtime-runes/samples/store-reassign-object/_config.js
@@ -2,9 +2,6 @@ import { test } from '../../test';
 
 export default test({
 	async test({ target, assert }) {
-		assert.htmlEqual(
-			target.innerHTML,
-			`<p>bar</p>`
-		);
+		assert.htmlEqual(target.innerHTML, `<p>bar</p>`);
 	}
 });
