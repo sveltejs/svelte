06d3c9faae32dc6c37460dfd3eb4f892e7937a1f
Rich Harris <rich.harris@vercel.com>
2025-02-21 15:39:00 -0500

    chore: create `binding.updated` getter

--- packages/svelte/src/compiler/phases/2-analyze/index.js
+++ packages/svelte/src/compiler/phases/2-analyze/index.js
@@ -566,7 +566,7 @@ export function analyze_component(root, source, options) {
 										binding.declaration_kind !== 'import'
 									) {
 										binding.kind = 'state';
-										binding.mutated = binding.updated = true;
+										binding.mutated = true;
 									}
 								}
 							}
