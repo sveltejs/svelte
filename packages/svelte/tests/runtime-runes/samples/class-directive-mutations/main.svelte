2ae5869f52e122b744043614c93b0102ad25b275
adiguba <frederic.martini@gmail.com>
2025-02-21 14:31:07 +0100

    add test for mutations on hydration

---
+++ packages/svelte/tests/runtime-runes/samples/class-directive-mutations/main.svelte
@@ -0,0 +1,49 @@
+<script>
+	let {
+        clazz = 'custom',
+        foo = true,
+        bar = true,
+        browser
+    } = $props();
+
+    let mutations = [];
+    let observer;
+
+    if (browser) {
+		observer = new MutationObserver(updateMutationRecords);
+        const main = document.querySelector('main#main');
+        if (main) {
+            observer.observe(main, { attributes: true, subtree: true });
+        }
+        
+	}
+    
+
+    function updateMutationRecords(results) {
+        for (const r of results) {
+			mutations.push(r.target.nodeName);
+		}
+    }
+
+    export function get_and_clear_mutations() {
+        updateMutationRecords(observer.takeRecords());
+        const result = mutations;
+        mutations = [];
+        return result;
+    }
+
+</script>
+
+<main id="main" class:browser>
+	<div class={clazz} class:foo class:bar></div>
+	<span class:foo class:bar></span>
+	<b class={clazz} class:foo class:bar></b>
+	<i class:foo class:bar></i>
+</main>
+
+<style>
+	div,
+	span {
+		color: red;
+	}
+</style>
