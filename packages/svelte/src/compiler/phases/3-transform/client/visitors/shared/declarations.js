a0fe6e38ff402b359710927e4b1b45945f492c44
Rich Harris <rich.harris@vercel.com>
2025-03-19 15:22:36 -0400

    chore: simplify set calls for proxyable values

--- packages/svelte/src/compiler/phases/3-transform/client/visitors/shared/declarations.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/shared/declarations.js
@@ -24,8 +24,8 @@ export function add_state_transformers(context) {
 		) {
 			context.state.transform[name] = {
 				read: binding.declaration_kind === 'var' ? (node) => b.call('$.safe_get', node) : get_value,
-				assign: (node, value) => {
-					let call = b.call('$.set', node, value);
+				assign: (node, value, proxy = false) => {
+					let call = b.call('$.set', node, value, proxy && b.true);
 
 					if (context.state.scope.get(`$${node.name}`)?.kind === 'store_sub') {
 						call = b.call('$.store_unsub', call, b.literal(`$${node.name}`), b.id('$$stores'));
