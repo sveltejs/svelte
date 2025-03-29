e13c7d9b22c89e598dabcece31f2dd480520f7c8
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-03-28 19:46:47 -0700

    lint

--- packages/svelte/tests/snapshot/samples/state-in-return/index.svelte.js
+++ packages/svelte/tests/snapshot/samples/state-in-return/index.svelte.js
@@ -1,8 +1,8 @@
 export default function proxy(object) {
-    return $state(object);
+	return $state(object);
 }
 export function createCounter() {
-    let count = $state(0);
-    count++;
+	let count = $state(0);
+	count++;
 }
-export const proxy_in_arrow = object => $state(object);
+export const proxy_in_arrow = (object) => $state(object);
