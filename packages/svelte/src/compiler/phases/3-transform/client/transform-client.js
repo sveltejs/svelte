799599638724dd5709dd37395dd241cb172bbbd8
Rich Harris <rich.harris@vercel.com>
2025-04-08 12:46:00 -0400

    mutation validation is only tangentially linked to context requirement

--- packages/svelte/src/compiler/phases/3-transform/client/transform-client.js
+++ packages/svelte/src/compiler/phases/3-transform/client/transform-client.js
@@ -393,6 +393,12 @@ export function client_component(analysis, options) {
 		);
 	}
 
+	if (analysis.needs_mutation_validation) {
+		component_block.body.unshift(
+			b.var('$$ownership_validator', b.call('$.create_ownership_validator', b.id('$$props')))
+		);
+	}
+
 	const should_inject_context =
 		dev ||
 		analysis.needs_context ||
@@ -402,12 +408,6 @@ export function client_component(analysis, options) {
 	// we want the cleanup function for the stores to run as the very last thing
 	// so that it can effectively clean up the store subscription even after the user effects runs
 	if (should_inject_context) {
-		if (analysis.needs_mutation_validation) {
-			component_block.body.unshift(
-				b.var('$$ownership_validator', b.call('$.create_ownership_validator', b.id('$$props')))
-			);
-		}
-
 		component_block.body.unshift(b.stmt(b.call('$.push', ...push_args)));
 
 		let to_push;
