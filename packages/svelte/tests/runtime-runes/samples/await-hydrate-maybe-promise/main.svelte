deabc374dc98c4247009d763aa72fc08a6b6cef8
Rich Harris <rich.harris@vercel.com>
2025-04-08 10:08:21 -0400

    failing test for #15704

---
+++ packages/svelte/tests/runtime-runes/samples/await-hydrate-maybe-promise/main.svelte
@@ -0,0 +1,16 @@
+<script>
+	let { browser } = $props();
+
+	let fulfil;
+	let promise = new Promise((f) => (fulfil = f));
+
+	let num = browser ? promise : 42;
+</script>
+
+<button onclick={() => fulfil(42)}>fulfil</button>
+
+{#await num}
+	{#if true}<p>loading...</p>{/if}
+{:then num}
+	<p>{num}</p>
+{/await}
