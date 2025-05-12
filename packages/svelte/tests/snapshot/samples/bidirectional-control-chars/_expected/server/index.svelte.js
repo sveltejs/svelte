4294d5b1caf00007735e107b54b8c49739ed523e
Rich Harris <rich.harris@vercel.com>
2025-05-12 18:33:55 -0400

    use validator test suite rather than snapshot (which should be used sparingly as it creates more git noise)

--- packages/svelte/tests/snapshot/samples/bidirectional-control-chars/_expected/server/index.svelte.js
+++
@@ -1,7 +0,0 @@
-import * as $ from 'svelte/internal/server';
-
-export default function Bidirectional_control_chars($$payload) {
-	let name = '\u2067\u2066rld\u2069\u2066wo\u2069\u2069';
-
-	$$payload.out += `<!---->⁧⁦def⁩⁦abc⁩⁩ <h1>Hello, ⁧⁦rld⁩⁦wo⁩⁩!</h1>`;
-}
