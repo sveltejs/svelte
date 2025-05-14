999a05a3b73c522b732e149ac55f77fecd2ff1af
Rich Harris <rich.harris@vercel.com>
2025-05-14 09:06:26 -0400

    regenerate

--- packages/svelte/types/index.d.ts
+++ packages/svelte/types/index.d.ts
@@ -625,6 +625,13 @@ declare module 'svelte/animate' {
 }
 
 declare module 'svelte/attachments' {
+	/**
+	 * An [attachment](https://svelte.dev/docs/svelte/@attach) is a function that runs when an element is mounted
+	 * to the DOM, and optionally returns a function that is called when the element is later removed.
+	 *
+	 * It can be attached to an element with an `{@attach ...}` tag, or by spreading an object containing
+	 * a property created with [`createAttachmentKey`](https://svelte.dev/docs/svelte/svelte-attachments#createAttachmentKey).
+	 */
 	export interface Attachment {
 		(element: Element): void | (() => void);
 	}
