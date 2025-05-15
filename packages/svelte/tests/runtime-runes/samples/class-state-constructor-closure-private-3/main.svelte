63950858d6a4def871f1dc1876ea028b64d8ed5e
Rich Harris <rich.harris@vercel.com>
2025-05-15 14:09:55 -0400

    failing test

---
+++ packages/svelte/tests/runtime-runes/samples/class-state-constructor-closure-private-3/main.svelte
@@ -0,0 +1,12 @@
+<script>
+	const counter = new class Counter {
+		constructor() {
+			this.count = $state(0);
+			$effect(() => {
+				this.count = 10;
+			});
+		}
+	}
+</script>
+
+<button on:click={() => counter.count++}>{counter.count}</button>
