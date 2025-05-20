110e37ee518b0e94bfca90ca1d539ae7e2c9cdf3
Rich Harris <rich.harris@vercel.com>
2025-05-19 21:09:12 -0400

    fix: only re-run directly applied attachment if it changed

---
+++ .changeset/slimy-drinks-divide.md
@@ -0,0 +1,5 @@
+---
+'svelte': patch
+---
+
+fix: only re-run directly applied attachment if it changed
--- packages/svelte/src/internal/client/dom/elements/attachments.js
+++ packages/svelte/src/internal/client/dom/elements/attachments.js
@@ -1,15 +1,27 @@
-import { effect } from '../../reactivity/effects.js';
+/** @import { Effect } from '#client' */
+import { block, branch, destroy_effect, effect } from '../../reactivity/effects.js';
 
 /**
  * @param {Element} node
  * @param {() => (node: Element) => void} get_fn
  */
 export function attach(node, get_fn) {
-	effect(() => {
-		const fn = get_fn();
+	/** @type {false | undefined | ((node: Element) => void)} */
+	var fn = undefined;
 
-		// we use `&&` rather than `?.` so that things like
-		// `{@attach DEV && something_dev_only()}` work
-		return fn && fn(node);
+	/** @type {Effect | null} */
+	var effect;
+
+	block(() => {
+		if (fn !== (fn = get_fn())) {
+			if (effect) {
+				destroy_effect(effect);
+				effect = null;
+			}
+
+			if (fn) {
+				effect = branch(() => /** @type {(node: Element) => void} */ (fn)(node));
+			}
+		}
 	});
 }
