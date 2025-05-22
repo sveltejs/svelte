f4323057a16b20f1127c25b140a908e1b7b02929
Rich Harris <rich.harris@vercel.com>
2025-05-21 22:23:46 -0400

    one snapshot test is probably enough

--- packages/svelte/tests/snapshot/samples/imports-in-modules/_expected/client-functional/index.svelte.js
+++
@@ -1,8 +0,0 @@
-import 'svelte/internal/disclose-version';
-import 'svelte/internal/flags/legacy';
-import * as $ from 'svelte/internal/client';
-import { random } from './module.svelte';
-
-export default function Imports_in_modules($$anchor) {
-	
-}
