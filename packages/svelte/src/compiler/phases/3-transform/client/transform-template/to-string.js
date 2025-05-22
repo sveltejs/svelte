1be912071d1b465fde6060f7e92949a8c49d675f
Rich Harris <rich.harris@vercel.com>
2025-05-21 21:28:59 -0400

    tweak

--- packages/svelte/src/compiler/phases/3-transform/client/transform-template/to-string.js
+++ packages/svelte/src/compiler/phases/3-transform/client/transform-template/to-string.js
@@ -14,33 +14,29 @@ export function template_to_string(items) {
  * @param {Node} node
  */
 function stringify(node) {
-	switch (node.type) {
-		case 'element': {
-			let str = `<${node.name}`;
-
-			for (const key in node.attributes) {
-				const value = node.attributes[key];
-
-				str += ` ${key}`;
-				if (value !== undefined) str += `="${escape_html(value, true)}"`;
-			}
-
-			str += `>`;
-			str += node.children.map(stringify).join('');
-
-			if (!is_void(node.name)) {
-				str += `</${node.name}>`;
-			}
-
-			return str;
-		}
-
-		case 'text': {
-			return node.nodes.map((node) => node.raw).join('');
-		}
-
-		case 'anchor': {
-			return node.data ? `<!--${node.data}-->` : '<!>';
-		}
+	if (node.type === 'text') {
+		return node.nodes.map((node) => node.raw).join('');
 	}
+
+	if (node.type === 'anchor') {
+		return node.data ? `<!--${node.data}-->` : '<!>';
+	}
+
+	let str = `<${node.name}`;
+
+	for (const key in node.attributes) {
+		const value = node.attributes[key];
+
+		str += ` ${key}`;
+		if (value !== undefined) str += `="${escape_html(value, true)}"`;
+	}
+
+	str += `>`;
+	str += node.children.map(stringify).join('');
+
+	if (!is_void(node.name)) {
+		str += `</${node.name}>`;
+	}
+
+	return str;
 }
