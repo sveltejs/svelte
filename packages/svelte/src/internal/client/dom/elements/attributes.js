600a7ad48b6fd55b4bf79125611fcfe31ef9a83e
Simon Holthausen <simon.holthausen@vercel.com>
2025-06-16 17:03:53 +0200

    fix: keep spread non-delegated event handlers up to date
    #15961 introduced a regression where non-delegated events that were spread and updated were not getting updated. This fixes that by ensuring prev is actually updated to the most recent value

--- packages/svelte/src/internal/client/dom/elements/attributes.js
+++ packages/svelte/src/internal/client/dom/elements/attributes.js
@@ -483,8 +483,8 @@ export function attribute_effect(
 
 	block(() => {
 		var next = fn(...deriveds.map(get));
-
-		set_attributes(element, prev, next, css_hash, skip_warning);
+		/** @type {Record<string | symbol, any>} */
+		var current = set_attributes(element, prev, next, css_hash, skip_warning);
 
 		if (inited && is_select && 'value' in next) {
 			select_option(/** @type {HTMLSelectElement} */ (element), next.value, false);
@@ -495,7 +495,7 @@ export function attribute_effect(
 		}
 
 		for (let symbol of Object.getOwnPropertySymbols(next)) {
-			var n = next[symbol];
+			var n = (current[symbol] = next[symbol]);
 
 			if (symbol.description === ATTACHMENT_KEY && (!prev || n !== prev[symbol])) {
 				if (effects[symbol]) destroy_effect(effects[symbol]);
@@ -503,7 +503,7 @@ export function attribute_effect(
 			}
 		}
 
-		prev = next;
+		prev = current;
 	});
 
 	if (is_select) {
