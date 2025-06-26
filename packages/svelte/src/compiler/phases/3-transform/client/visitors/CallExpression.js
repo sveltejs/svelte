2961124ba688efc09f319c3f2fabea43c8067d31
Rich Harris <rich.harris@vercel.com>
2025-06-26 09:07:54 -0400

    simplify

--- packages/svelte/src/compiler/phases/3-transform/client/visitors/CallExpression.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/CallExpression.js
@@ -64,7 +64,7 @@ export function CallExpression(node, context) {
 			);
 
 		case '$effect.pending':
-			return b.call(b.id('$.pending'));
+			return b.call('$.pending');
 
 		case '$inspect':
 		case '$inspect().with':
