1b5857f48c19b3fecb81af22a16599731f11e040
Rich Harris <rich.harris@vercel.com>
2025-06-30 15:25:49 -0400

    Update packages/svelte/src/internal/server/abort-signal.js
    Co-authored-by: Elliott Johnson <sejohnson@torchcloudconsulting.com>

--- packages/svelte/src/internal/server/abort-signal.js
+++ packages/svelte/src/internal/server/abort-signal.js
@@ -4,10 +4,8 @@ import { STALE_REACTION } from '#client/constants';
 export let controller = null;
 
 export function abort() {
-	if (controller !== null) {
-		controller.abort(STALE_REACTION);
-		controller = null;
-	}
+	controller?.abort(STALE_REACTION);
+	controller = null;
 }
 
 export function getAbortSignal() {
