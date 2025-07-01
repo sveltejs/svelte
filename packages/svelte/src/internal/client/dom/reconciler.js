5673d6042400ad9e61dc7cb5a03b00c396d82a28
Jay Harris <jay.harris@outlook.co.nz>
2025-07-01 15:12:23 +1200

    [trustedTypes]: Add TrustedTypes support to Svelte

--- packages/svelte/src/internal/client/dom/reconciler.js
+++ packages/svelte/src/internal/client/dom/reconciler.js
@@ -1,6 +1,25 @@
+/** @import { TrustedTypePolicy } from 'trusted-types' */
+
+/** @type {Pick<TrustedTypePolicy, 'createHTML'> | undefined} */
+let policy;
+
+if (globalThis?.window?.trustedTypes) {
+	policy = globalThis.window.trustedTypes.createPolicy('svelte-trusted-html', {
+		/** @param {string} html */
+		createHTML: (html) => {
+			return html;
+		}
+	});
+}
+
+/** @param {string} html */
+function create_trusted_html(html) {
+	return /** @type {string} */(policy?.createHTML(html) ?? html);
+}
+
 /** @param {string} html */
 export function create_fragment_from_html(html) {
 	var elem = document.createElement('template');
-	elem.innerHTML = html.replaceAll('<!>', '<!---->'); // XHTML compliance
+	elem.innerHTML = create_trusted_html(html.replaceAll('<!>', '<!---->')); // XHTML compliance
 	return elem.content;
 }
