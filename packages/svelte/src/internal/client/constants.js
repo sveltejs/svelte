b033d66d38dca8a6681d3cef51395d7088e933dc
Rich Harris <rich.harris@vercel.com>
2025-07-02 11:16:19 -0400

    chore: rename EFFECT_HAS_DERIVED to EFFECT_PRESERVED

--- packages/svelte/src/internal/client/constants.js
+++ packages/svelte/src/internal/client/constants.js
@@ -17,7 +17,7 @@ export const EFFECT_RAN = 1 << 15;
 export const EFFECT_TRANSPARENT = 1 << 16;
 export const INSPECT_EFFECT = 1 << 18;
 export const HEAD_EFFECT = 1 << 19;
-export const EFFECT_HAS_DERIVED = 1 << 20;
+export const EFFECT_PRESERVED = 1 << 20;
 export const EFFECT_IS_UPDATING = 1 << 21;
 
 export const STATE_SYMBOL = Symbol('$state');
