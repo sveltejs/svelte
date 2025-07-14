be0818552de9d282f5033bf8d1be3de34ce01794
Rich Harris <rich.harris@vercel.com>
2025-07-14 16:20:14 -0400

    chore: squelch hydration warning in test suite (#16336)

--- packages/svelte/tests/runtime-runes/samples/error-boundary-9/_config.js
+++ packages/svelte/tests/runtime-runes/samples/error-boundary-9/_config.js
@@ -1,7 +1,13 @@
 import { test } from '../../test';
 
 export default test({
-	test({ assert, target, logs }) {
+	test({ assert, target, logs, warnings, variant }) {
+		if (variant === 'hydrate') {
+			assert.deepEqual(warnings, [
+				'Hydration failed because the initial UI does not match what was rendered on the server'
+			]);
+		}
+
 		assert.deepEqual(logs, ['error caught']);
 		assert.htmlEqual(target.innerHTML, `<div>Error!</div><button>Retry</button>`);
 	}
