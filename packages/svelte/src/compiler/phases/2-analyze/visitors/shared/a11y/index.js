63b5ebf36eea3fb1fab138068f3731b122228edb
Ahmad S. <navorite@gmail.com>
2025-07-14 23:02:25 +0300

    fix: silence a11y warning for inert elements (#16339)
    * fix: silence a11y warning for inert elements
    
    * changeset
    
    * handle `inert=""`
    
    * oops
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>

--- packages/svelte/src/compiler/phases/2-analyze/visitors/shared/a11y/index.js
+++ packages/svelte/src/compiler/phases/2-analyze/visitors/shared/a11y/index.js
@@ -387,7 +387,10 @@ export function check_element(node, context) {
 	switch (node.name) {
 		case 'a':
 		case 'button': {
-			const is_hidden = get_static_value(attribute_map.get('aria-hidden')) === 'true';
+			const is_hidden =
+				get_static_value(attribute_map.get('aria-hidden')) === 'true' ||
+				get_static_value(attribute_map.get('inert')) !== null;
+
 			if (!has_spread && !is_hidden && !is_labelled && !has_content(node)) {
 				w.a11y_consider_explicit_label(node);
 			}
