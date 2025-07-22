02876d3e3353cbd8869bc3aadefcb26018241eb9
Rich Harris <rich.harris@vercel.com>
2025-07-22 12:45:58 -0400

    skip_no_async

--- packages/svelte/tests/runtime-runes/samples/effect-order-7/_config.js
+++ packages/svelte/tests/runtime-runes/samples/effect-order-7/_config.js
@@ -2,6 +2,12 @@ import { flushSync } from 'svelte';
 import { test } from '../../test';
 
 export default test({
+	// For this to work in non-async mode, we would need to abort
+	// inside `#traverse_effect_tree`, which would be very
+	// complicated and annoying. Since this hasn't been
+	// a real issue (AFAICT), we ignore it
+	skip_no_async: true,
+
 	async test({ target }) {
 		const [open, close] = target.querySelectorAll('button');
 
