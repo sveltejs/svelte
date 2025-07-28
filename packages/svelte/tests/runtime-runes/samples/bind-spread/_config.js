c8294cc6329902b91d2378166f3c8875ba9c5698
Jack Goodall <jack@datawrapper.de>
2025-07-28 15:25:28 +0200

    test IIFE support

--- packages/svelte/tests/runtime-runes/samples/bind-spread/_config.js
+++ packages/svelte/tests/runtime-runes/samples/bind-spread/_config.js
@@ -5,14 +5,9 @@ export default test({
 	async test({ assert, target, logs }) {
 		const checkboxes = target.querySelectorAll('input');
 
-		// input.value = '2';
-		// input.dispatchEvent(new window.Event('input'));
-
 		flushSync();
 
-		assert.htmlEqual(target.innerHTML, `<input type="checkbox" >`.repeat(4));
-
-		// assert.deepEqual(logs, ['b', '2', 'a', '2']);
+		assert.htmlEqual(target.innerHTML, `<input type="checkbox" >`.repeat(checkboxes.length));
 
 		flushSync(() => {
 			checkboxes.forEach((checkbox) => checkbox.click());
@@ -20,12 +15,12 @@ export default test({
 		assert.deepEqual(logs, [
 			'getArrayBindings',
 			'getObjectBindings',
-			...repeatArray(4, ['check', false])
+			...repeatArray(checkboxes.length, ['check', false])
 		]);
 	}
 });
 
 /** @template T */
-function repeatArray(/** @type {number} */ times = 1, /** @type {T[]} */ array) {
+function repeatArray(/** @type {number} */ times, /** @type {T[]} */ array) {
 	return /** @type {T[]} */ Array.from({ length: times }, () => array).flat();
 }
