7b19d12d34de72cb78a8ea4054b98e7eb8e93694
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-08-02 16:39:18 -0700

    add test, changeset

---
+++ packages/svelte/tests/runtime-runes/samples/async-const/_config.js
@@ -0,0 +1,16 @@
+import { test } from '../../test';
+
+export default test({
+	html: `<h1>Loading...</h1>`,
+
+	async test({ assert, target }) {
+		await new Promise((resolve) => setTimeout(resolve, 100));
+		assert.htmlEqual(
+			target.innerHTML,
+			`
+            <h1>Hello, world!</h1>
+            <input type="text"/>
+			`
+		);
+	}
+});
