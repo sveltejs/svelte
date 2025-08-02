4d677151ab7f08e4f718f983f7d706432dc31f59
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-08-01 23:13:43 -0700

    maybe this works?

--- packages/svelte/src/compiler/phases/2-analyze/visitors/Fragment.js
+++ packages/svelte/src/compiler/phases/2-analyze/visitors/Fragment.js
@@ -7,23 +7,23 @@
  */
 export function Fragment(node, context) {
 	const parent = /** @type {AST.TemplateNode} */ (context.path.at(-1));
-	if (
-		!parent ||
-		parent.type === 'Component' ||
-		parent.type === 'Root' ||
-		parent.type === 'IfBlock' ||
-		parent.type === 'KeyBlock' ||
-		parent.type === 'EachBlock' ||
-		parent.type === 'SnippetBlock' ||
-		parent.type === 'AwaitBlock'
-	) {
+	// if (
+	// 	!parent ||
+	// 	parent.type === 'Component' ||
+	// 	parent.type === 'Root' ||
+	// 	parent.type === 'IfBlock' ||
+	// 	parent.type === 'KeyBlock' ||
+	// 	parent.type === 'EachBlock' ||
+	// 	parent.type === 'SnippetBlock' ||
+	// 	parent.type === 'AwaitBlock'
+	// ) {
 		const fragment_metadata = {
 			has_await: false,
 			node
 		};
 		context.next({ ...context.state, fragment: fragment_metadata });
 		node.metadata.has_await = fragment_metadata.has_await;
-	} else {
-		context.next();
-	}
+	// } else {
+	// 	context.next();
+	// }
 }
