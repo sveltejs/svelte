55f49c58776afa60312dacbe9278d0b2c1ca393c
Chew Tee Ming <chew.tee.ming@nindatech.com>
2025-08-05 11:35:36 +0800

    regenerate types

--- packages/svelte/types/index.d.ts
+++ packages/svelte/types/index.d.ts
@@ -3313,7 +3313,7 @@ declare namespace $derived {
  *
  * If you return a function from the effect, it will be called right before the effect is run again, or when the component is unmounted.
  *
- * Does not run during server side rendering.
+ * Does not run during server-side rendering.
  *
  * https://svelte.dev/docs/svelte/$effect
  * @param fn The function to execute
@@ -3332,7 +3332,7 @@ declare namespace $effect {
 	 *
 	 * If you return a function from the effect, it will be called right before the effect is run again, or when the component is unmounted.
 	 *
-	 * Does not run during server side rendering.
+	 * Does not run during server-side rendering.
 	 *
 	 * https://svelte.dev/docs/svelte/$effect#$effect.pre
 	 * @param fn The function to execute
