b4cacabb427c95d14f434f4ed74356167f7b9655
Rich Harris <rich.harris@vercel.com>
2025-08-15 15:23:02 -0400

    add test

---
+++ packages/svelte/tests/runtime-runes/samples/dynamic-component-destroy-then-create/main.svelte
@@ -0,0 +1,13 @@
+<script>
+	import A from './A.svelte';
+	import B from './B.svelte';
+
+	let condition = $state(true);
+	let Component = $derived(condition ? A : B);
+</script>
+
+<button onclick={() => condition = !condition}>
+	toggle ({condition})
+</button>
+
+<Component />
