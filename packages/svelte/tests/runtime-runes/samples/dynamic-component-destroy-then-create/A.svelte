f5d4350f3d60606c556e195a80f21427a4b6738c
Rich Harris <rich.harris@vercel.com>
2025-08-18 08:52:44 -0400

    Revert "fix: do not defer unmount; immediately unmount components (#16624)" (#16639)
    This reverts commit 95e51755814329d596ced98fb1829d0f797e503d.

--- packages/svelte/tests/runtime-runes/samples/dynamic-component-destroy-then-create/A.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	$effect.pre(() => {
-		console.log('create A');
-		return () => console.log('destroy A');
-	});
-</script>
-
-<h1>A</h1>
--- packages/svelte/tests/runtime-runes/samples/dynamic-component-destroy-then-create/B.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	$effect.pre(() => {
-		console.log('create B');
-		return () => console.log('destroy B');
-	});
-</script>
-
-<h1>B</h1>
