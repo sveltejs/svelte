8bcd9e5f00b832c11f7aba133b25de1f0aac0cc3
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-08-25 01:07:52 -0700

    lint

--- packages/svelte/tests/runtime-runes/samples/async-reference-in-snippet/_config.js
+++ packages/svelte/tests/runtime-runes/samples/async-reference-in-snippet/_config.js
@@ -2,8 +2,8 @@ import { tick } from 'svelte';
 import { test } from '../../test';
 
 export default test({
-    async test({ assert, target }) {
-        await tick();
-        assert.htmlEqual(target.innerHTML, 'value');
-    }
+	async test({ assert, target }) {
+		await tick();
+		assert.htmlEqual(target.innerHTML, 'value');
+	}
 });
