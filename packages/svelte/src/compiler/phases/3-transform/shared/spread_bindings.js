9d74e2220d0d6ca0605632d52a6e81fdc6941aac
Jack Goodall <jack@datawrapper.de>
2025-09-02 18:56:40 +0200

    run bindings in derived

--- packages/svelte/src/compiler/phases/3-transform/shared/spread_bindings.js
+++ packages/svelte/src/compiler/phases/3-transform/shared/spread_bindings.js
@@ -16,15 +16,21 @@ export function init_spread_bindings(spread_expression, { state, visit }) {
 		? b.literal(source.slice(spread_expression.start, spread_expression.end))
 		: undefined;
 
-	const id = state.scope.generate('$$spread_binding');
-	const get = b.id(id + '_get');
-	const set = b.id(id + '_set');
+	const id = b.id(state.scope.generate('$$spread_binding'));
 	state.init.push(
 		b.const(
-			b.array_pattern([get, set]),
-			b.call('$.validate_spread_bindings', expression, expression_text)
+			id,
+			b.call(
+				'$.derived',
+				b.thunk(b.call('$.validate_spread_bindings', expression, expression_text))
+			)
 		)
 	);
 
+	const is_server = state.options.generate === 'server';
+	const binding = is_server ? b.call(id) : b.call('$.get', id);
+
+	const get = b.thunk(b.call(b.member(binding, b.literal(0), true)));
+	const set = b.thunk(b.call(b.member(binding, b.literal(1), true), b.id('$$value')));
 	return { get, set };
 }
