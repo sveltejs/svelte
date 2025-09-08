dca7f9bc63ee9384d0da5e20572fefac01b3096f
Simon Holthausen <simon.holthausen@vercel.com>
2025-09-08 23:15:23 +0200

    enhance test

--- packages/svelte/tests/runtime-runes/samples/async-attachment/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/async-attachment/main.svelte
@@ -1,9 +1,14 @@
 <script>
 	import Inner from './Inner.svelte';
+
+	let show = $state(true);
 </script>
 
 <svelte:boundary>
-	<Inner />
+	<button onclick={() => show = !show}>toggle</button>
+	{#if show}
+		<Inner />
+	{/if}
 
 	{#snippet pending()}
 		<p>pending</p>
