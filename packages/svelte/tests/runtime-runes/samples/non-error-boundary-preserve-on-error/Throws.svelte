89fdc02004ac535582089247c1864e3fad94cb0e
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2025-09-10 15:53:15 -0600

    test

---
+++ packages/svelte/tests/runtime-runes/samples/non-error-boundary-preserve-on-error/Throws.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { SvelteSet } from 'svelte/reactivity';
+	
+  let outside_basic = $state(false);
+	let outside_basic_set = new SvelteSet();
+	const throws_basic = $derived.by(() => {
+		outside_basic_set.add(1);
+		return outside_basic_set;
+	})
+</script>
+
+<button onclick={() => (outside_basic = true)}>click me if u dare</button>
+{#if outside_basic && throws_basic}
+	{throws_basic}
+{/if}
+<p>this should be rendered</p>
---
+++ packages/svelte/tests/runtime-runes/samples/non-error-boundary-preserve-on-error/main.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Throws from './Throws.svelte';
+</script>
+
+<svelte:boundary>
+  <Throws />
+</svelte:boundary>
