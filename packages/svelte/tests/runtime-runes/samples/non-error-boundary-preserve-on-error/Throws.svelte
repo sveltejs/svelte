9e6f4853b8f0b69f468cd37feff912c5c316e294
Rich Harris <rich.harris@vercel.com>
2025-09-10 18:00:58 -0400

    prettier

--- packages/svelte/tests/runtime-runes/samples/non-error-boundary-preserve-on-error/Throws.svelte
+++ packages/svelte/tests/runtime-runes/samples/non-error-boundary-preserve-on-error/Throws.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { SvelteSet } from 'svelte/reactivity';
-	
-  let outside_basic = $state(false);
+
+	let outside_basic = $state(false);
 	let outside_basic_set = new SvelteSet();
 	const throws_basic = $derived.by(() => {
 		outside_basic_set.add(1);
