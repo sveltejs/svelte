2de7ba118590a3240d28c238d7099da37123c0fe
Rich Harris <rich.harris@vercel.com>
2025-09-10 20:14:47 -0400

    add test

---
+++ packages/svelte/tests/runtime-runes/samples/snippet-hoisting-4/main.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Component from './Component.svelte';
+
+	const object = {
+		property: Component
+	};
+</script>
+
+{#snippet not_hoisted()}
+	<object.property />
+{/snippet}
+
+{@render not_hoisted()}
