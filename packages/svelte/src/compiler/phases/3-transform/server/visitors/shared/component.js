8632107fe2318480084409cd90aa004a5e8e61e0
Rich Harris <rich.harris@vercel.com>
2025-09-14 12:19:13 -0400

    unused

--- packages/svelte/src/compiler/phases/3-transform/server/visitors/shared/component.js
+++ packages/svelte/src/compiler/phases/3-transform/server/visitors/shared/component.js
@@ -1,7 +1,7 @@
 /** @import { BlockStatement, Expression, Pattern, Property, SequenceExpression, Statement } from 'estree' */
 /** @import { AST } from '#compiler' */
 /** @import { ComponentContext } from '../../types.js' */
-import { empty_comment, build_attribute_value, call_child_payload } from './utils.js';
+import { empty_comment, build_attribute_value } from './utils.js';
 import * as b from '#compiler/builders';
 import { is_element_node } from '../../../../nodes.js';
 import { dev } from '../../../../../state.js';
