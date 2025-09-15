9889437bff0588fc62c88f398882e36cf90b600c
Rich Harris <rich.harris@vercel.com>
2025-09-14 21:23:45 -0400

    unused

--- packages/svelte/src/compiler/phases/3-transform/server/visitors/Fragment.js
+++ packages/svelte/src/compiler/phases/3-transform/server/visitors/Fragment.js
@@ -1,14 +1,8 @@
 /** @import { AST } from '#compiler' */
-/** @import { Statement } from 'estree' */
 /** @import { ComponentContext, ComponentServerTransformState } from '../types.js' */
 import { clean_nodes, infer_namespace } from '../../utils.js';
 import * as b from '#compiler/builders';
-import {
-	empty_comment,
-	process_children,
-	build_template,
-	call_child_payload
-} from './shared/utils.js';
+import { empty_comment, process_children, build_template } from './shared/utils.js';
 
 /**
  * @param {AST.Fragment} node
