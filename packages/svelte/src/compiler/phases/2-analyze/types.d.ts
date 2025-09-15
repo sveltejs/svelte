3c3360cfe96ac81b90a783b70f8aefabbc017465
Rich Harris <rich.harris@vercel.com>
2025-09-15 09:23:25 -0400

    chore: remove analysis.state.title

--- packages/svelte/src/compiler/phases/2-analyze/types.d.ts
+++ packages/svelte/src/compiler/phases/2-analyze/types.d.ts
@@ -9,7 +9,6 @@ export interface AnalysisState {
 	options: ValidatedCompileOptions;
 	ast_type: 'instance' | 'template' | 'module';
 	fragment: AST.Fragment | null;
-	title: AST.TitleElement | null;
 	/**
 	 * Tag name of the parent element. `null` if the parent is `svelte:element`, `#snippet`, a component or the root.
 	 * Parent doesn't necessarily mean direct path predecessor because there could be `#each`, `#if` etc in-between.
