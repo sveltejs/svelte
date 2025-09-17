6076bab730ee7752898926ac6e10bbaf5e665a22
Rich Harris <rich.harris@vercel.com>
2025-09-17 19:30:45 -0400

    fix: issue `state_proxy_unmount` warning when unmounting a state proxy (#16747)
    * fix: issue `state_proxy_unmount` warning when unmounting a state proxy
    
    * regenerate

---
+++ .changeset/fuzzy-pillows-tell.md
@@ -0,0 +1,5 @@
+---
+'svelte': patch
+---
+
+fix: issue `state_proxy_unmount` warning when unmounting a state proxy
--- packages/svelte/src/internal/client/render.js
+++ packages/svelte/src/internal/client/render.js
@@ -30,7 +30,7 @@ import * as w from './warnings.js';
 import * as e from './errors.js';
 import { assign_nodes } from './dom/template.js';
 import { is_passive_event } from '../../utils.js';
-import { COMMENT_NODE } from './constants.js';
+import { COMMENT_NODE, STATE_SYMBOL } from './constants.js';
 import { boundary } from './dom/blocks/boundary.js';
 
 /**
@@ -316,7 +316,11 @@ export function unmount(component, options) {
 	}
 
 	if (DEV) {
-		w.lifecycle_double_unmount();
+		if (STATE_SYMBOL in component) {
+			w.state_proxy_unmount();
+		} else {
+			w.lifecycle_double_unmount();
+		}
 	}
 
 	return Promise.resolve();
