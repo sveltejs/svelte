d50b4d955a8633aceeddad5711f65434e02c3a39
Rich Harris <rich.harris@vercel.com>
2025-09-22 14:58:02 -0400

    add another test

---
+++ packages/svelte/tests/runtime-runes/samples/async-boundary-coordination/main.svelte
@@ -0,0 +1,35 @@
+<script>
+	let browser = typeof window !== 'undefined';
+
+	let a = Promise.withResolvers();
+	let b = Promise.withResolvers();
+
+	if (!browser) {
+		a.resolve(true);
+		b.resolve(true);
+	}
+</script>
+
+<button onclick={() => a.resolve(true)}>
+	resolve a
+</button>
+
+<button onclick={() => b.resolve(true)}>
+	resolve b
+</button>
+
+<svelte:boundary>
+	{#if await a.promise}
+		<p>hello from {browser ? 'browser' : 'server'}</p>
+	{/if}
+
+	<p>hello from {browser ? 'browser' : 'server'}</p>
+</svelte:boundary>
+
+<svelte:boundary>
+	{#if await b.promise}
+		<p>hello from {browser ? 'browser' : 'server'}</p>
+	{/if}
+
+	<p>hello from {browser ? 'browser' : 'server'}</p>
+</svelte:boundary>
