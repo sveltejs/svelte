2c0304ffec984a301d4110aaff82de21b5b5e92d
Rich Harris <rich.harris@vercel.com>
2025-10-13 16:26:57 -0400

    get rid of logs

--- packages/svelte/tests/runtime-runes/samples/async-resolve-stale/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/async-resolve-stale/main.svelte
@@ -7,26 +7,22 @@
 		const r = Promise.withResolvers();
 
 		if (prev || v === '') {
-			console.log('hello', !!prev)
 			Promise.resolve().then(async () => {
-				console.log('count++')
 				count++;
 				r.resolve(v);
 				await new Promise(r => setTimeout(r, 0));
 				// TODO with a microtask like below it still throws a mutation error
 				// await Promise.resolve();
 				prev?.resolve();
-			})
+			});
 		} else {
-			console.log('other')
 			prev = Promise.withResolvers();
 			prev.promise.then(() => {
-				console.log('other coun++')
 				count++;
 				r.resolve(v)
-			})
+			});
 		}
-		
+
 		return r.promise;
 	}
 
