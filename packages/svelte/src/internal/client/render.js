b9208dbe90fc5f2ac1887131f4ded42f5af141cf
Rich Harris <rich.harris@vercel.com>
2025-10-17 21:18:36 -0400

    fix

--- packages/svelte/src/internal/client/render.js
+++ packages/svelte/src/internal/client/render.js
@@ -137,7 +137,6 @@ export function hydrate(component, options) {
 		if (error !== HYDRATION_ERROR) {
 			// eslint-disable-next-line no-console
 			console.warn('Failed to hydrate: ', error);
-			console.error(error);
 		}
 
 		if (options.recover === false) {
