3d4f3e1353ddf51dfa4d7819551c0189fb1a5589
Rich Harris <rich.harris@vercel.com>
2025-10-21 20:14:59 -0700

    argh

--- packages/svelte/tests/helpers.js
+++ packages/svelte/tests/helpers.js
@@ -210,6 +210,9 @@ export function normalise_inspect_logs(logs) {
 
 			const match = last_line && /(at .+) /.exec(last_line);
 
+			// fucking windows jfc
+			console.error({ stack: log.stack, last_line, match });
+
 			return match && match[1];
 		}
 
