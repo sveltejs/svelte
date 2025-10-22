723d9eb5c8bd832cb6bb38d15e3f822361585647
Rich Harris <rich.harris@vercel.com>
2025-10-21 19:54:58 -0700

    fix the dang tests

--- packages/svelte/tests/runtime-runes/samples/inspect-nested-state/_config.js
+++ packages/svelte/tests/runtime-runes/samples/inspect-nested-state/_config.js
@@ -1,3 +1,4 @@
+import { normalise_inspect_logs } from '../../../helpers.js';
 import { test } from '../../test';
 
 export default test({
@@ -10,13 +11,12 @@ export default test({
 		b1.click();
 		await Promise.resolve();
 
-		assert.deepEqual(logs, [
-			'init',
+		assert.deepEqual(normalise_inspect_logs(logs), [
 			{ x: { count: 0 } },
 			[{ count: 0 }],
-			'update',
 			{ x: { count: 1 } },
-			[{ count: 1 }]
+			[{ count: 1 }],
+			'at HTMLButtonElement.<anonymous>'
 		]);
 	}
 });
