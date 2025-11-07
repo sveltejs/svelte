a793d914ba306a8ca11558882c80fd7ecf3b7ccb
Simon Holthausen <simon.holthausen@vercel.com>
2025-11-07 22:45:52 +0100

    restore each context upon commit - prevents `run` getting undefined boundaries

--- packages/svelte/src/internal/client/dom/blocks/each.js
+++ packages/svelte/src/internal/client/dom/blocks/each.js
@@ -42,6 +42,7 @@ import { active_effect, get } from '../../runtime.js';
 import { DEV } from 'esm-env';
 import { derived_safe_equal } from '../../reactivity/deriveds.js';
 import { current_batch } from '../../reactivity/batch.js';
+import { capture } from '../../reactivity/async.js';
 
 /**
  * The row of a keyed each block that is currently updating. We track this
@@ -159,7 +160,14 @@ export function each(node, flags, get_collection, get_key, render_fn, fallback_f
 	/** @type {Effect} */
 	var each_effect;
 
+	/** @type {ReturnType<typeof capture>} */
+	var restore;
+
 	function commit() {
+		// If async work was pending commit could happen long after the block has ran.
+		// Restore so batch etc are created in its correct parent context.
+		restore?.(false);
+
 		reconcile(
 			each_effect,
 			array,
@@ -188,6 +196,7 @@ export function each(node, flags, get_collection, get_key, render_fn, fallback_f
 	}
 
 	block(() => {
+		restore ??= capture();
 		// store a reference to the effect so that we can update the start/end nodes in reconciliation
 		each_effect ??= /** @type {Effect} */ (active_effect);
 
