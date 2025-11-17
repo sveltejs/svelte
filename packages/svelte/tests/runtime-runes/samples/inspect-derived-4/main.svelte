99e670f63297ca86831cd4d786ef2adf8fdca5a4
Simon H <5968653+dummdidumm@users.noreply.github.com>
2025-11-17 23:00:00 +0100

    fix: ensure eager effects don't break reactions chain (#17138)
    Execution of eager effects didn't set `is_updating_effect`, which meant the logic in `get` would wrongfully prevent dependencies being added to `reactions` of sources/deriveds.
    
    Fixes #17133

---
+++ packages/svelte/tests/runtime-runes/samples/inspect-derived-4/main.svelte
@@ -0,0 +1,14 @@
+<script>
+	import {SvelteSet} from "svelte/reactivity";
+	const ids = [0,1,2];
+	const seenIds = new SvelteSet();
+
+	const unseenIds = $derived(ids.filter((id) => !seenIds.has(id)));
+	
+	const currentId = $derived(unseenIds.at(0));
+	$inspect(unseenIds)
+</script>
+
+<button onclick={() => seenIds.add(currentId)}>
+	first unseen: {currentId}
+</button>
