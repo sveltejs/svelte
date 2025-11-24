915e70c86f47cc235a29a44106ef940b5c399d59
Manuel Serret <mserret99@gmail.com>
2025-11-24 19:42:27 +0100

    add formatting related test

--- packages/svelte/tests/print/samples/block/input.svelte
+++ packages/svelte/tests/print/samples/block/input.svelte
@@ -3,3 +3,7 @@
 {#each items as item, i}
   <p>{i}: {item}</p>
 {/each}
+
+{#if condition}yes{:else}no{/if}
+
+{#each items as item, i}<p>{i}: {item}</p>{/each}
--- packages/svelte/tests/print/samples/block/output.svelte
+++ packages/svelte/tests/print/samples/block/output.svelte
@@ -7,3 +7,13 @@
 {#each items as item, i}
 	<p>{i}: {item}</p>
 {/each}
+
+{#if condition}
+	yes
+{:else}
+	no
+{/if}
+
+{#each items as item, i}
+	<p>{i}: {item}</p>
+{/each}
