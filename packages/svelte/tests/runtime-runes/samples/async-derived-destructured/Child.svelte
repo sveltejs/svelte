c1b1fa554bae1368b505bb6efa51623fac3b8a89
Simon Holthausen <simon.holthausen@vercel.com>
2025-11-25 22:47:43 +0100

    adjust existing test so it fails on main

--- packages/svelte/tests/runtime-runes/samples/async-derived-destructured/Child.svelte
+++ packages/svelte/tests/runtime-runes/samples/async-derived-destructured/Child.svelte
@@ -1,17 +1,17 @@
 <script>
 	let count = $state(1);
 
-	// Simple init
-	let { toFixed } = $derived(count);
 	// More complex init
 	let { squared, cubed } = $derived(await {
 		squared: count ** 2,
 		cubed: count ** 3
 	});
+	// Simple init with multiple destructurings after await
+	let { toFixed, toString } = $derived(count);
 </script>
 
 <button onclick={() => count++}>increment</button>
 
 <p>{count} ** 2 = {squared}</p>
 <p>{count} ** 3 = {cubed}</p>
-<p>{typeof toFixed}</p>
+<p>{typeof toFixed} {typeof toString}</p>
