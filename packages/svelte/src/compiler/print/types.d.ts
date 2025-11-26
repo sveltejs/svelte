83206d93b66296a71a898bd328548f1e97bc6474
Manuel Serret <mserret99@gmail.com>
2025-11-26 20:16:48 +0100

    fix types?

--- packages/svelte/src/compiler/print/types.d.ts
+++ packages/svelte/src/compiler/print/types.d.ts
@@ -1,4 +1,16 @@
 export type Options = {
-	getLeadingComments?: ((node: any) => Array<{ type: 'Line' | 'Block'; value: string; start?: number; end?: number; }> | undefined) | undefined;
-	getTrailingComments?: ((node: any) => Array<{ type: 'Line' | 'Block'; value: string; start?: number; end?: number; }> | undefined) | undefined;
+	getLeadingComments?:
+		| ((
+				node: any
+		  ) =>
+				| Array<{ type: 'Line' | 'Block'; value: string; start?: number; end?: number }>
+				| undefined)
+		| undefined;
+	getTrailingComments?:
+		| ((
+				node: any
+		  ) =>
+				| Array<{ type: 'Line' | 'Block'; value: string; start?: number; end?: number }>
+				| undefined)
+		| undefined;
 };
