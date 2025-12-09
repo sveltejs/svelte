7a5886de7c2803fbbc73841ff4ddb3fc94cb8ca0
Elliott Johnson <hello@ell.iott.dev>
2025-12-09 16:02:41 -0700

    typescript

--- packages/svelte/src/internal/server/crypto.js
+++ packages/svelte/src/internal/server/crypto.js
@@ -4,5 +4,6 @@ let text_encoder;
 export async function sha256(data) {
 	text_encoder ??= new TextEncoder();
 	const hash_buffer = await crypto.subtle.digest('SHA-256', text_encoder.encode(data));
+	// @ts-ignore - we don't install node types in the prod build
 	return Buffer.from(hash_buffer).toString('base64');
 }
