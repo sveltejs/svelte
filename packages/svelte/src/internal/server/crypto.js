25b9b9346a117a9249add8177fb4783b6d458c11
Elliott Johnson <hello@ell.iott.dev>
2025-12-09 16:09:38 -0700

    ts

--- packages/svelte/src/internal/server/crypto.js
+++ packages/svelte/src/internal/server/crypto.js
@@ -1,14 +1,13 @@
-// TODO - remove this and use global `crypto` when we drop Node 18
-// @ts-ignore - we don't install node types in the prod build
-import { webcrypto } from 'node:crypto';
-
 let text_encoder;
+// TODO - remove this and use global `crypto` when we drop Node 18
+let crypto;
 
 /** @param {string} data */
 export async function sha256(data) {
 	text_encoder ??= new TextEncoder();
 	// @ts-ignore - we don't install node types in the prod build
-	const hash_buffer = await webcrypto.subtle.digest('SHA-256', text_encoder.encode(data));
+	crypto ??= (await import('node:crypto')).webcrypto;
+	const hash_buffer = await crypto.subtle.digest('SHA-256', text_encoder.encode(data));
 	// @ts-ignore - we don't install node types in the prod build
 	return Buffer.from(hash_buffer).toString('base64');
 }
