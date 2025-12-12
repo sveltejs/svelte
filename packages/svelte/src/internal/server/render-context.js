237fde05330d20e54e4269551cf74a583f660733
Elliott Johnson <hello@ell.iott.dev>
2025-12-11 18:16:47 -0700

    ugh

--- packages/svelte/src/internal/server/render-context.js
+++ packages/svelte/src/internal/server/render-context.js
@@ -58,30 +58,24 @@ export async function with_render_context(fn) {
 let als = null;
 /** @type {Promise<void> | null} */
 let als_import = null;
-let resolved_promise = null;
 
 /**
  *
  * @returns {Promise<void>}
  */
 export function init_render_context() {
-	if (als === null) {
-		try {
-			// It's important the right side of this assignment can run a maximum of one time
-			// otherwise it's possible for a very, very well-timed race condition to assign to `als`
-			// at the beginning of a render, and then another render to assign to it again, which causes
-			// the first render's second half to use a new instance of `als` which doesn't have its
-			// context anymore.
-			// @ts-ignore -- we don't include node types in the production build
-			als_import ??= import('node:async_hooks')
-				.then((hooks) => {
-					als = new hooks.AsyncLocalStorage();
-				})
-				.then(noop);
-			return als_import;
-		} catch {}
-	}
-	return (resolved_promise ??= Promise.resolve());
+	// It's important the right side of this assignment can run a maximum of one time
+	// otherwise it's possible for a very, very well-timed race condition to assign to `als`
+	// at the beginning of a render, and then another render to assign to it again, which causes
+	// the first render's second half to use a new instance of `als` which doesn't have its
+	// context anymore.
+	// @ts-ignore -- we don't include node types in the production build
+	als_import ??= import('node:async_hooks')
+		.then((hooks) => {
+			als = new hooks.AsyncLocalStorage();
+		})
+		.then(noop, noop);
+	return als_import;
 }
 
 // this has to be a function because rollup won't treeshake it if it's a constant
