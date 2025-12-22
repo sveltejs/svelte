819442eaf7d4c018fc58f61285e2b7f67feee9ad
Rich Harris <rich.harris@vercel.com>
2025-12-22 13:41:52 -0500

    tweak names for clarity, simplify `update` slightly

--- packages/svelte/src/internal/client/dev/hmr.js
+++ packages/svelte/src/internal/client/dev/hmr.js
@@ -3,15 +3,17 @@ import { FILENAME, HMR } from '../../../constants.js';
 import { EFFECT_TRANSPARENT } from '#client/constants';
 import { hydrate_node, hydrating } from '../dom/hydration.js';
 import { block, branch, destroy_effect } from '../reactivity/effects.js';
-import { set, source, update } from '../reactivity/sources.js';
+import { set, source } from '../reactivity/sources.js';
 import { set_should_intro } from '../render.js';
 import { get } from '../runtime.js';
 
 /**
  * @template {(anchor: Comment, props: any) => any} Component
- * @param {Component} original_component
+ * @param {Component} fn
  */
-export function hmr(original_component) {
+export function hmr(fn) {
+	const current = source(fn);
+
 	/**
 	 * @param {TemplateNode} anchor
 	 * @param {any} props
@@ -26,7 +28,7 @@ export function hmr(original_component) {
 		let ran = false;
 
 		block(() => {
-			if (component === (component = get(wrapper[HMR].source))) {
+			if (component === (component = get(current))) {
 				return;
 			}
 
@@ -63,26 +65,23 @@ export function hmr(original_component) {
 	}
 
 	// @ts-expect-error
-	wrapper[FILENAME] = original_component[FILENAME];
+	wrapper[FILENAME] = fn[FILENAME];
 
 	// @ts-ignore
 	wrapper[HMR] = {
-		original: original_component,
-		source: source(original_component),
-		update: (/** @type {any} */ c) => {
+		fn,
+		current,
+		update: (/** @type {any} */ incoming) => {
 			// This logic ensures that the first version of the component is the one
 			// whose update function and therefore block effect is preserved across updates.
-
-			// We do that by first updating the component instance with the latest one
-			// (c is the HMR-updated version of the component, already wrapped, so we use c[HMR].original)...
-			original_component = wrapper[HMR].original = c[HMR].original;
-			// ...then go the other way to tell the latest version to always
-			// use the source of the very first version...
-			c[HMR].source = wrapper[HMR].source;
-			// ...and finally trigger the block effect update
-			set(wrapper[HMR].source, original_component);
-			// If we don't do this dance and instead just use c as the new component
+			// If we don't do this dance and instead just use `incoming` as the new component
 			// and then update, we'll create an ever-growing stack of block effects.
+
+			// Trigger the original block effect
+			set(wrapper[HMR].current, incoming[HMR].fn);
+
+			// Replace the incoming source with the original one
+			incoming[HMR].current = wrapper[HMR].current;
 		}
 	};
 
