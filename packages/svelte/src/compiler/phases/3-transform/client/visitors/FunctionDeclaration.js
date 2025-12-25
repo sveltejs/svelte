11d6452b623cbba3d0640646ad4748c9265ca0a2
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2025-12-25 12:22:25 -0800

    lint

--- packages/svelte/src/compiler/phases/3-transform/client/visitors/FunctionDeclaration.js
+++ packages/svelte/src/compiler/phases/3-transform/client/visitors/FunctionDeclaration.js
@@ -10,7 +10,10 @@ export function FunctionDeclaration(node, context) {
 	const state = { ...context.state, in_constructor: false, in_derived: false };
 	if (node.id) {
 		const binding = context.state.scope.get(node.id.name);
-		if (binding && context.state.analysis.instance_body?.top_function_declarations.includes(binding)) {
+		if (
+			binding &&
+			context.state.analysis.instance_body?.top_function_declarations.includes(binding)
+		) {
 			const fn = /** @type {FunctionDeclaration} */ (context.next(state) ?? node);
 			const expression = b.function(fn.id, fn.params, fn.body, fn.async, fn.generator);
 			const declaration = b.let(fn.id, b.call('$.state', expression));
