61d17cab9b782341eed279de5108f65f0abda6e2
Rich Harris <rich.harris@vercel.com>
2026-01-03 15:35:37 +0000

    remove comment

--- packages/svelte/tests/runtime-runes/samples/async-derived-with-effect-and-boundary/_config.js
+++ packages/svelte/tests/runtime-runes/samples/async-derived-with-effect-and-boundary/_config.js
@@ -1,10 +1,6 @@
 import { tick } from 'svelte';
 import { test } from '../../test';
 
-// Ensure async derived remains reactive with associated effect and boundary with guard (#17271)
-//
-// Accounts for both UNINITIALIZED leaking from get and the baz derived remaining NaN
-// due to having both an $effect and a boundary with an if in the same template
 export default test({
 	skip_no_async: true,
 
