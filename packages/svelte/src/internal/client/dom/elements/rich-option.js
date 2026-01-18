f5acb01d67ab499f9a6687f74024f75d6b031540
Rich Harris <rich.harris@vercel.com>
2026-01-18 15:18:04 -0500

    fix hydration

--- packages/svelte/src/internal/client/dom/elements/rich-option.js
+++ packages/svelte/src/internal/client/dom/elements/rich-option.js
@@ -1,5 +1,5 @@
-import { hydrating, set_hydrating } from '../hydration.js';
-import { support_customizable_select, create_comment, create_text } from '../operations.js';
+import { hydrating, reset, set_hydrate_node, set_hydrating } from '../hydration.js';
+import { support_customizable_select, create_comment } from '../operations.js';
 
 /**
  * Handles rich HTML content inside `<option>`, `<optgroup>`, or `<select>` elements with browser-specific branching.
@@ -10,14 +10,23 @@ import { support_customizable_select, create_comment, create_text } from '../ope
  */
 export function customizable_select_element(element, rich_fn) {
 	var was_hydrating = hydrating;
+
 	if (!support_customizable_select()) {
 		set_hydrating(false);
 		element.textContent = '';
 		element.append(create_comment(''));
 	}
+
 	try {
 		rich_fn();
 	} finally {
-		set_hydrating(was_hydrating);
+		if (was_hydrating) {
+			if (hydrating) {
+				reset(element);
+			} else {
+				set_hydrating(true);
+				set_hydrate_node(element);
+			}
+		}
 	}
 }
