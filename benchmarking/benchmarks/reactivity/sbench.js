6632a11dfb26a63d24628a5c7fdcd6501a6a3ce9
Rich Harris <rich.harris@vercel.com>
2026-01-19 16:20:17 -0500

    reduce indirection

--- benchmarking/benchmarks/reactivity/sbench.js
+++ benchmarking/benchmarks/reactivity/sbench.js
@@ -79,64 +79,6 @@ function create_computation_4(s1, s2, s3, s4) {
 	$.derived(() => $.get(s1) + $.get(s2) + $.get(s3) + $.get(s4));
 }
 
-function create_computations_1000to1(n, sources) {
-	for (let i = 0; i < n; i++) {
-		create_computation_1000(sources, i * 1000);
-	}
-}
-
-function create_computations_1to2(n, sources) {
-	for (let i = 0; i < n / 2; i++) {
-		const source = sources[i];
-		create_computation_1(source);
-		create_computation_1(source);
-	}
-}
-
-function create_computations_1to4(n, sources) {
-	for (let i = 0; i < n / 4; i++) {
-		const source = sources[i];
-		create_computation_1(source);
-		create_computation_1(source);
-		create_computation_1(source);
-		create_computation_1(source);
-	}
-}
-
-function create_computations_1to8(n, sources) {
-	for (let i = 0; i < n / 8; i++) {
-		const source = sources[i];
-		create_computation_1(source);
-		create_computation_1(source);
-		create_computation_1(source);
-		create_computation_1(source);
-		create_computation_1(source);
-		create_computation_1(source);
-		create_computation_1(source);
-		create_computation_1(source);
-	}
-}
-
-function create_computations_1to1000(n, sources) {
-	for (let i = 0; i < n / 1000; i++) {
-		const source = sources[i];
-		for (let j = 0; j < 1000; j++) {
-			create_computation_1(source);
-		}
-	}
-}
-
-function create_computations_4to1(n, sources) {
-	for (let i = 0; i < n; i++) {
-		create_computation_4(
-			sources[i * 4],
-			sources[i * 4 + 1],
-			sources[i * 4 + 2],
-			sources[i * 4 + 3]
-		);
-	}
-}
-
 /**
  * @param {any} fn
  * @param {number} count
@@ -209,42 +151,88 @@ export const sbench_create_2to1 = create_sbench_test(
 
 export const sbench_create_4to1 = create_sbench_test(
 	'sbench_create_4to1',
-	create_computations_4to1,
+	function create_computations_4to1(n, sources) {
+		for (let i = 0; i < n; i++) {
+			create_computation_4(
+				sources[i * 4],
+				sources[i * 4 + 1],
+				sources[i * 4 + 2],
+				sources[i * 4 + 3]
+			);
+		}
+	},
 	COUNT / 4,
 	COUNT
 );
 
 export const sbench_create_1000to1 = create_sbench_test(
 	'sbench_create_1000to1',
-	create_computations_1000to1,
+	function create_computations_1000to1(n, sources) {
+		for (let i = 0; i < n; i++) {
+			create_computation_1000(sources, i * 1000);
+		}
+	},
 	COUNT / 1000,
 	COUNT
 );
 
 export const sbench_create_1to2 = create_sbench_test(
 	'sbench_create_1to2',
-	create_computations_1to2,
+	function create_computations_1to2(n, sources) {
+		for (let i = 0; i < n / 2; i++) {
+			const source = sources[i];
+			create_computation_1(source);
+			create_computation_1(source);
+		}
+	},
 	COUNT,
 	COUNT / 2
 );
 
 export const sbench_create_1to4 = create_sbench_test(
 	'sbench_create_1to4',
-	create_computations_1to4,
+	function create_computations_1to4(n, sources) {
+		for (let i = 0; i < n / 4; i++) {
+			const source = sources[i];
+			create_computation_1(source);
+			create_computation_1(source);
+			create_computation_1(source);
+			create_computation_1(source);
+		}
+	},
 	COUNT,
 	COUNT / 4
 );
 
 export const sbench_create_1to8 = create_sbench_test(
 	'sbench_create_1to8',
-	create_computations_1to8,
+	function create_computations_1to8(n, sources) {
+		for (let i = 0; i < n / 8; i++) {
+			const source = sources[i];
+			create_computation_1(source);
+			create_computation_1(source);
+			create_computation_1(source);
+			create_computation_1(source);
+			create_computation_1(source);
+			create_computation_1(source);
+			create_computation_1(source);
+			create_computation_1(source);
+		}
+	},
 	COUNT,
 	COUNT / 8
 );
 
 export const sbench_create_1to1000 = create_sbench_test(
 	'sbench_create_1to1000',
-	create_computations_1to1000,
+	function create_computations_1to1000(n, sources) {
+		for (let i = 0; i < n / 1000; i++) {
+			const source = sources[i];
+			for (let j = 0; j < 1000; j++) {
+				create_computation_1(source);
+			}
+		}
+	},
 	COUNT,
 	COUNT / 1000
 );
