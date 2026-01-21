cca6491c872bd30388785e18dbf52b0758ed43cc
Rich Harris <rich.harris@vercel.com>
2026-01-21 15:43:39 -0500

    better logging for bench:compare

--- benchmarking/compare/runner.js
+++ benchmarking/compare/runner.js
@@ -5,12 +5,9 @@ const results = [];
 for (let i = 0; i < reactivity_benchmarks.length; i += 1) {
 	const benchmark = reactivity_benchmarks[i];
 
-	process.stderr.write(`${i + 1}/${reactivity_benchmarks.length} (${benchmark.label})`);
-
-	console.error(benchmark.label);
+	process.stderr.write(`Running ${i + 1}/${reactivity_benchmarks.length} ${benchmark.label} `);
 	results.push(await benchmark.fn());
-
-	process.stderr.write('\x1b[2K');
+	process.stderr.write('\x1b[2K\r');
 }
 
 process.send(results);
