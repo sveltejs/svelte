6ee67d9081be0b50e5192d9d13ff671ac80ca5f0
Rich Harris <rich.harris@vercel.com>
2026-01-21 15:40:53 -0500

    try this

--- benchmarking/compare/runner.js
+++ benchmarking/compare/runner.js
@@ -1,9 +1,16 @@
 import { reactivity_benchmarks } from '../benchmarks/reactivity/index.js';
 
 const results = [];
-for (const benchmark of reactivity_benchmarks) {
+
+for (let i = 0; i < reactivity_benchmarks.length; i += 1) {
+	const benchmark = reactivity_benchmarks[i];
+
+	process.stderr.write(`${i + 1}/${reactivity_benchmarks.length} (${benchmark.label})`);
+
 	console.error(benchmark.label);
 	results.push(await benchmark.fn());
+
+	process.stderr.write('\x1b[2K');
 }
 
 process.send(results);
