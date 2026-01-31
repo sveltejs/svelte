60fe9ba3925d72bc4de48dc78e24abb39a6c7493
7nik <kifiranet@gmail.com>
2026-01-31 21:11:34 +0200

    more tests

---
+++ packages/svelte/tests/runtime-xhtml/samples/autoclosed-tags/main.svelte
@@ -0,0 +1,6 @@
+<ul><li><li></ul>
+<dl><dt><dd><dd></dl>
+<p><p><div></div>
+<ruby><rp><rt><rt></ruby>
+<select><option><optgroup><optgroup></select>
+<table><thead><tbody><tfoot><tbody><tr><td><th></tr><tr></table>
