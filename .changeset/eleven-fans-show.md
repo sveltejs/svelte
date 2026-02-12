0b7a3c65c87e821caaf98596b28088e484992da3
7nik <kifiranet@gmail.com>
2026-02-12 20:20:57 +0200

    fix: take into account `nodeName` case sensitivity on XHTML pages

---
+++ .changeset/eleven-fans-show.md
@@ -0,0 +1,5 @@
+---
+'svelte': patch
+---
+
+fix: render `mutltiple` and `selected` attributes as empty strings for XHTML compliance
--- packages/svelte/tests/runtime-xhtml/samples/select-multiple-spread/_config.js
+++ packages/svelte/tests/runtime-xhtml/samples/select-multiple-spread/_config.js
@@ -1,3 +1,4 @@
+import { flushSync } from 'svelte';
 import { test } from '../../test';
 
 export default test({
@@ -7,7 +8,10 @@ export default test({
 		assert.equal(options[0].selected, true);
 		assert.equal(options[1].selected, false);
 
+		// Shouldn't change the value because the value is not bound.
 		component.attrs = { value: ['2'] };
+		flushSync();
+
 		assert.equal(options[0].selected, false);
 		assert.equal(options[1].selected, true);
 	}
