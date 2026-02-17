b9445edd01cc7145c46914d21e68d7220c0d8fef
Rich Harris <rich.harris@vercel.com>
2026-02-17 15:55:06 -0500

    Apply suggestions from code review
    Co-authored-by: 7nik <kifiranet@gmail.com>

--- packages/svelte/tests/runtime-runes/samples/each-key-volatile/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/each-key-volatile/main.svelte
@@ -4,15 +4,8 @@
 		{ group: 'b', id: 2 }
 	]);
 
-	function update() {
-		things = [
-			{ group: 'c', id: 3 },
-			{ group: 'd', id: 4 }
-		];
-	}
 </script>
 
-<button onclick={update}>update</button>
 
 {#each things as thing ([thing.group, thing.id])}
 	<p>{thing.group}-{thing.id}</p>
