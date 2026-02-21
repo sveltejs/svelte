6aa7b9c64a14a1e5f97510d7801c9d6173cb48e3
Paolo Ricciuti <ricciutipaolo@gmail.com>
2026-02-21 20:33:48 +0100

    fix: update expressions on server deriveds (#17767)
    Closes #17760

--- packages/svelte/src/compiler/phases/3-transform/server/visitors/UpdateExpression.js
+++ packages/svelte/src/compiler/phases/3-transform/server/visitors/UpdateExpression.js
@@ -9,17 +9,26 @@ import * as b from '#compiler/builders';
 export function UpdateExpression(node, context) {
 	const argument = node.argument;
 
-	if (
-		argument.type === 'Identifier' &&
-		context.state.scope.get(argument.name)?.kind === 'store_sub'
-	) {
-		return b.call(
-			node.prefix ? '$.update_store_pre' : '$.update_store',
-			b.assignment('??=', b.id('$$store_subs'), b.object([])),
-			b.literal(argument.name),
-			b.id(argument.name.slice(1)),
-			node.operator === '--' && b.literal(-1)
-		);
+	if (argument.type === 'Identifier') {
+		const binding = context.state.scope.get(argument.name);
+
+		if (binding?.kind === 'store_sub') {
+			return b.call(
+				node.prefix ? '$.update_store_pre' : '$.update_store',
+				b.assignment('??=', b.id('$$store_subs'), b.object([])),
+				b.literal(argument.name),
+				b.id(argument.name.slice(1)),
+				node.operator === '--' && b.literal(-1)
+			);
+		}
+
+		if (binding?.kind === 'derived') {
+			return b.call(
+				node.prefix ? '$.update_derived_pre' : '$.update_derived',
+				binding.node,
+				node.operator === '--' && b.literal(-1)
+			);
+		}
 	}
 
 	return context.next();
