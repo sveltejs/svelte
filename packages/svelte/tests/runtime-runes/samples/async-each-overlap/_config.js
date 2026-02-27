2cb3759e47d9223e588fa33f01364ef60e910ed6
Rich Harris <rich.harris@vercel.com>
2026-02-26 21:05:01 -0500

    fix test

--- packages/svelte/tests/runtime-runes/samples/async-each-overlap/_config.js
+++ packages/svelte/tests/runtime-runes/samples/async-each-overlap/_config.js
@@ -3,6 +3,8 @@ import { test } from '../../test';
 
 export default test({
 	async test({ assert, target }) {
+		await tick();
+
 		const [add, shift] = target.querySelectorAll('button');
 
 		add.click();
@@ -12,21 +14,19 @@ export default test({
 		add.click();
 		await tick();
 
-		// TODO pending count / number of pushes is off
-
 		assert.htmlEqual(
 			target.innerHTML,
 			`
-			<button>add</button>
-			<button>shift</button>
-			<p>pending=6 values.length=1 values=[1]</p>
-			<div>not keyed:
-				<div>1</div>
-			</div>
-			<div>keyed:
-				<div>1</div>
-			</div>
-		`
+				<button>add</button>
+				<button>shift</button>
+				<p>pending=6 values.length=1 values=[1]</p>
+				<div>not keyed:
+					<div>1</div>
+				</div>
+				<div>keyed:
+					<div>1</div>
+				</div>
+			`
 		);
 
 		shift.click();
@@ -36,18 +36,18 @@ export default test({
 		assert.htmlEqual(
 			target.innerHTML,
 			`
-			<button>add</button>
-			<button>shift</button>
-			<p>pending=4 values.length=2 values=[1,2]</p>
-			<div>not keyed:
-				<div>1</div>
-				<div>2</div>
-			</div>
-			<div>keyed:
-				<div>1</div>
-				<div>2</div>
-			</div>
-		`
+				<button>add</button>
+				<button>shift</button>
+				<p>pending=4 values.length=2 values=[1,2]</p>
+				<div>not keyed:
+					<div>1</div>
+					<div>2</div>
+				</div>
+				<div>keyed:
+					<div>1</div>
+					<div>2</div>
+				</div>
+			`
 		);
 
 		shift.click();
@@ -57,20 +57,20 @@ export default test({
 		assert.htmlEqual(
 			target.innerHTML,
 			`
-			<button>add</button>
-			<button>shift</button>
-			<p>pending=2 values.length=3 values=[1,2,3]</p>
-			<div>not keyed:
-				<div>1</div>
-				<div>2</div>
-				<div>3</div>
-			</div>
-			<div>keyed:
-				<div>1</div>
-				<div>2</div>
-				<div>3</div>
-			</div>
-		`
+				<button>add</button>
+				<button>shift</button>
+				<p>pending=2 values.length=3 values=[1,2,3]</p>
+				<div>not keyed:
+					<div>1</div>
+					<div>2</div>
+					<div>3</div>
+				</div>
+				<div>keyed:
+					<div>1</div>
+					<div>2</div>
+					<div>3</div>
+				</div>
+			`
 		);
 
 		shift.click();
@@ -80,22 +80,22 @@ export default test({
 		assert.htmlEqual(
 			target.innerHTML,
 			`
-			<button>add</button>
-			<button>shift</button>
-			<p>pending=0 values.length=4 values=[1,2,3,4]</p>
-			<div>not keyed:
-				<div>1</div>
-				<div>2</div>
-				<div>3</div>
-				<div>4</div>
-			</div>
-			<div>keyed:
-				<div>1</div>
-				<div>2</div>
-				<div>3</div>
-				<div>4</div>
-			</div>
-		`
+				<button>add</button>
+				<button>shift</button>
+				<p>pending=0 values.length=4 values=[1,2,3,4]</p>
+				<div>not keyed:
+					<div>1</div>
+					<div>2</div>
+					<div>3</div>
+					<div>4</div>
+				</div>
+				<div>keyed:
+					<div>1</div>
+					<div>2</div>
+					<div>3</div>
+					<div>4</div>
+				</div>
+			`
 		);
 	}
 });
