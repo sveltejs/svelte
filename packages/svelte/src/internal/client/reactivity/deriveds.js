ea3b7230c52b6a8d30c02d60e9571188b5e1afc3
Rich Harris <rich.harris@vercel.com>
2026-03-05 08:18:03 -0500

    chore: deactivate batch after async derived resolves

--- packages/svelte/src/internal/client/reactivity/deriveds.js
+++ packages/svelte/src/internal/client/reactivity/deriveds.js
@@ -197,6 +197,8 @@ export function async_derived(fn, label, location) {
 			if (decrement_pending) {
 				decrement_pending();
 			}
+
+			batch.deactivate();
 		};
 
 		d.promise.then(handler, (e) => handler(null, e || 'unknown'));
