7f7c29865fcb684e51ed8edd32b68560058b803c
Simon Holthausen <simon.holthausen@vercel.com>
2026-03-10 16:02:25 +0100

    fix

--- packages/svelte/src/internal/client/reactivity/batch.js
+++ packages/svelte/src/internal/client/reactivity/batch.js
@@ -269,12 +269,7 @@ export class Batch {
 			this.#deferred?.resolve();
 		}
 
-		var next_batch =
-			/** @type {Batch | null} */ (/** @type {unknown} */ (current_batch)) ??
-			// Edge case: During traversal new branches might create effects that run immediately and set state,
-			// causing an effect and therefore a root to be scheduled again. We need to traverse the current batch
-			// once more in that case - most of the time this will just clean up dirty branches.
-			(this.#roots.length > 0 ? this : null);
+		var next_batch = /** @type {Batch | null} */ (/** @type {unknown} */ (current_batch));
 
 		if (next_batch !== null) {
 			batches.add(next_batch);
@@ -286,6 +281,21 @@ export class Batch {
 			}
 
 			next_batch.#process();
+		}
+
+		// Edge case: During traversal new branches might create effects that run immediately and set state,
+		// causing an effect and therefore a root to be scheduled again. We need to traverse the current batch
+		// once more in that case - most of the time this will just clean up dirty branches.
+		if (this.#roots.length > 0) {
+			batches.add(this);
+
+			if (DEV) {
+				for (const source of this.current.keys()) {
+					/** @type {Set<Source>} */ (source_stacks).add(source);
+				}
+			}
+
+			this.#process();
 		}
 	}
 
