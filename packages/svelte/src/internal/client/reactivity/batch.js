6af7f9068c7879df8c8f3a312453c0162999f66b
Rich Harris <rich.harris@vercel.com>
2026-03-10 22:00:49 -0400

    tweak

--- packages/svelte/src/internal/client/reactivity/batch.js
+++ packages/svelte/src/internal/client/reactivity/batch.js
@@ -503,6 +503,7 @@ export class Batch {
 				batch.#blockers.delete(this);
 
 				if (batch.#blockers.size === 0 && !batch.#is_deferred()) {
+					batch.activate();
 					batch.#revive();
 					batch.#process();
 				}
@@ -527,9 +528,7 @@ export class Batch {
 		this.#pending -= 1;
 		if (blocking) this.#blocking_pending -= 1;
 
-		if (this.#decrement_queued || skip) {
-			return;
-		}
+		if (this.#decrement_queued || skip) return;
 
 		this.#decrement_queued = true;
 
