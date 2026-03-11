a5b7ac869e6a1444e19bdee1192015ac19787160
Rich Harris <rich.harris@vercel.com>
2026-03-10 21:43:55 -0400

    WIP test

---
+++ packages/svelte/tests/runtime-runes/samples/async-overlapping-svelte-set/_config.js
@@ -0,0 +1,9 @@
+import { test } from '../../test';
+
+export default test({
+	async test({ assert, target }) {
+		const [add, shift, pop] = target.querySelectorAll('button');
+
+		// TODO
+	}
+});
---
+++ packages/svelte/tests/runtime-runes/samples/async-overlapping-svelte-set/main.svelte
@@ -0,0 +1,43 @@
+<script>
+	import { SvelteSet } from 'svelte/reactivity';
+
+	let values = new SvelteSet([1]);
+
+	const queue = [];
+
+	function push(v) {
+		if (v === 1) return v;
+
+		const p = Promise.withResolvers();
+		queue.push(() => p.resolve(v));
+		return p.promise;
+	}
+
+	function addValue() {
+		values.add(values.size + 1);
+	}
+</script>
+
+<button onclick={addValue}>add</button>
+<button onclick={() => queue.shift()?.()}>shift</button>
+<button onclick={() => queue.pop()?.()}>pop</button>
+
+<p>
+	pending={$effect.pending()}
+	values.length={values.size}
+	values=[{Array.from(values)}]
+</p>
+
+<hr>
+
+<p>1: {values.has(1)}</p>
+<p>2: {values.has(2)}</p>
+<p>3: {values.has(3)}</p>
+<p>4: {values.has(4)}</p>
+<p>5: {values.has(5)}</p>
+
+<hr>
+
+{#each values as v(v)}
+	<p>{await push(v)}</p>
+{/each}
