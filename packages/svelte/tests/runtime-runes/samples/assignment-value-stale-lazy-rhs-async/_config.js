5455eaf33e8dae150d1f05cd69e704590b0ccf76
Simon Holthausen <simon.holthausen@vercel.com>
2026-03-13 17:59:33 +0100

    fix test

--- packages/svelte/tests/runtime-runes/samples/assignment-value-stale-lazy-rhs-async/_config.js
+++ packages/svelte/tests/runtime-runes/samples/assignment-value-stale-lazy-rhs-async/_config.js
@@ -5,14 +5,19 @@ export default test({
 	compileOptions: {
 		dev: true
 	},
-	skip_no_async: true,
 	async test({ assert, target }) {
 		const button = /** @type {HTMLElement} */ (target.querySelector('button'));
 		await tick();
 		assert.htmlEqual(target.innerHTML, `<button>go</button><p>count1: 0, count2: 0</p>`);
+
 		button.click();
 		await tick();
 		assert.htmlEqual(target.innerHTML, `<button>go</button><p>count1: 1, count2: 1</p>`);
+
+		// additional tick necessary in legacy mode because it's using Promise.resolve() which finishes before the await in the component,
+		// causing the cache to not be set yet, which would result in count2 becoming 2
+		await tick();
+
 		button.click();
 		await tick();
 		assert.htmlEqual(target.innerHTML, `<button>go</button><p>count1: 2, count2: 1</p>`);
