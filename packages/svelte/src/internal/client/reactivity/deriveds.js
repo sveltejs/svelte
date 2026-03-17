5faf102782b6efc60df4290ad2858650594f1ff2
Rich Harris <rich.harris@vercel.com>
2026-03-17 16:06:37 -0400

    fix: reinstate reactivity loss tracking (#17801)
    We commented out this code in #17038 because it was broken. I suspect it
    was broken because we weren't correctly calling `unset_context` inside
    `run`, leading to false positives — this is now fixed, and as such I
    _think_ we can safely reinstate it.
    
    One small change — I got rid of the `was_read` check. I assume this
    existed to prevent duplicate warnings, but it actually causes false
    negatives in the case where you read a signal while the reaction is
    being tracked then again while it's untracked.
    
    ### Before submitting the PR, please make sure you do the following
    
    - [x] It's really useful if your PR references an issue where it is
    discussed ahead of time. In many cases, features are absent for a
    reason. For large changes, please create an RFC:
    https://github.com/sveltejs/rfcs
    - [x] Prefix your PR title with `feat:`, `fix:`, `chore:`, or `docs:`.
    - [x] This message body should clearly illustrate what problems it
    solves.
    - [ ] Ideally, include a test that fails without this PR but passes with
    it.
    - [x] If this PR changes code within `packages/svelte/src`, add a
    changeset (`npx changeset`).
    
    ### Tests and linting
    
    - [x] Run the tests with `pnpm test` and lint the project with `pnpm
    lint`
    
    ---------
    
    Co-authored-by: Tee Ming <chewteeming01@gmail.com>

--- packages/svelte/src/internal/client/reactivity/deriveds.js
+++ packages/svelte/src/internal/client/reactivity/deriveds.js
@@ -45,12 +45,16 @@ import { increment_pending, unset_context } from './async.js';
 import { deferred, includes, noop } from '../../shared/utils.js';
 import { set_signal_status, update_derived_status } from './status.js';
 
-/** @type {Effect | null} */
-export let current_async_effect = null;
+/**
+ * This allows us to track 'reactivity loss' that occurs when signals
+ * are read after a non-context-restoring `await`. Dev-only
+ * @type {{ effect: Effect, warned: boolean } | null}
+ */
+export let reactivity_loss_tracker = null;
 
-/** @param {Effect | null} v */
-export function set_from_async_derived(v) {
-	current_async_effect = v;
+/** @param {{ effect: Effect, warned: boolean } | null} v */
+export function set_reactivity_loss_tracker(v) {
+	reactivity_loss_tracker = v;
 }
 
 export const recent_async_deriveds = new Set();
@@ -124,7 +128,12 @@ export function async_derived(fn, label, location) {
 	var deferreds = new Map();
 
 	async_effect(() => {
-		if (DEV) current_async_effect = active_effect;
+		if (DEV) {
+			reactivity_loss_tracker = {
+				effect: /** @type {Effect} */ (active_effect),
+				warned: false
+			};
+		}
 
 		var effect = /** @type {Effect} */ (active_effect);
 
@@ -142,7 +151,9 @@ export function async_derived(fn, label, location) {
 			unset_context();
 		}
 
-		if (DEV) current_async_effect = null;
+		if (DEV) {
+			reactivity_loss_tracker = null;
+		}
 
 		var batch = /** @type {Batch} */ (current_batch);
 
@@ -174,7 +185,9 @@ export function async_derived(fn, label, location) {
 		 * @param {unknown} error
 		 */
 		const handler = (value, error = undefined) => {
-			if (DEV) current_async_effect = null;
+			if (DEV) {
+				reactivity_loss_tracker = null;
+			}
 
 			if (decrement_pending) {
 				// don't trigger an update if we're only here because
