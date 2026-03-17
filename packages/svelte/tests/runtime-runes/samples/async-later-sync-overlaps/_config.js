b9f62882f477ad5dd1bb6e2557b101e015c8354e
Simon H <5968653+dummdidumm@users.noreply.github.com>
2026-03-17 23:26:55 +0100

    oops

--- packages/svelte/tests/runtime-runes/samples/async-later-sync-overlaps/_config.js
+++ packages/svelte/tests/runtime-runes/samples/async-later-sync-overlaps/_config.js
@@ -10,21 +10,21 @@ export default test({
 		await tick();
 		assert.htmlEqual(
 			target.innerHTML,
-			'<button>a_b 0_0</button> <button>b 0</button> 0'
+			'<button>a_b 0_0</button> <button>b 0</button> <button>resolve</button> 0'
 		);
 
 		b.click();
 		await tick();
 		assert.htmlEqual(
 			target.innerHTML,
-			'<button>a_b 0_0</button> <button>b 0</button> 0'
+			'<button>a_b 0_0</button> <button>b 0</button> <button>resolve</button> 0'
 		);
 
 		resolve.click();
 		await tick();
 		assert.htmlEqual(
 			target.innerHTML,
-			'<button>a_b 1_2</button> <button>b 2</button> 1'
+			'<button>a_b 1_2</button> <button>b 2</button> <button>resolve</button> 1'
 		);
 	}
 });
