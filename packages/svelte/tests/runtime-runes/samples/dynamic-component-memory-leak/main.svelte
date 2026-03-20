509868bc41831aa33a6a472e87c7e9aff8709ca2
Nitin Sahu <ns0779687@gmail.com>
2026-03-21 04:56:41 +0530

    fix: null out effect.b in destroy_effect to prevent memory leak

---
+++ packages/svelte/tests/runtime-runes/samples/dynamic-component-memory-leak/main.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Home from './Home.svelte';
+	import About from './About.svelte';
+	let { page = 'Home' } = $props();
+
+	const components = { Home, About };
+	let Component = $derived(components[page]);
+</script>
+
+<Component />
