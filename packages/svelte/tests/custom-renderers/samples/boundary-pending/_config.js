0f447331839665b552d106fa83249992fd721880
paoloricciuti <ricciutipaolo@gmail.com>
2026-03-30 12:26:19 +0200

    chore: boundary test

---
+++ packages/svelte/tests/custom-renderers/samples/boundary-pending/_config.js
@@ -0,0 +1,33 @@
+import { flushSync } from 'svelte';
+import { test } from '../../test';
+
+/** @type {(value: string) => void} */
+let resolve;
+
+const promise = new Promise((r) => {
+	resolve = r;
+});
+
+export default test({
+	props: { promise },
+	compileOptions: {
+		experimental: {
+			async: true
+		}
+	},
+	html: '<p>loading...</p> <p>Something outside</p>',
+	async test({ assert, target, serialize }) {
+		resolve('hello');
+
+		// wait enough microtasks for the async chain to fully resolve:
+		// promise.then(() => Promise.resolve()).finally(() => decrement_pending())
+		await new Promise((r) => setTimeout(r, 0));
+		flushSync();
+
+		const html = serialize(target);
+		assert.equal(
+			html,
+			'<root><p>hello</p> <p>Something inside</p> <p>Something outside</p></root>'
+		);
+	}
+});
