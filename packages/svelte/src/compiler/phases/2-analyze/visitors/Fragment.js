92422f24e7a0927e90a6cd2e1c54108348d306b5
Simon Holthausen <simon.holthausen@vercel.com>
2026-03-31 12:53:26 +0200

    leftover

--- packages/svelte/src/compiler/phases/2-analyze/visitors/Fragment.js
+++ packages/svelte/src/compiler/phases/2-analyze/visitors/Fragment.js
@@ -6,9 +6,5 @@
  * @param {Context} context
  */
 export function Fragment(node, context) {
-	// node.metadata.consts.sync = [];
-	// node.metadata.consts.async = [];
-	// node.metadata.consts.promise_id = undefined;
-
 	context.next({ ...context.state, fragment: node });
 }
