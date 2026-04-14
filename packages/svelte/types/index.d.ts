550e0a02a5fa6cba7c5f41ca32cf31cd15adfe71
Jungzl <13jungzl@gmail.com>
2026-04-14 14:54:53 +0800

    chore: tweak

--- packages/svelte/types/index.d.ts
+++ packages/svelte/types/index.d.ts
@@ -2203,10 +2203,10 @@ declare module 'svelte/motion' {
 		 * 	const tween = Tween.of(() => number);
 		 * </script>
 		 * ```
-		 *
+		 * 
 		 */
 		static of<U>(fn: () => U, options?: TweenOptions<U> | undefined): Tween<U>;
-
+		
 		constructor(value: T, options?: TweenOptions<T>);
 		/**
 		 * Sets `tween.target` to `value` and returns a `Promise` that resolves if and when `tween.current` catches up to it.
@@ -2257,7 +2257,7 @@ declare module 'svelte/reactivity' {
 	 * ```
 	 */
 	export class SvelteDate extends Date {
-
+		
 		constructor(...params: any[]);
 		#private;
 	}
@@ -2293,12 +2293,12 @@ declare module 'svelte/reactivity' {
 	 * {#if monkeys.has('🙊')}<p>speak no evil</p>{/if}
 	 * ```
 	 *
-	 *
+	 * 
 	 */
 	export class SvelteSet<T> extends Set<T> {
-
+		
 		constructor(value?: Iterable<T> | null | undefined);
-
+		
 		add(value: T): this;
 		#private;
 	}
@@ -2344,12 +2344,12 @@ declare module 'svelte/reactivity' {
 	 * {/if}
 	 * ```
 	 *
-	 *
+	 * 
 	 */
 	export class SvelteMap<K, V> extends Map<K, V> {
-
+		
 		constructor(value?: Iterable<readonly [K, V]> | null | undefined);
-
+		
 		set(key: K, value: V): this;
 		#private;
 	}
@@ -2412,7 +2412,7 @@ declare module 'svelte/reactivity' {
 	 * ```
 	 */
 	export class SvelteURLSearchParams extends URLSearchParams {
-
+		
 		[REPLACE](params: URLSearchParams): void;
 		#private;
 	}
@@ -2488,7 +2488,7 @@ declare module 'svelte/reactivity' {
 	 */
 	export function createSubscriber(start: (update: () => void) => (() => void) | void): () => void;
 	class ReactiveValue<T> {
-
+		
 		constructor(fn: () => T, onsubscribe: (update: () => void) => void);
 		get current(): T;
 		#private;
@@ -2553,7 +2553,7 @@ declare module 'svelte/reactivity/window' {
 		get current(): number | undefined;
 	};
 	class ReactiveValue<T> {
-
+		
 		constructor(fn: () => T, onsubscribe: (update: () => void) => void);
 		get current(): T;
 		#private;
