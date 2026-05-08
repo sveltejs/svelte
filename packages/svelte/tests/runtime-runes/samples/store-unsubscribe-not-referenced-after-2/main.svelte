e527e1e8acbfab3718825b5146f89420ad35ae31
Rich Harris <rich.harris@vercel.com>
2026-05-08 10:33:16 -0400

    test

---
+++ packages/svelte/tests/runtime-runes/samples/store-unsubscribe-not-referenced-after-2/main.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { writable, derived } from "svelte/store";
+
+	const obj = writable({ a: 1 });
+	let count = $state(0);
+	let watcherA = $state();
+
+	function watch (prop) {
+		return derived(obj, (o) => {
+			count++;
+			return o[prop];
+		});
+	}
+</script>
+
+<input type="number" bind:value={$obj.a}>
+<p>{count}</p>
+
+{#if watcherA}
+	<!-- make sure the presence of async work doesn't break the `legacy_updates` mechanism -->
+	{await 'hello'}
+
+	{$watcherA}
+	<button on:click={() => watcherA = null}>remove watcher</button>
+{:else}
+	<button on:click={() => watcherA = watch("a")}>add watcher</button>
+{/if}
