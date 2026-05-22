fe9ab936b6aa646ef87c636ec8803d42f8b7bb1d
Simon H <5968653+dummdidumm@users.noreply.github.com>
2026-05-22 17:48:31 +0200

    fix: allow `$derived(await ...)` in disconnected effect roots (#18273)
    If `$effect.root` is created outside the component tree, the boundary
    property is null
    
    Fixes #18188

---
+++ packages/svelte/tests/runtime-runes/samples/async-async-disconnected-effect-root/main.svelte
@@ -0,0 +1,11 @@
+<script>
+	setTimeout(() => {
+		$effect.root(() => {
+			async function fn() {
+				const value = $derived(await 1);
+				return { get value() { return value } };
+			}
+			fn().then(r => console.log(r.value));
+		})
+	})
+</script>
