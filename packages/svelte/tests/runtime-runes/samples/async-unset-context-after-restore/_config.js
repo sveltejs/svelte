6e23ab0dc57c8bf7e21260ca5dafbeb65fe56f29
Simon H <5968653+dummdidumm@users.noreply.github.com>
2026-05-26 21:43:32 +0200

    Apply suggestion from @dummdidumm

--- packages/svelte/tests/runtime-runes/samples/async-unset-context-after-restore/_config.js
+++ packages/svelte/tests/runtime-runes/samples/async-unset-context-after-restore/_config.js
@@ -2,7 +2,6 @@ import { tick } from 'svelte';
 import { test } from '../../test';
 
 export default test({
-	// Test that an async derived inside an $effect.root not connected to the component tree still works
 	async test({ assert, logs }) {
 		await new Promise((resolve) => setTimeout(resolve, 10));
 		assert.deepEqual(logs, [1, 1]);
