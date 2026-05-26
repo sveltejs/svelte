b40c359d44196b7b87745259243333763ed61ec5
Simon H <5968653+dummdidumm@users.noreply.github.com>
2026-05-26 23:37:17 +0200

    fix: don't assume boundary exists during increment/decrement (#18289)
    Follow-up to #18273 (not merged yet hence no changeset here): We can run
    into a null-pointer when wanting to increment/decrement inside an effect
    root that is outside a the component tree. Similarly, if not using the
    component logic to unset context at the right time we gotta do it
    "manually" inside `save`.
    
    ---------
    
    Co-authored-by: Rich Harris <rich.harris@vercel.com>
    Co-authored-by: vercel[bot] <35613825+vercel[bot]@users.noreply.github.com>

--- packages/svelte/tests/runtime-runes/samples/async-disconnected-effect-root/_config.js
+++ packages/svelte/tests/runtime-runes/samples/async-disconnected-effect-root/_config.js
@@ -1,9 +1,15 @@
+import { tick } from 'svelte';
 import { test } from '../../test';
 
 export default test({
 	// Test that an async derived inside an $effect.root not connected to the component tree still works
 	async test({ assert, logs }) {
 		await new Promise((resolve) => setTimeout(resolve, 10));
-		assert.deepEqual(logs, [1]);
+		assert.deepEqual(logs, [1, 1]);
+		const [button] = document.querySelectorAll('button');
+
+		button.click();
+		await tick();
+		assert.deepEqual(logs, [1, 1, 2]);
 	}
 });
