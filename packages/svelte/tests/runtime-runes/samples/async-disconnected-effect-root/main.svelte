5152bab2ec479848dbe0bd5625ef1b16c08a1b59
Rich Harris <rich.harris@vercel.com>
2026-05-26 22:38:32 -0400

    fix

--- packages/svelte/tests/runtime-runes/samples/async-disconnected-effect-root/main.svelte
+++ packages/svelte/tests/runtime-runes/samples/async-disconnected-effect-root/main.svelte
@@ -1,11 +1,18 @@
 <script>
+	let increment;
+
 	setTimeout(() => {
 		$effect.root(() => {
 			async function fn() {
-				const value = $derived(await 1);
+				let count = $state(1);
+				increment = () => { count++; };
+				$effect.pre(() => console.log(count))
+				const value = $derived(await count);
 				return { get value() { return value } };
 			}
 			fn().then(r => console.log(r.value));
 		})
 	})
 </script>
+
+<button onclick={() => increment()}>increment</button>
