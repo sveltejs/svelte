6d26dce265de128d65a99e1735e576651d9c1a75
Rich Harris <rich.harris@vercel.com>
2026-05-29 17:05:12 -0400

    allow class/function expressions in tags (#18324)
    We shouldn't error on encountering `class` and `function`, because they
    are treated as expressions rather than declarations

--- packages/svelte/src/compiler/phases/1-parse/state/tag.js
+++ packages/svelte/src/compiler/phases/1-parse/state/tag.js
@@ -14,8 +14,7 @@ const regex_whitespace_with_closing_curly_brace = /\s*}/y;
 const regex_supported_declaration = /(?:let|const)\b/y;
 // All except `type` are reserved keywords and cannot be used as variable names.
 // For type we check if it's not something like `type .x` / `type ()` / `type % 2` / ...
-const regex_unsupported_declaration =
-	/(?:(?:var|function|class|interface|enum)\b)|(?:type\s+[^?.(`<[&|%^}])/y;
+const regex_unsupported_declaration = /(?:(?:var|interface|enum)\b)|(?:type\s+[^?.(`<[&|%^}])/y;
 
 const pointy_bois = { '<': '>' };
 
