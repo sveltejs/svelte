224092b37b9ed79afe84cc5b3b03dabb75be946d
paoloricciuti <ricciutipaolo@gmail.com>
2026-06-08 11:11:26 +0200

    fix: types in renderers tests

--- packages/svelte/tests/custom-renderers/samples/event-handler-no-propagation/_config.js
+++ packages/svelte/tests/custom-renderers/samples/event-handler-no-propagation/_config.js
@@ -2,19 +2,17 @@ import { flushSync } from 'svelte';
 import { test } from '../../test';
 
 export default test({
-	test({ assert, target, serialize, logs }) {
-		const button = target.children.find(
-			(/** @type {any} */ n) => n.type === 'element' && n.name === 'button'
-		);
+	test({ assert, target, utils, logs }) {
+		const button = target.children.find(utils.filter_elements((n) => n.name === 'button'));
 		assert.ok(button);
 
-		const listeners = button.listeners?.click;
+		const listeners = button?.listeners?.click;
 		assert.ok(listeners, 'button should have click listeners');
 
 		// Call the handler with multiple arguments.
 		// Custom renderers may pass multiple arguments to event handlers,
 		// so we need to make sure all arguments are forwarded.
-		for (const { handler } of listeners) {
+		for (const { handler } of listeners ?? []) {
 			handler.call(button, { type: 'click' }, 'extra', 42);
 		}
 		flushSync();
