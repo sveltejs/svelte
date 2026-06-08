fc145299cba0d8917e6e78d408459a1dec031167
paoloricciuti <ricciutipaolo@gmail.com>
2026-06-08 08:55:10 +0200

    chore: add tests

---
+++ packages/svelte/tests/custom-renderers/samples-dom/hydration-interleaved-dom-custom/main.svelte
@@ -0,0 +1,23 @@
+<svelte:options customRenderer={null} />
+
+<script>
+	import Child from './Child.svelte';
+
+	let visible = $state(true);
+
+	export function hide() {
+		visible = false;
+	}
+
+	export function show() {
+		visible = true;
+	}
+</script>
+
+<div>
+	{#if visible}
+		Cool: <Child />
+	{/if}
+</div>
+
+<div id="keep-me"></div>
