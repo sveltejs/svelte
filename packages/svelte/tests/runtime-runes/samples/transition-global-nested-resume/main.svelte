128be7625ebaef39bc95afd48249abcceced1de5
Nic <162764842+Nic-Polumeyv@users.noreply.github.com>
2026-06-12 21:03:42 -0400

    fix: don't resurrect outroing elements when an ancestor block is paused and resumed

---
+++ packages/svelte/tests/runtime-runes/samples/transition-global-nested-resume/main.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { fade } from 'svelte/transition';
+
+	let fetching = $state(false);
+	let shown = $state(true);
+</script>
+
+<button onclick={() => (shown = false)}>hide</button>
+<button onclick={() => (fetching = !fetching)}>fetch</button>
+
+{#if fetching}
+	<p>loading</p>
+{:else}
+	<div transition:fade={{ duration: 100 }}>
+		{#if shown}
+			<div class="red" transition:fade|global={{ duration: 100 }}>red</div>
+		{/if}
+	</div>
+{/if}
