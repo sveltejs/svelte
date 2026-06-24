275c1827d5651fb1334933755c0b3524635e69f6
paoloricciuti <ricciutipaolo@gmail.com>
2026-06-24 08:52:44 +0200

    fix: update snapshots

--- packages/svelte/tests/snapshot/samples/custom-renderer-single-node/_expected/client/main.svelte.js
+++ packages/svelte/tests/snapshot/samples/custom-renderer-single-node/_expected/client/main.svelte.js
@@ -1,4 +1,5 @@
 import $renderer from 'my-custom-renderer';
+import 'svelte/internal/flags/custom-renderer';
 import 'svelte/internal/disclose-version';
 import 'svelte/internal/flags/legacy';
 import * as $ from 'svelte/internal/client';
