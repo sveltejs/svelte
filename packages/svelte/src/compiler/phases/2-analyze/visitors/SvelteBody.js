0ba0e7683d1d8bb38ae3f1745577da70b90cc7bf
Alexander Kireev <ak.chatman.media@gmail.com>
2026-06-28 21:15:58 +0700

    fix: warn on undeclared shorthand event handlers on svelte:window/document/body

--- packages/svelte/src/compiler/phases/2-analyze/visitors/SvelteBody.js
+++ packages/svelte/src/compiler/phases/2-analyze/visitors/SvelteBody.js
@@ -2,7 +2,7 @@
 /** @import { Context } from '../types' */
 import * as e from '../../../errors.js';
 import { is_event_attribute } from '../../../utils/ast.js';
-import { disallow_children } from './shared/special-element.js';
+import { check_global_event_reference, disallow_children } from './shared/special-element.js';
 
 /**
  * @param {AST.SvelteBody} node
@@ -11,10 +11,9 @@ import { disallow_children } from './shared/special-element.js';
 export function SvelteBody(node, context) {
 	disallow_children(node);
 	for (const attribute of node.attributes) {
-		if (
-			attribute.type === 'SpreadAttribute' ||
-			(attribute.type === 'Attribute' && !is_event_attribute(attribute))
-		) {
+		if (attribute.type === 'Attribute' && is_event_attribute(attribute)) {
+			check_global_event_reference(attribute, context);
+		} else if (attribute.type === 'SpreadAttribute' || attribute.type === 'Attribute') {
 			e.svelte_body_illegal_attribute(attribute);
 		}
 	}
