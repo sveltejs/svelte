6c807a50cc8ba9ef4912491e06ba2d115680f9d5
Wey <wzy398848627@gmail.com>
2026-07-12 13:40:59 +1200

    fix: force the first run innerText/innerHTML/textContent written for content_editable_bind

---
+++ packages/svelte/tests/runtime-runes/samples/svelte-element-contenteditable/_config.js
@@ -0,0 +1,19 @@
+import { flushSync } from '../../../../src/index-client';
+import { test } from '../../test';
+
+export default test({
+	test({ assert, target }) {
+		const h1 = /** @type {HTMLDivElement} */ (target.querySelector('h1'));
+		assert.equal(h1.textContent, '');
+		// don't use h1.textContent because h1.textContent will overwrite all childNodes
+		// The childNode created by {myTextBlock.current} is the reason for duplication
+		const textNode =
+			[...h1.childNodes].findLast((n) => n.nodeType === Node.TEXT_NODE) ??
+			h1.appendChild(document.createTextNode(''));
+		textNode.nodeValue = 'h';
+		h1.dispatchEvent(new window.Event('input'));
+		flushSync();
+
+		assert.equal(h1.textContent, 'h');
+	}
+});
