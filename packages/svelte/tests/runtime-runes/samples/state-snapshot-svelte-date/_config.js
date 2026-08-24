fa9b4d12a1a212cb9f81025aad2116744c478bad
Simon H <5968653+dummdidumm@users.noreply.github.com>
2026-08-24 14:03:23 +0200

    format /fix

--- packages/svelte/tests/runtime-runes/samples/state-snapshot-svelte-date/_config.js
+++ packages/svelte/tests/runtime-runes/samples/state-snapshot-svelte-date/_config.js
@@ -3,17 +3,11 @@ import { tick } from 'svelte';
 
 export default test({
 	async test({ assert, target }) {
-		assert.htmlEqual(
-			target.innerHTML,
-			'<button>update</button><p>1970-01-01T00:00:00.000Z</p>'
-		);
+		assert.htmlEqual(target.innerHTML, '<button>update</button><p>1970-01-01T00:00:00.000Z</p>');
 
-		target.querySelector('button').click();
+		target.querySelector('button')?.click();
 		await tick();
 
-		assert.htmlEqual(
-			target.innerHTML,
-			'<button>update</button><p>1970-01-02T00:00:00.000Z</p>'
-		);
+		assert.htmlEqual(target.innerHTML, '<button>update</button><p>1970-01-02T00:00:00.000Z</p>');
 	}
 });
