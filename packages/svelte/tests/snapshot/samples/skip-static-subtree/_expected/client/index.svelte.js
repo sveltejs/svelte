bb9cdae6087eab70659a85fece78dc58d36f0131
Mathias Picker <48158184+MathiasWP@users.noreply.github.com>
2026-08-25 11:20:27 +0200

    perf: treat `<img loading>` as a static element again

--- packages/svelte/tests/snapshot/samples/skip-static-subtree/_expected/client/index.svelte.js
+++ packages/svelte/tests/snapshot/samples/skip-static-subtree/_expected/client/index.svelte.js
@@ -40,10 +40,7 @@ export default function Skip_static_subtree($$anchor, $$props) {
 
 	option.value = option.__value = 'a';
 	$.reset(select);
-
-	var img = $.sibling(select, 2);
-
-	$.next(2);
+	$.next(4);
 	$.template_effect(() => $.set_text(text, $$props.title));
 	$.append($$anchor, fragment);
 }
