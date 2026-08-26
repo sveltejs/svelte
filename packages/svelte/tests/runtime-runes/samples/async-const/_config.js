34e750f2945ec3b40e9279e046fa3578543daee3
Simon Holthausen <simon.holthausen@vercel.com>
2026-08-26 11:16:49 +0200

    chore: set Vitest reporter for github actions

--- packages/svelte/tests/runtime-runes/samples/async-const/_config.js
+++ packages/svelte/tests/runtime-runes/samples/async-const/_config.js
@@ -3,7 +3,7 @@ import { test } from '../../test';
 
 export default test({
 	mode: ['async-server', 'client', 'hydrate'],
-	ssrHtml: `<h1>Hello, world!</h1> 5 01234 5 sync 6 5 0 10`,
+	ssrHtml: `<h1>Hello,1 world!</h1> 5 01234 5 sync 6 5 0 10`,
 
 	async test({ assert, target }) {
 		await tick();
